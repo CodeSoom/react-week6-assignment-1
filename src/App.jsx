af4e19f39cb16af4ddb52b54acd412c069c3952d
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 02:10:16 +0900

    Refactor: App에서 HomePage 사용

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
+import HomePage from './HomePage';
 import RegionsContainer from './RegionsContainer';
 import CategoriesContainer from './CategoriesContainer';
 import RestaurantsContainer from './RestaurantsContainer';
@@ -9,11 +10,6 @@ import RestaurantsContainer from './RestaurantsContainer';
 import {
   loadInitialData,
 } from './actions';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 export default function App() {
   const dispatch = useDispatch();
@@ -24,6 +20,7 @@ export default function App() {
 
   return (
     <div>
+      <HomePage />
       <RegionsContainer />
       <CategoriesContainer />
       <RestaurantsContainer />
