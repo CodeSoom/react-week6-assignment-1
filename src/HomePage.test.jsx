2a3e91eae35fbee5c097420a0c6d8642e38b725c
jiunkoo <jiunkoo0106@gmail.com>
2020-07-07 00:52:48 +0900

    [추가] HomePage 테스트 추가

---
+++ src/HomePage.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import HomePage from './HomePage';
+
+test('HomePage', () => {
+  const { container } = render((
+    <HomePage />
+  ));
+
+  expect(container).toHaveTextContent(/Home/);
+  expect(container).toHaveTextContent(/About/);
+  expect(container).toHaveTextContent(/Restaurants/);
+});
