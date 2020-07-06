004e66cb939b60d0b5001f166d30f6d1fb2b9f67
jiunkoo <jiunkoo0106@gmail.com>
2020-07-07 00:53:17 +0900

    [추가] NotFound 페이지 테스트 추가

---
+++ src/NotFoundPage.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import NotFoundPage from './NotFoundPage';
+
+test('NotFoundPage', () => {
+  const { container } = render((
+    <NotFoundPage />
+  ));
+
+  expect(container).toHaveTextContent(/404 Not Found/);
+});
