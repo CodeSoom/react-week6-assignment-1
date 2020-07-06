a54d117b9195233c6904d3264a76aa59c259f42a
jiunkoo <jiunkoo0106@gmail.com>
2020-07-07 00:54:54 +0900

    [리팩토링] App에 HomePage, AboutPage, NotFoundPage 추가 및 간단한 라우팅 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
+import NotFoundPage from './NotFoundPage';
 
 export default function App() {
+  const { location: { pathname } } = window;
+
+  const MyComponent = {
+    '/': HomePage,
+    '/about': AboutPage,
+    '/Restaurants': RestaurantsPage,
+  }[pathname] || NotFoundPage;
+
   return (
     <div>
-      <RestaurantsPage />
+      <MyComponent />
     </div>
   );
 }
