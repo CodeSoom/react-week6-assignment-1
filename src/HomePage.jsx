5ac0aa14068f7705a0e6cab0e16eef40110816c4
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-06 16:22:11 +0900

    feat(HomePage): render
    - 메인 화면에 필요한 요소들(제목, About 링크, Restaurants 링크) 렌더링 기능 구현

---
+++ src/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <>
+      <h1>Home</h1>
+      <ul>
+        <li>About</li>
+        <li>Restaurants</li>
+      </ul>
+    </>
+  );
+}
