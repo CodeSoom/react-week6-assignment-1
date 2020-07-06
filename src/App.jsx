1146886e9b843d0669c15a98c58465dcdb5f25cb
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-06 16:42:19 +0900

    refactor(App): render restaurants
    - 라우팅 경로에 따라 레스토랑 목록 렌더링 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,14 @@ import {
 
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
+import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
+      <Route path="/restaurants" component={RestaurantsPage} />
     </Switch>
   );
 }
