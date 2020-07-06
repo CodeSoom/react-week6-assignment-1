74d5969febd8dab7e05b2a81227544f8b138186c
aumilkyeong <aumilkyeong@gmail.com>
2020-07-06 18:20:45 +0900

    Test: App 컴포넌트 수정
    레스토랑 페이지가 라우팅되는지 확인

--- src/index.jsx
+++ src/index.jsx
@@ -3,14 +3,18 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 
-import App from './App';
+import { BrowserRouter } from 'react-router-dom';
 
 import store from './store';
+
+import App from './App';
 
 ReactDOM.render(
   (
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   ),
   document.getElementById('app'),
