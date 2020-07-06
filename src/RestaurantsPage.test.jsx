81cd6d5273ad51244275353abfe213b930aba618
aumilkyeong <aumilkyeong@gmail.com>
2020-07-06 17:20:01 +0900

    Test: 레스토랑 페이지
    렌더되는지만 확인

---
+++ src/RestaurantsPage.test.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import RestaurantsPage from './RestaurantsPage';
+
+describe('RestaurantsPage', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+    categories: [
+      { id: 1, name: '한식' },
+    ],
+    restaurants: [
+      { id: 1, name: '마법사주방' },
+    ],
+  }));
+
+  it('renders restaurant page', () => {
+    render(
+      <RestaurantsPage />,
+    );
+  });
+});
