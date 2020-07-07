43e3c4971e3a6717b59293fa4fbefade96213040
sogoagain <imyong0@gmail.com>
2020-07-08 00:23:34 +0900

    Test - App '/restaurants' path 이동 테스트

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,34 +1,53 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { render, screen } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
-test('App', () => {
+function renderApp({ path }) {
+  render((
+    <MemoryRouter initialEntries={[path]}>
+      <App />
+    </MemoryRouter>
+  ));
+  return {
+    queryByText: (text) => screen.queryByText(text),
+  };
+}
+
+describe('App', () => {
   const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+  });
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+  context('with path /restaurants', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        regions: [
+          { id: 1, name: '서울' },
+        ],
+        categories: [
+          { id: 1, name: '한식' },
+        ],
+        restaurants: [
+          { id: 1, name: '마법사주방' },
+        ],
+      }));
+    });
 
-  const { queryByText } = render((
-    <App />
-  ));
+    it('renders restaurants page', () => {
+      const { queryByText } = renderApp({ path: '/restaurants' });
 
-  expect(dispatch).toBeCalled();
+      expect(dispatch).toBeCalled();
 
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+      expect(queryByText('서울')).not.toBeNull();
+      expect(queryByText('한식')).not.toBeNull();
+    });
+  });
 });
