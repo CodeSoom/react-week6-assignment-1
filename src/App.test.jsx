8a30326513894cf1352e95f2e293ef384d742831
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-07 21:18:35 +0900

    Add test for App
    Test if header is rendered
    Test if restaurants routing works

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,31 +4,51 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
-test('App', () => {
-  const dispatch = jest.fn();
+describe('App', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+    useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+      restaurants: [
+        { id: 1, name: '마법사주방' },
+      ],
+    }));
+  });
 
-  const { queryByText } = render((
-    <App />
-  ));
+  function renderApp({ path }) {
+    return render(
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>,
+    );
+  }
 
-  expect(dispatch).toBeCalled();
+  context('with path /restaurants', () => {
+    it('renders Homepage', () => {
+      const { getByText } = renderApp({ path: './restaurants' });
 
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+      expect(getByText(/헤더/)).not.toBeNull();
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders Homepage', () => {
+      const { queryByText } = renderApp({ path: './restaurants' });
+
+      expect(queryByText('서울')).not.toBeNull();
+      expect(queryByText('한식')).not.toBeNull();
+    });
+  });
 });
