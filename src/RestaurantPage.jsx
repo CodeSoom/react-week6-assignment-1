0a86a27a53d287854fd2aef257efd4bbf990ba43
daadaadaah <daadaadaah@gmail.com>
2020-07-08 01:07:04 +0900

    [Green] Implement a RestaurantPage for showing restaurant info

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -5,16 +5,19 @@ import { useParams } from 'react-router-dom';
 export default function RestaurantPage() {
   const { restaurantId } = useParams();
 
-  useEffect(() => {
-    dispatch(loadRestaurant(restaurantId));
-  }, []);
-
   return (
     <div>
       <h2>
         Restaurant
         {restaurantId}
       </h2>
+      <h3>양천주가</h3>
+      <p>주소: 서울시 강남구</p>
+      <h3>메뉴</h3>
+      <ul>
+        <li>탕수육</li>
+        <li>팔보채</li>
+      </ul>
     </div>
   );
 }
