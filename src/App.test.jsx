012429cef47fbd2607d8ba770e64599836c87d1f
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 15:43:32 +0900

    About 페이지 접속 테스트
    App에서 About을 클릭 시 /about 페이지에 접속하는지 테스트.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,26 +1,38 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
-test('App', () => {
+describe('App', () => {
   const dispatch = jest.fn();
-  useDispatch.mockImplementation(() => dispatch);
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+      restaurants: [
+        { id: 1, name: '마법사주방' },
+      ],
+    }));
+  });
 
-  render((
-    <App />
-  ));
+  context('About을 클릭하면', () => {
+    it('About 페이지로 들어간다.', () => {
+      const { getByText } = render((
+        <MemoryRouter initialEntries={['/about']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(getByText('About')).toBeInTheDocument();
+      expect(getByText('About페이지 입니다.')).toBeInTheDocument();
+    });
+  });
 });
