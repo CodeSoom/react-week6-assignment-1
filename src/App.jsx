b49ae741e7b1d7db981c46a2e312090b9b6ab4e9
aumilkyeong <aumilkyeong@gmail.com>
2020-07-07 18:59:45 +0900

    Feat: 홈페이지 기본 페이지로 설정

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 
 import {
   Route,
+  Switch,
 } from 'react-router-dom';
 
+import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
     <div>
-      <Route path="/restaurants" component={RestaurantsPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+      </Switch>
     </div>
   );
 }
