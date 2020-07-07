f56cc77aeee1ac7c8c93515fa4f3e699fefa1e90
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 16:05:44 +0900

    Refactor: 중복 제거, fixtures 사용

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,32 +4,39 @@ import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
+import CATEGORIES from '../fixtures/categories';
+import REGIONS from '../fixtures/regions';
+import RESTAURANTS from '../fixtures/restaurants';
+
 import App from './App';
 
 describe('App', () => {
   const dispatch = jest.fn();
+
+  const SEOUL = REGIONS[0];
+  const KOREAN_FOOD = CATEGORIES[0];
+  const RESTAURANT = RESTAURANTS[0];
+
+  function renderApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
+
   beforeEach(() => {
     useDispatch.mockImplementation(() => dispatch);
     useSelector.mockImplementation((selector) => selector({
-      regions: [
-        { id: 1, name: '서울' },
-      ],
-      categories: [
-        { id: 1, name: '한식' },
-      ],
-      restaurants: [
-        { id: 1, name: '마법사주방' },
-      ],
+      regions: [SEOUL],
+      categories: [KOREAN_FOOD],
+      restaurants: [RESTAURANT],
     }));
   });
 
   context('/에 접속하면', () => {
     it('기본 메인 페이지가 나온다.', () => {
-      const { getByText } = render((
-        <MemoryRouter initialEntries={['/']}>
-          <App />
-        </MemoryRouter>
-      ));
+      const { getByText } = renderApp({ path: '/' });
 
       expect(getByText('Home')).toBeInTheDocument();
       expect(getByText('About')).toBeInTheDocument();
@@ -39,11 +46,7 @@ describe('App', () => {
 
   context('/about에 접속하면', () => {
     it('About 페이지로 들어간다.', () => {
-      const { getByText } = render((
-        <MemoryRouter initialEntries={['/about']}>
-          <App />
-        </MemoryRouter>
-      ));
+      const { getByText } = renderApp({ path: '/about' });
 
       expect(getByText('About')).toBeInTheDocument();
       expect(getByText('About 페이지 입니다.')).toBeInTheDocument();
