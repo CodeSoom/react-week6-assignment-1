8a202abc853f94e185a3cce62497fe9f5833d315
daadaadaah <daadaadaah@gmail.com>
2020-07-07 19:04:37 +0900

    [Red] Add a App test for Homepage

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -12,20 +12,12 @@ test('App', () => {
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
+    regions: [{ id: 1, name: '서울' }],
+    categories: [{ id: 1, name: '한식' }],
+    restaurants: [{ id: 1, name: '마법사주방' }],
   }));
 
-  const { queryByText } = render((
-    <App />
-  ));
+  const { queryByText } = render(<App />);
 
   expect(dispatch).toBeCalled();
 
@@ -33,8 +25,40 @@ test('App', () => {
   expect(queryByText('한식')).not.toBeNull();
 });
 
-test('App with router', () => {
-  it('render a different page according to URL', () => {
+describe('App with router', () => {
+  context('with URL included /HomePage', () => {
+    it('shows page name', () => {
+      const { container } = render(
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>,
+      );
 
+      expect(container).toHaveTextContent('Home');
+    });
   });
+
+  // context('with URL included /AboutPage', () => {
+  //   it('shows page name', () => {
+
+  //   });
+  // });
+
+  // context('with URL included /RestaurantsPage', () => {
+  //   it('shows page name', () => {
+
+  //   });
+  // });
+
+  // context('with URL included /RestaurantPage', () => {
+  //   it('shows page name', () => {
+
+  //   });
+  // });
+
+  // context('with URL included /NotFoundPage', () => {
+  //   it('shows page name', () => {
+
+  //   });
+  // });
 });
