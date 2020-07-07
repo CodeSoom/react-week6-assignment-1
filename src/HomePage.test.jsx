8e8c01e03904d9ef2801b96a38f3d4a79b4d95de
aumilkyeong <aumilkyeong@gmail.com>
2020-07-07 18:46:58 +0900

    Test: 홈페이지 컴포넌트

---
+++ src/HomePage.test.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import HomePage from './HomePage';
+
+test('HomePage', () => {
+  render(
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>,
+  ); // TODO: 실질적으로 홈페이지가 가지는 네비게이션 기능을 테스트하려면?
+});
