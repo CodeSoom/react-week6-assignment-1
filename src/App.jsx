b5a50fb7cd2efd868b29b6447872047ef2901967
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-07 21:42:41 +0900

    Refactor App with Switch and Route

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 
+import { Switch, Route } from 'react-router-dom';
+
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
     <div>
       <h1>헤더</h1>
-      <RestaurantsPage />
+      <Switch>
+        <Route path="/" component={RestaurantsPage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+      </Switch>
     </div>
   );
 }
