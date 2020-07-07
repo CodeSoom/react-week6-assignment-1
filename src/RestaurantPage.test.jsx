c4c9167635187d3bc43f8feeea5a5a126659a0ed
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 19:07:13 +0900

    test 코드 수정

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -7,16 +7,14 @@ import RestaurantPage from './RestaurantPage';
 import RESTAURANT from '../fixtures/restaurant';
 
 describe('RestaurantPage', () => {
-  const SEOUL_KOREAN_RESTAURANT = RESTAURANT[0];
-
   context('레스토랑 id가 있으면', () => {
     it('해당 레스토랑 정보를 보여준다.', () => {
       const { getByText } = render((
         <RestaurantPage />
       ));
 
-      expect(getByText(SEOUL_KOREAN_RESTAURANT.name)).toBeInTheDocument();
-      expect(getByText(SEOUL_KOREAN_RESTAURANT.address)).toBeInTheDocument();
+      expect(getByText(RESTAURANT.name)).toBeInTheDocument();
+      expect(getByText(`주소: ${RESTAURANT.address}`)).toBeInTheDocument();
     });
   });
 });
