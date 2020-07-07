a932397a86c87a8e093d94fcd7b20bde0a3aff90
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-07 21:02:24 +0900

    Add BrowerRouter to index

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
 
+import { BrowerRouter } from 'react-router-dom';
+
 import App from './App';
 
 import store from './store';
@@ -10,7 +12,9 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <App />
+      <BrowerRouter>
+        <App />
+      </BrowerRouter>
     </Provider>
   ),
   document.getElementById('app'),
