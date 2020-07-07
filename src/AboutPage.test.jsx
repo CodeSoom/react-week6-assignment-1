d0a870629e183179243c776a7d38f7ea46685121
Minwook Park <mwpark0975@gmail.com>
2020-07-08 00:15:45 +0900

    test - AboutPage

---
+++ src/AboutPage.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import AboutPage from './AboutPage';
+
+test('AboutPage', () => {
+  const { container } = render((
+    <AboutPage />
+  ));
+
+  expect(container).toHaveTextContent('About 페이지 입니다.');
+});
