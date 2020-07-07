c214590ca13eee12a13929fbbb839ebbaefecfba
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 18:39:15 +0900

    RestaurantContainer 삭제

--- src/RestaurantContainer.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-export default function RestaurantContainer({ restaurant }) {
-  return (
-    <li>{restaurant.name}</li>
-  );
-}
--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -5,22 +5,21 @@ import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import { get } from './utils';
 
-import RestaurantContainer from './RestaurantContainer';
-
 export default function RestaurantsContainer() {
   const restaurants = useSelector(get('restaurants'));
 
   return (
     <ul>
       {restaurants.map((restaurant) => (
-        <Link
+        <li
           key={restaurant.id}
-          to={`/restaurant/${restaurant.id}`}
         >
-          <RestaurantContainer
-            restaurant={restaurant}
-          />
-        </Link>
+          <Link
+            to={`/restaurant/${restaurant.id}`}
+          >
+            {restaurant.name}
+          </Link>
+        </li>
       ))}
     </ul>
   );
