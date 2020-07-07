acd812e12395f95c7c62db5ffa6483af25840dc7
daadaadaah <daadaadaah@gmail.com>
2020-07-07 22:59:45 +0900

    [Red] Add a HomePage test for showing menus

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -4,8 +4,14 @@ import { render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
-test('HomePage', () => {
-  const { container } = render(<HomePage />);
+describe('<HomePage />', () => {
+  context('render HomePage', () => {
+    it('show page name & menus', () => {
+      const { container } = render(<HomePage />);
 
-  expect(container).toHaveTextContent('Home');
+      expect(container).toHaveTextContent('Home');
+      expect(container).toHaveTextContent('About');
+      expect(container).toHaveTextContent('Restaurants');
+    });
+  });
 });
