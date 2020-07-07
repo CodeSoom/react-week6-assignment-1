eaa0e1af126e2a113bb78d5bd0a35b59bb66eef2
Minwook Park <mwpark0975@gmail.com>
2020-07-08 00:04:43 +0900

    test - HomePage

---
+++ src/HomePage.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import HomePage from './HomePage';
+
+test('HomePage', () => {
+  render((
+    <HomePage />
+  ));
+});
