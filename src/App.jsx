c1fac5922dc165de10931802fbc14660a186c77a
Minwook Park <mwpark0975@gmail.com>
2020-07-08 00:36:37 +0900

    use Router, fix app test error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
+import { Switch, Route } from 'react-router-dom';
+
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
+import NotFoundPage from './NotFoundPage';
 
 export default function App() {
   return (
-    <div>
-      <RestaurantsPage />
-    </div>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/restaurants" component={RestaurantsPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   );
 }
