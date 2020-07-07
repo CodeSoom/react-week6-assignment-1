53893d2b2b8787de1e1844feb8428981a865b4b5
aumilkyeong <aumilkyeong@gmail.com>
2020-07-07 19:26:51 +0900

    Feat: 헤더 추가

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   Route,
   Switch,
+  Link,
 } from 'react-router-dom';
 
 import HomePage from './HomePage';
@@ -11,6 +12,11 @@ import RestaurantsPage from './RestaurantsPage';
 export default function App() {
   return (
     <div>
+      <header>
+        <h1>
+          <Link to="/">헤더</Link>
+        </h1>
+      </header>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/restaurants" component={RestaurantsPage} />
