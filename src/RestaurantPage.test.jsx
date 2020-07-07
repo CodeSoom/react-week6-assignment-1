5b6cd62d89bd7af54498e76b3a90324208bbdb25
daadaadaah <daadaadaah@gmail.com>
2020-07-08 01:06:32 +0900

    Update a RestaurantPage test for adding a useParams mock

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -2,21 +2,30 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useParams } from 'react-router-dom';
+
 import RestaurantPage from './RestaurantPage';
 
+jest.mock('react-router-dom');
+
 describe('<RestaurantPage />', () => {
-  it('shows header and page name', () => {
+  beforeEach(() => {
+    useParams.mockReturnValue({ restaurantId: 1 });
+  });
+
+  it('shows page name', () => {
     const { container } = render(<RestaurantPage />);
-    expect(container).toHaveTextContent('헤더');
+
     expect(container).toHaveTextContent('Restaurant');
   });
 
   it('shows restaurant info', () => {
     const { container } = render(<RestaurantPage />);
 
+    expect(container).toHaveTextContent('Restaurant1');
     expect(container).toHaveTextContent('양천주가');
-    expect(container).toHaveTextContent('서울시 강남구');
-    expect(container).toHaveTextContent('탕수육');
-    expect(container).toHaveTextContent('팔보채');
+    expect(container).toHaveTextContent(/서울시 강남구/i);
+    expect(container).toHaveTextContent(/탕수육/i);
+    expect(container).toHaveTextContent(/팔보채/i);
   });
 });
