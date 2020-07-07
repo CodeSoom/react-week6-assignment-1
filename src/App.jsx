9853c4578a68feb97bab29fa8dd9f84fc4b89d1a
sogoagain <imyong0@gmail.com>
2020-07-08 07:10:48 +0900

    Implement - App '/restaurants' path 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
 
+import { Switch, Route } from 'react-router-dom';
+
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
     <div>
-      <RestaurantsPage />
+      <Switch>
+        <Route path="/restaurants" component={RestaurantsPage} />
+      </Switch>
     </div>
   );
 }
