3c2a19ae8945fa59b6e9deede5adb8537126bca7
sogoagain <imyong0@gmail.com>
2020-07-08 07:29:54 +0900

    Test - App '/about' 라우팅 구현

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './HomePage';
+import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
@@ -10,6 +11,7 @@ export default function App() {
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
         <Route path="/restaurants" component={RestaurantsPage} />
       </Switch>
     </div>
