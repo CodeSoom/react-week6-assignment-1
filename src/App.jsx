b8a164632faa3d6c8fbd0305ad6d3bec4313494a
sogoagain <imyong0@gmail.com>
2020-07-08 07:25:41 +0900

    Implement - App '/' 라우팅 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Switch, Route } from 'react-router-dom';
 
+import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
     <div>
       <Switch>
+        <Route exact path="/" component={HomePage} />
         <Route path="/restaurants" component={RestaurantsPage} />
       </Switch>
     </div>
