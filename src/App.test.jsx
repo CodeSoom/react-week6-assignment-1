175bc6158bc5ed8eaa7a097130a3c2ab7f8488d0
Minwook Park <mwpark0975@gmail.com>
2020-07-08 00:47:49 +0900

    test add to App

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,21 +2,64 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
-test('App', () => {
-  useSelector.mockImplementation((selector) => selector({
-    regions: [],
-    categories: [],
-    restaurants: [],
-  }));
+describe('App', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
 
-  render((
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  ));
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      regions: [{
+        id: 101, name: '서울',
+      }],
+      categories: [],
+      restaurants: [],
+    }));
+  });
+
+  function renderApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
+
+  context('with path /', () => {
+    it('renders HomePage', () => {
+      const { container } = renderApp({ path: '/' });
+
+      expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders AboutPage', () => {
+      const { container } = renderApp({ path: '/about' });
+
+      expect(container).toHaveTextContent('About 페이지 입니다');
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders RestaurantsPage', () => {
+      const { container } = renderApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent('서울');
+    });
+  });
+
+  context('with invalid path /xxx', () => {
+    it('renders NotFoudPage', () => {
+      const { container } = renderApp({ path: '/xxx' });
+
+      expect(container).toHaveTextContent('Not Found');
+    });
+  });
 });
