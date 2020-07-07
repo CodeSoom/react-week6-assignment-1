cdb175b3d7d194455e8d44c555e3ee989b04f796
BcTech-Saul <saul@BcTech-Saului-MacBookPro.local>
2020-07-07 09:20:42 +0900

    feat(App): render header
    - 홈으로 돌아갈 수 있는 헤더 엘레멘트 렌더링

--- src/App.jsx
+++ src/App.jsx
@@ -11,10 +11,15 @@ import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
-      <Route path="/restaurants" component={RestaurantsPage} />
-    </Switch>
+    <>
+      <header>
+        헤더
+      </header>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+      </Switch>
+    </>
   );
 }
