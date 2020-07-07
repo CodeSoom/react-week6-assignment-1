8b3aabcafaa62ff16ac35c542030f116666ecf35
daadaadaah <daadaadaah@gmail.com>
2020-07-07 23:06:21 +0900

    [Refactor] Refactor HomePage.jsx and HomePage.test.jsx for showing menus

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import HomePage from './HomePage';
 
 describe('<HomePage />', () => {
   context('render HomePage', () => {
     it('show page name & menus', () => {
-      const { container } = render(<HomePage />);
+      const { container } = render(<MemoryRouter><HomePage /></MemoryRouter>);
 
       expect(container).toHaveTextContent('Home');
       expect(container).toHaveTextContent('About');
