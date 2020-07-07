f0420b1ce2be9270fe3a7524ef6373f8a6532482
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 15:49:30 +0900

    About 페이지 테스트 통과

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,30 @@
 import React from 'react';
 
+import {
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
+
+function AboutPage() {
+  return (
+    <div>
+      <h2>About</h2>
+      <p>About 페이지 입니다.</p>
+    </div>
+  );
+}
 
 export default function App() {
   return (
     <div>
-      <HomePage />
-      <RestaurantsPage />
+      <Switch>
+        <Route path="/about" component={AboutPage} />
+        {/* <HomePage /> */}
+        {/* <RestaurantsPage /> */}
+      </Switch>
     </div>
   );
 }
