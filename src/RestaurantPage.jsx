2fd6a48fd1259ce69a1d11cb29628a6945209fde
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 19:24:29 +0900

    Refactor: RestaurantContainer 사용

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,15 +1,10 @@
 import React from 'react';
+import RestaurantContainer from './RestaurantContainer';
 
 export default function RestaurantPage() {
   return (
     <div>
-      <h2>양천주가</h2>
-      <p>주소: 서울 강남구</p>
-      <h3>메뉴</h3>
-      <ul>
-        <li>탕수육</li>
-        <li>팔보채</li>
-      </ul>
+      <RestaurantContainer />
     </div>
   );
 }
