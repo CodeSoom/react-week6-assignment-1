e4c4b009f20957d68435e5b8d080ff1db8bb894d
Kwakcena <rhkrgudwh@gmail.com>
2020-07-07 17:36:43 +0900

    Restaurant 테스트
    하나의 레스토랑에 대한 컴포넌트 테스트

---
+++ src/Restaurant.test.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Restaurant from './Restaurant';
+
+import RESTAURANTS from '../fixtures/restaurants';
+
+describe('Restaurant', () => {
+  const RESTAURANT = RESTAURANTS[0];
+
+  it('레스토랑 정보를 보여준다.', () => {
+    const { getByText } = render((
+      <Restaurant restaurant={RESTAURANT} />
+    ));
+
+    expect(getByText('양천주가')).toBeInTheDocument();
+  });
+});
