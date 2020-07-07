855ea8c1a3f273d19bb838f06db3e7e77c2538a2
daadaadaah <daadaadaah@gmail.com>
2020-07-07 20:06:40 +0900

    [Red] Add a HomePage test

---
+++ src/HomePage.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import HomePage from './HomePage';
+
+test('HomePage', () => {
+  const { container } = render(<HomePage />);
+
+  expect(container).toHaveTextContent('Home');
+});
