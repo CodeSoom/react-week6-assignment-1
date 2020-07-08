777c81e6b015744fc2af682c0ab93650f281ae02
KiChang Shin <kc7851@gmail.com>
2020-07-09 00:26:25 +0900

    NotFoundPage test 구현

--- src/NotFoundPage.jsx
+++ src/NotFoundPage.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export default function NotFoundPage() {
   return (
-    <p>Not Found</p>
+    <>
+      <p>Not Found</p>
+      <p><Link to="/">Go to Home</Link></p>
+    </>
   );
 }
