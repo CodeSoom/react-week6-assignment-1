6fd036cc0282702a0d448333acdc20d6ec8bfae0
Kwakcena <rhkrgudwh@gmail.com>
2020-07-08 16:21:07 +0900

    match 추가 테스트 구현

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -4,14 +4,16 @@ import { useDispatch } from 'react-redux';
 
 import RestaurantContainer from './RestaurantContainer';
 
-function loadRestaurant() {
-  return null;
-}
+import {
+  setRestaurantId,
+  loadRestaurant,
+} from './actions';
 
-export default function RestaurantPage() {
+export default function RestaurantPage({ match: { params: { id } } }) {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    dispatch(setRestaurantId(id));
     dispatch(loadRestaurant());
   });
 
