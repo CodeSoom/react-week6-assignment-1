526b19ba74418af20bd8bf74b3d79088adb910d7
KiChang Shin <kc7851@gmail.com>
2020-07-08 23:56:56 +0900

    HomePage test 작성
    Link를 사용하기 위해 MemoryRouter를 사용하였습니다.

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
 import HomePage from './HomePage';
 
 test('HomePage', () => {
   const { container } = render(
-    <HomePage />,
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>,
   );
 
   expect(container).toHaveTextContent('Home');
