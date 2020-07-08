4c36817ff7571c5af2b32ed86a12e09d51df4a2a
jiunkoo <jiunkoo0106@gmail.com>
2020-07-08 21:57:56 +0900

    [리팩토링] react-router-dom 사용을 위해 MemoryRouter 컴포넌트로 App 테스트 리팩토링

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,9 +3,19 @@ import React from 'react';
 import { render } from '@testing-library/react';
 
 import App from './App';
+import { useSelector } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 
 test('App', () => {
+  useSelector.mockImplementation((selector) => selector({
+    regions: [],
+    categories: [],
+    restaurants: [],
+  }));
+
   render((
-    <App />
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
   ));
 });
