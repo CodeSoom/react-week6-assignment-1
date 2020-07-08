0cf931f76b9768e93351fb3feee02b5f255bd786
aumilkyeong <aumilkyeong@gmail.com>
2020-07-08 12:33:09 +0900

    Test: about, restaurants 라우팅 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,22 +14,31 @@ describe('App', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
-    regions: [],
-    categories: [],
-    restaurants: [],
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+    categories: [
+      { id: 1, name: '한식' },
+    ],
+    restaurants: [
+      { id: 1, name: '달빛술담' },
+    ],
   }));
 
   it('renders header all the time', () => {
-    const containers = routes.map((route) => render(
-      <MemoryRouter initialEntries={[route]}>
-        <App />
-      </MemoryRouter>,
-    ));
-    containers.map(({ container }) => expect(container).toHaveTextContent('헤더'));
+    const containers = routes.map((route) => {
+      const { container } = render(
+        <MemoryRouter initialEntries={[route]}>
+          <App />
+        </MemoryRouter>,
+      );
+      return container;
+    });
+    containers.map((container) => expect(container).toHaveTextContent('헤더'));
   });
 
   context('with path /', () => {
-    it('renders the HomePage', () => {
+    it('renders the home page', () => {
       const { container } = render((
         <MemoryRouter initialEntries={['/']}>
           <App />
@@ -37,6 +46,32 @@ describe('App', () => {
       ));
 
       expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders the about page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/about']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('About');
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders the restaurants page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/restaurants']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('서울');
+      expect(container).toHaveTextContent('한식');
+      expect(container).toHaveTextContent('달빛술담');
     });
   });
 });
