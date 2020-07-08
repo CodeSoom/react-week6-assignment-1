ebce24819079cef9f2da72b19e4c8779ca6b4749
KiChang Shin <kc7851@gmail.com>
2020-07-09 00:08:23 +0900

    App test 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ import { Switch, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
 
+function AboutPage() {
+  return (
+    <p>About</p>
+  );
+}
+
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
         <Route path="/restaurants" component={RestaurantsPage} />
       </Switch>
     </div>
