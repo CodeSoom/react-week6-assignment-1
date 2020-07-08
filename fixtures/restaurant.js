d5edd09cf7e6463cc73434b04952ea948ebf221c
aumilkyeong <aumilkyeong@gmail.com>
2020-07-08 16:02:46 +0900

    Test: 식당 상세정보 불러오는 api 추가

---
+++ fixtures/restaurant.js
@@ -0,0 +1,15 @@
+const restaurant = {
+  id: 1,
+  categoryId: 1,
+  name: '김밥제국',
+  category: '분식',
+  address: '서울시 강남구 역삼동',
+  menuItems: [
+    {
+      id: 1,
+      name: '김밥',
+    },
+  ],
+};
+
+export default restaurant;
