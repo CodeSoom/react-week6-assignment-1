b92998bdb02b6ef104b6f4e10d06f171cfd8c21f
aumilkyeong <aumilkyeong@gmail.com>
2020-07-08 13:37:09 +0900

    Test: 식당 상세 페이지

---
+++ src/Restaurant.test.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import Restaurant from './Restaurant';
+
+import restaurants from '../fixtures/restaurants';
+
+test('Restaurant', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurant: restaurants[0],
+    menu: [
+      { id: 1, name: '탕수육' },
+    ],
+  }));
+
+  const { container } = render((
+    <Restaurant />
+  ));
+
+  expect(container).toHaveTextContent('주소: 서울시 강남구 역삼동');
+  expect(container).toHaveTextContent('메뉴');
+  expect(container).toHaveTextContent('탕수육');
+});
