6a1f31fc8edce90bf0433ae9fd30a1592eb0466f
Paul An <anpaul0615@gmail.com>
2020-07-08 10:22:43 +0900

    RestaurantContainer 테스트 작성 및 구현

---
+++ src/RestaurantContainer.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import { get } from './utils';
+
+export default function RestaurantContainer() {
+  const restaurant = useSelector(get('restaurant'));
+  const { name, address, menuItems } = restaurant;
+
+  return (
+    <div>
+      <h2>{name}</h2>
+
+      <h2>주소</h2>
+      <p>{address}</p>
+
+      <h2>메뉴</h2>
+      <ul>
+        {menuItems.map((menu) => <li key={menu.id}>{menu.name}</li>)}
+      </ul>
+    </div>
+  );
+}
---
+++ src/RestaurantContainer.test.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import RestaurantContainer from './RestaurantContainer';
+
+test('RestaurantContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurant: {
+      name: '레스토랑 이름',
+      address: '레스토랑 주소',
+      menuItems: [
+        { name: '메뉴 이름' },
+      ],
+    },
+  }));
+
+  const { container } = render((
+    <RestaurantContainer />
+  ));
+
+  expect(container).toHaveTextContent('레스토랑 이름');
+  expect(container).toHaveTextContent('레스토랑 주소');
+  expect(container).toHaveTextContent('메뉴 이름');
+});
