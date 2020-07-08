bd1b738ce6f51e5fdede5ae1f008849aea854fac
Paul An <anpaul0615@gmail.com>
2020-07-08 21:43:11 +0900

    App 컴포넌트 테스트 명세 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -35,27 +35,31 @@ describe('App', () => {
     }));
   });
 
-  it('with path /', () => {
-    const { queryByText } = renderComponent({ path: '/' });
-
-    expect(queryByText('헤더')).not.toBeNull();
+  describe('with path /', () => {
+    it('return Home Page', () => {
+      const { queryByText } = renderComponent({ path: '/' });
+      expect(queryByText('헤더')).not.toBeNull();
+    });
   });
 
-  it('with path /about', () => {
-    const { queryByText } = renderComponent({ path: '/about' });
-
-    expect(queryByText('헤더')).not.toBeNull();
+  describe('with path /about', () => {
+    it('return About Page', () => {
+      const { queryByText } = renderComponent({ path: '/about' });
+      expect(queryByText('헤더')).not.toBeNull();
+    });
   });
 
-  it('with path /restaurants', () => {
-    const { queryByText } = renderComponent({ path: '/restaurants' });
-
-    expect(queryByText('헤더')).not.toBeNull();
+  describe('with path /restaurants', () => {
+    it('return Restaurant List Page', () => {
+      const { queryByText } = renderComponent({ path: '/restaurants' });
+      expect(queryByText('헤더')).not.toBeNull();
+    });
   });
 
-  it('with path /restaurant/1', () => {
-    const { queryByText } = renderComponent({ path: '/restaurants/1' });
-
-    expect(queryByText('헤더')).not.toBeNull();
+  describe('with path /restaurant/1', () => {
+    it('return Restaurant View Page', () => {
+      const { queryByText } = renderComponent({ path: '/restaurants/1' });
+      expect(queryByText('헤더')).not.toBeNull();
+    });
   });
 });
