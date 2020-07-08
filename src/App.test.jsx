2f579386cf8303834259a0c1c442ddf038b79597
jiunkoo <jiunkoo0106@gmail.com>
2020-07-08 22:03:31 +0900

    [추가] App에서 구현한 Homepage 라우터 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,16 +6,24 @@ import App from './App';
 import { useSelector } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
 
-test('App', () => {
-  useSelector.mockImplementation((selector) => selector({
-    regions: [],
-    categories: [],
-    restaurants: [],
-  }));
+describe('App', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+      categories: [],
+      restaurants: [],
+    }));
+  });
 
-  render((
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  ));
+  context('with path /', ()=> {
+    it('renders the home page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('Home');
+    });
+  });
 });
