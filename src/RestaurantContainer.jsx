2142b55fab126763b80b5c39c81b234eef837d11
Kwakcena <rhkrgudwh@gmail.com>
2020-07-08 19:50:44 +0900

    refactor RestaurantContainer

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+
 import { useSelector } from 'react-redux';
 
 import { get } from './utils';
@@ -9,13 +10,11 @@ export default function RestaurantContainer() {
   return (
     <div>
       <h2>{name}</h2>
-      <p>
-        {`주소: ${address}`}
-      </p>
+      <p>{`주소: ${address}`}</p>
       <h3>메뉴</h3>
       <ul>
-        {menuItems.map((item) => (
-          <li key={item.id}>{item.name}</li>
+        {menuItems.map((menuItem) => (
+          <li key={menuItem.id}>{menuItem.name}</li>
         ))}
       </ul>
     </div>
