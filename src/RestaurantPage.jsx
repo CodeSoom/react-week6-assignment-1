a63ae6b6ec3da21e8f519152f4958270ca4829ad
Kwakcena <rhkrgudwh@gmail.com>
2020-07-08 14:48:42 +0900

    RestaurantPage dispatch 호출 테스트 구현

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
 import RestaurantContainer from './RestaurantContainer';
 
+function loadRestaurant() {
+  return null;
+}
+
 export default function RestaurantPage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRestaurant());
+  });
+
   return (
     <div>
       <RestaurantContainer />
