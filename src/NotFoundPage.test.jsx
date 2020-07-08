e0a04ec61a7c63b53d90885cd048a22c5d2270b4
KiChang Shin <kc7851@gmail.com>
2020-07-09 00:24:57 +0900

    NotFoundPage test 작성

--- src/NotFoundPage.test.jsx
+++ src/NotFoundPage.test.jsx
@@ -2,12 +2,17 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import NotFoundPage from './NotFoundPage';
 
 test('NotFoundPage', () => {
   const { container } = render(
-    <NotFoundPage />,
+    <MemoryRouter>
+      <NotFoundPage />
+    </MemoryRouter>,
   );
 
   expect(container).toHaveTextContent('Not Found');
+  expect(container).toHaveTextContent('Go to Home');
 });
