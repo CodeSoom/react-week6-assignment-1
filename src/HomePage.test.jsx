7651acef6c590a40b4ee7c9ef28356d48d852cbd
aumilkyeong <aumilkyeong@gmail.com>
2020-07-08 12:36:22 +0900

    Test: 홈페이지에서 어바웃페이지 링크 노출 여부 테스트 추가

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -7,9 +7,15 @@ import { MemoryRouter } from 'react-router-dom';
 import HomePage from './HomePage';
 
 test('HomePage', () => {
-  render(
+  const { container } = render(
     <MemoryRouter>
       <HomePage />
     </MemoryRouter>,
-  ); // TODO: 실질적으로 홈페이지가 가지는 네비게이션 기능을 테스트하려면?
+  );
+
+  expect(container).toHaveTextContent('Home');
+  expect(container).toHaveTextContent('About');
+  expect(container).toHaveTextContent('Restaurants');
+
+  // TODO: 실질적으로 홈페이지가 가지는 네비게이션 기능을 테스트하려면?
 });
