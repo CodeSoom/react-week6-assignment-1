fa91879cc70b3344b483bb239076ede25a0841e3
jiunkoo <jiunkoo0106@gmail.com>
2020-07-08 22:40:10 +0900

    [추가] App에서 구현한 About, Restaurants, NotFound 라우터 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,20 +2,27 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import App from './App';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { MemoryRouter } from 'react-router-dom';
+import App from './App';
 
 describe('App', () => {
   beforeEach(() => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
     useSelector.mockImplementation((selector) => selector({
-      regions: [],
+      regions: [
+        {
+          id: '1',
+          name: '서울',
+        },
+      ],
       categories: [],
       restaurants: [],
     }));
   });
 
-  context('with path /', ()=> {
+  context('with path /', () => {
     it('renders the home page', () => {
       const { container } = render((
         <MemoryRouter initialEntries={['/']}>
@@ -24,6 +31,42 @@ describe('App', () => {
       ));
 
       expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders the about page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/about']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('About 페이지 입니다.');
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders the restaurants page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/restaurants']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('서울');
+    });
+  });
+
+  context('with invalid path', () => {
+    it('renders the not found page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/xxx']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('404 Not Found');
     });
   });
 });
