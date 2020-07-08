2161c52fd9b35660cb462f7be774e18ddaee16d1
KiChang Shin <kc7851@gmail.com>
2020-07-08 23:36:15 +0900

    App test 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,31 +4,39 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
 import App from './App';
 
-test('App', () => {
-  const dispatch = jest.fn();
+describe('App', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+    useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+      restaurants: [
+        { id: 1, name: '마법사주방' },
+      ],
+    }));
+  });
 
-  const { queryByText } = render((
-    <App />
-  ));
+  context('with path /restaurants', () => {
+    it('RestaurantsPage가 렌더된다.', () => {
+      const { container } = render(
+        <MemoryRouter initialEntries={['/restaurants']}>
+          <App />
+        </MemoryRouter>,
+      );
 
-  expect(dispatch).toBeCalled();
-
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+      expect(container).toHaveTextContent('서울');
+      expect(container).toHaveTextContent('한식');
+      expect(container).toHaveTextContent('마법사주방');
+    });
+  });
 });
