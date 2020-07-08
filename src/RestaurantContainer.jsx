d4e6dac6b89fdc72221a6a465fe59549adac5d1e
Kwakcena <rhkrgudwh@gmail.com>
2020-07-08 14:42:32 +0900

    Refactor: RestaurantContainer 리팩토링
    utils의 get 사용
    레스토랑 정보 출력

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
+import { get } from './utils';
+
 export default function RestaurantContainer() {
-  const restaurant = useSelector((state) => state.restaurant);
+  const { name, address, menuItems } = useSelector(get('restaurant'));
 
   return (
     <div>
-      <h2>{restaurant.name}</h2>
-      <p>주소: 서울 강남구</p>
+      <h2>{name}</h2>
+      <p>
+        {`주소: ${address}`}
+      </p>
       <h3>메뉴</h3>
       <ul>
-        <li>탕수육</li>
-        <li>팔보채</li>
+        {menuItems.forEach((item) => (
+          <li>{item.name}</li>
+        ))}
       </ul>
     </div>
   );
