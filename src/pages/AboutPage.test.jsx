0a8b5d607d100dc43697cc1bc42342994ae21c64
Paul An <anpaul0615@gmail.com>
2020-07-08 09:40:43 +0900

    AboutPage, NotFoundPage 테스트코드에서 MemoryRouter 제거

--- src/pages/AboutPage.test.jsx
+++ src/pages/AboutPage.test.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import { MemoryRouter } from 'react-router-dom';
-
 import { render } from '@testing-library/react';
 
 import AboutPage from './AboutPage';
 
 test('AboutPage', () => {
   const { queryByText } = render((
-    <MemoryRouter initialEntries={['/about']}>
-      <AboutPage />
-    </MemoryRouter>
+    <AboutPage />
   ));
 
   expect(queryByText('About 페이지 입니다.')).not.toBeNull();
--- src/pages/NotFoundPage.test.jsx
+++ src/pages/NotFoundPage.test.jsx
@@ -1,16 +1,12 @@
 import React from 'react';
 
-import { MemoryRouter } from 'react-router-dom';
-
 import { render } from '@testing-library/react';
 
 import NotFoundPage from './NotFoundPage';
 
 test('NotFoundPage', () => {
   const { queryByText } = render((
-    <MemoryRouter initialEntries={['/any_not_exist_url']}>
-      <NotFoundPage />
-    </MemoryRouter>
+    <NotFoundPage />
   ));
 
   expect(queryByText('404 Not Found')).not.toBeNull();
