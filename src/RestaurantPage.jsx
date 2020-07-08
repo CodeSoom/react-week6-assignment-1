1fdd8a2c1ef5c754c1330db5f360390f54de9e89
daadaadaah <daadaadaah@gmail.com>
2020-07-08 21:43:52 +0900

    [Refactor] Refactor a RestaurantPage for adding a RestaurantContainer

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import { useParams } from 'react-router-dom';
 
@@ -8,7 +8,7 @@ import {
   loadRestaurant,
 } from './actions';
 
-import { get } from './utils';
+import RestaurantContainer from './RestaurantContainer';
 
 export default function RestaurantPage({ params }) {
   const { restaurantId } = params || useParams();
@@ -19,31 +19,13 @@ export default function RestaurantPage({ params }) {
     dispatch(loadRestaurant(restaurantId));
   }, []);
 
-  const restaurant = useSelector(get('restaurant'));
-
-  if (!restaurant) {
-    return <p>식당 정보를 불러오고 있습니다...</p>;
-  }
-
   return (
     <div>
       <h2>
         Restaurant
         {restaurantId}
       </h2>
-      <h3>{restaurant.name}</h3>
-      <p>
-        주소:
-        {restaurant.address}
-      </p>
-      <h3>메뉴</h3>
-      <ul>
-        {
-          restaurant.menuItems.map((menu) => (
-            <li key={menu.id}>{menu.name}</li>
-          ))
-        }
-      </ul>
+      <RestaurantContainer />
     </div>
   );
 }
