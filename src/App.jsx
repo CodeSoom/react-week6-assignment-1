42580e4401e24bbe5f2d6a0b380d828ec9f93230
Law Woo <law@cupist.com>
2020-07-09 17:03:58 +0900

    Add source codes after week6-1 video lecture

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,20 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch } from 'react-redux';
-
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
-
-import {
-  loadInitialData,
-} from './actions';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import RestaurantsPage from './RestaurantsPage';
+import NotFoundPage from './NotFoundPage';
 
 export default function App() {
-  const dispatch = useDispatch();
+  const { location: { pathname } } = window;
 
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
+  const MyComponent = {
+    '/': HomePage,
+    '/about': AboutPage,
+    '/restaurants': RestaurantsPage,
+  }[pathname] || NotFoundPage;
 
   return (
-    <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
-    </div>
+    <MyComponent />
   );
 }
---
+++ src/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <ul>
+        <li><a href="/about">About</a></li>
+        <li><a href="/restaurants">Restaurants</a></li>
+      </ul>
+    </div>
+  );
+}
