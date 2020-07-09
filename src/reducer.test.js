aff6d58682e7abee788e9a23d45db16f02f58035
sogoagain <imyong0@gmail.com>
2020-07-09 23:21:16 +0900

    Test - setRestaurant 리듀서 테스트

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -6,6 +6,7 @@ import {
   setRestaurants,
   selectRegion,
   selectCategory,
+  setRestaurant,
 } from './actions';
 
 describe('reducer', () => {
@@ -70,6 +71,37 @@ describe('reducer', () => {
       const state = reducer(initialState, setRestaurants(restaurants));
 
       expect(state.restaurants).toHaveLength(1);
+    });
+  });
+
+  describe('setRestaurant', () => {
+    it('changes restaurant', () => {
+      const initialState = {
+        restaurant: {},
+      };
+
+      const restaurant = {
+        id: 1,
+        categoryId: 1,
+        name: '양천주가',
+        address: '서울 강남구',
+        menuItems: [
+          {
+            id: 16,
+            restaurantId: 1,
+            name: '탕수육',
+          },
+          {
+            id: 17,
+            restaurantId: 1,
+            name: '팔보채',
+          },
+        ],
+      };
+
+      const state = reducer(initialState, setRestaurant(restaurant));
+
+      expect(state.restaurant).toBe(restaurant);
     });
   });
 
