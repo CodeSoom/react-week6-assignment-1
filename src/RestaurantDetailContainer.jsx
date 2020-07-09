17f2063785ee6a6990692b6ffc7c71797a2cbeba
jiunkoo <jiunkoo0106@gmail.com>
2020-07-09 10:02:32 +0900

    [구현] RestaurantContainer에 레스토랑 상세 정보를 dispatch하는 테스트 구현

---
+++ src/RestaurantDetailContainer.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { useSelector } from 'react-redux';
+
+import RestaurantDetail from './RestaurantDetail';
+
+export default function RestaurantDetailContainer() {
+  const { restaurant } = useSelector((state) => ({
+    restaurant: state.restaurant,
+  }));
+
+  return (
+    <RestaurantDetail
+      restaurant={restaurant}
+    />
+  );
+}
