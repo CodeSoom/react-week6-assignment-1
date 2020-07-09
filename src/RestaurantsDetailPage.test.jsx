fcc2c4d3c4a2dbc3ee7e721717e914fb5eeef375
morykwon <kwonmory@gmail.com>
2020-07-10 00:34:34 +0900

    사용되지 않는 변수 삭제

--- src/RestaurantsDetailPage.test.jsx
+++ src/RestaurantsDetailPage.test.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 
-import { render, fireEvent } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import RestaurantsDetailPage from './RestaurantsDetailPage';
 
