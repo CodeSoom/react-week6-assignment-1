3fec2604aefd0e051766b73dbd308d5820eaf78b
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-09 21:15:44 +0900

    Add selectRestaurant to RestaurantsContainer

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,17 +1,33 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  selectRestaurant,
+} from './actions';
 
 import { get } from './utils';
 
 export default function RestaurantsContainer() {
+  const dispatch = useDispatch();
+
   const restaurants = useSelector(get('restaurants'));
+
+  function handleClick(restaurantId) {
+    dispatch(selectRestaurant(restaurantId));
+    // TODO: dispatch(loadRestaurantDetails());
+  }
 
   return (
     <ul>
       {restaurants.map((restaurant) => (
         <li key={restaurant.id}>
-          {restaurant.name}
+          <button
+            type="button"
+            onClick={handleClick}
+          >
+            {restaurant.name}
+          </button>
         </li>
       ))}
     </ul>
