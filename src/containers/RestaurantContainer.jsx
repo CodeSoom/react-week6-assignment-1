0e72c7d0ce61b7fe4994f49dd5d7295db0302dc7
Paul An <anpaul0615@gmail.com>
2020-07-09 15:02:51 +0900

    RestaurantsContainer 리팩토링
    - Presentational 관심사를 RestaurantViewPanel 컴포넌트로 위임

--- src/containers/RestaurantContainer.jsx
+++ src/containers/RestaurantContainer.jsx
@@ -2,23 +2,14 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import RestaurantViewPanel from '../components/RestaurantViewPanel';
+
 import { get } from '../utils';
 
 export default function RestaurantContainer() {
   const restaurant = useSelector(get('restaurant'));
-  const { name, address, menuItems } = restaurant;
 
   return (
-    <div>
-      <h3>{name}</h3>
-
-      <h3>주소</h3>
-      <p>{address}</p>
-
-      <h3>메뉴</h3>
-      <ul>
-        {menuItems && menuItems.map((menu) => <li key={menu.id}>{menu.name}</li>)}
-      </ul>
-    </div>
+    <RestaurantViewPanel restaurant={restaurant} />
   );
 }
