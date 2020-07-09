f190ca5fdef70639b68dda980effdc05aa7279ef
KiChang Shin <kc7851@gmail.com>
2020-07-10 02:01:49 +0900

    RestaurantDetailPage test 구현

---
+++ src/RestaurantDetailPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import RestaurantDetailContainer from './RestaurantDetailContainer';
+
+export default function RestaurantDetailPage({ match }) {
+  const { id } = match.params;
+
+  return (
+    <RestaurantDetailContainer
+      restauranId={id}
+    />
+  );
+}
