8b4d6d720dd0cc8b72a8015575f84d2956f381e3
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-09 21:01:53 +0900

    Update RestaurantsContainer test
    Test if dispatch is called twice

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
 test('RestaurantsContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     restaurants: [
-      { id: 1, name: '마법사주방' },
+      { id: 1, name: '양천주가' },
     ],
   }));
 
-  const { container } = render((
+  const { getByText } = render((
     <RestaurantsContainer />
   ));
 
-  expect(container).toHaveTextContent('마법사주방');
+  expect(getByText('양천주가')).not.toBeNull();
+
+  fireEvent.click(getByText(/양천주가/));
+
+  expect(dispatch).toBeCalledTimes(2);
 });
