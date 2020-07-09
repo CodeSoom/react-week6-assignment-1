d2c088420365760aa5ed77e8d266488201380c4e
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-09 23:06:23 +0900

    Update reducer test for initial state
    Add restaurantDetails initial state

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -16,6 +16,7 @@ describe('reducer', () => {
       regions: [],
       categories: [],
       restaurants: [],
+      restaurantDetails: null,
       selectedRegion: null,
       selectedCategory: null,
       selectedRestaurant: null,
