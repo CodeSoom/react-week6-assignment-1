02c27785f724de43e17753240374838e6da07442
Law Woo <law@cupist.com>
2020-07-11 00:48:58 +0900

    Add test about restaurant detail container

---
+++ src/RestaurantContainer.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import { render } from '@testing-library/react';
+
+import { useSelector } from 'react-redux';
+
+import RestaurantContainer from './RestaurantContainer';
+
+test('RestaurantsContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurant: {
+      id: 1,
+      categoryId: 1,
+      name: '양천주가',
+      address: '서울 강남구',
+      menuItems: [
+        { id: 16, restaurantId: 1, name: '탕수육' },
+        { id: 17, restaurantId: 1, name: '팔보채' },
+      ],
+      information: '양천주가 in 서울 강남구',
+    },
+  }));
+
+  const { container } = render((
+    <MemoryRouter>
+      <RestaurantContainer />
+    </MemoryRouter>
+  ));
+
+  expect(container).toHaveTextContent('양천주가');
+  expect(container).toHaveTextContent('서울 강남구');
+});
