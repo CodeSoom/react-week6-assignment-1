39f1740dfc796921adc24d4b6c50d214096e6eda
SeaFalcon <seafalcon323@gmail.com>
2020-07-11 04:26:25 +0900

    [RED+GREEN] App.jsx - RestaurantsPage 렌더 테스트 추가
    - react-router-dom 설치
    - webpack historyApiFallback - index.html 설정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,20 +4,30 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
-test('App', () => {
+describe('App', () => {
   const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [],
-    categories: [],
-    restaurants: [],
-  }));
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+      categories: [],
+      restaurants: [],
+    }));
+  });
 
-  render((
-    <App />
-  ));
+  context('경로가 /restaurants 일떄', () => {
+    it('RestaurantsPage가 렌더된다.', () => {
+      render((
+        <MemoryRouter initialEntries={['/restaurants']}>
+          <App />
+        </MemoryRouter>
+      ));
+    });
+  });
 });
