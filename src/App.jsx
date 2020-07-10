a440d62ff36889e7576372a832ceaf86afe01f0d
SeaFalcon <seafalcon323@gmail.com>
2020-07-11 04:55:13 +0900

    [Green] App.jsx - NotFoundPage 렌더 테스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,19 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
 
+function NotFoundPage() {
+  return (
+    <p>404 Not Found !!!</p>
+  );
+}
+
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
       <Route exact path="/about" component={AboutPage} />
       <Route path="/restaurants" component={RestaurantsPage} />
+      <Route component={NotFoundPage} />
     </Switch>
   );
 }
