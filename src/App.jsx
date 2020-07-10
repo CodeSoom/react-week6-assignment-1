cfe1e95a5fcca5ffb25ae32ccbccf1a569811216
SeaFalcon <seafalcon323@gmail.com>
2020-07-11 04:35:04 +0900

    [Green] App.jsx - HomePage 렌더 테스트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,16 @@ import { Route, Switch } from 'react-router-dom';
 
 import RestaurantsPage from './RestaurantsPage';
 
+function HomePage() {
+  return (
+    <p>Home</p>
+  );
+}
+
 export default function App() {
   return (
     <Switch>
+      <Route exact path="/" component={HomePage} />
       <Route path="/restaurants" component={RestaurantsPage} />
     </Switch>
   );
