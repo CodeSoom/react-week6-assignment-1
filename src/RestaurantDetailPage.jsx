0296d2c1c7b3c7d54a02ff022ee6a726b537788f
KiChang Shin <kc7851@gmail.com>
2020-07-12 00:01:22 +0900

    RestaurantDetailPage 리팩토링
    match.params를 useParams를 사용하도록 변경하였습니다.

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
+import { useParams } from 'react-router-dom';
+
 import RestaurantDetailContainer from './RestaurantDetailContainer';
 
-export default function RestaurantDetailPage({ match }) {
-  const { id } = match.params;
+export default function RestaurantDetailPage() {
+  const { id } = useParams();
 
   return (
     <RestaurantDetailContainer
