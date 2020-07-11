c3d8ace56f0125ae41b969c5168d9c79af8f38de
morykwon <kwonmory@gmail.com>
2020-07-11 23:57:08 +0900

    RestaurantDetail에 대한 테스트를 작성한다

---
+++ src/RestaurantDetail.test.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import RestaurantDetail from './RestaurantDetail';
+
+import restaurant from '../fixtures/restaurants';
+
+jest.mock('react-redux');
+
+describe('restaurantDetail', () => {
+  context('with restaurant', () => {
+    it('renders restarant detail', () => {
+      const firstRestaurants = restaurant[0];
+
+      const { container } = render(<RestaurantDetail restaurant={restaurant} />);
+
+      expect(container).toHaveTextContent(firstRestaurants.name);
+      expect(container).toHaveTextContent(firstRestaurants.address);
+      expect(container).toHaveTextContent(firstRestaurants.menuItems[0].name);
+      expect(container).toHaveTextContent(firstRestaurants.menuItems[1].name);
+    });
+  });
+});
