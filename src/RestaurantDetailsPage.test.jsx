99c6658b27eded82bcd8a02fbb03acf2de266297
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-11 16:03:17 +0900

    Update test for RestaurantDetailsPage
    Test if dispatch is called

--- src/RestaurantDetailsPage.test.jsx
+++ src/RestaurantDetailsPage.test.jsx
@@ -4,25 +4,20 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
 
 import RestaurantDetailsPage from './RestaurantDetailsPage';
 
 test('RestaurantDetailsPage', () => {
-  useSelector.mockImplementation((selector) => selector({
-    restaurantDetails: {
-      id: 1,
-      name: '양천주가',
-      address: '서울시 강남구',
-      menuItems: [
-        { id: 1, name: '탕수육' },
-      ],
-    },
-  }));
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
 
   render((
     <MemoryRouter>
       <RestaurantDetailsPage />
     </MemoryRouter>
   ));
+
+  expect(dispatch).toBeCalled();
 });
