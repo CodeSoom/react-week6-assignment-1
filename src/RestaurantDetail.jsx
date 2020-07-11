41ce4dd115ba1e4c8a3a87171fdfaa4d0a401d97
morykwon <kwonmory@gmail.com>
2020-07-12 00:00:31 +0900

    RestaurantDetail을 구현한다

---
+++ src/RestaurantDetail.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+export default function RestaurantDetail({ restaurant }) {
+  if (!restaurant) {
+    return <>레스토랑을 불러오거나 없습니다.</>;
+  }
+
+  return (
+    <>
+      <h1>{restaurant.name}</h1>
+      <p>
+        주소:
+        {' '}
+        {restaurant.address}
+      </p>
+      <h2>메뉴</h2>
+      <ul>
+        {
+          restaurant.menuItems.map((menu) => (
+            <li key={menu.id}>{menu.name}</li>
+          ))
+        }
+      </ul>
+    </>
+  );
+}
