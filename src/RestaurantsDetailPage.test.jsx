0e8781f10dab692a24d156b943ab307629d452b9
morykwon <kwonmory@gmail.com>
2020-07-12 03:05:40 +0900

    RestaurantsDetailPage 테스트보완

--- src/RestaurantsDetailPage.test.jsx
+++ src/RestaurantsDetailPage.test.jsx
@@ -2,34 +2,39 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { MemoryRouter } from 'react-router-dom';
 import RestaurantsDetailPage from './RestaurantsDetailPage';
 
 import restaurants from '../fixtures/restaurants';
 
+jest.mock('react-redux');
+
 describe('RestaurantsDetailPage', () => {
   const firstRestaurants = restaurants[0];
 
   context('with restaurant', () => {
-    beforeEach(() => {
+    it('renders details of restaurant', () => {
+      const dispatch = jest.fn();
+
+      useDispatch.mockImplementation(() => dispatch);
+
       useSelector.mockImplementation((selector) => selector({
         restaurant: firstRestaurants,
       }));
-    });
 
-    it('renders details of restaurant', () => {
-      // const { container } = render((
-      //   <MemoryRouter initialEntries={['1']}>
-      //     <RestaurantsDetailPage />
-      //   </MemoryRouter>
-      // ));
+      const { container } = render((
+        <MemoryRouter initialEntries={['1']}>
+          <RestaurantsDetailPage />
+        </MemoryRouter>
+      ));
 
-      // expect(container).toHaveTextContent(firstRestaurants.name);
-      // expect(container).toHaveTextContent(firstRestaurants.address);
-      // expect(container).toHaveTextContent(firstRestaurants.menuItems[0].name);
-      // expect(container).toHaveTextContent(firstRestaurants.menuItems[1].name);
+      expect(dispatch).toBeCalled();
+      expect(container).toHaveTextContent(firstRestaurants.name);
+      expect(container).toHaveTextContent(firstRestaurants.address);
+      expect(container).toHaveTextContent(firstRestaurants.menuItems[0].name);
+      expect(container).toHaveTextContent(firstRestaurants.menuItems[1].name);
     });
   });
 });
