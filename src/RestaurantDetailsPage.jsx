f870f659239f58aed2dc3a5709c98400687796e0
naraekn <34229459+naraekn@users.noreply.github.com>
2020-07-11 16:46:41 +0900

    Update RestaurantDetailsPage to use parameter

--- src/RestaurantDetailsPage.jsx
+++ src/RestaurantDetailsPage.jsx
@@ -2,15 +2,18 @@ import React, { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
+import { useParams } from 'react-router-dom';
+
 import RestaurantDetailsContainer from './RestaurantDetailsContainer';
 
 import { loadRestaurantDetails } from './actions';
 
 export default function RestaurantDetailsPage() {
   const dispatch = useDispatch();
+  const param = useParams();
 
   useEffect(() => {
-    dispatch(loadRestaurantDetails(1));
+    dispatch(loadRestaurantDetails(param.id));
   });
 
   return (
