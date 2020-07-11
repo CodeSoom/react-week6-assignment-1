e9dff6a2a38d9a8799ca733656b672b4916f42b6
KiChang Shin <kc7851@gmail.com>
2020-07-11 22:54:45 +0900

    App test 구현

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ import Header from './Header';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
+import RestaurantDetailPage from './RestaurantDetailPage';
 import NotFoundPage from './NotFoundPage';
 
 export default function App() {
@@ -18,7 +19,8 @@ export default function App() {
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
-        <Route path="/restaurants" component={RestaurantsPage} />
+        <Route exact path="/restaurants" component={RestaurantsPage} />
+        <Route path="/restaurants/:id" component={RestaurantDetailPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
