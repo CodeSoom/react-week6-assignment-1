12a716171c12080a04531c48c9f614871057bec7
morykwon <kwonmory@gmail.com>
2020-07-11 22:58:01 +0900

    RestaurantsDetailPage를 구현한다

---
+++ src/RestaurantsDetailPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function RestaurantsDetailPage() {
+  return (
+    <>
+      <h1>김밥제국</h1>
+      <p>주소: 서울시 강남구 역삼동</p>
+      <h2>메뉴</h2>
+      <ul>
+        <li>탕수육</li>
+        <li>팔보채</li>
+      </ul>
+    </>
+  );
+}
