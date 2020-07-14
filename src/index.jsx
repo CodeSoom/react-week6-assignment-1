463bdf4c9398f67326866e76b91c821fbef60df8
yeonhwa <yeonhwa6717@gmail.com>
2020-07-15 00:06:42 +0900

    App 테스트 수정
    - path에 따른 router 테스트

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -10,7 +11,9 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   ),
   document.getElementById('app'),
