c00a6ab878feeb99bc7ab75adc2d05a39533a4be
yeonhwa <yeonhwa6717@gmail.com>
2020-07-15 00:43:52 +0900

    헤더 <Link/> 연결

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import React from 'react';
 import {
   Switch,
   Route,
+  Link,
 } from 'react-router-dom';
 
 import HomePage from './HomePage';
@@ -18,7 +19,7 @@ import NotFoundPage from './NotFoundPage';
 export default function App() {
   return (
     <div>
-      <h1>헤더</h1>
+      <Link to="/"><h1>헤더</h1></Link>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
