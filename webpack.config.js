f0bc4e0bdfcd6703c0ebbb24652d6d9bca59ba15
morykwon <kwonmory@gmail.com>
2020-07-15 00:17:58 +0900

    webpack.config.js - output & plugins 추가
    - HtmlWebpackplugin
    - output 의 path 와 filename 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
@@ -12,6 +13,8 @@ module.exports = {
     ],
   },
   output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
     publicPath: '/',
   },
   resolve: {
@@ -20,4 +23,9 @@ module.exports = {
   devServer: {
     historyApiFallback: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 };
