d3f5caa7181a835543a7e0b3343dd4b2c3f9aa4c
Dong A Kim <dongakim90@gmail.com>
2020-10-26 10:25:51 +0900

    App 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Header from './Header';
+import Home from './Home';
 
 export default function App() {
   return (
     <div>
       <Header />
+      <Home />
     </div>
   );
 }
