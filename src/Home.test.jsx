52711cf989ec42349c7295fd86b2bf693dbf4a31
Dong A Kim <dongakim90@gmail.com>
2020-10-26 10:22:24 +0900

    Home 테스트 추가

--- src/Home.test.jsx
+++ src/Home.test.jsx
@@ -5,9 +5,19 @@ import { render } from '@testing-library/react';
 import Home from './Home';
 
 describe('<Home />', () => {
+  it('renders heading home', () => {
+    // When
+    const { getByRole } = render(<Home />);
+
+    // Then
+    expect(getByRole('heading')).toHaveTextContent('Home');
+  });
+
   it('renders about and restaurants link', () => {
+    // When
     const { getByText } = render(<Home />);
 
+    // Then
     expect(getByText('about')).toBeInTheDocument();
     expect(getByText('restaurants')).toBeInTheDocument();
   });
