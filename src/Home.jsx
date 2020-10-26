b86cb8a95efcf4ed24c66d769db0d7a9697e42ad
Dong A Kim <dongakim90@gmail.com>
2020-10-26 09:42:03 +0900

    Home 구현

---
+++ src/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <ul>
+      <li>
+        <a href="/about">about</a>
+      </li>
+      <li>
+        <a href="/restaurants">restaurants</a>
+      </li>
+    </ul>
+  );
+}
