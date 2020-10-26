f8ea2921b8a30f72eb10d905b3ef11c917c3215c
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-27 08:55:37 +0900

    [green] Implement "/about" path in App.

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,21 @@ import { Route, Switch } from 'react-router-dom';
 import RestaurantsPage from './RestaurantsPage';
 import HomePage from './HomePage';
 
+function AboutPage() {
+  return (
+    <div>
+      이 페이지는 소개 페이지 입니다.
+    </div>
+  );
+}
+
 export default function App() {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/restaurants" component={RestaurantsPage} />
+        <Route path="/about" component={AboutPage} />
       </Switch>
     </div>
   );
