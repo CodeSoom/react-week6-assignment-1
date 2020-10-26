8790c7bc79963a2affc25f229ac992a088a8e930
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-26 21:53:39 +0900

    [Green] Implement HomePage and routes it through "/" in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
+import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   const { location: { pathname } } = window;
 
   const Page = {
+    '/': HomePage,
     '/restaurants': RestaurantsPage,
   }[pathname];
 
