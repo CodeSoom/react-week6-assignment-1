2ee44990d370da65f53f2396fdc61d3f80fccbda
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-26 22:05:24 +0900

    [refactor] Apply HomePage to App.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import RestaurantsPage from './RestaurantsPage';
+import HomePage from './HomePage';
 
 export default function App() {
   return (
     <div>
       <RestaurantsPage />
-      Home
+      <HomePage />
     </div>
   );
 }
