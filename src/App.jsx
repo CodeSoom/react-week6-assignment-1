d2248ccfedfe6236ce835d2d778b54651b03d133
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-26 21:31:06 +0900

    [Refactor] Create object to route pages

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
+  const { location: { pathname } } = window;
+
+  const Page = {
+    '/restaurants': RestaurantsPage,
+  }[pathname];
+
   return (
-    <RestaurantsPage />
+    <Page />
   );
 }
