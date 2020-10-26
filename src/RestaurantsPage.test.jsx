7c328b422c7c34843cd3148819f57526c1a67323
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-26 20:43:21 +0900

    [red] Copy App test to RestaurantsPage test.

--- src/RestaurantsPage.test.jsx
+++ src/RestaurantsPage.test.jsx
@@ -2,10 +2,33 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import RestaurantsPage from './RestaurantsPage';
 
 test('RestaurantsPage', () => {
-  const { container } = render((<RestaurantsPage />));
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+    categories: [
+      { id: 1, name: '한식' },
+    ],
+    restaurants: [
+      { id: 1, name: '마법사주방' },
+    ],
+  }));
+
+  const { container, queryByText } = render((<RestaurantsPage />));
 
   expect(container).toHaveTextContent('Restaurants');
+
+  expect(dispatch).toBeCalled();
+
+  expect(queryByText('서울')).not.toBeNull();
+  expect(queryByText('한식')).not.toBeNull();
 });
