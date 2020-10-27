4973899201f157c80c52c13b87a6f1c92e7f046f
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-27 19:39:48 +0900

    [green] Implement dispatch in RestaurantPage.

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 
+import { useDispatch } from '../__mocks__/react-redux';
+
 import RestaurantContainer from './RestaurantContainer';
 
 export default function RestaurantPage({ match: { params: { restaurantId } } }) {
+  const dispatch = useDispatch();
+
+  dispatch();
+
   const id = Number(restaurantId);
 
   return ((
