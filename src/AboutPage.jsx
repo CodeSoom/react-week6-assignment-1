dc050b30fabbddf112fbbf00dbaf2dc271d092a4
ParkDongJo <pdj10211@gmail.com>
2020-10-27 13:36:18 +0900

    [week6-1] 강의보고 따라하기

---
+++ src/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h2>이 서비스에 대해서...</h2>
+      <p>이 서비스는 영국에서 시작되었습니다</p>
+      <p>이 서비스를 보셨다면 블라블라블라~ 20명에게 추천 ...</p>
+    </div>
+  );
+}
--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import {
+  BrowserRouter,
+} from 'react-router-dom';
+
 import { Provider } from 'react-redux';
 
 import App from './App';
@@ -10,7 +14,9 @@ import store from './store';
 ReactDOM.render(
   (
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
     </Provider>
   ),
   document.getElementById('app'),
