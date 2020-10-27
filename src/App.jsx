aedc502ffa591be3f6c43c70ba3685f78299e5a1
Dong A Kim <dongakim90@gmail.com>
2020-10-27 09:49:27 +0900

    App 구현
    react-router-dom를 활용해 라우트 기능 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
 import Header from './Header';
 import Home from './Home';
+import About from './About';
 
 export default function App() {
   return (
     <>
       <Header />
-      <Home />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+      </Switch>
     </>
   );
 }
