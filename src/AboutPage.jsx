96014d844b9a0892653e546ea6e05949c59f4ca3
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-27 09:00:01 +0900

    [refactor] Refactoring AboutPage.

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 export default function AboutPage() {
   return (
     <div>
-      이 페이지는 소개 페이지 입니다.
+      <h2>About ...</h2>
+      <p>
+        이 페이지는 소개 페이지 입니다.
+      </p>
     </div>
   );
 }
