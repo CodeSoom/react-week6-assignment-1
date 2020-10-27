9c06f0491512ada2c8b7cb5f7f3072ba5813b066
wjy5446 <wjy5446@gmail.com>
2020-10-27 22:44:18 +0900

    feat) HomePage에 Link 추가

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,18 +1,27 @@
 import React from 'react';
 
+import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
+function renderHomePage() {
+  return render((
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  ));
+}
+
 describe('HomePage', () => {
   it('"About" 리스트를 보여줍니다.', () => {
-    const { container } = render(<HomePage />);
+    const { container } = renderHomePage();
 
     expect(container).toHaveTextContent('About');
   });
 
   it('"Restaurants" 리스트를 보여줍니다.', () => {
-    const { container } = render(<HomePage />);
+    const { container } = renderHomePage();
 
     expect(container).toHaveTextContent('Restaurants');
   });
