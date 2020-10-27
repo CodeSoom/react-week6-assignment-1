987876f0b274f59c65c2b346fe8c84e3d8909198
Noah <mike1124@naver.com>
2020-10-27 23:37:07 +0900

    [Green] Implement RestaurantDetailsPage

---
+++ src/RestaurantDetailsPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function RestaurantsDetailsPage() {
+  return (
+    <>
+      <h1>양천 주가</h1>
+      <p>주소 : 서울시 강남구</p>
+      <h2>메뉴</h2>
+      <ul>
+        <li>탕수육</li>
+        <li>팔보채</li>
+      </ul>
+    </>
+  );
+}
