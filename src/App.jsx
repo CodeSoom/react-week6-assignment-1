b44d19f84ccd73ec6711d5b25824167acc05be4f
Noah <mike1124@naver.com>
2020-10-27 20:07:58 +0900

    [Green] Implement RestaurantsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch } from 'react-redux';
-
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
-
-import {
-  loadInitialData,
-} from './actions';
+import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
-  const { location: { pathname } } = window;
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
-
   return (
-    <div>
-      <RestaurantsPage />
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
-    </div>
+    <RestaurantsPage />
   );
 }
