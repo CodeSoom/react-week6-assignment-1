245196384f6071d6370a80becc656296cfb11976
wjy5446 <wjy5446@gmail.com>
2020-10-27 22:31:35 +0900

    feat) AboutPage 테스트 코드 작성

--- src/AboutPage.test.jsx
+++ src/AboutPage.test.jsx
@@ -4,6 +4,10 @@ import { render } from '@testing-library/react';
 
 import AboutPage from './AboutPage';
 
-test('AboutPage', () => {
-  render(<AboutPage />);
+describe('AboutPage', () => {
+  it('"About 페이지 입니다." 문구를 보여줍니다.', () => {
+    const { container } = render(<AboutPage />);
+
+    expect(container).toHaveTextContent('About 페이지 입니다.');
+  });
 });
