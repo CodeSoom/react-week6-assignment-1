16facca5be01e0118cda41e6e90360eec0ed8db2
wjy5446 <wjy5446@gmail.com>
2020-10-27 21:34:39 +0900

    feat) Router을 이용해 기초적인 구조 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,27 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch } from 'react-redux';
+import { Switch, Route } from 'react-router-dom';
 
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import RestaurantsPage from './RestaurantsPage';
+import NotFoundPage from './NotFoundPage';
 
-import {
-  loadInitialData,
-} from './actions';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
+// TODO:
+// Home Page 구현
+// About Page 구현
+// Restaurants Page 구현
+// Notfound Page 구현
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
-
   return (
     <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
-    </div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/restaurants" component={RestaurantsPage} />
+        <Route exact component={NotFoundPage} />
+      </Switch>
+    </div >
   );
 }
