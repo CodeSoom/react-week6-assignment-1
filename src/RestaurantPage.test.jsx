adb611739d2739055d976488e266f0fc0e172125
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-27 19:38:23 +0900

    [red] Add dispatch test to RestaurantPage.

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { MemoryRouter } from 'react-router-dom';
 
@@ -13,11 +13,16 @@ import RestaurantPage from './RestaurantPage';
 jest.mock('react-redux');
 
 test('RestaurantPage', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     restaurants: RESTAURANTS,
   }));
 
   const match = { params: { restaurantId: '1' } };
+
   const { container } = render((
     <MemoryRouter>
       <RestaurantPage match={match} />
@@ -25,4 +30,8 @@ test('RestaurantPage', () => {
   ));
 
   expect(container).toHaveTextContent('김밥제국');
+
+  // New test
+  expect(dispatch).toBeCalled();
+
 });
