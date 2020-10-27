2d5a9dc2a4f91d1d208075f23dbe776ee7b1937a
juunini <juuni.ni.i@gmail.com>
2020-10-27 21:22:29 +0900

    Add App in route AboutPage and NotFoundPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import { Route, Switch } from 'react-router-dom';
+import AboutPage from './pages/AboutPage';
 
 import HomePage from './pages/HomePage';
+import NotFoundPage from './pages/NotFoundPage';
 import RestaurantsPage from './pages/RestaurantsPage';
 
 // 0. 지역, 분류 목록을 얻기
@@ -10,16 +12,16 @@ import RestaurantsPage from './pages/RestaurantsPage';
 // 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 // TODO: "/" 페이지가 아니면 상단에 "뒤로가기" 텍스트가 보이며, 클릭할 수 있다.
-// TODO: "/About" 링크에 가면 "About 페이지입니다." 텍스트가 보인다.
 // TODO: "/Restaurants" 링크에 가면 지역, 분류 버튼들이 있으며 각각 클릭 시 하단에 음식점 링크들이 나타난다.
 // TODO: "/Restaurants/:id" 링크에서는 음식점의 정보가 나온다.
-// TODO: 등록된 링크가 아니면 404 페이지를 띄워준다.
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
       <Route path="/restaurants" component={RestaurantsPage} />
+      <Route component={NotFoundPage} />
     </Switch>
   );
 }
