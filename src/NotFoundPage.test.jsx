61edfd82a84fa7dd2ed7115f90dcd3858ece7b3a
wjy5446 <wjy5446@gmail.com>
2020-10-27 22:34:47 +0900

    feat) NotFoundPage 테스트 코드 작성

--- src/NotFoundPage.test.jsx
+++ src/NotFoundPage.test.jsx
@@ -4,6 +4,10 @@ import { render } from '@testing-library/react';
 
 import NotFoundPage from './NotFoundPage';
 
-test('NotFoundPage', () => {
-  render(<NotFoundPage />);
+describe('NotFoundPage', () => {
+  it('"페이지를 찾을 수 없습니다." 문구를 보여줍니다.', () => {
+    const { container } = render(<NotFoundPage />);
+
+    expect(container).toHaveTextContent('페이지를 찾을 수 없습니다.');
+  });
 });
