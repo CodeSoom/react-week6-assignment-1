5a4b028d28188ae176ebcb8601a5551f9f6d652e
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-27 17:11:38 +0900

    [red] Add test for Restaurant.

--- src/Restaurant.test.jsx
+++ src/Restaurant.test.jsx
@@ -7,6 +7,7 @@ import Restaurant from './Restaurant';
 test('Restaurant', () => {
   const restaurant = {
     name: '김밥제국',
+    address: '서울시 강남구 역삼동',
   };
 
   const { container } = render((
@@ -14,4 +15,5 @@ test('Restaurant', () => {
   ));
 
   expect(container).toHaveTextContent('김밥제국');
+  expect(container).toHaveTextContent('서울시 강남구 역삼동');
 });
