e02e15fee46781b8ba86129ee5182776a848793d
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-27 12:35:01 +0900

    [red] Create test for RestaurantPage.

---
+++ src/RestaurantPage.test.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import RestaurantPage from './RestaurantPage';
+
+test('RestaurantPage', () => {
+  const { container } = render((
+    <MemoryRouter>
+      <RestaurantPage />
+    </MemoryRouter>
+  ));
+
+  expect(container).toHaveTextContent('양천주가');
+});
