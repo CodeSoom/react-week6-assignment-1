dfdb68ca4d12a9982a004ef6f413c36a6101dd2f
Noah <mike1124@naver.com>
2020-10-27 22:03:52 +0900

    [Refactor] Move browserRouter from App to index

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 
 import {
-  BrowserRouter,
   Switch,
   Route,
 } from 'react-router-dom';
@@ -16,13 +15,11 @@ import NotFoundPage from './NotFoundPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/restaurants" component={RestaurantsPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </BrowserRouter>
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route path="/about" component={AboutPage} />
+      <Route path="/restaurants" component={RestaurantsPage} />
+      <Route component={NotFoundPage} />
+    </Switch>
   );
 }
