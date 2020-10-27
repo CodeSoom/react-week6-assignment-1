75e410522b598166057cdf239f4975190118bcd9
juunini <juuni.ni.i@gmail.com>
2020-10-27 22:42:00 +0900

    Add RestaurantInfoContainer [Green]

--- src/containers/RestaurantInfoContainer.jsx
+++ src/containers/RestaurantInfoContainer.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { get } from '../utils';
 
 export default function RestaurantInfoContainer() {
-  return (<></>);
+  const restaurant = useSelector(get('restaurant'));
+
+  return (
+    <p>{restaurant.info.name}</p>
+  );
 }
