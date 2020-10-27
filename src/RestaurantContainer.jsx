f8f0c9bfd4dc5273e458bf015814f34bab994110
Dong A Kim <dongakim90@gmail.com>
2020-10-28 03:42:02 +0900

    RestaurantContainer 구현
    테스트를 통과할 정도로만 간단하게 구현

---
+++ src/RestaurantContainer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function RestaurantContainer() {
+  return (
+    <>
+      <h2>양천주가</h2>
+      <p>주소: 서울 강남구</p>
+      <h3>메뉴</h3>
+      <ul>
+        <li>비빔밥</li>
+      </ul>
+    </>
+  );
+}
