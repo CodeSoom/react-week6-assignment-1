4b3f19cebd0e330159d7d6e727402e11a77eb7f8
Noah <mike1124@naver.com>
2020-10-27 20:10:58 +0900

    Add route in App

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,13 @@ import React from 'react';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
+  const { location: { pathname } } = window;
+
+  if (pathname === '/') {
+    return (
+      <p>Home</p>
+    );
+  }
   return (
     <RestaurantsPage />
   );
