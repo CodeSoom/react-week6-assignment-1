6954768ec78d82e8019c7dfbfa0d1ad0c91de82e
comejtome508 <youngnoh.goh@gmail.com>
2020-10-28 06:04:12 +0900

    [Red] Add homepage route in App test

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -4,11 +4,9 @@ import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
-    <div>
-      <ul>
-        <li><Link to="/">About</Link></li>
-        <li><Link to="/">Restaurants</Link></li>
-      </ul>
-    </div>
+    <ul>
+      <li><Link to="/">About</Link></li>
+      <li><Link to="/">Restaurants</Link></li>
+    </ul>
   );
 }
