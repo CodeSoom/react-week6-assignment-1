c373e0a6b0355860127bd3ca6ca2e18c3adc0228
saseungmin <dbd02169@naver.com>
2020-10-28 00:24:28 +0900

    RestaurantDetailPage에 대한 테스트를 수정한다.

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -2,10 +2,28 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
 import RestaurantDetailPage from './RestaurantDetailPage';
+import RESTAURANT_DETAIL from '../fixtures/restaurantDetail';
 
 test('RestaurantDetailPage', () => {
-  const { container } = render((<RestaurantDetailPage />));
+  const dispatch = jest.fn();
 
-  expect(container).toHaveTextContent('Detail');
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    selectedRestaurant: RESTAURANT_DETAIL,
+  }));
+
+  const { container } = render((
+    <MemoryRouter>
+      <RestaurantDetailPage />
+    </MemoryRouter>
+  ));
+
+  expect(dispatch).toBeCalled();
+
+  expect(container).toHaveTextContent(RESTAURANT_DETAIL.name);
 });
