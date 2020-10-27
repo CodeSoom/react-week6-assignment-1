4039c8ac304c40d30124cf0a01a5a161d9f2bba9
oceanide <oceanide@gmail.com>
2020-10-28 00:34:32 +0900

    [REFACTOR] remove Restaurants

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch } from 'react-redux';
-
-import RestaurantsPage from './RestaurantsPage';
 import HomePage from './HomePage';
 
-import {
-  loadInitialData,
-} from './actions';
-
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
-
   return (
-    <>
-      <HomePage />
-      <RestaurantsPage />
-    </>
+    <HomePage />
   );
 }
