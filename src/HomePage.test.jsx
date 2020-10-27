d8a60aa3c37850d5abb45d5ecf91b56e93efa6f6
Noah <mike1124@naver.com>
2020-10-27 22:01:50 +0900

    [Green] Add test code in Homepage MemoryRouter

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
   it('renders home page', () => {
-    render(<HomePage />);
+    render((
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    ));
   });
 });
