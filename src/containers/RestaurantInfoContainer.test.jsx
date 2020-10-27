95f61e7771d1069f516d023da121242072ea4944
juunini <juuni.ni.i@gmail.com>
2020-10-27 23:04:26 +0900

    Add RestaurantInfoContainer Details [Red]

--- src/containers/RestaurantInfoContainer.test.jsx
+++ src/containers/RestaurantInfoContainer.test.jsx
@@ -17,6 +17,23 @@ describe('RestaurantInfoContainer', () => {
       render(<RestaurantInfoContainer />);
 
       expect(screen.getByText(RESTAURANT_INFO.name)).toBeInTheDocument();
+      expect(screen.getByText(RESTAURANT_INFO.address)).toBeInTheDocument();
+
+      const menuWrapper = screen.getByTestId('menu');
+
+      RESTAURANT_INFO.menuItems.forEach((item) => {
+        const itemId = menuWrapper.children[item.id - 1].textContent;
+
+        expect(itemId).toBe(item.name);
+      });
+
+      RESTAURANT_INFO.reviews.forEach((review) => {
+        const wrapper = screen.getByTestId(review.id);
+
+        expect(wrapper.querySelector('.name').textContent).toBe(review.name);
+        expect(wrapper.querySelector('.score').textContent).toBe(review.score);
+        expect(wrapper.querySelector('.description').textContent).toBe(review.description);
+      });
     });
   });
 });
