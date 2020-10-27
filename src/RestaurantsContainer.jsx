5b8e1df283ae5731e247f73d5da52b01107dfa4d
Dong A Kim <dongakim90@gmail.com>
2020-10-28 03:22:56 +0900

    RestaurantsContainer 리팩토링
    link 수정 및 비구조할당 적용

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -10,10 +10,10 @@ export default function RestaurantsContainer() {
 
   return (
     <ul>
-      {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>
-          <Link to="/restaurant">
-            {restaurant.name}
+      {restaurants.map(({ id, name }) => (
+        <li key={id}>
+          <Link to={`/restaurants/${id}`}>
+            {name}
           </Link>
         </li>
       ))}
