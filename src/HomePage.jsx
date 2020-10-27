0f99b50e3f2335d4f46ab745d037fd3542224e53
Noah <mike1124@naver.com>
2020-10-27 21:59:47 +0900

    [Refactor] code home page apply Link

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   return (
     <>
       <h1>Home</h1>
       <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/restaurants">Restaurants</a></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/restaurants">Restaurants</Link></li>
       </ul>
     </>
   );
