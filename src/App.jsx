4b27cde6b1bfcc507c2bc2a3b7327eee6f1ae3f1
oceanide <oceanide@gmail.com>
2020-10-27 22:59:10 +0900

    [REFACTOR] add RestaurantsPage to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React, { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
+import RestaurantsPage from './RestaurantsPage';
 
 import {
   loadInitialData,
@@ -23,10 +21,6 @@ export default function App() {
   });
 
   return (
-    <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
-    </div>
+    <RestaurantsPage />
   );
 }
