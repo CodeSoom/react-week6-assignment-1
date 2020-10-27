440bbaf6784abb229f689837df75115666a8b283
Noah <mike1124@naver.com>
2020-10-27 20:20:31 +0900

    Add router and  Home/about/NotFound components

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,49 @@ import React from 'react';
 
 import RestaurantsPage from './RestaurantsPage';
 
+function HomePage() {
+  return (
+    <>
+      <h1>Home</h1>
+      <ul>
+        <li><a href="/about">About</a></li>
+        <li><a href="/restaurants">Restaurants</a></li>
+      </ul>
+    </>
+  );
+}
+
+function AboutPage() {
+  return (
+    <p>About...</p>
+  );
+}
+
+function NotFoundPage() {
+  return (
+    <p>404 Not Found</p>
+  );
+}
+
 export default function App() {
   const { location: { pathname } } = window;
 
-  if (pathname === '/') {
-    return (
-      <p>Home</p>
-    );
-  }
+  const MyComponent = {
+    '/': HomePage,
+    '/restaurants': RestaurantsPage,
+    '/about': AboutPage,
+  }[pathname] || NotFoundPage;
+
   return (
-    <RestaurantsPage />
+    <MyComponent />
   );
+
+  // if (pathname === '/') {
+  //   return (
+  //     <p>Home</p>
+  //   );
+  // }
+  // return (
+  //   <RestaurantsPage />
+  // );
 }
