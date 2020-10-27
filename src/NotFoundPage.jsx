a94ef6f86da6b85de950555aabf2f598f2b7107b
wjy5446 <wjy5446@gmail.com>
2020-10-27 22:35:50 +0900

    feat) NotFoundPage 테스트 코드 통과

--- src/NotFoundPage.jsx
+++ src/NotFoundPage.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 
 export default function NotFoundPage() {
   return (
-    <div>
-      NotFoundPage
-    </div>
+    <>
+      <h2>NotFoundPage</h2>
+      <p>페이지를 찾을 수 없습니다.</p>
+    </>
   );
 }
