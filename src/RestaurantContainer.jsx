3cb45b3b2069c439a7ec5b01ce17120f26a1b5af
Dong A Kim <dongakim90@gmail.com>
2020-10-28 04:55:10 +0900

    RestaurantContainer 구현

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -1,13 +1,35 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useParams } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { loadRestaurant } from './actions';
 
 export default function RestaurantContainer() {
+  const { restaurantId } = useParams();
+  const dispatch = useDispatch();
+  const { restaurant } = useSelector((state) => state);
+  const { name, address, menuItems } = restaurant;
+
+  useEffect(() => {
+    dispatch(loadRestaurant(restaurantId));
+  }, []);
+
   return (
     <>
-      <h2>양천주가</h2>
-      <p>주소: 서울 강남구</p>
+      <h2>{name}</h2>
+      <p>
+        주소:&nbsp;
+        {address}
+      </p>
       <h3>메뉴</h3>
       <ul>
-        <li>비빔밥</li>
+        {menuItems.map((menuItem) => (
+          <li key={menuItem.id}>
+            {menuItem.name}
+          </li>
+        ))}
       </ul>
     </>
   );
