28d4f8e64408ec56d5bf6f3abb80f99d696aa195
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-29 00:33:38 +0900

    [Red] HomePage에 대한 신규 테스트 추가
    1st, MemoryRouter 추가.
    2nd, About 및 Restaurants 텍스트 존재유무 테스트.
    
    *TODO: Refector router in App component to index.jsx!

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
 test('HomePage', () => {
-  render(<HomePage />);
+  const { container } = render((
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  ));
+
+  expect(container).toHaveTextContent('About');
+  expect(container).toHaveTextContent('Restaurants');
 });
