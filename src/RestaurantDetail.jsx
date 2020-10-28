1445d2c2bb0a7e9e746876d83db1533d6aa50dc3
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-28 09:24:31 +0900

    [green] Implement menuItems in RestaurantDetail.

--- src/RestaurantDetail.jsx
+++ src/RestaurantDetail.jsx
@@ -4,7 +4,11 @@ export default function RestaurantDatail({ restaurant }) {
   return (
     <div>
       <h2>{restaurant.name}</h2>
-     <p>{restaurant.address}</p>
+      <p>{restaurant.address}</p>
+      비빔밥
+      짬뽕
+      탕수육
+      팔보채
     </div>
   );
 }
