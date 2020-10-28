b1dd7e2476971d6baf9516147322c00da507e98c
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-28 09:52:50 +0900

    [refactor] Remove MemoryRouter in AboutPage test and NotFoundPage test.

--- src/AboutPage.test.jsx
+++ src/AboutPage.test.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { MemoryRouter } from 'react-router-dom';
-
 import AboutPage from './AboutPage';
 
 test('AboutPage', () => {
   const { container } = render((
-    <MemoryRouter>
-      <AboutPage />
-    </MemoryRouter>
+    <AboutPage />
   ));
 
   expect(container).toHaveTextContent('이 페이지는 소개 페이지 입니다.');
--- src/NotFoundPage.test.jsx
+++ src/NotFoundPage.test.jsx
@@ -2,15 +2,11 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { MemoryRouter } from 'react-router-dom';
-
 import NotFoundPage from './NotFoundPage';
 
 test('NotFoundPage', () => {
   const { container } = render((
-    <MemoryRouter>
-      <NotFoundPage />
-    </MemoryRouter>
+    <NotFoundPage />
   ));
 
   expect(container).toHaveTextContent('404 Not Found');
