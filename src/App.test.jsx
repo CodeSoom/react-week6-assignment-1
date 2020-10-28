0e1aa0c0afb7cbd9c3367129f8bc8e9e2fd26b22
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-28 22:28:51 +0900

    [Router] 강의를 듣고 라우터 적용
    코드숨 강의를 듣고 라우터에 대한 기본적인 사용법 습득.
    *TODO: git reverit 후, react-router-dom을 TDD로 다시 구현!

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,34 +1,65 @@
 import React from 'react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
-test('App', () => {
-  const dispatch = jest.fn();
+describe('App', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+    useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+      categories: [],
+      restaurants: [],
+    }));
+  });
 
-  const { queryByText } = render((
-    <App />
-  ));
+  function rederApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
 
-  expect(dispatch).toBeCalled();
+  context('with path /', () => {
+    it('renders the home page', () => {
+      const { container } = rederApp({ path: '/' });
 
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+      expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders the about page', () => {
+      const { container } = rederApp({ path: '/about' });
+
+      expect(container).toHaveTextContent('서비스에 대해서');
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders the restaurants page', () => {
+      const { container } = rederApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent('서울');
+    });
+  });
+
+  context('with undefined path', () => {
+    it('renders the not found page', () => {
+      const { container } = rederApp({ path: '/undefined' });
+
+      expect(container).toHaveTextContent('404 - Not Found');
+    });
+  });
 });
