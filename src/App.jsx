c87ca97e335427f2b6f0bf99b33a5f5f74906405
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-28 19:38:36 +0900

    [Green] Implement App

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,16 @@ import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './HomePage';
 import ErrorPage from './NotFoundPage';
+import AboutPage from './AboutPage';
 
 export default function App() {
   return (
     <Switch>
       <Route exact path="/">
         <HomePage />
+      </Route>
+      <Route path="/about">
+        <AboutPage />
       </Route>
       <ErrorPage />
     </Switch>
