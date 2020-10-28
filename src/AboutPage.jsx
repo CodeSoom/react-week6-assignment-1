32fbd50beaf99d729c6830ffda99d4e85dd9a355
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-29 00:15:36 +0900

    [Green] AboutPage 구현
    테스트를 기반으로 About component 구현.
    *TODO: Make NotFoundPage component!

---
+++ src/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>About...</h1>
+      <p>이 서비스는 라우팅 연습을 위한 트레이닝 공간입니다.</p>
+      <p>꾸준하게 반복 숙달 연습해봅시다!</p>
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import HomePage from './HomePage';
+import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
