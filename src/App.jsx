d1160ef603bbff0fd511c53954a494e0aa06802a
Woong <37302935+TaeWoongKim2@users.noreply.github.com>
2020-10-29 00:25:30 +0900

    [Refactor] Apply react router
    1st, intall 'react-router-dom'.
    2nd, App component refactor for router.
    *TODO: SPA - used to Link in 'react-router-dom'!

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
 
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
+import NotFoundPage from './NotFoundPage';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
-    <RestaurantsPage />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
