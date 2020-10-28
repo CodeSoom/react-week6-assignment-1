dc9b5873be812ce7f01a0ed34d789b7d65e2e281
goodzzong <goodzzong@gmail.com>
2020-10-28 23:15:58 +0900

    Header 에 대한 테스트 코드 추가

---
+++ src/Header.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import Header from './Header';
+
+test('Header', () => {
+  const { container } = render((
+    <Header />
+  ));
+
+  expect(container).toHaveTextContent('헤더');
+});
