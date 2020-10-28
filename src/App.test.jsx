c6f93372d358d341f82e304f2ca73b6db02d7360
oceanide <oceanide@gmail.com>
2020-10-28 19:37:02 +0900

    [REFACTOR] use MemoryRouter in App.test.jsx

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,10 +2,16 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
 describe('App', () => {
-  const renderApp = () => render(<App />);
+  const renderApp = () => render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>,
+  );
 
   it('renders App', () => {
     const { container } = renderApp();
