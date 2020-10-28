abd052e3e31282566032b2886b946228dff9b364
davidyang2149 <davidyang2149@gmail.com>
2020-10-28 23:36:07 +0900

    [Green] Update App.jsx
    6강 라우터 - 6주 차 과제 1
    - App.jsx 수정.
    - RestaurantsPage 추가.

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,9 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch } from 'react-redux';
-
-import { Link } from 'react-router-dom';
-
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
-
-import {
-  loadInitialData,
-} from './actions';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
+import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
-
   return (
-    <div>
-      <header>
-        <Link to="/">
-          <h1>헤더</h1>
-        </Link>
-      </header>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
-    </div>
+    <RestaurantsPage />
   );
 }
