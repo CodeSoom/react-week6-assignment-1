18bab0112521179641c6f4ab06d4872564056df3
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-28 09:44:41 +0900

    [refactor] Fix moodule in RestaurantPage.

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from 'react';
 
-import { useDispatch, useSelector } from '../__mocks__/react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetail from './RestaurantDetail';
 import Loading from './Loading';
