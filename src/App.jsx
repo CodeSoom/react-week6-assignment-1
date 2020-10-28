6b0f0793ab8193b9ba3bf176e1488b10c279eb3d
oceanide <oceanide@gmail.com>
2020-10-28 14:04:07 +0900

    [REFACTOR] add link of AboutPage and RestaurantsPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
 import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   const { location: { pathname } } = window;
 
   const MyComponent = {
     '/': HomePage,
+    '/restaurants': RestaurantsPage,
+    '/about': AboutPage,
   }[pathname];
 
   return (
