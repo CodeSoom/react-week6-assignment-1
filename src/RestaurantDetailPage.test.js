4ad73ad4f7109d27db19b504dc15d45107ec2465
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-28 23:34:02 +0900

    [Red] Add RestaurantDetailPage.test.jsx

---
+++ src/RestaurantDetailPage.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import RestaurantDetailPage from './RestaurantDetailPage';
+
+jest.mock('react-redux');
+
+describe('RestaurantDetailPage', () => {
+  it('show restaurant detail  ', () => {
+    const { container } = render((
+      <RestaurantDetailPage />
+    ));
+
+    expect(container).toHaveTextContent('양천주가');
+    expect(container).toHaveTextContent('서울 강남구 123456');
+  });
+});
