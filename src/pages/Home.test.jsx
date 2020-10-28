4e1023265dcac9399a46dcceb350537179516a56
goodzzong <goodzzong@gmail.com>
2020-10-29 00:38:43 +0900

    Home 을 구현

--- src/pages/Home.test.jsx
+++ src/pages/Home.test.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
 import Home from './Home';
 
 test('Home', () => {
   const { container, getByText } = render((
-    <Home />
+    <MemoryRouter>
+      <Home />
+    </MemoryRouter>
   ));
 
   expect(getByText('Home')).not.toBeNull();
