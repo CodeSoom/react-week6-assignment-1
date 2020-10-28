4559fa73667eeb2039c46f7be3588e3adbb09092
goodzzong <goodzzong@gmail.com>
2020-10-28 23:53:10 +0900

    About 를 구현

--- src/pages/About.test.jsx
+++ src/pages/About.test.jsx
@@ -5,9 +5,10 @@ import { render } from '@testing-library/react';
 import About from './About';
 
 test('About', () => {
-  const { container } = render((
+  const { container, getByText } = render((
     <About />
   ));
 
+  expect(getByText('About')).not.toBeNull();
   expect(container).toHaveTextContent('About 페이지 입니다.');
 });
