4dba76f8bf47d083771983260bdfedab1f2abfef
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-28 22:07:11 +0900

    [Red] Add AboutPage.test.jsx

---
+++ src/AboutPage.test.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import AboutPage from './AboutPage';
+
+describe('AboutPage', () => {
+  it('show About title and text', () => {
+    const { container } = render((
+      <AboutPage />
+    ));
+
+    expect(container).toHaveTextContent('About');
+    expect(container).toHaveTextContent('About 페이지 입니다.');
+  });
+});
