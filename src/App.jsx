53e10378ad2360558b6d1d08ae4638afe68bd5dc
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-28 19:02:46 +0900

    [Refactor] Refactor App to use react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,12 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 
-import AboutPage from './AboutPage';
-import ErrorPage from './ErrorPage';
 import HomePage from './HomePage';
-import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
-  const { location: { pathname } } = window;
-
-  const Page = {
-    '/': HomePage,
-    '/restaurants': RestaurantsPage,
-    '/about': AboutPage,
-  }[pathname] || ErrorPage;
-
   return (
-    <Page />
+    <BrowserRouter>
+      <HomePage />
+    </BrowserRouter>
   );
 }
