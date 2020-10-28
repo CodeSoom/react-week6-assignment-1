cd37d402b5885931bea2c024060dd5a5f1a338cd
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-28 23:47:31 +0900

    [Green] Implement RestaurantDetailPage

---
+++ src/RestaurantDetailPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function RestaurantDetailPage() {
+  return (
+    <div>
+      <h2>양천주가</h2>
+      <p>서울 강남구 123456</p>
+
+      <h3>메뉴</h3>
+      <ul>
+        <li>탕어쩌구</li>
+        <li>저쩌구</li>
+        <li>얼씨구</li>
+      </ul>
+    </div>
+  );
+}
