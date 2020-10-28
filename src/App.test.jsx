e031f453de1ecf5fd39fb05d32edd3cd4d602d7b
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-28 18:58:25 +0900

    [Refactor] Initilize App test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -23,67 +23,9 @@ describe('App', () => {
     ],
   }));
 
-  const mockLocation = (pathname) => {
-    delete window.location;
-    window.location = new URL(`..${pathname}`, 'https://www.example.com');
-  };
+  it('shows Home page with path /', () => {
+    const { container } = render(<App />);
 
-  describe('routes pages through pathname', () => {
-    context('when pathname is /restaurants', () => {
-      beforeEach(() => {
-        mockLocation('/restaurants');
-      });
-
-      it('shows restaurants page', () => {
-        const { queryByText } = render((
-          <App />
-        ));
-
-        expect(queryByText('서울')).not.toBeNull();
-        expect(queryByText('한식')).not.toBeNull();
-      });
-    });
-
-    context('when pathname is /', () => {
-      beforeEach(() => {
-        mockLocation('/');
-      });
-
-      it('shows home page', () => {
-        const { queryByText } = render((
-          <App />
-        ));
-
-        expect(queryByText('Home')).not.toBeNull();
-      });
-    });
-
-    context('when pathname is /about', () => {
-      beforeEach(() => {
-        mockLocation('/about');
-      });
-
-      it('shows about page', () => {
-        const { queryByText } = render((
-          <App />
-        ));
-
-        expect(queryByText('About')).not.toBeNull();
-      });
-    });
-  });
-
-  context('when path doesnt exist', () => {
-    beforeEach(() => {
-      mockLocation('/404');
-    });
-
-    it('shows error message', () => {
-      const { queryByText } = render((
-        <App />
-      ));
-
-      expect(queryByText('존재하지 않는 페이지 입니다')).not.toBeNull();
-    });
+    expect(container).toHaveTextContent('Home');
   });
 });
