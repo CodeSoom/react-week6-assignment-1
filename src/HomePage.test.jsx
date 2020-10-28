f9487bdf4631456ab5dfb4d4aa99ea8e0bae458f
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-28 18:31:33 +0900

    [Refactor] Refactor HomePage test

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -5,18 +5,18 @@ import { render } from '@testing-library/react';
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
+  const renderHomePage = () => render((
+    <HomePage />
+  ));
+
   it('shows heading', () => {
-    const { container } = render((
-      <HomePage />
-    ));
+    const { container } = renderHomePage();
 
     expect(container).toHaveTextContent('Home');
   });
 
   it('shows about link', () => {
-    const { container } = render((
-      <HomePage />
-    ));
+    const { container } = renderHomePage();
 
     expect(container).toHaveTextContent('About');
   });
