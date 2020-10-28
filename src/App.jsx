27f113843d2e7fe67b762e4bc7323edaca5f8bde
oceanide <oceanide@gmail.com>
2020-10-28 14:00:57 +0900

    [REFACTOR] add MyComponent using pathname of window location

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,13 @@ import React from 'react';
 import HomePage from './HomePage';
 
 export default function App() {
+  const { location: { pathname } } = window;
+
+  const MyComponent = {
+    '/': HomePage,
+  }[pathname];
+
   return (
-    <HomePage />
+    <MyComponent />
   );
 }
