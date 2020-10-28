8491e3bf9abc96a2ffc6964c837a6dbadd3887bb
comejtome508 <youngnoh.goh@gmail.com>
2020-10-29 06:50:18 +0900

    [Green] Add dispatch mock and contemporary data to make run app test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { MemoryRouter } from 'react-router-dom';
 
@@ -10,10 +10,20 @@ import App from './App';
 
 describe('App', () => {
   beforeEach(() => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+    
     useSelector.mockImplementation((selector) => selector({
-      regions: [],
-      categories: [],
-      restaurants: [],
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+      restaurants: [
+        { id: 1, name: '마법사주방' },
+      ],
     }));
   });
 
