09caaa34442c13a05b5ee4d6b38a959bc3b2f2f6
saseungmin <dbd02169@naver.com>
2020-10-28 22:45:25 +0900

    Fixed test

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import { BrowserRouter } from 'react-router-dom';
-
 import { Provider } from 'react-redux';
 
 import App from './App';
