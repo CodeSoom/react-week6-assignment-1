d2682aaa6d084cff86b3f88f10ec9d807359856d
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-28 10:44:06 +0900

    [refactor] Refactoring App test.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -25,7 +25,15 @@ describe('App', () => {
     }));
   });
 
-  function renderApp({ path }) {
+  function renderApp(path) {
+    if (!path) {
+      return render((
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      ));
+    }
+
     return render((
       <MemoryRouter initialEntries={[path]}>
         <App />
@@ -34,60 +42,56 @@ describe('App', () => {
   }
 
   it('renders Header', () => {
-    const { container } = render((
-      <MemoryRouter>
-        <App />
-      </MemoryRouter>
-    ));
+    const { container } = renderApp();
 
     expect(container).toHaveTextContent('헤더');
   });
 
   context('with path /', () => {
-    it('renders HomePage', () => {
-      const path = '/';
+    const path = '/';
 
-      const { container } = renderApp({ path });
+    it('renders HomePage', () => {
+      const { container } = renderApp(path);
 
       expect(container).toHaveTextContent('Home');
     });
   });
 
   context('with path /restaurants', () => {
-    it('renders RestaurantsPage', () => {
-      const path = '/restaurants';
+    const path = '/restaurants';
 
-      const { container } = renderApp({ path });
+    it('renders RestaurantsPage', () => {
+      const { container } = renderApp(path);
 
       expect(container).toHaveTextContent('서울');
     });
   });
 
   context('with path /about', () => {
-    it('renders AboutPage', () => {
-      const path = '/about';
+    const path = '/about';
 
-      const { container } = renderApp({ path });
+    it('renders AboutPage', () => {
+      const { container } = renderApp(path);
 
       expect(container).toHaveTextContent('이 페이지는 소개 페이지 입니다.');
     });
   });
 
   context('with path /restaurants/restaurantId ', () => {
-    it('renders RestaurantPage', () => {
-      const path = '/restaurants/1';
+    const path = '/restaurants/1';
 
-      const { container } = renderApp({ path });
+    it('renders RestaurantPage', () => {
+      const { container } = renderApp(path);
 
       expect(container).toHaveTextContent('양천주가');
     });
   });
 
   context('with invalid path ', () => {
-    it('renders NotFoundPage', () => {
-      const path = '/www';
+    const path = '/www';
 
-      const { container } = renderApp({ path });
+    it('renders NotFoundPage', () => {
+      const { container } = renderApp(path);
 
       expect(container).toHaveTextContent('Not Found');
     });
