45d09f1b512187dd696ff309bceedbd9edb41365
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-29 14:35:45 +0900

    Add RestaurantDatail.test.jsx

---
+++ src/RestaurantDatail.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import RESTAURANT from '../fixtures/restaurant';
+
+import RestaurantDetail from './RestaurantDetail';
+
+test('RestaurantDetail', () => {
+  const { container } = render((
+    <RestaurantDetail restaurant={RESTAURANT} />
+  ));
+
+  expect(container).toHaveTextContent(RESTAURANT.name);
+  expect(container).toHaveTextContent(RESTAURANT.address);
+
+  RESTAURANT.menuItems.forEach(({ name }) => {
+    expect(container).toHaveTextContent(name);
+  });
+});
