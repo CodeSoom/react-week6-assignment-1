59a65ce9a7a3d3221623bd7b03c48f23d8d7b79f
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-29 11:04:03 +0900

    [Red] Add a test case about setRestaurant action

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -108,4 +108,35 @@ describe('reducer', () => {
       });
     });
   });
+
+  describe('setRestaurant', () => {
+    it('changes restaurant', () => {
+      const initialState = {
+        restaurant: {},
+      };
+
+      const restaurant = {
+        id: 1,
+        categoryId: 1,
+        name: '양천주가',
+        address: '서울 강남구 123456',
+        menuItems: [
+          {
+            id: 1,
+            restaurantId: 1,
+            name: '비빔밥',
+          },
+          {
+            id: 2,
+            restaurantId: 1,
+            name: '짬뽕',
+          },
+        ],
+      };
+
+      const state = reducer(initialState, setRestaurant(restaurant));
+
+      expect(state.restaurant).toBe(restaurant);
+    });
+  });
 });
