d0e2592f633b3834caf823bf6b090b2dabf2232e
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-29 10:53:25 +0900

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Switch, Route, Link } from 'react-router-dom';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
+import RestaurantPage from './RestaurantPage';
 import NotFoundPage from './NotFoundPage';
 
 export default function App() {
@@ -15,6 +16,7 @@ export default function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/restaurants" component={RestaurantsPage} />
+        <Route path="/restaurants/:restaurantId" component={RestaurantPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
