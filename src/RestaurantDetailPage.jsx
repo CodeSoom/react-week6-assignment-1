cc45fccb8639d6590ab51e9f63e46862dee796f5
wjy5446 <wjy5446@gmail.com>
2020-10-29 23:56:20 +0900

    feat) RestaurantDetail 컴포넌트 출력

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,13 +1,20 @@
 import React from 'react';
 
+import RestaurantDetail from './RestaurantDetail';
+import RESTAURANTDETAIL from '../fixtures/restaurantDetail.js'
+
 // 1. useEffect를 통해서 id를 통해서 fetchRestaurantDetail을 통해서 정보를 얻는다
 // 2. 받는 동안 Loading... 문구가 보인다
 // 3. 완료시 detail Page를 생성한다.
 
 export default function RestaurantDetailPage() {
+  // TODO: clear fixture
+  const { name, address, menuItems } = RESTAURANTDETAIL;
   return (
-    <div>
-      RestaurantDetailPage
-    </div>
+    <RestaurantDetail
+      name={name}
+      address={address}
+      menuItems={menuItems}
+    />
   );
 }
