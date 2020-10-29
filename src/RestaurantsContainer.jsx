6569037f0d74d249c7b573c8d6f87c83c544d371
oceanide <oceanide@gmail.com>
2020-10-30 08:57:40 +0900

    Revert "[REFACTOR] add router to Restaurants and RestaurantDetail"
    This reverts commit 2891c1edbccf0a9f4c430358e00f9a13fb73eae0.

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -2,54 +2,20 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
-import {
-  Switch,
-  Route,
-  useRouteMatch,
-} from 'react-router-dom';
-
 import { get } from './utils';
 
 import Restaurants from './Restaurants';
-import RestaurantDetail from './RestaurantDetail';
 
 export default function RestaurantsContainer() {
-  // For Test: to render RestaurantDetail component
-  const restaurant = {
-    id: 1,
-    name: '양천주가',
-    address: '서울 강남구',
-    menuItems: [
-      { id: 1, name: '비빔밥' },
-      { id: 2, name: '짬뽕' },
-    ],
-  };
-
   const restaurants = useSelector(get('restaurants'));
-
-  const { path } = useRouteMatch();
 
   const handleClickRestaurant = () => {
   };
 
   return (
-    <Switch>
-      <Route
-        exact
-        path={path}
-        render={() => (
-          <Restaurants
-            restaurants={restaurants}
-            onClickRestaurant={handleClickRestaurant}
-          />
-        )}
-      />
-      <Route
-        path={`${path}/:id`}
-        render={() => (
-          <RestaurantDetail restaurant={restaurant} />
-        )}
-      />
-    </Switch>
+    <Restaurants
+      restaurants={restaurants}
+      onClickRestaurant={handleClickRestaurant}
+    />
   );
 }
