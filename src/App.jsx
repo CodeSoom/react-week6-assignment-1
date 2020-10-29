0bade2029cacbc6c0226220b441d550bf5b2c994
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-29 10:06:35 +0900

    Refactor App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 export default function App() {
   return (
     <div>
       <h1><Link to="/">헤더</Link></h1>
-      <p>Home</p>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      </Switch>
     </div>
   );
 }
