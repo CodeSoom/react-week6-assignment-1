79e4887750db4e15b5f0e0fbdc17990e4ecf5f83
juunini <juuni.ni.i@gmail.com>
2020-10-29 23:29:52 +0900

    Add RestaurantMenu [Refactoring end]

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default function RestaurantMenu({ menus = [{ id: 16, restaurantId: 1, name: '탕수육' }] }) {
+export default function RestaurantMenu({ menus }) {
   return (
     <>
       <h3>메뉴</h3>
--- src/components/RestaurantMenu.test.jsx
+++ src/components/RestaurantMenu.test.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 
 import RestaurantMenu from './RestaurantMenu';
+import restaurant from '../../fixtures/restaurant';
 
 describe('RestaurantMenu', () => {
   context('with menus', () => {
     it('renders menus', () => {
-      render(<RestaurantMenu />);
+      render(<RestaurantMenu menus={restaurant.menuItems} />);
 
+      expect(screen.getByText('메뉴')).toBeInTheDocument();
+      expect(screen.getByText('비빔밥')).toBeInTheDocument();
+      expect(screen.getByText('짬뽕')).toBeInTheDocument();
       expect(screen.getByText('탕수육')).toBeInTheDocument();
+      expect(screen.getByText('팔보채')).toBeInTheDocument();
     });
   });
 });
