93cc66985a534c4caa00d5cbdeea3ddcc07c1f7d
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-29 12:18:28 +0900

    [green] Apply Loading to RestaurantContainer.

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -3,11 +3,16 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import RestaurantDetail from './RestaurantDetail';
+import Loading from './Loading';
 
 export default function RestaurantContainer() {
   const { restaurant } = useSelector((state) => ({
     restaurant: state.restaurant,
   }));
+
+  if (!restaurant.id) {
+    return (<Loading />);
+  }
 
   return ((
     <RestaurantDetail restaurant={restaurant} />
