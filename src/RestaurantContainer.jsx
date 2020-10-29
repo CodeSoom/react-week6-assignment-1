6c5522e6a1231c7da2ddbc0166fc484963f8f1a7
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-29 11:59:29 +0900

    [refactoring] Apply useSelector to RestaurantContainer.

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
 export default function RestaurantContainer() {
+  const { restaurant } = useSelector((state) => ({
+    restaurant: state.restaurant,
+  }));
+
   return (
-    <h2>양천주가</h2>
+    <div>
+      <h2>{restaurant.name}</h2>
+    </div>
   );
 }
