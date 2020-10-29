7a62caf356fc0099b83f1d3af21ea1d12d1b964b
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-29 12:16:18 +0900

    [green] Apply RestaurantDetail to RestaurantPage.

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 import { useSelector } from 'react-redux';
 
+import RestaurantDetail from './RestaurantDetail';
+
 export default function RestaurantContainer() {
   const { restaurant } = useSelector((state) => ({
     restaurant: state.restaurant,
   }));
 
-  return (
-    <div>
-      <h2>{restaurant.name}</h2>
-    </div>
-  );
+  return ((
+    <RestaurantDetail restaurant={restaurant} />
+  ));
 }
