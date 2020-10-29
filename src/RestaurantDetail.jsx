dc6c4e4bbafc420ca9f9741c895e4df41bae12c7
wjy5446 <wjy5446@gmail.com>
2020-10-29 23:48:27 +0900

    feat) RestaurantDetail 테스트 통과

---
+++ src/RestaurantDetail.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function RestaurantDetail({ name, address, menuItems }) {
+  return (
+    <>
+      <h2>{name}</h2>
+      <p>{`주소 : ${address}`}</p>
+      <h3>메뉴</h3>
+      <ul>
+        {
+          menuItems.map((menuItem) => (
+            <li key={menuItem.id}>
+              {menuItem.name}
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
+}
