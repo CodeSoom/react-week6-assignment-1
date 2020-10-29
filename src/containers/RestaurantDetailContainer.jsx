367408c35931c7657cfae99d97dcf83eb655ee2c
saseungmin <dbd02169@naver.com>
2020-10-29 21:31:01 +0900

    RestaurantDetailContainer를 수정한다.

--- src/containers/RestaurantDetailContainer.jsx
+++ src/containers/RestaurantDetailContainer.jsx
@@ -1,28 +1,17 @@
 import React from 'react';
 
 import { useSelector } from 'react-redux';
+import RestaurantDetail from '../components/detail/RestaurantDetail';
 
 function RestaurantDetailContainer() {
   const { selectedRestaurant } = useSelector((state) => ({
     selectedRestaurant: state.selectedRestaurant,
   }));
 
-  if (!selectedRestaurant) {
-    return <div>로딩중...</div>;
-  }
-
-  const { name, address, menuItems } = selectedRestaurant;
-
   return (
-    <div>
-      <h2>{name}</h2>
-      <p>{`주소: ${address}`}</p>
-      {menuItems.map((menu) => (
-        <li key={menu.id}>
-          {menu.name}
-        </li>
-      ))}
-    </div>
+    <RestaurantDetail
+      restaurant={selectedRestaurant}
+    />
   );
 }
 
