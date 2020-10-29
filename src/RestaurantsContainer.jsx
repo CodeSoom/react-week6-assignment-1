0aa6b0144a3a535de6a6fa1b0dfeffea33597e2b
oceanide <oceanide@gmail.com>
2020-10-29 15:28:43 +0900

    [REFACTOR] add Restaurants to RestaurantsContainer

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -4,16 +4,12 @@ import { useSelector } from 'react-redux';
 
 import { get } from './utils';
 
+import Restaurants from './Restaurants';
+
 export default function RestaurantsContainer() {
   const restaurants = useSelector(get('restaurants'));
 
   return (
-    <ul>
-      {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>
-          {restaurant.name}
-        </li>
-      ))}
-    </ul>
+    <Restaurants restaurants={restaurants} />
   );
 }
