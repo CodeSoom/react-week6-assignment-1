a38f0cdafed38660e6bfa738bc0483c7e5a8a9d9
oceanide <oceanide@gmail.com>
2020-10-29 16:12:34 +0900

    [RED] add testcase to click link of restaurant

--- src/Restaurants.test.jsx
+++ src/Restaurants.test.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Restaurants from './Restaurants';
 
 describe('Restaurants', () => {
+  const handleClickRestaurant = jest.fn();
+
   const restaurants = [
     { id: 1, name: '양천주가' },
     { id: 2, name: '마법사주방' },
   ];
 
   const renderRestaurants = () => render(
-    <Restaurants restaurants={restaurants} />,
+    <Restaurants
+      restaurants={restaurants}
+      onClickRestaurant={handleClickRestaurant}
+    />,
   );
 
   it('render restaurants', () => {
@@ -19,6 +24,18 @@ describe('Restaurants', () => {
 
     restaurants.forEach((restaurant) => {
       expect(container).toHaveTextContent(restaurant.name);
+    });
+  });
+
+  context('when restaurant button is clicked', () => {
+    it('called handleClickRestaurant', () => {
+      const { getByText } = renderRestaurants();
+
+      restaurants.forEach((restaurant) => {
+        fireEvent.click(getByText(restaurant.name));
+      });
+
+      expect(handleClickRestaurant).toBeCalledTimes(2);
     });
   });
 });
