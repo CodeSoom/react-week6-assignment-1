aca08fbb2ba481c70445805d0eb386f709db7449
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-29 13:50:41 +0900

    Refactor RestaurantContainer.jsx

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 
+import { useSelector } from 'react-redux';
+
+import { get } from './utils';
+
 export default function RestaurantContainer() {
+  const { name, address, menuItems } = useSelector(get('restaurant'));
+
   return (
     <div>
-      <p>양천주가</p>
-      <p>서울시</p>
-      <p>탕수육</p>
-      <p>팔보채</p>
+      <h3>{name}</h3>
+      <p>
+        주소:
+        {address}
+      </p>
+      <h3>메뉴</h3>
+      <ul>
+        {
+          menuItems.map((menu) => (
+            <li key={menu.id}>{menu.name}</li>
+          ))
+        }
+      </ul>
     </div>
   );
 }
