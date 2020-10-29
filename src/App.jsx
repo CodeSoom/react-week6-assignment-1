1a7d14d24c1bce7cdbdf6ea4b073950193354e11
thkim <thkim2017@gmail.com>
2020-10-29 19:01:13 +0900

    [green] Implement detaching HomePage from App

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,12 @@ import {
   Route,
 } from 'react-router-dom';
 
+import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
 import AboutPage from './AboutPage';
 import NotFoundPage from './NotFoundPage';
 
 export default function App() {
-  function HomePage() {
-    return (
-      <h2>Home</h2>
-    );
-  }
-
   return (
     <div>
       <Switch>
--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -6,7 +6,7 @@ import { MemoryRouter } from 'react-router-dom';
 
 import HomePage from './HomePage';
 
-test('AboutPage', () => {
+test('HomePage', () => {
   render((
     <MemoryRouter>
       <HomePage />
