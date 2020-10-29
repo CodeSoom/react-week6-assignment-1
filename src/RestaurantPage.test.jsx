ff6860c55ec7f62a434844678bcb8eb446c95c26
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-29 12:58:00 +0900

    [refactor] Remove unnecessary test in RestaurantPage test.

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -6,62 +6,26 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import { MemoryRouter } from 'react-router-dom';
 
-import RESTAURANT from '../fixtures/restaurant';
-
 import RestaurantPage from './RestaurantPage';
 
 jest.mock('react-redux');
 
-describe('RestaurantPage', () => {
+test('RestaurantPage', () => {
   const dispatch = jest.fn();
 
-  beforeEach(() => {
-    useDispatch.mockImplementation(() => dispatch);
-  });
+  useDispatch.mockImplementation(() => dispatch);
 
-  function mockUseSelector(restaurant) {
-    return useSelector.mockImplementation((selector) => selector({
-      restaurant,
-    }));
-  }
+  useSelector.mockImplementation((selector) => selector({
+    restaurant: {},
+  }));
 
-  const match = { params: { restaurantId: RESTAURANT.id } };
+  const match = { params: { restaurantId: 1 } };
 
-  function renderRestaurantPage() {
-    return render((
-      <MemoryRouter>
-        <RestaurantPage match={match} />
-      </MemoryRouter>
-    ));
-  }
+  render((
+    <MemoryRouter>
+      <RestaurantPage match={match} />
+    </MemoryRouter>
+  ));
 
-  context('with restaurant', () => {
-    const restaurant = RESTAURANT;
-
-    it('renders RestaurantDetail', () => {
-      mockUseSelector(restaurant);
-
-      const { container } = renderRestaurantPage();
-
-      expect(dispatch).toBeCalled();
-
-      expect(container).toHaveTextContent(RESTAURANT.name);
-
-      RESTAURANT.menuItems.forEach(({ name }) => {
-        expect(container).toHaveTextContent(name);
-      });
-    });
-  });
-
-  context('without restaurant', () => {
-    const restaurant = [];
-
-    it('renders Loading', () => {
-      mockUseSelector(restaurant);
-
-      const { container } = renderRestaurantPage();
-
-      expect(container).toHaveTextContent('loading ...');
-    });
-  });
+  expect(dispatch).toBeCalled();
 });
