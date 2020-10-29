1c7582c1590c937a098bbeb56b15d6487de349ed
juunini <juuni.ni.i@gmail.com>
2020-10-29 23:27:46 +0900

    Add RestaurantMenu [Refactoring]

--- src/components/RestaurantMenu.jsx
+++ src/components/RestaurantMenu.jsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-export default function RestaurantMenu() {
-  return (<li>탕수육</li>);
+export default function RestaurantMenu({ menus = [{ id: 16, restaurantId: 1, name: '탕수육' }] }) {
+  return (
+    <>
+      <h3>메뉴</h3>
+      <ul>
+        {
+          menus.map((menu) => (
+            <li
+              key={menu.id}
+            >
+              {menu.name}
+            </li>
+          ))
+        }
+      </ul>
+    </>
+  );
 }
