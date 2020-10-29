a504dae5fa247ff359a47cf0991d6c6ebe00efef
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-29 11:16:47 +0900

    Implement RestaurantPage.jsx

---
+++ src/RestaurantPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function RestaurantPage() {
+  return (
+    <div>
+      <p>양천주가</p>
+      <p>서울시</p>
+      <p>탕수육</p>
+      <p>팔보채</p>
+    </div>
+  );
+}
