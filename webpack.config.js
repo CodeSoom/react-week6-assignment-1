d0caf3b2ac8799101dff1a5f80894141482c2af5
JamieShin0201 <jamieshin1450@gmail.com>
2020-10-29 17:14:46 +0900

    Install html-webpack-plugin and Edit webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src/index.jsx'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js',
+    publicPath: '/',
+  },
   module: {
     rules: [
       {
@@ -16,8 +22,11 @@ module.exports = {
   },
 
   devServer: {
-    historyApiFallback: {
-      index: 'index.html',
-    },
+    historyApiFallback: true,
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
 };
