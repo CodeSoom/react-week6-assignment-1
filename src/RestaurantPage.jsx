187f21af242bd21a19247c38a7326a622dff5bfd
davidyang2149 <davidyang2149@gmail.com>
2020-10-30 22:51:38 +0900

    [Green] Add RestaurantPage.jsx
    6강 라우터 - 6주 차 과제 1
    - RestaurantPage.jsx 생성.

---
+++ src/RestaurantPage.jsx
@@ -0,0 +1,27 @@
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import { useParams } from 'react-router-dom';
+
+import RestaurantContainer from './RestaurantContainer';
+
+import {
+  loadRestaurant,
+} from './actions';
+
+export default function RestaurantsPage() {
+  const { restaurantId } = useParams();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRestaurant(restaurantId));
+  });
+
+  return (
+    <div>
+      <RestaurantContainer />
+    </div>
+  );
+}
