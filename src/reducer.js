ca33541364f58818c7b0517c42092410b424d314
oceanide <oceanide@gmail.com>
2020-10-30 14:08:33 +0900

    [REFACTOR] delete initial value of restaurant

--- src/reducer.js
+++ src/reducer.js
@@ -4,15 +4,7 @@ const initialState = {
   regions: [],
   categories: [],
   restaurants: [],
-  restaurant: {
-    id: 1,
-    name: '양천주가',
-    address: '서울 강남구',
-    menuItems: [
-      { id: 1, name: '비빔밥' },
-      { id: 2, name: '짬뽕' },
-    ],
-  },
+  restaurant: {},
 
   selectedRegion: null,
   selectedCategory: null,
--- src/reducer.test.js
+++ src/reducer.test.js
@@ -16,15 +16,7 @@ describe('reducer', () => {
       regions: [],
       categories: [],
       restaurants: [],
-      restaurant: {
-        id: 1,
-        name: '양천주가',
-        address: '서울 강남구',
-        menuItems: [
-          { id: 1, name: '비빔밥' },
-          { id: 2, name: '짬뽕' },
-        ],
-      },
+      restaurant: {},
       selectedRegion: null,
       selectedCategory: null,
       selectedRestaurant: null,
