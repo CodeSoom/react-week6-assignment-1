f75433f8458ffb37600b8c960b4ca2e5d7bd6c92
oceanide <oceanide@gmail.com>
2020-10-30 09:32:26 +0900

    [GREEN] add RestaurantPage.jsx

---
+++ src/RestaurantPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import RestaurantContainer from './RestaurantContainer';
+
+export default function RestaurantPage() {
+  return (
+    <RestaurantContainer />
+  );
+}
