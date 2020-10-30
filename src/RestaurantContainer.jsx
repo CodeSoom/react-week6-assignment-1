ad9f4d16f4862c11f1a7afed6759c13c7fa10b98
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-30 15:01:19 +0900

    [Green] Implement RestaurantContainer

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -13,6 +13,9 @@ export default function RestaurantConatiner() {
   }
 
   return (
-    <p>{restaurant.name}</p>
+    <div>
+      <p>{restaurant.name}</p>
+      <p>{restaurant.address}</p>
+    </div>
   );
 }
