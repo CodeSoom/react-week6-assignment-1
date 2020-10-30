6fcff6f79c3a6d263247428c87c4e22316f80557
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-30 14:53:55 +0900

    [Green] Implement RestaurantContainer

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -6,6 +6,11 @@ import { get } from './utils';
 
 export default function RestaurantConatiner() {
   const restaurant = useSelector(get('restaurant'));
+  const loading = useSelector(get('restaurantLoading'));
+
+  if (loading) {
+    return 'loading...';
+  }
 
   return (
     <p>{restaurant.name}</p>
