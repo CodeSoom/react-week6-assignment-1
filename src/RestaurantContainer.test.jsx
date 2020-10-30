b12946806a41990c2da734b93b960e2769dd8258
oceanide <oceanide@gmail.com>
2020-10-30 10:20:46 +0900

    [REFACTOR] add useSelector mock in RestaurantContainer.test.jsx

--- src/RestaurantContainer.test.jsx
+++ src/RestaurantContainer.test.jsx
@@ -2,9 +2,25 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import RestaurantContainer from './RestaurantContainer';
 
+jest.mock('react-redux');
+
 describe('RestaurantContainer', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurant: {
+      id: 1,
+      name: '양천주가',
+      address: '서울 강남구',
+      menuItems: [
+        { id: 1, name: '비빔밥' },
+        { id: 2, name: '짬뽕' },
+      ],
+    },
+  }));
+
   const renderRestaurantContainer = () => render(
     <RestaurantContainer />,
   );
