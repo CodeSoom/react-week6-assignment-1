8b7c2d2d7691af65fa4082582c388e1c67dc1ae0
oceanide <oceanide@gmail.com>
2020-10-30 23:20:19 +0900

    [REFACTOR] add testcase without restaurant props

--- src/Restaurant.test.jsx
+++ src/Restaurant.test.jsx
@@ -7,18 +7,31 @@ import restaurant from '../fixtures/restaurant';
 import Restaurant from './Restaurant';
 
 describe('Restaurant', () => {
-  const renderRestaurant = () => render(
-    <Restaurant restaurant={restaurant} />,
+  const renderRestaurant = (restaurantInfo = '') => render(
+    <Restaurant restaurant={restaurantInfo} />,
   );
 
   it('renders restaurant', () => {
-    const { container } = renderRestaurant();
+    const { container } = renderRestaurant(restaurant);
 
     expect(container).toHaveTextContent(restaurant.name);
     expect(container).toHaveTextContent(restaurant.address);
 
     restaurant.menuItems.forEach((menu) => {
       expect(container).toHaveTextContent(menu.name);
+    });
+  });
+
+  context('without restaurant props', () => {
+    it('does not render restaurant', () => {
+      const { container } = renderRestaurant();
+
+      expect(container).not.toHaveTextContent(restaurant.name);
+      expect(container).not.toHaveTextContent(restaurant.address);
+
+      restaurant.menuItems.forEach((menu) => {
+        expect(container).not.toHaveTextContent(menu.name);
+      });
     });
   });
 });
