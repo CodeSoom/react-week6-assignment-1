12920d4e5f77cd71f81b0f4ec9417b742dd6fe42
wjy5446 <wjy5446@gmail.com>
2020-10-30 23:01:20 +0900

    feat) RestaurantDeatilPage에서 done이 false인 경우 loading 문구 출력 기능 추가

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { loadRestaurantDetail } from './actions';
 import RestaurantDetail from './RestaurantDetail';
 
+import { get } from './utils';
 // 1. useEffect를 통해서 id를 통해서 fetchRestaurantDetail을 통해서 정보를 얻는다
 // 2. 받는 동안 Loading... 문구가 보인다
 // 3. 완료시 detail Page를 생성한다.
@@ -16,12 +17,9 @@ export default function RestaurantDetailPage() {
     dispatch(loadRestaurantDetail());
   });
 
+  const { done } = useSelector(get('selectedRestaurantDetail'));
+
   return (
-    <div>aa</div>
-    /* <RestaurantDetail
-      name={name}
-      address={address}
-      menuItems={menuItems}
-    /> */
+    done === false ? <div>loading</div> : <div>unloading</div>
   );
 }
--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -19,21 +19,33 @@ describe('RestaurantDetailPage', () => {
 
   useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
-
   it('dispatch가 호출됩니다.', () => {
     renderRestaurantDetailPage();
 
     expect(dispatch).toBeCalled();
+  });
+
+  context('done이 false일 때,', () => {
+    useSelector.mockImplementation((selector) => selector({
+      selectedRestaurantDetail: { done: false, info: null },
+    }));
+
+    it('loading 문구가 출력됩니다.', () => {
+      const { container } = renderRestaurantDetailPage();
+
+      expect(container).toHaveTextContent('loading');
+    });
+  });
+
+  context('done이 true일 때,', () => {
+    useSelector.mockImplementation((selector) => selector({
+      selectedRestaurantDetail: { done: true, info: null },
+    }));
+
+    it('loading 문구가 출력됩니다.', () => {
+      const { container } = renderRestaurantDetailPage();
+
+      expect(container).toHaveTextContent('unloading');
+    });
   });
 });
