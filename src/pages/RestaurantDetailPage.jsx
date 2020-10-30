69e2d5378dc057394b90ce02edf7212297b7007c
saseungmin <dbd02169@naver.com>
2020-10-30 19:22:59 +0900

    RestaurantDetailPage를 수정한다.

--- src/pages/RestaurantDetailPage.jsx
+++ src/pages/RestaurantDetailPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 
-import { loadRestaurantDetail, resetRestaurantDetail } from '../actions';
+import { loadRestaurantDetail } from '../actions';
 
 import RestaurantDetailContainer from '../containers/RestaurantDetailContainer';
 
@@ -12,9 +12,6 @@ function RestaurantDetailPage({ match }) {
 
   useEffect(() => {
     dispatch(loadRestaurantDetail(id));
-    return () => {
-      dispatch(resetRestaurantDetail());
-    };
   }, []);
 
   return (
