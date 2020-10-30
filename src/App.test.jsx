77fe668e77d147060efa0728a0317173aee22661
oceanide <oceanide@gmail.com>
2020-10-30 17:42:05 +0900

    [REFACTOR] add testcase for restaurants and restaurants/:id

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,11 +2,31 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import { MemoryRouter } from 'react-router-dom';
+
+import regions from '../fixtures/regions';
+import categories from '../fixtures/categories';
+import restaurants from '../fixtures/restaurants';
+import restaurant from '../fixtures/restaurant';
 
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions,
+    categories,
+    restaurants,
+    restaurant,
+  }));
+
   const renderApp = ({ path }) => render(
     <MemoryRouter initialEntries={[path]}>
       <App />
@@ -28,6 +48,23 @@ describe('App', () => {
       const { container } = renderApp({ path: '/about' });
 
       expect(container).toHaveTextContent(/About 페이지/);
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders restaurants page', () => {
+      const { container } = renderApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent(/서울/);
+      expect(container).toHaveTextContent(/한식/);
+    });
+  });
+
+  context('with path /restaurants/:id', () => {
+    it('renders restaurant page', () => {
+      const { container } = renderApp({ path: '/restaurants/1' });
+
+      expect(container).toHaveTextContent(/양천주가/);
     });
   });
 
