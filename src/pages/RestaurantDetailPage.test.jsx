b5402e97685492de92775845d3f40827eb5271ee
saseungmin <dbd02169@naver.com>
2020-10-30 19:22:46 +0900

    RestaurantDetailPage테스트를 수정한다.

--- src/pages/RestaurantDetailPage.test.jsx
+++ src/pages/RestaurantDetailPage.test.jsx
@@ -1,9 +1,8 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
 
-import { MemoryRouter, Router } from 'react-router-dom';
+import { MemoryRouter } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetailPage from './RestaurantDetailPage';
@@ -12,13 +11,11 @@ import RESTAURANT_DETAIL from '../../fixtures/restaurantDetail';
 describe('RestaurantDetailPage', () => {
   const dispatch = jest.fn();
 
-  beforeEach(() => {
-    useDispatch.mockImplementation(() => dispatch);
+  useDispatch.mockImplementation(() => dispatch);
 
-    useSelector.mockImplementation((selector) => selector({
-      selectedRestaurant: RESTAURANT_DETAIL,
-    }));
-  });
+  useSelector.mockImplementation((selector) => selector({
+    selectedRestaurant: RESTAURANT_DETAIL,
+  }));
 
   it('renders restaurant detail', () => {
     const { container } = render((
@@ -27,23 +24,8 @@ describe('RestaurantDetailPage', () => {
       </MemoryRouter>
     ));
 
-    expect(dispatch).toBeCalled();
+    expect(dispatch).toBeCalledTimes(1);
 
     expect(container).toHaveTextContent(RESTAURANT_DETAIL.name);
-  });
-
-  it('renders initial restaurant', () => {
-    const history = createMemoryHistory();
-    history.push('/');
-
-    render((
-      <Router history={history}>
-        <RestaurantDetailPage match={{ params: { id: null } }} />
-      </Router>
-    ));
-
-    expect(dispatch).toBeCalledWith({
-      type: 'resetRestaurantDetail',
-    });
   });
 });
