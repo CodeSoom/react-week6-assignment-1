9dde499634e07d9dd295105fbf366a0efae3ebec
wjy5446 <wjy5446@gmail.com>
2020-10-30 22:35:59 +0900

    feat) RestaurantDeatilPage Dispatch 코드 작성

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,20 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { useDispatch } from 'react-redux';
+
+import { loadRestaurantDetail } from './actions';
 import RestaurantDetail from './RestaurantDetail';
-import RESTAURANTDETAIL from '../fixtures/restaurantDetail.js'
 
 // 1. useEffect를 통해서 id를 통해서 fetchRestaurantDetail을 통해서 정보를 얻는다
 // 2. 받는 동안 Loading... 문구가 보인다
 // 3. 완료시 detail Page를 생성한다.
 
 export default function RestaurantDetailPage() {
-  // TODO: clear fixture
-  const { name, address, menuItems } = RESTAURANTDETAIL;
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRestaurantDetail());
+  });
+
   return (
-    <RestaurantDetail
+    <div>aa</div>
+    /* <RestaurantDetail
       name={name}
       address={address}
       menuItems={menuItems}
-    />
+    /> */
   );
 }
