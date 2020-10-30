c81179393d7b3f8f284cf3c552db31d08c5e69bf
davidyang2149 <davidyang2149@gmail.com>
2020-10-30 15:37:46 +0900

    [Update] Update AboutPage.jsx
    6강 라우터 - 6주 차 과제 1
    - AboutPage.jsx 수정.

--- src/AboutPage.jsx
+++ src/AboutPage.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 export default function AboutPage() {
   return (
-    <p>About...</p>
+    <div>
+      <h1>About</h1>
+      <p>About 페이지 입니다.</p>
+    </div>
   );
 }
