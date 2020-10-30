68f6f1b930036bf5a581304be2af152fbb6a4079
davidyang2149 <davidyang2149@gmail.com>
2020-10-30 23:02:31 +0900

    [Green] Update App.jsx
    6강 라우터 - 6주 차 과제 1
    - App.jsx 수정.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import NotFoundPage from './NotFoundPage';
 import RestaurantsPage from './RestaurantsPage';
+import RestaurantPage from './RestaurantPage';
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/restaurants" component={RestaurantsPage} />
+        <Route path="/restaurant/:restaurantId" component={RestaurantPage} />
         <Route component={NotFoundPage} />
       </Switch>
     </div>
