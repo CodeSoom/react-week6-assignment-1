75e6dd987a7a6044bf2c025f8b361f0faedf2c4d
bbhye1 <65213094+bbhye1@users.noreply.github.com>
2020-10-30 09:31:13 +0900

    [refactoring] Add destructuring to RestaurantsContainer.

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -11,10 +11,10 @@ export default function RestaurantsContainer() {
 
   return (
     <ul>
-      {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>
-          <Link to={`/restaurants/${restaurant.id}`}>
-            {restaurant.name}
+      {restaurants.map(({ id, name }) => (
+        <li key={id}>
+          <Link to={`/restaurants/${id}`}>
+            {name}
           </Link>
         </li>
       ))}
