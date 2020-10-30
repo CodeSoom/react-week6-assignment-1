fba307734e3f4df3cf5b4c5caf9e7b68454b0339
gringrape <53764714+gringrape@users.noreply.github.com>
2020-10-30 12:01:34 +0900

    [Red] Add test about calling dispatch in restaurantDetailPage

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import RestaurantDetailPage from './RestaurantDetailPage';
 
 test('RestaurantDetailPage', () => {
-  render(<RestaurantDetailPage />);
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({}));
+
+  render((
+    <RestaurantDetailPage />
+  ));
+
+  expect(dispatch).toBeCalled();
 });
