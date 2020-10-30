906263bdde3dcc1b60632cff30a5f62d682804f6
juunini <juuni.ni.i@gmail.com>
2020-10-30 22:17:13 +0900

    Fix working to direct into page

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,6 @@ import NotFoundPage from './pages/NotFoundPage';
 // 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 // TODO: "/" 페이지가 아니면 상단에 "뒤로가기" 텍스트가 보이며, 클릭할 수 있다.
-// TODO: "/Restaurants" 링크에 가면 지역, 분류 버튼들이 있으며 각각 클릭 시 하단에 음식점 링크들이 나타난다.
-// TODO: "/Restaurants/:id" 링크에서는 음식점의 정보가 나온다.
 
 export default function App() {
   return (
