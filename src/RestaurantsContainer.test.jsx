ed51610092ff07b11c2d392353d38ed7c7939218
oceanide <oceanide@gmail.com>
2020-10-30 14:17:45 +0900

    [REFACTOR] add testcase to check to be called dispatch

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { MemoryRouter } from 'react-router-dom';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
+jest.mock('react-redux');
+
 describe('RestaurantsContainer', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
     restaurants: [
       { id: 1, name: '마법사주방' },
@@ -25,5 +31,15 @@ describe('RestaurantsContainer', () => {
     const { container } = renderRestaurantsContainer();
 
     expect(container).toHaveTextContent('마법사주방');
+  });
+
+  context('when restaurant link is clicked', () => {
+    it('dispatch selectRestaurant and loadRestaurant', () => {
+      const { getByText } = renderRestaurantsContainer();
+
+      fireEvent.click(getByText('마법사주방'));
+
+      expect(dispatch).toBeCalledTimes(2);
+    });
   });
 });
