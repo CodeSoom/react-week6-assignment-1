3a60e880c4f4bdc578fe540dfa1fbfd597a90032
ParkDongJo <pdj10211@gmail.com>
2020-10-30 19:25:57 +0900

    [week6-2] HomePage.test 테스트 코드 작성

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,15 +1,37 @@
 import React from 'react';
 
-import { MemoryRouter } from 'react-router-dom';
+import { Router } from 'react-router-dom';
 
-import { render } from '@testing-library/react';
+import { createMemoryHistory } from 'history';
+
+import { render, fireEvent } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
 test('HomePage', () => {
-  render((
-    <MemoryRouter>
+  const history = createMemoryHistory({ initialEntries: ['/'] });
+
+  history.push = jest.fn();
+
+  const { getByText } = render((
+    <Router history={history}>
       <HomePage />
-    </MemoryRouter>
+    </Router>
   ));
+
+  expect(getByText('Home')).not.toBeNull();
+  expect(getByText('About')).not.toBeNull();
+  expect(getByText('Restaurants')).not.toBeNull();
+
+  fireEvent.click(getByText('About'));
+
+  expect(history.push).toHaveBeenCalledWith('/about');
+
+  fireEvent.click(getByText('Restaurants'));
+
+  expect(history.push).toHaveBeenCalledWith('/restaurants');
+
+  fireEvent.click(getByText('멸망의길'));
+
+  expect(history.push).toHaveBeenCalledWith('/xxx');
 });
