828ba8244d383ad92215150d69b08fcaeaefed58
oceanide <oceanide@gmail.com>
2020-11-01 00:13:11 +0900

    [REFACTOR] add useEffect in RestaurantPage.jsx

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
+
+import { useParams } from 'react-router-dom';
+
+import {
+  selectRestaurant,
+  loadRestaurant,
+} from './actions';
 
 import RestaurantContainer from './RestaurantContainer';
 
 export default function RestaurantPage() {
+  const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(selectRestaurant(id));
+    dispatch(loadRestaurant());
+  });
+
   return (
     <RestaurantContainer />
   );
