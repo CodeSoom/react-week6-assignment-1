5820a54aa3c7b2253141a9cc4781fe3f9c35dab5
hyeonhyejin <dev.hyejin04@gmail.com>
2020-10-31 20:52:22 +0900

    [Green] Implement Restaurant

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -2,6 +2,8 @@ import React, { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import Restaurant from './Restaurant';
+
 import {
   loadRestaurant,
 } from './actions';
