8feb366a32ed1e6dae573de9de71dc5da87fa7f9
saseungmin <dbd02169@naver.com>
2020-10-31 15:26:05 +0900

    Refactoring Restaurant를 수정한다.

--- src/components/restaurant/Restaurant.jsx
+++ src/components/restaurant/Restaurant.jsx
@@ -2,12 +2,16 @@ import React from 'react';
 
 import { Link } from 'react-router-dom';
 
-const Restaurant = ({ restaurant: { id, name } }) => (
-  <li>
-    <Link to={`restaurants/${id}`}>
-      {name}
-    </Link>
-  </li>
-);
+const Restaurant = ({ restaurant }) => {
+  const { id, name } = restaurant;
+
+  return (
+    <li>
+      <Link to={`restaurants/${id}`}>
+        {name}
+      </Link>
+    </li>
+  );
+};
 
 export default Restaurant;
