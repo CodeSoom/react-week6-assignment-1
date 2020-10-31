a6f66889ec275b066a79c24a13dd9aea165a8ba6
juunini <juuni.ni.i@gmail.com>
2020-10-31 09:36:00 +0900

    Add Header[Green]

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,3 +1,12 @@
 import React from 'react';
-
 import { Link, useLocation } from 'react-router-dom';
+
+export default function Header() {
+  const { pathname } = useLocation();
+
+  if (pathname === '/') return null;
+
+  return (
+    <Link to="/">홈으로</Link>
+  );
+}
--- src/layout/Header.test.jsx
+++ src/layout/Header.test.jsx
@@ -5,14 +5,14 @@ import { render, screen } from '@testing-library/react';
 import Header from './Header';
 
 describe('Header', () => {
-  const renderHeader = () => render(
-    <MemoryRouter>
+  const renderHeader = (pathname) => render(
+    <MemoryRouter initialEntries={[{ pathname }]}>
       <Header />
     </MemoryRouter>,
   );
   context('when pathname is "/"', () => {
     it('renders not "홈으로"', () => {
-      renderHeader();
+      renderHeader('/');
 
       expect(screen.queryByText('홈으로')).toBeNull();
     });
@@ -20,7 +20,7 @@ describe('Header', () => {
 
   context('when pathname is not "/"', () => {
     it('renders "홈으로"', () => {
-      renderHeader();
+      renderHeader('/about');
 
       expect(screen.getByText('홈으로')).toBeInTheDocument();
     });
