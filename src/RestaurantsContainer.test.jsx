f35a3da0248df2a93a922c4578617cce6dbd68e7
ParkDongJo <pdj10211@gmail.com>
2020-10-31 11:43:54 +0900

    [week6-2] RestaurantsContainer.test 테스트 코드 작성하기

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,21 +1,51 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { Router } from 'react-router-dom';
+
+import { render, fireEvent } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
+import { createMemoryHistory } from 'history';
+
 import RestaurantsContainer from './RestaurantsContainer';
 
-test('RestaurantsContainer', () => {
-  useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+describe('RestaurantsContainer', () => {
+  const history = createMemoryHistory({ initialEntries: ['/'] });
 
-  const { container } = render((
-    <RestaurantsContainer />
-  ));
+  beforeEach(() => {
+    history.push = jest.fn();
 
-  expect(container).toHaveTextContent('마법사주방');
+    useSelector.mockImplementation((selector) => selector({
+      restaurants: [
+        { id: 1, name: '양천주가' },
+      ],
+    }));
+  });
+
+  function renderApp() {
+    return render((
+      <Router history={history}>
+        <RestaurantsContainer />
+      </Router>
+    ));
+  }
+
+  context('with path /restaurants', () => {
+    it('renders the restaurants page', () => {
+      const { container } = renderApp();
+
+      expect(container).toHaveTextContent('양천주가');
+    });
+  });
+
+  context('with path restaurants/{id}', () => {
+    it('renders the restaurant detail page', () => {
+      const { getByText } = renderApp();
+
+      fireEvent.click(getByText('양천주가'));
+
+      expect(history.push).toHaveBeenCalledWith('restaurants/1');
+    });
+  });
 });
