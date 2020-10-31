751d64ca6ad470de981bee4c4ef96cd556562425
JamieShin0201 <65442049+JamieShin0201@users.noreply.github.com>
2020-10-31 11:42:23 +0900

    Refactor RestaurantPage.jsx

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,24 +1,15 @@
-import React, { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
+import React from 'react';
 
 import { useParams } from 'react-router-dom';
 
 import RestaurantContainer from './RestaurantContainer';
 
-import { loadRestaurant } from './actions';
-
 export default function RestaurantPage({ params }) {
   const { id } = params || useParams();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadRestaurant(id));
-  }, []);
 
   return (
     <div>
-      <RestaurantContainer />
+      <RestaurantContainer restaurantId={id} />
     </div>
   );
 }
