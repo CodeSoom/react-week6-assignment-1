f41d985b8de23ebcba370df462a7d9eccb1990bb
oceanide <oceanide@gmail.com>
2020-11-01 22:12:23 +0900

    [REFACTOR] add Loading message

--- src/RestaurantContainer.jsx
+++ src/RestaurantContainer.jsx
@@ -8,6 +8,13 @@ import { get } from './utils';
 
 export default function RestaurantContainer() {
   const restaurant = useSelector(get('restaurant'));
+  const loading = useSelector(get('loading'));
+
+  if (loading) {
+    return (
+      <p>Loading...</p>
+    );
+  }
 
   return (
     <Restaurant restaurant={restaurant} />
