44ad8fef091ea5de61416806c96cad8c4c33a827
Seungmin Lee <seungmin@uxplus.dev>
2020-11-01 12:57:44 +0900

    Add code
    Add code to render 'Home'.

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,9 @@ import React, { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
-
 import {
   loadInitialData,
 } from './actions';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 export default function App() {
   const dispatch = useDispatch();
@@ -24,9 +15,7 @@ export default function App() {
 
   return (
     <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
+      <h2>Home</h2>
     </div>
   );
 }
