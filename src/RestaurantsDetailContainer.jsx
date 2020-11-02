cf68b0a10d29dc028c2939bea80e529f78b99a5c
dev cloubot <goodzzong@gmail.com>
2020-11-02 11:45:49 +0900

    RestaurantsDetailContainer 를 구현

---
+++ src/RestaurantsDetailContainer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function RestaurantsDetailContainer() {
+  return (
+    <div>
+      <h2>양천주가</h2>
+      <p>주소: 서울 강남구</p>
+      <h2>메뉴</h2>
+      <ul>
+        <li>탕수육</li>
+      </ul>
+    </div>
+  );
+}
+
+export default RestaurantsDetailContainer;
