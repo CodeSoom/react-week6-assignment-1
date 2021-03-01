47b19a174ea8a52539291cf8bf611fc319879163
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-01 17:14:30 +0900

    Red) HomePage/test.jsx: 추가 및 테스트 케이스 추가

---
+++ src/HomePage.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import HomePage from './HomePage';
+
+test('HomePage', () => {
+  const { queryByText } = render((<HomePage />));
+
+  expect(queryByText('Home')).not.toBeNull();
+  expect(queryByText('About')).not.toBeNull();
+  expect(queryByText('Restaurants')).not.toBeNull();
+});
