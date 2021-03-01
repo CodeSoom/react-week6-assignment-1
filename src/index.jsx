ac03577622a6fcb3d00659ac80bae8da24b07647
dodoongtak <themighty.tak@gmail.com>
2021-03-02 02:19:43 +0900

    ⚰️ remove: no-unused variable, Link

--- src/index.jsx
+++ src/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import ReactDOM from 'react-dom';
 
-import { BrowserRouter, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import { Provider } from 'react-redux';
 
