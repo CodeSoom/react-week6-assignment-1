e7cf2b5f414e3821624632ca27973ab80b89fb41
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-01 18:36:51 +0900

    Refactoring) App.jsx: 헤더 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
@@ -10,6 +10,9 @@ import NotFoundPage from './NotFoundPage';
 export default function App() {
   return (
     <div>
+      <header>
+        <h4><Link to="/">헤더</Link></h4>
+      </header>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/restaurants" component={RestaurantsPage} />
