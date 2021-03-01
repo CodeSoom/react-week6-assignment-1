07eb4a9764ebc960ca3441d1199997891572d6aa
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-01 16:54:58 +0900

    Green) RestaurantsPage.jsx : 추가 및 App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 
-import { useDispatch } from 'react-redux';
-
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
-
-import {
-  loadInitialData,
-} from './actions';
+import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
-
   return (
     <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
+      <RestaurantsPage />
     </div>
   );
 }
