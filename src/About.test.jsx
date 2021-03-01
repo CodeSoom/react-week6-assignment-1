6cf5b8d1a8e749044dd8a705dda6f629d8aa8c4a
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-01 17:50:23 +0900

    Green) About.jsx: 추가 및 테스트 통과

--- src/About.test.jsx
+++ src/About.test.jsx
@@ -7,6 +7,5 @@ import About from './About';
 test('About', () => {
   const { queryByText } = render((<About />));
 
-  expect(queryByText(/About/)).not.toBeNull();
   expect(queryByText(/About 페이지 입니다./)).not.toBeNull();
 });
