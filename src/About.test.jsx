3d4f372d2a1e6a8e7d123db8231b38beb7f7bea1
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-01 17:46:20 +0900

    Red) About.test.jsx : 테스트 케이스 추가

---
+++ src/About.test.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import About from './About';
+
+test('About', () => {
+  const { queryByText } = render((<About />));
+
+  expect(queryByText(/About/)).not.toBeNull();
+  expect(queryByText(/About 페이지 입니다./)).not.toBeNull();
+});
