140d28dd2ed9ab84c89d45292a536aaaab95c1fc
dodoongtak <themighty.tak@gmail.com>
2021-03-01 19:51:57 +0900

    ✅ test: add scenarios for rendering corresponding pages with given path
    It renders the page the following:
    Home / About / Restaurants / NotFound

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
@@ -15,13 +17,67 @@ describe('App', () => {
     useDispatch.mockImplementation(() => dispatch);
 
     useSelector.mockImplementation((selector) => selector({
-      regions: [],
-      categories: [],
-      restaurants: [],
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+      restaurants: [
+        { id: 1, name: '마법사주방' },
+      ],
     }));
   });
 
-  it('renders links', () => {
-    render(<App />);
+  context('with path /', () => {
+    it('renders the home page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('Home');
+      expect(container).toHaveTextContent('About');
+      expect(container).toHaveTextContent('Restaurants');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders the about page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/about']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('About');
+    });
+  });
+
+  context('with path /restaurnats', () => {
+    it('renders the restaurants page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/restaurants']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('서울');
+      expect(container).toHaveTextContent('한식');
+      expect(container).toHaveTextContent('마법사주방');
+    });
+  });
+
+  context('with unspecified path /kdaskhkajsdh', () => {
+    it('renders the NotFound page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/kdaskhkajsdh']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('404 Not Found');
+    });
   });
 });
