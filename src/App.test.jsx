155eeaa17e610972a070201839e232e6f7342604
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-01 20:29:43 +0900

    App test 수정(RestaurantsPage 보여준 것 -> 각각의 화면을 pathName에 따라서 잘 보여주는 것)

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,31 +4,62 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
-test('App', () => {
+function renderAppWithPath({ path }) {
+  return render(
+    <MemoryRouter initialEntries={[path]}>
+      <App />
+    </MemoryRouter>,
+  );
+}
+
+describe('App', () => {
   const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+  beforeEach(() => {
+    jest.clearAllMocks();
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+      restaurants: [
+        { id: 1, name: '마법사주방' },
+      ],
+    }));
+  });
 
-  const { queryByText } = render((
-    <App />
-  ));
+  it('Header는 올바른 pathName에 연결되어 있다.', () => {
+    const { queryByText } = renderAppWithPath({ path: '/' });
 
-  expect(dispatch).toBeCalled();
+    expect(queryByText('Header').getAttribute('href')).toBe('/');
+  });
 
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+  it('HomePage를 그린다.', () => {
+    const { queryByText } = renderAppWithPath({ paht: '/' });
+
+    expect(queryByText('Header')).toBeInTheDocument();
+    expect(queryByText('Home')).toBeInTheDocument();
+  });
+
+  it('AboutPage를 그린다.', () => {
+    const { queryByText } = renderAppWithPath({ path: '/About' });
+
+    expect(queryByText('Header')).toBeInTheDocument();
+    expect(queryByText('About 페이지 입니다.')).toBeInTheDocument();
+  });
+
+  it('RestaurantsPage를 그린다.', () => {
+    const { queryByText } = renderAppWithPath({ path: '/Restaurants' });
+
+    expect(queryByText('Header')).toBeInTheDocument();
+    expect(queryByText('서울')).toBeInTheDocument();
+  });
 });
