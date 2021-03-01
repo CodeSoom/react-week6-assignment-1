3f89a933be3ad08c5559f90e38e9821cb1ea596e
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-01 17:20:00 +0900

    Green) HomePage.jsx: 작성 및 테스트케이스 통과

---
+++ src/HomePage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <ul>
+        <li>About</li>
+        <li>Restaurants</li>
+      </ul>
+    </div>
+  );
+}
