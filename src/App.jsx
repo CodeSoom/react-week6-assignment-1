b0d810cc679727da2a5b0c1bad2c66414fed30fa
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-01 17:39:10 +0900

    Refactor) react-router 추가 및 테스트 케이스 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import React from 'react';
 
+import {
+  BrowserRouter, Switch, Route,
+} from 'react-router-dom';
+
+import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
     <div>
-      <RestaurantsPage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/restaurants" component={RestaurantsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,11 +1,23 @@
 import React from 'react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
+function renderHomePage() {
+  return (
+    render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>,
+    )
+  );
+}
+
 test('HomePage', () => {
-  const { queryByText } = render((<HomePage />));
+  const { queryByText } = renderHomePage();
 
   expect(queryByText('Home')).not.toBeNull();
   expect(queryByText('About')).not.toBeNull();
