4c9927b31ae5d7760341a08b70c7948e8e6d4714
jjkim01 <jaen6563@gmail.com>
2021-03-01 17:32:30 +0900

    test: HomePage에서 2개의 링크가 있는지 테스트

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -6,13 +6,29 @@ import { render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
-test('HomePage', () => {
-  const { queryByText } = render((
-    <MemoryRouter>
+function renderApp({ path }) {
+  return render((
+    <MemoryRouter initialEntries={[path]}>
       <HomePage />
     </MemoryRouter>
   ));
-  expect(queryByText('Home')).not.toBeNull();
-  expect(queryByText('Restaurants')).not.toBeNull();
-  expect(queryByText('About')).not.toBeNull();
+}
+describe('HomePage', () => {
+  context('Show homepage with /', () => {
+    it('renders the homepage', () => {
+      const { queryByText } = renderApp({ path: '/' });
+
+      expect(queryByText('Home')).not.toBeNull();
+      expect(queryByText('Restaurants')).not.toBeNull();
+      expect(queryByText('About')).not.toBeNull();
+    });
+
+    it('Links exist', () => {
+      renderApp({ path: '/' });
+
+      const aTags = document.querySelectorAll('a');
+      expect(aTags[0].getAttribute('href')).toEqual('/about');
+      expect(aTags[1].getAttribute('href')).toEqual('/restaurants');
+    });
+  });
 });
