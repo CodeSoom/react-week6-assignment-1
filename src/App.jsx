1f2722bd7a36eea666a9887a1a999054f8797c99
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-01 21:07:39 +0900

    :construction: Green-Working: App
    - 헤더 Link 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
-import { Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 
 import RestaurantPage from './RestaurantPage';
 
 export default function App() {
   return (
     <div>
+      <Link to="/">
+        <h1>헤더</h1>
+      </Link>
       <Switch>
         <Route exact path="/restaurants" component={RestaurantPage} />
       </Switch>
