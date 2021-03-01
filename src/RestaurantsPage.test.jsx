8091750c4767f6a08214d07eeea289cc7137c220
jjkim01 <jaen6563@gmail.com>
2021-03-01 17:51:10 +0900

    test: Restaurants 컴포넌트 호출 테스트

---
+++ src/RestaurantsPage.test.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import RestaurantsPage from './RestaurantsPage';
+
+test('RestaurantsPage', () => {
+  render(<RestaurantsPage />);
+});
