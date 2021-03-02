d0c50b2d55372156bd2da97bd5a60d8add0e13a5
EL <ickyicky@gmail.com>
2021-03-02 21:50:38 +0900

    Red: 테스트 추가. 경로에 따라 렌더링

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,10 +4,20 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
 describe('App', () => {
-  it('초기 렌더링 후 요소를 검사한다.', () => {
+  function renderApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
+
+  beforeEach(() => {
     const dispatch = jest.fn();
 
     useDispatch.mockImplementation(() => dispatch);
@@ -17,13 +27,26 @@ describe('App', () => {
       categories: [],
       restaurants: [],
     }));
+  });
 
-    const { queryByText } = render((
-      <App />
-    ));
+  context('경로가 /', () => {
+    it('HomePage를 그려준다.', () => {
+      const { container } = renderApp({ path: '/' });
 
-    expect(queryByText('헤더')).not.toBeNull();
-    expect(queryByText('About')).not.toBeNull();
-    expect(queryByText('Restraunts')).not.toBeNull();
+      expect(container).toHaveTextContent('헤더');
+
+      expect(container).toHaveTextContent('About');
+      expect(container).toHaveTextContent('Restraunts');
+    });
+  });
+
+  context('경로가 /about', () => {
+    it('About을 그려준다.', () => {
+      const { container } = renderApp({ path: '/about' })
+
+      expect(container).toHaveTextContent('헤더');
+
+      expect(container).toHaveTextContent('About this..');
+    });
   });
 });
