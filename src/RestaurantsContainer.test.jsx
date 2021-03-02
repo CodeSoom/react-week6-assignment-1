3f630faa6d58001cf008f13937d7e92df56c8caf
Yoon <yoonhe@gmail.com>
2021-03-02 20:35:20 +0900

    [refactoring] RestaurantsContainer를 리팩토링합니다
    버튼 클릭시 라우팅될 수 있도록 `Link`로 감싸주었습니다

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -4,18 +4,25 @@ import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
 import RestaurantsContainer from './RestaurantsContainer';
 
-test('RestaurantsContainer', () => {
-  useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+describe('RestaurantsContainer', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      restaurants: [
+        { id: 1, name: '마법사주방' },
+      ],
+    }));
+  });
 
-  const { container } = render((
-    <RestaurantsContainer />
-  ));
+  it('renders restaurants', () => {
+    const { container } = render((
+      <MemoryRouter>
+        <RestaurantsContainer />
+      </MemoryRouter>
+    ));
 
-  expect(container).toHaveTextContent('마법사주방');
+    expect(container).toHaveTextContent('마법사주방');
+  });
 });
