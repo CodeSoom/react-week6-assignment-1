455c9a93ab300bc17974ec1264521b9ae772ac94
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-02 14:46:42 +0900

    :white_check_mark: Red-Test: actions
    - loadRestaurantDetail
    - setRestaurantDetail

--- src/actions.test.js
+++ src/actions.test.js
@@ -8,6 +8,8 @@ import {
   setCategories,
   loadRestaurants,
   setRestaurants,
+  setRestaurantDetail,
+  loadRestaurantDetail,
 } from './actions';
 
 const middlewares = [thunk];
@@ -81,6 +83,22 @@ describe('actions', () => {
 
         expect(actions).toHaveLength(0);
       });
+    });
+  });
+
+  describe('loadRestaurantDetail', () => {
+    beforeEach(() => {
+      store = mockStore({
+        restaurant: null,
+      });
+    });
+
+    it('runs setRestaurantDetail', async () => {
+      await store.dispatch(loadRestaurantDetail(4));
+
+      const actions = store.getActions();
+
+      expect(actions[0]).toEqual(setRestaurantDetail([]));
     });
   });
 });
