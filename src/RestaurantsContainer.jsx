03fa29a1a244f7d1e4b394c83289c744895db16a
jjkim01 <jaen6563@gmail.com>
2021-03-02 20:21:06 +0900

    레스토랑 정보를 미리 받아 state로 넘기는 코드 삭제

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -3,10 +3,6 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 import {
-  loadRestaurantDetail,
-} from './actions';
-
-import {
   Link,
 } from 'react-router-dom';
 
@@ -14,24 +10,13 @@ import { get } from './utils';
 
 export default function RestaurantsContainer() {
   const restaurants = useSelector(get('restaurants'));
-  const restaurantDetail = useSelector(get('restaurantDetail'));
-
-  function x() {
-    console.log('누르는 순간 api 호출하게 하기');
-  }
 
   return (
     <ul>
       {restaurants.map((restaurant) => (
-        <li key={restaurant.id} onClick={ () => x() }>
+        <li key={restaurant.id}>
           <Link to={{
             pathname: `/restaurant/${restaurant.id}`,
-            state: {
-              id: restaurantDetail.id || null,
-              name: restaurantDetail.name || null,
-              address: restaurantDetail.address || null,
-              menuItems: restaurantDetail.menuItems || null,
-            },
           }}
           >
             {restaurant.name}
