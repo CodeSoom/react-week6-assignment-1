b8efa1589c6e5b720ef62b78f2a167806ff6bc22
EL <ickyicky@gmail.com>
2021-03-02 20:38:43 +0900

    Green: 테스트 통과. 헤더 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,6 @@
-import React, { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
+import React from 'react';
 
 import Restraunts from './Restraunts';
-
-import {
-  loadInitialData,
-} from './actions';
 
 // 0. 지역, 분류 목록을 얻기
 // 1. 지역 선택 - Regions <- API (0)
@@ -14,14 +8,9 @@ import {
 // 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
-
   return (
     <div>
+      <header>헤더</header>
       <Restraunts />
     </div>
   );
