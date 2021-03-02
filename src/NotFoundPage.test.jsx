a24ee1b8ec9879c0caee860d0a24f1f018d7a175
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-03 04:58:34 +0900

    Refactor) NotFoundPage: 테스트 케이스 추가

---
+++ src/NotFoundPage.test.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import NotFoundPage from './NotFoundPage';
+
+describe('NotFoundPage', () => {
+  it('shows "404 Not Found"', () => {
+    const { queryByText } = render((<NotFoundPage />));
+
+    expect(queryByText(/404 Not Found/)).not.toBeNull();
+  });
+});
