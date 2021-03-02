4c5d7330cc07da5f583823eaf75db3938c1a6161
Yoon <yoonhe@gmail.com>
2021-03-02 17:32:24 +0900

    [red] AboutPage에 대한 테스트를 추가합니다

---
+++ src/AboutPage.test.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import AboutPage from './AboutPage';
+
+test('AboutPage', () => {
+  render((
+    <AboutPage />
+  ));
+});
