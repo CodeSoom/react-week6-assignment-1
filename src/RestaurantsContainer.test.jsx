4cd607b8cfb129d781378319044053ae411fa7dd
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-02 16:51:28 +0900

    RestaurantsContainer test 작성

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -4,18 +4,28 @@ import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import RestaurantsContainer from './RestaurantsContainer';
 
-test('RestaurantsContainer', () => {
+describe('RestaurantsContainer', () => {
   useSelector.mockImplementation((selector) => selector({
     restaurants: [
       { id: 1, name: '마법사주방' },
+      { id: 2, name: '성전떡볶이' },
     ],
   }));
 
-  const { container } = render((
-    <RestaurantsContainer />
-  ));
+  it('식당목록을 보여준다.', () => {
+    const { container, queryByText } = render(
+      <MemoryRouter>
+        <RestaurantsContainer />
+      </MemoryRouter>,
+    );
 
-  expect(container).toHaveTextContent('마법사주방');
+    expect(container).toHaveTextContent('마법사주방');
+    expect(container).toHaveTextContent('성전떡볶이');
+    expect(queryByText('마법사주방').getAttribute('href')).toBe('/Restaurants/1');
+    expect(queryByText('성전떡볶이').getAttribute('href')).toBe('/Restaurants/2');
+  });
 });
