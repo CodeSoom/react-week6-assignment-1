67106b62e379e613ab4075785bb2d5c5d08db0a2
EL <ickyicky@gmail.com>
2021-03-02 21:32:01 +0900

    Red: 테스트 추가. 홈페이지 컴포넌트 작성

---
+++ src/HomePage.test.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import HomePage from './HomePage';
+
+describe('HomePage', () => {
+  it('초기 렌더링 후 요소를 검사한다.', () => {
+    const { queryByText } = render((
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    ));
+
+    expect(queryByText('About')).not.toBeNull();
+    expect(queryByText('Restraunts')).not.toBeNull();
+  });
+});
