320cba2ccd1dc7ac966112eb2468cf0a804f21b0
jjkim01 <jaen6563@gmail.com>
2021-03-02 21:22:02 +0900

    loadRestaurantDetail() 호출 시점을 식당 목록 클릭했을 때로 이동

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,20 +1,26 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import {
   Link,
 } from 'react-router-dom';
 
 import { get } from './utils';
+import { loadRestaurantDetail } from './actions';
 
 export default function RestaurantsContainer() {
+  const dispatch = useDispatch();
   const restaurants = useSelector(get('restaurants'));
+
+  function handleClick(restaurantId) {
+    dispatch(loadRestaurantDetail(restaurantId));
+  }
 
   return (
     <ul>
       {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>
+        <li key={restaurant.id} onClick={() => handleClick(restaurant.id)}>
           <Link to={{
             pathname: `/restaurant/${restaurant.id}`,
           }}
