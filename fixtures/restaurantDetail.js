735643b5b8157561d0021599b148ae9b156f8a06
jjkim01 <jaen6563@gmail.com>
2021-03-03 00:07:30 +0900

    잘못 선언된 자료형 변경 list to object

--- fixtures/restaurantDetail.js
+++ fixtures/restaurantDetail.js
@@ -1,7 +1,5 @@
-const restaurants = [
-  {
-    id: 1, name: '양천주가', category: '한식', address: '서울 강남구 123456', menuItems: [{ id: 1, restaurantId: 1, name: '비빔밥' }, { id: 2, restaurantId: 1, name: '짬뽕' }, { id: 16, restaurantId: 1, name: '탕수육' }, { id: 17, restaurantId: 1, name: '팔보채' }, { id: 1229, restaurantId: 1, name: '짜짱면' }],
-  },
-];
+const restaurants = {
+  id: 1, name: '양천주가', category: '한식', address: '서울 강남구 123456', menuItems: [{ id: 1, restaurantId: 1, name: '비빔밥' }, { id: 2, restaurantId: 1, name: '짬뽕' }, { id: 16, restaurantId: 1, name: '탕수육' }, { id: 17, restaurantId: 1, name: '팔보채' }, { id: 1229, restaurantId: 1, name: '짜짱면' }],
+};
 
 export default restaurants;
