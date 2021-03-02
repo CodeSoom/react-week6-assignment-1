c790c4eeb5a3c1dfd87072f1f57f8b03e6d1cbc6
EL <ickyicky@gmail.com>
2021-03-02 20:43:50 +0900

    Green: 테스트 통과

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,15 @@ import Restraunts from './Restraunts';
 export default function App() {
   return (
     <div>
-      <header>헤더</header>
+      <header>
+        <h1>
+          헤더
+        </h1>
+      </header>
+      <ul>
+        <li>About</li>
+        <li>Restraunts</li>
+      </ul>
       <Restraunts />
     </div>
   );
