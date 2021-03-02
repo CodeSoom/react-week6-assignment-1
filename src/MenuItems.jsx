6c1bfb80a3dc4de899af61d33fff1577a3ac2e37
Blueberry <54464320+lee01097@users.noreply.github.com>
2021-03-03 04:36:09 +0900

    Green) menuItems: 코드 수정 및 테스트 코드 통과

--- src/MenuItems.jsx
+++ src/MenuItems.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 
-export default function MenuItems({ menuItems = [] }) {
+export default function MenuItems({ menuItems }) {
   return (
 
     <ul>
-      {menuItems.map((item) => (
-        <li key={item.id}>
-          {item.name}
-        </li>
-      ))}
+      {(menuItems)
+        ? menuItems.map((item) => (
+          <li key={item.id}>
+            {item.name}
+          </li>
+        )) : '값이 없어요!'}
     </ul>
   );
 }
