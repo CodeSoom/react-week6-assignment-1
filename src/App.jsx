5f4add18b13b9d004a559da8bc048fa7b43b6aa9
canary4651 <canary4651@gmail.com>
2021-03-02 09:51:00 +0900

    🔴 Create `RestaurantsPage` test

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,11 @@ import React, { useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
+import RestaurantsPage from './RestaurantsPage';
 
 import {
   loadInitialData,
 } from './actions';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 export default function App() {
   const dispatch = useDispatch();
@@ -24,9 +17,7 @@ export default function App() {
 
   return (
     <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
+      <RestaurantsPage />
     </div>
   );
 }
