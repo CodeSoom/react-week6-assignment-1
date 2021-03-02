f51c3cf4cb0533861c2aef8a2e5e8b49dbdcc898
jjkim01 <jaen6563@gmail.com>
2021-03-02 21:23:20 +0900

    API로 받아온 RestaurantDetail  정보를 화면에 뿌려주기 (undefined 버그 수정 필요함)

--- src/RestaurantsDetailPage.jsx
+++ src/RestaurantsDetailPage.jsx
@@ -6,23 +6,24 @@ import { useParams } from 'react-router-dom';
 import { get } from './utils';
 
 export default function RestaurantsDetailPage() {
-
   const { id } = useParams();
   const restaurantDetail = useSelector(get('restaurantDetail'));
-  console.log(restaurantDetail);
 
   return (
     <div>
       <h2>{id}</h2>
-      {/*<h4>주소: {address}</h4>*/}
-      {/*<h3>메뉴</h3>*/}
-      {/*<ul>*/}
-      {/*  {menuItems.map((menuItem) => (*/}
-      {/*    <li key={menuItem.id}>*/}
-      {/*      {menuItem.name}*/}
-      {/*    </li>*/}
-      {/*  ))}*/}
-      {/*</ul>*/}
+      <h4>
+        주소:
+        {restaurantDetail.address}
+      </h4>
+      <h3>메뉴</h3>
+      <ul>
+        {restaurantDetail.menuItems.map((menuItem) => (
+          <li key={menuItem.id}>
+            {menuItem.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
