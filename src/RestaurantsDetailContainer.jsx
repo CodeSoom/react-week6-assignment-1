757dbd38ffaa72ae9268ccd519aebe2c585a93f5
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-02 17:28:18 +0900

    RestaurantsDetailContainer test 통과

--- src/RestaurantsDetailContainer.jsx
+++ src/RestaurantsDetailContainer.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantsDetail from './RestaurantsDetail';
 
 import { get } from './utils';
 
 export default function RestaurantsDetailContainer() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch();
+  }, []);
+
   const restaurantsDetail = useSelector(get('restaurantsDetail'));
 
   return <RestaurantsDetail restaurantsDetail={restaurantsDetail} />;
