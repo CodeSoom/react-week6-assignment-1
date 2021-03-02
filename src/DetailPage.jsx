e481ad784aa7756e7af079aa5b7f699d86ee6d49
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-03-02 20:49:09 +0900

    DetailPage 형태 구현

---
+++ src/DetailPage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function DetailPage() {
+  return (
+    <div>
+      <h2>양천주가</h2>
+      <p>주소: 서울 강남구</p>
+      <h3>메뉴</h3>
+      <ul>
+        <li>탕수육</li>
+        <li>팔보채</li>
+      </ul>
+    </div>
+  )
+};
