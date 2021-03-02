028f10efb17d2f4e5c418e072fe46a3a0f54fc73
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-02 20:51:25 +0900

    리뷰를 위해 RestaurantsDetailContainer에서 id를 useParams를 이용하는 과정 commit

--- src/RestaurantsDetailContainer.jsx
+++ src/RestaurantsDetailContainer.jsx
@@ -2,15 +2,18 @@ import React, { useEffect } from 'react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
+import { useParams } from 'react-router-dom';
+import { loadRestaurantsDetail } from './actions';
 import RestaurantsDetail from './RestaurantsDetail';
-
 import { get } from './utils';
 
 export default function RestaurantsDetailContainer() {
   const dispatch = useDispatch();
+  const { id } = useParams();
 
   useEffect(() => {
-    dispatch();
+    dispatch(loadRestaurantsDetail({ restaurantsId: 1 }));
+    // dispatch();
   }, []);
 
   const restaurantsDetail = useSelector(get('restaurantsDetail'));
