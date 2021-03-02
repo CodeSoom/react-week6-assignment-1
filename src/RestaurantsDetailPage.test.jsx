b3b7c994813f9e960c350b621f4a0cb27d0cd351
jjkim01 <jaen6563@gmail.com>
2021-03-03 00:08:10 +0900

    test: Restaurant Detail 페이지에 지역, 메뉴가 잘 표시되는지 테스트

--- src/RestaurantsDetailPage.test.jsx
+++ src/RestaurantsDetailPage.test.jsx
@@ -2,12 +2,33 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import {
+  MemoryRouter,
+} from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+
 import RestaurantsDetailPage from './RestaurantsDetailPage';
 
-test('RestaurantsDetailPage', () => {
-  // Todo: useLocation() 테스트 할 수 있는 방법 찾기
+import restaurants from '../fixtures/restaurantDetail';
 
+describe('RestaurantsDetailPage', () => {
+  context('with restaurant detail', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        restaurantDetail: restaurants,
+      }));
+    });
 
-  
-  // render(<RestaurantsDetailPage />);
+    it('renders detail with restaurant detail', () => {
+      const { container } = render((
+        <MemoryRouter>
+          <RestaurantsDetailPage />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('서울 강남구');
+      expect(container).toHaveTextContent('짬뽕');
+    });
+  });
 });
