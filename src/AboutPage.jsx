9d58331271e05f1c5a943e35aaf7e0c5e6d25fad
younghoon <secret9179@gmail.com>
2021-03-03 00:23:23 +0900

    강의 수강 후 커밋

---
+++ src/AboutPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function AboutPage() {
+    return (
+      <div>
+        <h1>React에 대하여..</h1>
+        <p>Restaurants 상세 페이지 구현</p>
+        <p>이번주 과제는 꼭 잘 끝내보자 화이팅!</p>
+      </div>
+    );
+  }
