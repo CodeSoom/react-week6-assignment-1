10736ba4ab60787a1e646a235333fcae1275583d
Yoon <yoonhe@gmail.com>
2021-03-02 20:55:55 +0900

    [refactoring] RestaurantsDetailPage를 리팩토링합니다

--- src/RestaurantsDetailPage.jsx
+++ src/RestaurantsDetailPage.jsx
@@ -1,20 +1,36 @@
 import React, { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
 
 import { loadRestaurantDetail } from './actions';
+import { get } from './utils';
 
 function RestaurantsDetailePage() {
   const dispatch = useDispatch();
+
+  const restaurantDetail = useSelector(get('restaurantDetail'));
 
   const { id } = useParams();
 
   useEffect(() => {
     dispatch(loadRestaurantDetail(id));
-  }, []);
+  }, [id]);
 
   return (
-    <div />
+    <>
+      {restaurantDetail ? (
+        <div>
+          <h2>{restaurantDetail.name}</h2>
+          <p>{restaurantDetail.address}</p>
+          <dl>
+            <dt>메뉴</dt>
+            {restaurantDetail.menuItems.map((menu) => (
+              <dd key={menu.id}>{menu.name}</dd>
+            ))}
+          </dl>
+        </div>
+      ) : <p>로딩중...</p>}
+    </>
   );
 }
 
