8c40d055b13d562df643eac9a1679d0d4c8093b8
shinhyogeun <tls1gy2rms3@naver.com>
2021-03-03 00:02:13 +0900

    InvalidPage test 통과

---
+++ src/InvalidPage.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function InvalidPage() {
+  return (<h2>404Page..!!</h2>);
+}
--- src/InvalidPage.test.jsx
+++ src/InvalidPage.test.jsx
@@ -6,8 +6,8 @@ import InvalidPage from './InvalidPage';
 
 describe('InvalidPage', () => {
   it('잘못된 경로임을 알려주는 내용을 보여준다.', () => {
-    const { qeuryByText } = render(<InvalidPage />);
+    const { queryByText } = render(<InvalidPage />);
 
-    expect(qeuryByText(/404/)).toBeInTheDocument();
+    expect(queryByText(/404/)).toBeInTheDocument();
   });
 });
