0365d892b21c74a5dfb3ad9276baad688b03d8ea
Yoon <yoonhe@gmail.com>
2021-03-03 11:10:44 +0900

    [refactoring] RestaurantsDetailPage를 리팩토링합니다

--- src/RestaurantsDetailPage.jsx
+++ src/RestaurantsDetailPage.jsx
@@ -1,18 +1,10 @@
 import React from 'react';
+import RestaurantsDetailContainer from './RestaurantsDetailContainer';
 
 function RestaurantsDetailePage() {
   return (
     <>
-      <div>
-        <h2>양천주가</h2>
-        <p>서울 강남구 123456</p>
-        <dl>
-          <dt>메뉴</dt>
-          <dd>
-            비빔밥
-          </dd>
-        </dl>
-      </div>
+      <RestaurantsDetailContainer />
     </>
   );
 }
--- src/RestaurantsDetailPage.test.jsx
+++ src/RestaurantsDetailPage.test.jsx
@@ -4,9 +4,22 @@ import { render } from '@testing-library/react';
 
 import { MemoryRouter } from 'react-router-dom';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import RestaurantsDetailPage from './RestaurantsDetailPage';
 
+import RESTAURANTDETAIL from '../fixtures/restaurantDetail';
+
 describe('RestaurantsDetailPage', () => {
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      restaurantDetail: RESTAURANTDETAIL,
+    }));
+  });
+
   it('renders restaurants detail', () => {
     const { queryByText } = render((
       <MemoryRouter>
