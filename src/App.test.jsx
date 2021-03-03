4acfce3c82affcd91ff00636d635ac1016f5416f
최창현 <chchoi_einz@einz.co.kr>
2021-03-03 15:05:10 +0900

    REFACTOR: describe 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -31,42 +31,44 @@ describe('App', () => {
     }));
   });
 
-  context('경로가 /', () => {
-    it('HomePage를 그려준다.', () => {
-      const { container } = renderApp({ path: '/' });
+  describe('경로가', () => {
+    context('/', () => {
+      it('HomePage를 그려준다.', () => {
+        const { container } = renderApp({ path: '/' });
 
-      expect(container).toHaveTextContent('헤더');
+        expect(container).toHaveTextContent('헤더');
 
-      expect(container).toHaveTextContent('About');
-      expect(container).toHaveTextContent('Restaurants');
+        expect(container).toHaveTextContent('About');
+        expect(container).toHaveTextContent('Restaurants');
+      });
     });
-  });
 
-  context('경로가 /about', () => {
-    it('About을 그려준다.', () => {
-      const { container } = renderApp({ path: '/about' });
+    context('/about', () => {
+      it('About을 그려준다.', () => {
+        const { container } = renderApp({ path: '/about' });
 
-      expect(container).toHaveTextContent('헤더');
+        expect(container).toHaveTextContent('헤더');
 
-      expect(container).toHaveTextContent('About this..');
+        expect(container).toHaveTextContent('About this..');
+      });
     });
-  });
 
-  context('경로가 /restaurants', () => {
-    it('Restaurants 그려준다.', () => {
-      const { container } = renderApp({ path: '/restaurants' });
+    context('/restaurants', () => {
+      it('Restaurants 그려준다.', () => {
+        const { container } = renderApp({ path: '/restaurants' });
 
-      expect(container).toHaveTextContent('헤더');
+        expect(container).toHaveTextContent('헤더');
 
-      expect(container).toHaveTextContent('서울');
+        expect(container).toHaveTextContent('서울');
+      });
     });
-  });
 
-  context('경로가 존재하지 않는 페이지인 경우', () => {
-    it('NotFoundPage 그려준다.', () => {
-      const { container } = renderApp({ path: '/wrong-path' });
+    context('존재하지 않는 페이지인 경우', () => {
+      it('NotFoundPage 그려준다.', () => {
+        const { container } = renderApp({ path: '/wrong-path' });
 
-      expect(container).toHaveTextContent('해당 페이지가 존재하지 않습니다.');
+        expect(container).toHaveTextContent('해당 페이지가 존재하지 않습니다.');
+      });
     });
   });
 });
