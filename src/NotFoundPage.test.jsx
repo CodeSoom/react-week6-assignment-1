56b90123abca2d390990e533afe59e0e70c51174
Yoon <yoonhe@gmail.com>
2021-03-03 19:08:46 +0900

    [refactoring] NotFoundPage를 리팩토링합니다 NotFoundPage 테스트코드를 리팩토링합니다

--- src/NotFoundPage.test.jsx
+++ src/NotFoundPage.test.jsx
@@ -5,7 +5,9 @@ import { render } from '@testing-library/react';
 import NotFoundPage from './NotFoundPage';
 
 test('NotFoundPage', () => {
-  render((
+  const { queryByText } = render((
     <NotFoundPage />
   ));
+
+  expect(queryByText('404 Not Found')).not.toBeNull();
 });
