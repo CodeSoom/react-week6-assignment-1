82f1746d2eb50d2d80a431289ac418a97ab4e32c
canary4651 <canary4651@gmail.com>
2021-03-03 22:35:13 +0900

    🟢 Link to other pages on the home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
   return (
     <div>
       <h1>Home</h1>
+      <ul>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/restaurants">Restaurants</Link></li>
+      </ul>
     </div>
   );
 }
