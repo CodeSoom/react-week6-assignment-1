716e2d4013a6695920fc3c3948b5c09ee325e27c
dodoongtak <themighty.tak@gmail.com>
2021-03-03 18:20:01 +0900

    🐛fix: proper about path

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -72,7 +72,7 @@ describe('App', () => {
 
   context('with path /about', () => {
     it('renders the about page', () => {
-      const { container } = renderApp({ path: '/' });
+      const { container } = renderApp({ path: '/about' });
 
       expect(container).toHaveTextContent('헤더');
       expect(container).toHaveTextContent('About');
