d7296f253bff2c29cc0ac8e1f19971f1536d905c
Yoon <yoonhe@gmail.com>
2021-03-03 09:45:13 +0900

    [green] RestaurantsDetailPage에 대한 테스트코드를 추가합니다.

--- src/RestaurantsDetailPage.jsx
+++ src/RestaurantsDetailPage.jsx
@@ -1,35 +1,18 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-
-import { loadRestaurantDetail } from './actions';
-import { get } from './utils';
+import React from 'react';
 
 function RestaurantsDetailePage() {
-  const dispatch = useDispatch();
-
-  const restaurantDetail = useSelector(get('restaurantDetail'));
-
-  const { id } = useParams();
-
-  useEffect(() => {
-    dispatch(loadRestaurantDetail(id));
-  }, [id]);
-
   return (
     <>
-      {restaurantDetail ? (
-        <div>
-          <h2>{restaurantDetail.name}</h2>
-          <p>{restaurantDetail.address}</p>
-          <dl>
-            <dt>메뉴</dt>
-            {restaurantDetail.menuItems.map((menu) => (
-              <dd key={menu.id}>{menu.name}</dd>
-            ))}
-          </dl>
-        </div>
-      ) : <p>로딩중...</p>}
+      <div>
+        <h2>양천주가</h2>
+        <p>서울 강남구 123456</p>
+        <dl>
+          <dt>메뉴</dt>
+          <dd>
+            비빔밥
+          </dd>
+        </dl>
+      </div>
     </>
   );
 }
