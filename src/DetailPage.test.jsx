8f6b21a28987f36a0b5707999f2106a390303402
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-03-03 17:50:27 +0900

    DetailPage 테스트 수정

--- src/DetailPage.test.jsx
+++ src/DetailPage.test.jsx
@@ -1,15 +1,18 @@
 import React from 'react';
 
-import { MemoryRouter } from 'react-router-dom';
-
 import { render } from '@testing-library/react';
 
 import DetailPage from './DetailPage';
 
+import detail from '../fixtures/detail'
+
 test('DetailPage', () => {
-  render((
-    <MemoryRouter>
-      <DetailPage />
-    </MemoryRouter>
+  const { container } = render((
+    <DetailPage detail={detail}/>
   ));
+
+  expect(container).toHaveTextContent('양천주가');
+  expect(container).toHaveTextContent('주소: 서울 강남구');
+  expect(container).toHaveTextContent('비빔밥');
+  expect(container).toHaveTextContent('육개장');
 });
