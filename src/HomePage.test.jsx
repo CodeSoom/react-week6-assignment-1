0ae2d7490351a49ab7b79d046aa9a219a5c64dbb
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-03 16:39:14 +0900

    HomePage를 테스트하여 나머지 경우를 link로 path연결하는것을 확인합니다.

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -8,7 +8,11 @@ import HomePage from './HomePage';
 
 describe('HomePage', () => {
   it('내용을 표시합니다.', () => {
-    const { queryByText } = render(<HomePage />);
+    const { queryByText } = render((
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    ));
 
     expect(queryByText(/Home/)).not.toBeNull();
     expect(queryByText(/About/)).not.toBeNull();
