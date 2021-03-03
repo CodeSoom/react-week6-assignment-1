40ab6a33991be92f575dc28db3258de9b24341d9
Yoon <yoonhe@gmail.com>
2021-03-03 09:43:43 +0900

    [red] RestaurantsDetailPage에 대한 테스트코드를 추가합니다.
    테스트코드 없이 작성된 코드들이 많은것 같아 다시 마크업부터 테스트코드를
    추가해보았습니다

--- src/RestaurantsDetailPage.test.jsx
+++ src/RestaurantsDetailPage.test.jsx
@@ -4,17 +4,19 @@ import { render } from '@testing-library/react';
 
 import { MemoryRouter } from 'react-router-dom';
 
-import { useDispatch } from 'react-redux';
 import RestaurantsDetailPage from './RestaurantsDetailPage';
 
-test('RestaurantsDetailPage', () => {
-  const dispatch = jest.fn();
+describe('RestaurantsDetailPage', () => {
+  it('renders restaurants detail', () => {
+    const { queryByText } = render((
+      <MemoryRouter>
+        <RestaurantsDetailPage />
+      </MemoryRouter>
+    ));
 
-  useDispatch.mockImplementation(() => dispatch);
-
-  render((
-    <MemoryRouter>
-      <RestaurantsDetailPage />
-    </MemoryRouter>
-  ));
+    expect(queryByText('양천주가')).not.toBeNull();
+    expect(queryByText('서울 강남구 123456')).not.toBeNull();
+    expect(queryByText('메뉴')).not.toBeNull();
+    expect(queryByText('비빔밥')).not.toBeNull();
+  });
 });
