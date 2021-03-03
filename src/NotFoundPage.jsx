71d1cc70d744ec7fe51fc5046d89c9736a92bb93
최창현 <chchoi_einz@einz.co.kr>
2021-03-03 14:56:19 +0900

    Green: 테스트 통과. Not Found Page

---
+++ src/NotFoundPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function NotFoundPage() {
+  return (
+    <div>
+      <strong>
+        Not Found Page
+      </strong>
+      <p>
+        해당 페이지가 존재하지 않습니다.
+      </p>
+    </div>
+  );
+}
