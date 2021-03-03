3bca120f462aae921f9d278b9163f1550ac3ddd4
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-03 16:36:13 +0900

    HomePage를 리팩토링하여 link로 path를 연결하는것으로 변경합니다.

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -2,18 +2,26 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
   it('내용을 표시합니다.', () => {
     const { queryByText } = render(<HomePage />);
+
     expect(queryByText(/Home/)).not.toBeNull();
     expect(queryByText(/About/)).not.toBeNull();
     expect(queryByText(/Restaurants/)).not.toBeNull();
   });
 
   it('알맞은 path에 연결합니다.', () => {
-    const { queryByText } = render(<HomePage />);
+    const { queryByText } = render((
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    ));
+
     expect(queryByText(/About/).getAttribute('href')).toBe('/about');
     expect(queryByText(/Restaurants/).getAttribute('href')).toBe('/restaurants');
   });
