6036dd42f1d947e906bd559a83eb2777efb617de
dodoongtak <themighty.tak@gmail.com>
2021-03-03 20:40:56 +0900

    ✅ test: add scenario for initializing state

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -4,17 +4,33 @@ import { render } from '@testing-library/react';
 
 import { MemoryRouter } from 'react-router-dom';
 
+import { useDispatch } from 'react-redux';
+
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
+  const dispatch = jest.fn();
+
+  const renderHomPage = () => render((
+    <MemoryRouter initialEntries={['/']}>
+      <HomePage />
+    </MemoryRouter>
+  ));
+
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('renders Home Page', () => {
-    const { container } = render((
-      <MemoryRouter initialEntries={['/']}>
-        <HomePage />
-      </MemoryRouter>
-    ));
+    const { container } = renderHomPage();
 
     expect(container).toHaveTextContent('About');
     expect(container).toHaveTextContent('Restaurants');
+  });
+
+  it('resets current state to initial state', () => {
+    renderHomPage();
+
+    expect(dispatch).toBeCalled();
   });
 });
