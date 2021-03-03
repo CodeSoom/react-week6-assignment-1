b6062b0cb6c27f5d842e9f59d922a3e71f43a56d
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-03-03 17:50:52 +0900

    DetailPage 구현

--- src/DetailPage.jsx
+++ src/DetailPage.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 
-export default function DetailPage() {
+export default function DetailPage({ detail }) {
+  const { name, address, menuItems } = detail;
+
   return (
     <div>
-      <h2>양천주가</h2>
-      <p>주소: 서울 강남구</p>
+      <h2>{name}</h2>
+      <p>주소: {address}</p>
       <h3>메뉴</h3>
       <ul>
-        <li>탕수육</li>
-        <li>팔보채</li>
+        {menuItems.map(({ id, name }) => (
+          <li key={id}>{name}</li>
+        ))}
       </ul>
     </div>
   )
