1ee70c03a890a6e689892f248d447c584fe82916
dodoongtak <themighty.tak@gmail.com>
2021-03-03 20:39:41 +0900

    ✅ test: remove useless scenario

--- src/Header.test.jsx
+++ src/Header.test.jsx
@@ -1,37 +1,21 @@
 import React from 'react';
 
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 import { MemoryRouter } from 'react-router-dom';
 
 import Header from './Header';
 
 describe('Header', () => {
-  const handleClick = jest.fn();
-
   const renderHeader = () => render((
     <MemoryRouter>
-      <Header handleClick={handleClick} />
+      <Header />
     </MemoryRouter>
   ));
-
-  beforeEach(() => {
-    jest.clearAllMocks();
-  });
 
   it('renders Header', () => {
     const { queryByText } = renderHeader();
 
     expect(queryByText('헤더')).not.toBeNull();
-  });
-
-  it('calls handleClick', () => {
-    const { queryByText } = renderHeader();
-
-    expect(queryByText('헤더')).not.toBeNull();
-
-    fireEvent.click(queryByText('헤더'));
-
-    expect(handleClick).toBeCalled();
   });
 });
