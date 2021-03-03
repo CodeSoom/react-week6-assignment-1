1d3617020f39b5d4120fc4dcd4f2a31d84a18299
Yoon <yoonhe@gmail.com>
2021-03-03 19:09:33 +0900

    [refactoring] RestaurantDetailPage를 리팩토링합니다

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -4,7 +4,6 @@ import RestaurantDetailContainer from './RestaurantDetailContainer';
 function RestaurantDetailePage() {
   return (
     <>
-      <h2>레스토랑 리스트</h2>
       <RestaurantDetailContainer />
     </>
   );
--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -4,15 +4,20 @@ import { render } from '@testing-library/react';
 
 import { MemoryRouter } from 'react-router-dom';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetailPage from './RestaurantDetailPage';
+
+import RESTAURANTDETAIL from '../fixtures/restaurantDetail';
 
 describe('RestaurantDetailPage', () => {
   const dispatch = jest.fn();
 
   beforeEach(() => {
     useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      restaurantDetail: RESTAURANTDETAIL,
+    }));
   });
 
   it('renders restaurants detail', () => {
@@ -22,6 +27,9 @@ describe('RestaurantDetailPage', () => {
       </MemoryRouter>
     ));
 
-    expect(queryByText('레스토랑 리스트')).not.toBeNull();
+    expect(queryByText('양천주가')).not.toBeNull();
+    expect(queryByText('서울 강남구 123456')).not.toBeNull();
+    expect(queryByText('메뉴')).not.toBeNull();
+    expect(queryByText('비빔밥')).not.toBeNull();
   });
 });
