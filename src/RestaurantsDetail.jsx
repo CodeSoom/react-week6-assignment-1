383651fb97341185673185048dba98fa11a03065
Yoon <yoonhe@gmail.com>
2021-03-03 16:33:21 +0900

    [green]] RestaurantsDetail을 구현합니다

---
+++ src/RestaurantsDetail.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+export default function RestaurantsDetail({ restaurant }) {
+  return (
+    <>
+      {restaurant ? (
+        <div>
+          <h2>{restaurant.name}</h2>
+          <p>{restaurant.address}</p>
+          <dl>
+            <dt>메뉴</dt>
+            {
+              restaurant.menuItems.map((menu) => (
+                <dd key={menu.id}>
+                  {menu.name}
+                </dd>
+              ))
+            }
+          </dl>
+        </div>
+      ) : <div>로딩중..</div>}
+    </>
+  );
+}
