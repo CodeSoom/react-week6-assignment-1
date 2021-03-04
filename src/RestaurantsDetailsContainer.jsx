6f5f52a594ab4618f85ecd4915197dca1a6bcdd6
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-04 19:49:30 +0900

    RestaurantsDetailContainer를 구현하여 식당 상세정보 불러오는 과정을 가짜함수를 이용해 테스트를 통과합니다.

--- src/RestaurantsDetailsContainer.jsx
+++ src/RestaurantsDetailsContainer.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+
+import { useDispatch } from 'react-redux';
 
 import restaurantsDetails from '../fixtures/restaurantsDetails';
 
 import RestaurantsDetails from './RestaurantsDetails';
 
 export default function RestaurantsDetailsContainer() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch();
+  }, []);
+
   return (
     <RestaurantsDetails restaurantsDetails={restaurantsDetails} />
   );
