0ad13c0af3455d064b84bbcf3390761a59fef5b7
2Zero1 <Good7219@hotmail.com>
2021-03-04 11:00:32 +0900

    린트 에러 처리

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { loadRestaurant } from './actions';
 import { useParams } from 'react-router-dom';
+import { loadRestaurant } from './actions';
 import RestaurantContainer from './RestaurantContainer';
 import RestaurantsMenuContainer from './RestaurantsMenuContainer';
 
