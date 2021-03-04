5bedee12896ee8652f95e9dd2b3ec54b6f8867b1
gimsesu <tkfrn4799@gmail.com>
2021-03-04 19:11:39 +0900

    Memory Router를 이용해 HomePage 테스트 작성하기

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
 test('App', () => {
-  render((
-    <App />
-  ));
+  context('requests HomePage', () => {
+    const { container } = render((
+      <MemoryRouter initialEntries={['/']}>
+        <App />
+      </MemoryRouter>
+    ));
+
+    expect(container).toHaveTextContent('Home');
+  });
 });
