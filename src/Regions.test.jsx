98c3293043efccbdc4669f686f6616f64c0b49f9
2Zero1 <Good7219@hotmail.com>
2021-03-04 16:40:44 +0900

    컴포넌트 Regions에 클릭 테스트 코드 작성

--- src/Regions.test.jsx
+++ src/Regions.test.jsx
@@ -1,17 +1,23 @@
 import React from 'react';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import Regions from './Regions';
 
 import regions from '../fixtures/regions';
 
 test('Regions', () => {
+
+  const handleClick = jest.fn();
+
   const { getByText } = render((
     <Regions
       regions={regions}
+      onClick={handleClick}
     />
   ));
   expect(getByText('서울')).not.toBeNull();
   expect(getByText('대전')).not.toBeNull();
+  fireEvent.click(getByText('서울'));
+  expect(handleClick).toBeCalled();
 });
