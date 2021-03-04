30845485ab8c1b884dce235069cf75b7aa826977
최창현 <chchoi_einz@einz.co.kr>
2021-03-04 10:40:34 +0900

    REFACTOR: useEffect 발생 조건 추가 [], 주석제거

--- src/Restaurants.jsx
+++ src/Restaurants.jsx
@@ -10,17 +10,12 @@ import {
   loadInitialData,
 } from './actions';
 
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
-
 export default function Restaurants() {
   const dispatch = useDispatch();
 
   useEffect(() => {
     dispatch(loadInitialData());
-  });
+  }, []);
 
   return (
     <div>
