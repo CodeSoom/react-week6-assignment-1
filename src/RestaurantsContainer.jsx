a160784c3f2a0dcbe0b458f3a66d69661f3412be
dodoongtak <themighty.tak@gmail.com>
2021-03-04 10:29:20 +0900

    ✨feat: renders message to let user click another button

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -12,10 +12,24 @@ export default function RestaurantsContainer() {
   const dispatch = useDispatch();
 
   const restaurants = useSelector(get('restaurants'));
+  const selectedRegion = useSelector(get('selectedRegion'));
+  const selectedCategory = useSelector(get('selectedCategory'));
 
   if (!restaurants.length) {
     return (
       <p>해당 지역에 선택하신 분야 식당이 없습니다.</p>
+    );
+  }
+
+  if (selectedRegion && !selectedCategory) {
+    return (
+      <p>카테고리를 클릭해주세요.</p>
+    );
+  }
+
+  if (selectedCategory && !selectedRegion) {
+    return (
+      <p>지역를 클릭해주세요.</p>
     );
   }
 
