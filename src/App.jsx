a78354df0ac59b1a065018f1dceb246b7b6b9790
datalater <the7mincheol@gmail.com>
2021-03-04 15:47:29 +0900

    App에서 헤더가 나오도록 구현하라

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,11 @@ import {
   loadInitialData,
 } from './actions';
 
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
+function Header() {
+  return (
+    <h1>헤더</h1>
+  );
+}
 
 export default function App() {
   const dispatch = useDispatch();
@@ -24,6 +25,7 @@ export default function App() {
 
   return (
     <div>
+      <Header />
       <RegionsContainer />
       <CategoriesContainer />
       <RestaurantsContainer />
