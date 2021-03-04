7f63803201ffcbc991f65d35c273ca9cb78666fd
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-04 21:30:56 +0900

    RestaurantsDetails를 테스트하여 식당이 없는 경우를 확인합니다.

--- src/RestaurantsDetails.test.jsx
+++ src/RestaurantsDetails.test.jsx
@@ -4,23 +4,34 @@ import { render } from '@testing-library/react';
 
 import RestaurantsDetails from './RestaurantsDetails';
 
+import restaurantsDetails from '../fixtures/restaurantsDetails';
+
 describe('RestaurantsDetails', () => {
-  const restaurantsDetails = {
-    id: 1,
-    name: '양천주가',
-    address: '서울 강남구',
-    menus: [
-      { id: 101, name: '탕수육' },
-      { id: 102, name: '짜장면' },
-    ],
-  };
+  context('식당이 없는 경우,', () => {
+    it('정보가 없음을 확인합니다.', () => {
+      const emptyDetails = {
+        id: 0,
+        name: '',
+        address: '',
+        menus: [],
+      };
 
-  it('식당의 상세 정보를 표시합니다.', () => {
-    const { queryByText } = render(<RestaurantsDetails restaurantsDetails={restaurantsDetails} />);
+      const { queryByText } = render(<RestaurantsDetails restaurantsDetails={emptyDetails} />);
 
-    expect(queryByText(/양천주가/)).not.toBeNull();
-    expect(queryByText(/서울 강남구/)).not.toBeNull();
-    expect(queryByText(/탕수육/)).not.toBeNull();
-    expect(queryByText(/짜장면/)).not.toBeNull();
+      expect(queryByText(/Now is loading/)).not.toBeNull();
+    });
+  });
+
+  context('식당이 있는 경우,', () => {
+    it('식당의 상세 정보를 표시합니다.', () => {
+      const { queryByText } = render(<RestaurantsDetails
+        restaurantsDetails={restaurantsDetails}
+      />);
+
+      expect(queryByText(/양천주가/)).not.toBeNull();
+      expect(queryByText(/서울 강남구/)).not.toBeNull();
+      expect(queryByText(/탕수육/)).not.toBeNull();
+      expect(queryByText(/짜장면/)).not.toBeNull();
+    });
   });
 });
