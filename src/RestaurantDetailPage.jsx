340d17f1dcbdf054e3ea04ff0f76cfc6094021a6
gyeongwon1275 <rlaruddnjs28@gmail.com>
2021-03-04 10:39:21 +0900

    :construction: Green-Working: RestaurantDetailPage
    - params 받아서 어떤 컴포넌트를 출력할지 결정하는 역할
    - params 를 서버에 요청하는 역할

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
+import { useParams } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+
+import NotFound from './NotFound';
 import RestaurantDetailContainer from './RestaurantDetailContainer';
 
+import { loadRestaurant } from './actions';
+
 export default function RestaurantDetailPage() {
+  const dispatch = useDispatch();
+
+  const { id } = useParams();
+
+  const isInValidParam = !Number.isInteger(parseInt(id, 10));
+
+  if (isInValidParam) {
+    return <NotFound />;
+  }
+
+  useEffect(() => {
+    dispatch(loadRestaurant(id));
+  }, [id]);
+
   return (
     <RestaurantDetailContainer />
   );
