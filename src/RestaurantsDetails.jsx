8e5bad5132388850dab1e23945bc4125f06573f0
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-04 16:47:00 +0900

    RestaurantsDetails를 구현합니다.

--- src/RestaurantsDetails.jsx
+++ src/RestaurantsDetails.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
+import restaurantsDetails from '../fixtures/restaurantsDetails';
+
 export default function RestaurantsDetails() {
+  const { name, address, menus } = restaurantsDetails;
   return (
-    <div />
+    <div>
+      <h1>{name}</h1>
+      <p>{`주소: ${address}`}</p>
+      <h2>메뉴</h2>
+      <ul>
+        {menus.map((menu) => (
+          <li key={menu.id}>{menu.name}</li>
+        ))}
+      </ul>
+    </div>
   );
 }
