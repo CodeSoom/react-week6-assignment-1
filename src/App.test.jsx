cbac32015a554dd9f7e165845e8804c331b3355e
datalater <the7mincheol@gmail.com>
2021-03-04 15:46:30 +0900

    App에서 헤더가 나오는지 테스트하라

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -6,29 +6,24 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
-test('App', () => {
-  const dispatch = jest.fn();
+describe('App', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+    useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+      categories: [],
+      restaurants: [],
+    }));
+  });
 
-  const { queryByText } = render((
-    <App />
-  ));
+  it('renders headers', () => {
+    const { container } = render((
+      <App />
+    ));
 
-  expect(dispatch).toBeCalled();
-
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+    expect(container).toHaveTextContent('헤더');
+  });
 });
