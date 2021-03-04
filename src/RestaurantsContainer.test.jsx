2597e3496c3b020e88c22c87dc388b8b5050a05d
datalater <the7mincheol@gmail.com>
2021-03-04 17:58:35 +0900

    레스토랑을 클릭하면 정보를 가져오는지 테스트하라

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 
 import { MemoryRouter } from 'react-router-dom';
 
-import { render } from '@testing-library/react';
+import { render, fireEvent } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
 describe('RestaurantsContainer', () => {
+  const dispatch = jest.fn;
+
   beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+
     useSelector.mockImplementation((selector) => selector({
       restaurants: [
         { id: 1, name: '마법사주방' },
@@ -25,5 +29,17 @@ describe('RestaurantsContainer', () => {
     ));
 
     expect(container).toHaveTextContent('마법사주방');
+  });
+
+  it('loads clicked restaurant information', () => {
+    const { getByText } = render((
+      <MemoryRouter>
+        <RestaurantsContainer />
+      </MemoryRouter>
+    ));
+
+    fireEvent.click(getByText('마법사주방'));
+
+    expect(dispatch).toBeCalled();
   });
 });
