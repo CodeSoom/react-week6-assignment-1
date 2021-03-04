8e34ca44ee73c92105838026ac4585827ebd16e3
dodoongtak <themighty.tak@gmail.com>
2021-03-04 20:30:27 +0900

    ✨feat: redirect to 404 NotFound Page

--- src/pages/RestaurantPage/RestaurantPage.jsx
+++ src/pages/RestaurantPage/RestaurantPage.jsx
@@ -6,12 +6,22 @@ import { useDispatch } from 'react-redux';
 
 import { loadRestaurant } from '../../redux/actions';
 
+import NotFoundPage from '../NotFoundPage';
+
 import RestaurantContainer from './RestaurantContainer';
+
+import { isInteger } from '../../utils/utils';
 
 export default function RestaurantPage() {
   const dispatch = useDispatch();
 
   const { id: restaurantId } = useParams();
+
+  if (!isInteger(restaurantId)) {
+    return (
+      <NotFoundPage />
+    );
+  }
 
   useEffect(() => {
     dispatch(loadRestaurant(restaurantId));
