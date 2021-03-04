61f68307f5fa28530b2b390900ddc130b1cf09ac
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-03-04 19:41:36 +0900

    DetailContainer에 params 추가

--- src/DetailContainer.jsx
+++ src/DetailContainer.jsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import DetailPage from './DetailPage';
 
+import { loadDetail } from './actions';
+
 import { get } from './utils';
 
-export default function DetailContainer() {
-  const detail = useSelector(get('detail'))
-  
+export default function DetailContainer({ match }) {
+  const dispatch = useDispatch();
+
+  const detail = useSelector(get('detail'));
+
+  const { id } = match.params;
+
+  useEffect(() => {
+    dispatch(loadDetail({ restaurantId: id }));
+  }, [id]);
+
   return <DetailPage detail={detail} />;
 }
