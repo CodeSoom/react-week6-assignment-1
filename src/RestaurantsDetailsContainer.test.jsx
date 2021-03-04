5940f88190e1a5843f11d4d3749ba84c0223ff5b
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-04 19:42:56 +0900

    RestaurantsDetailsContainer테스트에서 식당 상세정보 불러오는 함수 호출 여부를 검사합니다.

--- src/RestaurantsDetailsContainer.test.jsx
+++ src/RestaurantsDetailsContainer.test.jsx
@@ -2,16 +2,23 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import restaurantsDetails from '../fixtures/restaurantsDetails';
 
 import RestaurantsDetailsContainer from './RestaurantsDetailsContainer';
 
 describe('RestaurantsDetails', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({ restaurantsDetails }));
+
   it('식당의 상세 정보를 표시합니다.', () => {
     const { queryByText } = render(<RestaurantsDetailsContainer />);
+
+    expect(dispatch).toBeCalled();
 
     expect(queryByText(/양천주가/)).not.toBeNull();
   });
