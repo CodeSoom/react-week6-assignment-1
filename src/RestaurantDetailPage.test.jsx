2533f6c429238320dc79a180df019a5117a3a16b
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-03-05 21:04:57 +0900

    RestaurantDetailPage 테스트 작성 및 통과

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -6,13 +6,28 @@ import RestaurantDetailPage from './RestaurantDetailPage';
 
 import restaurantDetail from '../fixtures/restaurantDetail'
 
-test('RestaurantDetailPage', () => {
-  const { container } = render((
-    <RestaurantDetailPage restaurantDetail={restaurantDetail}/>
-  ));
+describe('RestaurantDetailPage', () => {
+  context('when restaurant detail exists', () => {
+    it('renders restaurant detail', () => {
+      const { container } = render((
+        <RestaurantDetailPage restaurantDetail={restaurantDetail}/>
+      ));
+    
+      expect(container).toHaveTextContent('양천주가');
+      expect(container).toHaveTextContent('주소: 서울 강남구');
+      expect(container).toHaveTextContent('비빔밥');
+      expect(container).toHaveTextContent('육개장');
+    })
+  })
 
-  expect(container).toHaveTextContent('양천주가');
-  expect(container).toHaveTextContent('주소: 서울 강남구');
-  expect(container).toHaveTextContent('비빔밥');
-  expect(container).toHaveTextContent('육개장');
+  context('when restaurant detail does\'nt exist', () => {
+    it('renders nothing', () => {
+      const { queryByText } = render((
+        <RestaurantDetailPage restaurantDetail={{}}/>
+      ));
+    
+      expect(queryByText('양천주가')).toBeNull();
+    })
+  })
+  
 });
