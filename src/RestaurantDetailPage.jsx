329065ec5f90788e3f90935446a8fcc0cca2db00
Yoon <yoonhe@gmail.com>
2021-03-05 18:01:41 +0900

    [refactoring] RestaurantDetailPage를 리팩토링합니다

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 import RestaurantDetailContainer from './RestaurantDetailContainer';
 
 function RestaurantDetailePage() {
-  return <RestaurantDetailContainer />;
+  return (
+    <RestaurantDetailContainer />
+  );
 }
 
 export default RestaurantDetailePage;
