5be1e84d9e364b53c39d50b5890e392be30ed9df
2Zero1 <Good7219@hotmail.com>
2021-03-05 16:14:07 +0900

    reducer에 setCategories와 selectCategory 를 만들기 위해 테스트 코드 작성

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -1,15 +1,26 @@
-import { selectRegion } from './actions';
+import { selectRegion, selectCategory } from './actions';
 import reducer from './reducer';
 import regions from '../fixtures/regions';
+import categories from '../fixtures/categories';
 
 describe('reducer', () => {
   const REGION = regions[0];
+  const CATEGORY = categories[0];
   context('when action type is selectRegion', () => {
     it('set selectedRegionId', () => {
       const previousState = {};
       const state = reducer(previousState, selectRegion(REGION.id));
       expect(state).toEqual({
         selectedRegionId: REGION.id,
+      });
+    });
+  });
+  context('when action type is selectCategory', () => {
+    it('set selectedCategoryId', () => {
+      const previousState = {};
+      const state = reducer(previousState, selectCategory(CATEGORY.id));
+      expect(state).toEqual({
+        selectedCategoryId: CATEGORY.id,
       });
     });
   });
