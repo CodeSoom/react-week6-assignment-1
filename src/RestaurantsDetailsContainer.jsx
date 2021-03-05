d3700653715ddc545054500b213e5d91003a6a16
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-05 20:31:05 +0900

    RestaurantsDetails를 구현하여 useParams에서 가져온 id로 식당상세정보를 load합니다.

--- src/RestaurantsDetailsContainer.jsx
+++ src/RestaurantsDetailsContainer.jsx
@@ -1,17 +1,24 @@
 import React, { useEffect } from 'react';
 
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
-import restaurantsDetails from '../fixtures/restaurantsDetails';
+import { useParams } from 'react-router-dom';
+import { get } from './utils';
+
+import { loadRestaurantsDetails } from './actions';
 
 import RestaurantsDetails from './RestaurantsDetails';
 
 export default function RestaurantsDetailsContainer() {
   const dispatch = useDispatch();
 
+  const { id } = useParams();
+
   useEffect(() => {
-    dispatch();
+    dispatch(loadRestaurantsDetails(id));
   }, []);
+
+  const restaurantsDetails = useSelector(get('restaurantsDetails'));
 
   return (
     <RestaurantsDetails restaurantsDetails={restaurantsDetails} />
