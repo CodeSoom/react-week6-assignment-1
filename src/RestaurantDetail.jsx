7f66d129e5bf76912c6e826523c3b42d9b896ac8
최창현 <chchoi_einz@einz.co.kr>
2021-03-05 13:20:30 +0900

    Green: 테스트 통과. 상세페이지 작성(하드코딩)

---
+++ src/RestaurantDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default function RestaurantDetail() {
+  return (
+    <div>
+      <div>
+        <p>
+          서울 강남구 123456
+        </p>
+      </div>
+    </div>
+  );
+}
