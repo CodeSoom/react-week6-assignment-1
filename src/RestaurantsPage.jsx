c98c604d1f7bcaab092489f0765579bfc2bcc978
2Zero1 <Good7219@hotmail.com>
2021-03-05 17:07:35 +0900

    restaurantsPage 구현

---
+++ src/RestaurantsPage.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import RegionsContainer from './RegionsContainer';
+import { loadRegions, loadCategories } from './actions';
+import { useDispatch } from 'react-redux';
+import CategoriesContainer from './CategoriesContainer';
+
+
+export default function RestaurantsPage() {
+  const dispatch = useDispatch();
+  dispatch(loadRegions());
+  dispatch(loadCategories());
+  return (
+    <div>
+      <RegionsContainer />
+      <CategoriesContainer />
+    </div>
+  );
+}
