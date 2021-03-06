3979575a960b7dc838fae42576b616cc3dcc4fc7
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-06 21:39:42 +0900

    RestaurantDetailsContainer테스트에서 경로가 존재하지 않는 경우에 대한 테스트를 진행합니다.

--- src/RestaurantsDetailsContainer.test.jsx
+++ src/RestaurantsDetailsContainer.test.jsx
@@ -4,7 +4,8 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-import { MemoryRouter } from 'react-router-dom';
+import { MemoryRouter, Route } from 'react-router-dom';
+
 import restaurantsDetails from '../fixtures/restaurantsDetails';
 
 import RestaurantsDetailsContainer from './RestaurantsDetailsContainer';
@@ -16,15 +17,35 @@ describe('RestaurantsDetails', () => {
 
   useSelector.mockImplementation((selector) => selector({ restaurantsDetails }));
 
-  it('식당의 상세 정보를 표시합니다.', () => {
-    const { queryByText } = render((
-      <MemoryRouter>
-        <RestaurantsDetailsContainer />
-      </MemoryRouter>
-    ));
+  context('경로가 존재하지 않을때,', () => {
+    it('경로가 존재하지 않다는 페이지 Not Found를 표시합니다.', () => {
+      const { queryByText } = render((
+        <MemoryRouter initialEntries={['/restaurants/any_not_exist_url']}>
+          <Route
+            path="/restaurants/:id"
+            component={RestaurantsDetailsContainer}
+          />
+        </MemoryRouter>
+      ));
 
-    expect(dispatch).toBeCalled();
+      expect(queryByText(/404 Not Found/)).not.toBeNull();
+    });
+  });
 
-    expect(queryByText(/양천주가/)).not.toBeNull();
+  context('경로가 존재할때,', () => {
+    it('식당의 상세 정보를 표시합니다.', () => {
+      const { queryByText } = render((
+        <MemoryRouter initialEntries={['/restaurants/1']}>
+          <Route
+            path="/restaurants/:id"
+            component={RestaurantsDetailsContainer}
+          />
+        </MemoryRouter>
+      ));
+
+      expect(dispatch).toBeCalled();
+
+      expect(queryByText(/양천주가/)).not.toBeNull();
+    });
   });
 });
