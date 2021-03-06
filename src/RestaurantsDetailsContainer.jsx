7336983286fec6518dc5c43cd180669670f5005f
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-06 21:41:52 +0900

    RestaurantsDetailsContainer를 구현하여 path경로가 존재하지않는 경우를 추가합니다.

--- src/RestaurantsDetailsContainer.jsx
+++ src/RestaurantsDetailsContainer.jsx
@@ -3,16 +3,23 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { useParams } from 'react-router-dom';
+
 import { get } from './utils';
 
 import { loadRestaurantsDetails } from './actions';
 
 import RestaurantsDetails from './RestaurantsDetails';
 
+import NotFound from './NotFound';
+
 export default function RestaurantsDetailsContainer() {
   const dispatch = useDispatch();
 
   const { id } = useParams();
+
+  if (!Number(id)) {
+    return (<NotFound />);
+  }
 
   useEffect(() => {
     dispatch(loadRestaurantsDetails(id));
