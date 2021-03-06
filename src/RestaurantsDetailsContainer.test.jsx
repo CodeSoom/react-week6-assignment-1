56e0f08290581946bb9c392ead8cffea5dfb5a0e
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-06 14:15:07 +0900

    RestaurantsDetailsContainer를 테스트하여 잘못된 주소페이지를 표시하는것을 확인합니다.

--- src/RestaurantsDetailsContainer.test.jsx
+++ src/RestaurantsDetailsContainer.test.jsx
@@ -17,14 +17,16 @@ describe('RestaurantsDetails', () => {
   useSelector.mockImplementation((selector) => selector({ restaurantsDetails }));
 
   it('식당의 상세 정보를 표시합니다.', () => {
-    const { queryByText } = render((
-      <MemoryRouter>
-        <RestaurantsDetailsContainer />
-      </MemoryRouter>
-    ));
+    const { queryByText } = render(<RestaurantsDetailsContainer />);
 
     expect(dispatch).toBeCalled();
 
     expect(queryByText(/양천주가/)).not.toBeNull();
+  });
+
+  it('잘못된 주소를 나타내는 페이지를 표시합니다.', () => {
+    const { queryByText } = render(<RestaurantsDetailsContainer />);
+
+    expect(queryByText(/404 Not Found/)).not.toBeNull();
   });
 });
