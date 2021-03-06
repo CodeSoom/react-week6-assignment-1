e97cbbd0ffe96ccf2ebcee37caea68651a571c3f
Najeong-Kim <najeong@Najeong-Kimui-MacBookPro.local>
2021-03-06 20:52:46 +0900

    RestaurantDetailPage lint 수정

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -7,15 +7,17 @@ export default function RestaurantDetailPage({ restaurantDetail }) {
     <div>
       <h2>{name}</h2>
       <p>
-        주소: {address}
+        {`주소: ${address}`}
       </p>
       <h3>메뉴</h3>
       { menuItems
-        ? <ul>
-          {menuItems.map(({ id, name: menuName }) => (
-            <li key={id}>{menuName}</li>
-          ))}
-        </ul> : null}
+        ? (
+          <ul>
+            {menuItems.map(({ id, name: menuName }) => (
+              <li key={id}>{menuName}</li>
+            ))}
+          </ul>
+        ) : null}
     </div>
   );
 }
