95deeb9500d9860a18ebd4c9e051f8c53103512d
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-07 20:47:10 +0900

    HomePage테스트를 리팩토링하여 중복되는 render함수를 하나의 함수를 호출하는것으로 변경합니다.

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -6,13 +6,17 @@ import { MemoryRouter } from 'react-router-dom';
 
 import HomePage from './HomePage';
 
+function renderHoemPage() {
+  return render((
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  ));
+}
+
 describe('HomePage', () => {
   it('내용을 표시합니다.', () => {
-    const { queryByText } = render((
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>
-    ));
+    const { queryByText } = renderHoemPage();
 
     expect(queryByText(/Home/)).not.toBeNull();
     expect(queryByText(/About/)).not.toBeNull();
@@ -20,11 +24,7 @@ describe('HomePage', () => {
   });
 
   it('알맞은 path에 연결합니다.', () => {
-    const { queryByText } = render((
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>
-    ));
+    const { queryByText } = renderHoemPage();
 
     expect(queryByText(/About/).getAttribute('href')).toBe('/about');
     expect(queryByText(/Restaurants/).getAttribute('href')).toBe('/restaurants');
