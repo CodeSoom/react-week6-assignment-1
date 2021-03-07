8582b922220652c3cc4000e36da7ee6eb0f9233d
EL <ickyicky@gmail.com>
2021-03-07 19:31:47 +0900

    Red: 테스트 추가.  dispatch 추가

--- src/RestaurantDetail.test.jsx
+++ src/RestaurantDetail.test.jsx
@@ -2,9 +2,17 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch } from 'react-redux';
+
 import RestaurantDetail from './RestaurantDetail';
 
 describe('RestaurantDetail', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+  });
+
   it('레스토랑 상세 정보를 보여준다.', () => {
     const params = { id: '1' };
 
