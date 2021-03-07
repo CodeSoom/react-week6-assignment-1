c42e2a2abc276b9acc3d910f165b99208158e103
HongJungKim-dev <hongjung.kim.dev@gmail.com>
2021-03-07 21:02:50 +0900

    RestaurantsDetails테스트를 수정하여 중복되는 render를 하나의 함수로 호출합니다.

--- src/RestaurantsDetails.test.jsx
+++ src/RestaurantsDetails.test.jsx
@@ -6,10 +6,16 @@ import RestaurantsDetails from './RestaurantsDetails';
 
 import restaurantsDetails from '../fixtures/restaurantsDetails';
 
+function renderRestaurantsDetails(details) {
+  return (
+    render(<RestaurantsDetails restaurantsDetails={details} />)
+  );
+}
+
 describe('RestaurantsDetails', () => {
   context('식당이 없는 경우,', () => {
     it('정보가 없음을 확인합니다.', () => {
-      const { queryByText } = render(<RestaurantsDetails restaurantsDetails={undefined} />);
+      const { queryByText } = renderRestaurantsDetails(undefined);
 
       expect(queryByText(/Now is loading/)).not.toBeNull();
     });
@@ -17,9 +23,7 @@ describe('RestaurantsDetails', () => {
 
   context('식당이 있는 경우,', () => {
     it('식당의 상세 정보를 표시합니다.', () => {
-      const { queryByText } = render(<RestaurantsDetails
-        restaurantsDetails={restaurantsDetails}
-      />);
+      const { queryByText } = renderRestaurantsDetails(restaurantsDetails);
 
       expect(queryByText(/양천주가/)).not.toBeNull();
       expect(queryByText(/서울 강남구/)).not.toBeNull();
