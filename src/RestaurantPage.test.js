43d6cef3a3a055023e945e7901a3062b6205312f
2Zero1 <Good7219@hotmail.com>
2021-03-08 17:18:07 +0900

    RestaurantPage 테스트 코드 작성

--- src/RestaurantPage.test.js
+++ src/RestaurantPage.test.js
@@ -4,32 +4,25 @@ import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
-import regions from '../fixtures/regions';
-import categories from '../fixtures/categories';
-import restaurants from '../fixtures/restaurants';
+import RESTAURANT from '../fixtures/restaurant';
 
-import RestaurantsPage from './RestaurantsPage';
+import RestaurantPage from './RestaurantPage';
+
 
 jest.mock('react-redux');
 jest.mock('./services/api');
 
-test('RestaurantsPage', () => {
-  const REGION = regions[0];
-  const CATEGORY = categories[0];
-  const RESTAURANT = restaurants[0];
+test('RestaurantPage', () => {
   const dispatch = jest.fn();
   useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({
-    regions,
-    categories,
-    restaurants,
-    selectedRegionId: REGION.id,
+    restaurant: RESTAURANT,
   }));
 
   const { container } = render((
-    <RestaurantsPage />
+    <RestaurantPage />
   ));
-  expect(container).toHaveTextContent(REGION.name);
-  expect(container).toHaveTextContent(CATEGORY.name);
   expect(container).toHaveTextContent(RESTAURANT.name);
+  expect(container).toHaveTextContent(RESTAURANT.address);
+  expect(container).toHaveTextContent(RESTAURANT.menuItems[0].name);
 });
