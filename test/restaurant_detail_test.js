d433721ee7d15fed837e4d4a3b10a0f0e2ae3568
hannut91 <hannut91@gmail.com>
2021-05-16 19:05:28 +0900

    Upgrade dependencies
    Upgrade React to 17.
    Upgrade Webpack to v5.

--- test/restaurant_detail_test.js
+++ test/restaurant_detail_test.js
@@ -2,7 +2,7 @@ Feature('RestaurantDetail');
 
 const menus = ['탕수육', '팔보채'];
 
-Scenario('레스토랑 정보와 메뉴 목록을 볼 수 있다.', (I) => {
+Scenario('레스토랑 정보와 메뉴 목록을 볼 수 있다.', ({ I }) => {
   I.amOnPage('/restaurants/1');
 
   I.see('양천주가');
