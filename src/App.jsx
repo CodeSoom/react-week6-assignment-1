1d6fbe81f0076196776e7a7695ac69dd11041d8a
seong-dodo <dyrnxm52@hanmail.net>
2021-06-07 21:14:16 +0900

    App컴포넌트 header 추가하기
    - 홈페이지로 연결되는 헤더 추가하기

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import {
   Switch,
   Route,
+  Link,
 } from 'react-router-dom';
 
 import HomePage from './HomePage';
@@ -12,11 +13,18 @@ import NotFoundPage from './NotFoundPage';
 
 export default function App() {
   return (
-    <Switch>
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
-      <Route path="/restaurants" component={RestaurantsPage} />
-      <Route component={NotFoundPage} />
-    </Switch>
+    <div>
+      <header>
+        <h1>
+          <Link to="/">헤더</Link>
+        </h1>
+      </header>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   );
 }
