9b7e14ad29212a5b35c5c5f236b1e2b37ff364da
dngwoodo <dngwoodo@gmail.com>
2021-06-07 21:57:54 +0900

    feat: Add HomePage component

---
+++ src/pages/Homepage/index.jsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <ul>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/restaurants">Restaurants</a>
+      </li>
+    </ul>
+  );
+}
