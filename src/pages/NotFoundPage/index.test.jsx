871535d86bc6c8de301b2a4b7d4c73afdd446930
dngwoodo <dngwoodo@gmail.com>
2021-06-07 22:49:09 +0900

    test: Write NotFoundPage test

---
+++ src/pages/NotFoundPage/index.test.jsx
@@ -0,0 +1,10 @@
+import { render } from '@testing-library/react';
+import NotFoundPage from '.';
+
+describe('NotFoundPage', () => {
+  it('renders "404 Not Found"', () => {
+    const { getByRole } = render(<NotFoundPage />);
+
+    expect(getByRole('heading', { name: '404 Not Found' })).toBeInTheDocument();
+  });
+});
