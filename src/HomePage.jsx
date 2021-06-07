46743de764f994affcde7e59843f1117c210194a
seong-dodo <dyrnxm52@hanmail.net>
2021-06-07 16:47:10 +0900

    Homepage 컴포넌트에 react router 라이브러리 적용
    - 리액트 라우터 라이브러리 이용해서 변경

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 
+import {
+  Link,
+} from 'react-router-dom';
+
 export default function HomePage() {
   return (
     <div>
       <h2>Home</h2>
       <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/restaurants">Restaurants</a></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/restaurants">Restaurants</Link></li>
       </ul>
     </div>
   );
