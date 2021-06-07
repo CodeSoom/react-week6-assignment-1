2a5183be5ad7d78974a8914bec66007dab8e0cbf
dngwoodo <dngwoodo@gmail.com>
2021-06-07 21:45:40 +0900

    style: Write todo list

--- src/App.jsx
+++ src/App.jsx
@@ -10,10 +10,10 @@ import {
   loadInitialData,
 } from './actions';
 
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
+// 1. Header, HomePage, AboutPage, RestaurantsPage 생성
+// 2. Header, HomePage, AboutPage, RestaurantsPage 라우팅 적용
+// 3. RestaurantDetailPage 생성
+// 4. RestaurantDetailPage 라우팅 적용
 
 export default function App() {
   const dispatch = useDispatch();
