fb30424e79750f73c766bd1a539de427cfb6bbd1
seong-dodo <dyrnxm52@hanmail.net>
2021-06-07 20:31:23 +0900

    NotFoundPage에 대한 테스트 추가한다
    - 리액트 라우터 라이브러리 추가에 대한 테스트 추가

--- src/NotFoundPage.test.jsx
+++ src/NotFoundPage.test.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import {
+  MemoryRouter,
+} from 'react-router-dom';
+
 import NotFoundPage from './NotFoundPage';
 
 test('NotFoundPage', () => {
-  const { queryByText } = render(<NotFoundPage />);
+  const { queryByText } = render((
+    <MemoryRouter>
+      <NotFoundPage />
+    </MemoryRouter>
+  ));
 
-  expect(queryByText(/Not/)).not.toBeNull();
+  expect(queryByText('Not')).not.toBeNull();
 });
