ca9dd1a97fb5e0e600912ea0eaa45135d111c605
seong-dodo <dyrnxm52@hanmail.net>
2021-06-08 01:12:43 +0900

    RestaurantDetailPage에 대한 뼈대 만들기 & 레스토랑 상세 정보보기 API 받아오기
    - RestaurantDetailPage에 대한 뼈대 구현
    - 레스토랑 상세 정보보기 API 받아오는 fetch함수 만들기

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 export default function RestaurantDetailPage() {
+  useEffect(() => {
+    // APi 호출 ...
+  }, []);
+
   return (
-    // Todo: ...
-    <></>
+    <div>
+      <h1>{name}</h1>
+      <p>
+        주소:
+        {' '}
+        {address}
+      </p>
+      <h2>메뉴</h2>
+      <ul>
+        {menuItems.map((menuItem) => (
+          <li key={menuItem.id}>
+            {menuItem.name}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
