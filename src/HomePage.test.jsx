5180bf5f9c3fdee35f662e738f04f20a15981a2e
seong-dodo <dyrnxm52@hanmail.net>
2021-06-07 16:22:40 +0900

    HomePage에 대한 테스트 추가한다
    - 리액트 라우터 라이브러리 추가에 대한 테스트 추가

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -2,10 +2,18 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import {
+  MemoryRouter,
+} from 'react-router-dom';
+
 import HomePage from './HomePage';
 
 test('HomePage', () => {
-  const { queryByText } = render(<HomePage />);
+  const { queryByText } = render((
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  ));
 
   expect(queryByText(/Home/)).not.toBeNull();
 });
