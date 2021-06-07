445f771b6c382abc1e60cdd38c547f5cc7e65c29
최한울 <hanul.choi95@nts-corp.com>
2021-06-07 20:49:26 +0900

    HomePage를 리팩토링한다
    - App 컴포넌트에서 HomePage 컴포넌트 분리

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
 
 import RestaurantsPage from './RestaurantsPage';
+import HomePage from './HomePage';
+
+function NotFoundPage() {
+  return (
+    <p>404 Not Found</p>
+  );
+}
 
 export default function App() {
+  const { location: { pathname } } = window;
+
+  const MyComponent = {
+    '/': HomePage,
+    '/restaurants': RestaurantsPage,
+  }[pathname] || NotFoundPage;
 
   return (
-    <RestaurantsPage />
+    <MyComponent />
   );
 }
