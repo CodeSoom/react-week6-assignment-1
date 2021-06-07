9b6b4915f80742e843dda21262d44b91efbd3d4d
yujonglee <yujonglee@g.skku.edu>
2021-06-07 21:26:04 +0900

    App테스트에서 불필요해진 mock 구현을 단순화하라

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -7,20 +7,13 @@ jest.mock('react-redux');
 
 describe('App', () => {
   const dispatch = jest.fn();
-
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
     restaurant: {
-      categories: [
-        { id: 1, name: '한식' },
-        { id: 2, name: '중식' },
-        { id: 3, name: '일식' },
-        { id: 4, name: '양식' },
-        { id: 5, name: '분식' },
-      ],
-      regions: ['서울', '대전', '대구', '부산', '광주', '강원도', '인천'],
-      selectedRestaurants: ['원초밥'],
+      categories: [],
+      regions: [],
+      selectedRestaurants: [],
 
       selected: {
         category: { id: null },
