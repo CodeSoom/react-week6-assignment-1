d4eea1a38e859609e702093fd93735c483da3661
dngwoodo <dngwoodo@gmail.com>
2021-06-07 23:11:57 +0900

    style: Check todo list

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import AboutPage from './pages/AboutPage';
 import RestaurantsPage from './pages/RestaurantsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
-// 1. Header, HomePage, AboutPage, RestaurantsPage 생성
-// 2. Header, HomePage, AboutPage, RestaurantsPage 라우팅 적용
+// 1. Header, HomePage, AboutPage, RestaurantsPage 생성 X
+// 2. Header, HomePage, AboutPage, RestaurantsPage 라우팅 적용 X
 // 3. RestaurantDetailPage 생성
 // 4. RestaurantDetailPage 라우팅 적용
 
