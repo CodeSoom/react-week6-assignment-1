c575e30029ec6e8a6bb6c62437c9308ed1427dd7
최한울 <hanul.choi95@nts-corp.com>
2021-06-07 20:53:54 +0900

    AboutPage를 리팩토링한다

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import RestaurantsPage from './RestaurantsPage';
 import HomePage from './HomePage';
 import NotFoundPage from './NotFoundPage';
+import AboutPage from './AboutPage';
 
 export default function App() {
   const { location: { pathname } } = window;
 
   const MyComponent = {
     '/': HomePage,
+    '/about': AboutPage,
     '/restaurants': RestaurantsPage,
   }[pathname] || NotFoundPage;
 
