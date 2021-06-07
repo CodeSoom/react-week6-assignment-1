8665b688a9426933939c9df3896a3c4a063a5a8b
최한울 <hanul.choi95@nts-corp.com>
2021-06-07 21:28:26 +0900

    Home 페이지 제목에 대한 테스트를 작성한다

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -10,6 +10,10 @@ test('HomePage', () => {
   ));
 
   expect(getByText('헤더')).not.toBeNull();
+
+  expect(getByText('Home')).not.toBeNull();
+  expect(getByText('Home')).toContainHTML('h2');
+
   expect(getByText('About')).not.toBeNull();
   expect(getByText('Restaurants')).not.toBeNull();
 });
