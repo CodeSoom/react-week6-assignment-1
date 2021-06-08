9f98cb3605ac17f0c6e4e3154f6dc4825342b0c5
dngwoodo <dngwoodo@gmail.com>
2021-06-08 17:34:45 +0900

    feat: Go to then restaurant page wen click restaurant

--- src/container/RestaurantsContainer/index.jsx
+++ src/container/RestaurantsContainer/index.jsx
@@ -1,19 +1,27 @@
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { loadRestaurant } from '../../actions';
 
 import { get } from '../../utils';
 
 export default function RestaurantsContainer() {
+  const dispatch = useDispatch();
+
   const restaurants = useSelector(get('restaurants'));
+
+  function handleClick(restaurantId) {
+    dispatch(loadRestaurant({ restaurantId }));
+
+    window.location.href = `${window.location.href}/${restaurantId}`;
+  }
 
   return (
     <ul>
       {restaurants.map((restaurant) => (
         <li key={restaurant.id}>
-          <a
-            href={`${window.location.href}/${restaurant.id}`}
-          >
+          <button type="button" onClick={() => handleClick(restaurant.id)}>
             {restaurant.name}
-          </a>
+          </button>
         </li>
       ))}
     </ul>
