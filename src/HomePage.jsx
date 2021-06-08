2b254f0bcd42b25cd9e1ca8fc229754a36ec1559
hxunz <guswl2843@gmail.com>
2021-06-08 22:11:18 +0900

    About 페이지 내용 수정

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -8,8 +8,7 @@ export default function HomePage() {
       <h2>Home</h2>
       <ul>
         <li><Link to="/about">About</Link></li>
-        <li><Link to="/restaurnats">Restaurants</Link></li>
-        <li><Link to="/xxx">멸망의 길</Link></li>
+        <li><Link to="/restaurants">Restaurants</Link></li>
       </ul>
     </div>
   );
