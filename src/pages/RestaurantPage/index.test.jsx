8841e3a09e286d8b2c30a2860bfecca11b1c9046
dngwoodo <dngwoodo@gmail.com>
2021-06-08 21:05:32 +0900

    test: Load restaurant

--- src/pages/RestaurantPage/index.test.jsx
+++ src/pages/RestaurantPage/index.test.jsx
@@ -1,30 +1,49 @@
 import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { MemoryRouter } from 'react-router';
 
-import RestaurantDetailPage from '.';
+import RestaurantPage from '.';
 
 import restaurant from '../../../fixtures/restaurant';
 
 jest.mock('react-redux');
 
-describe('RestaurantsDetailPage', () => {
+describe('RestaurantsPage', () => {
+  const dispatch = jest.fn();
+
   beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+
     useSelector.mockImplementation((selector) => selector({
       restaurant,
     }));
   });
 
+  function renderRestaurantPage() {
+    return render((
+      <MemoryRouter>
+        <RestaurantPage />
+      </MemoryRouter>
+    ));
+  }
+
   it('renders "메뉴" title', () => {
-    const { getByRole } = render(<RestaurantDetailPage />);
+    const { getByRole } = renderRestaurantPage();
 
     expect(getByRole('heading', { name: '메뉴' })).toBeInTheDocument();
   });
 
   it('renders menus', () => {
-    const { getByText } = render(<RestaurantDetailPage />);
+    const { getByText } = renderRestaurantPage();
 
     restaurant.menuItems.forEach(({ name }) => {
       expect(getByText(name)).toBeInTheDocument();
     });
+  });
+
+  it('loads restaurant', () => {
+    renderRestaurantPage();
+
+    expect(dispatch).toBeCalled();
   });
 });
