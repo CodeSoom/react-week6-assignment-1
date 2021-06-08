54cc2b321a1a3e982f915d69ab6b719ce6c4d412
최한울 <hanul.choi95@nts-corp.com>
2021-06-09 00:12:06 +0900

    RestaurantDetailPage에서 레스토랑 정보를 가져오는 것에 대한 테스트를 작성한다

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -1,29 +1,37 @@
 import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetailPage from './RestaurantDetailPage';
 
-test('RestaurantDetailPage', () => {
-  useSelector.mockImplementation((selector) => selector({
-    regions: [],
-    categories: [],
-    restaurants: [],
-    restaurant: {
-      id: 1,
-      categoryId: 1,
-      name: '양천주가',
-      address: '서울 강남구',
-      menuItems: [
-        { id: 1, restaurantId: 1, name: '비빔밥' },
-      ],
-    },
-  }));
+describe('RestaurantDetailPage', () => {
+  it('renders', () => {
+    const dispatch = jest.fn();
 
-  const { getByText } = render((
-    <RestaurantDetailPage />
-  ));
+    useDispatch.mockImplementation(() => dispatch);
 
-  expect(getByText(/서울 강남구/)).not.toBeNull();
-  expect(getByText('양천주가')).not.toBeNull();
-  expect(getByText('비빔밥')).not.toBeNull();
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+      categories: [],
+      restaurants: [],
+      restaurant: {
+        id: 1,
+        categoryId: 1,
+        name: '양천주가',
+        address: '서울 강남구',
+        menuItems: [
+          { id: 1, restaurantId: 1, name: '비빔밥' },
+        ],
+      },
+    }));
+
+    const { getByText } = render((
+      <RestaurantDetailPage />
+    ));
+
+    expect(getByText(/서울 강남구/)).not.toBeNull();
+    expect(getByText('양천주가')).not.toBeNull();
+    expect(getByText('비빔밥')).not.toBeNull();
+
+    expect(dispatch).toBeCalled();
+  });
 });
