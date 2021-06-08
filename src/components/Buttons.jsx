2dbe5619c5a9d01ece9c65e827a2dc9e62db3c3f
yujonglee <yujonglee@g.skku.edu>
2021-06-09 02:34:28 +0900

    (V)를 추가하는 코드의 가독성을 개선하라

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,16 +1,20 @@
 export default function ButtonList({ items, onClick, selected }) {
   return (
     <ul>
-      {items.map(({ id, name }) => (
-        <li key={id}>
-          <button
-            type="button"
-            onClick={() => onClick({ id, name })}
-          >
-            {(selected === name) ? `${name}(V)` : name}
-          </button>
-        </li>
-      ))}
+      {items.map(({ id, name }) => {
+        const suffix = (selected === name) ? '(V)' : '';
+
+        return (
+          <li key={id}>
+            <button
+              type="button"
+              onClick={() => onClick({ id, name })}
+            >
+              {name + suffix}
+            </button>
+          </li>
+        );
+      })}
     </ul>
   );
 }
