f10fea7850f080897b5b8ed68119e48b7512a95e
yujonglee <yujonglee@g.skku.edu>
2021-06-09 01:48:32 +0900

    info보다는 더 명확한 restaurants라는 이름을 사용하라

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -1,11 +1,13 @@
 import { useSelector } from 'react-redux';
 
 export default function RestaurantList() {
-  const infos = useSelector((state) => state.restaurant.selectedRestaurants);
+  const restaurants = useSelector(
+    (state) => state.restaurant.selectedRestaurants,
+  );
 
   return (
     <ul>
-      {infos.map(
+      {restaurants.map(
         ({ id, name }) => (
           <li key={id}>
             {name}
