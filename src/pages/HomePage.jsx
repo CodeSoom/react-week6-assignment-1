c30b383ba65e4cf5d2ad1efdb35f6223a748ddf2
yujonglee <yujonglee@g.skku.edu>
2021-06-08 14:53:06 +0900

    Home페이지에 링크를 추가하라

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
   return (
-    <p>
-      Home
-    </p>
+    <>
+      <p>
+        Home
+      </p>
+      <ul>
+        <Link to="/about">About</Link>
+        <Link to="/restaurants">Restaurants</Link>
+      </ul>
+    </>
   );
 }
--- src/pages/HomePage.test.jsx
+++ src/pages/HomePage.test.jsx
@@ -1,10 +1,34 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
   it('renders Home', () => {
-    const { container } = render(<HomePage />);
+    const { container } = render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>,
+    );
 
     expect(container).toHaveTextContent('Home');
+  });
+  it('renders About Link', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>,
+    );
+
+    expect(container).toHaveTextContent('About');
+  });
+  it('renders Restaurants Link', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>,
+    );
+
+    expect(container).toHaveTextContent('Restaurants');
   });
 });
