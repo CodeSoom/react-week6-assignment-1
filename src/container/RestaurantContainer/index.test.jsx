c197047ddc0432454d3c754952a318465043ae73
dngwoodo <dngwoodo@gmail.com>
2021-06-08 21:06:47 +0900

    test: Wrap component with MemeoryRouter

--- src/container/RestaurantContainer/index.test.jsx
+++ src/container/RestaurantContainer/index.test.jsx
@@ -1,5 +1,6 @@
 import { render } from '@testing-library/react';
 import { useSelector } from 'react-redux';
+import { MemoryRouter } from 'react-router';
 
 import RestaurantContainer from '.';
 
@@ -14,19 +15,27 @@ describe('RestaurantContainer', () => {
     }));
   });
 
+  function renderRestaurantContainer() {
+    return render((
+      <MemoryRouter>
+        <RestaurantContainer />
+      </MemoryRouter>
+    ));
+  }
+
   it('renders "메뉴" title', () => {
-    const { getByRole } = render(<RestaurantContainer />);
+    const { getByRole } = renderRestaurantContainer();
 
     expect(getByRole('heading', { name: '메뉴' })).toBeInTheDocument();
   });
 
   it('renders address', () => {
-    const { getByText } = render(<RestaurantContainer />);
+    const { getByText } = renderRestaurantContainer();
 
     expect(getByText('주소: 양천주가 in 서울 강남구 123456')).toBeInTheDocument();
   });
   it('renders menus', () => {
-    const { getByText } = render(<RestaurantContainer />);
+    const { getByText } = renderRestaurantContainer();
 
     restaurant.menuItems.forEach(({ name }) => {
       expect(getByText(name)).toBeInTheDocument();
