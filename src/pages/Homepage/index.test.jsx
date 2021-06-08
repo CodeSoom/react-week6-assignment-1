6d3450d666734e90a8bf9a5fb684f286ab0a453b
dngwoodo <dngwoodo@gmail.com>
2021-06-08 19:56:20 +0900

    feat: Change a link to Link

--- src/pages/Homepage/index.test.jsx
+++ src/pages/Homepage/index.test.jsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router';
 
 import HomePage from '.';
 
 describe('Home', () => {
   it('renders link list', () => {
-    const { getByRole } = render(<HomePage />);
+    const { getByRole } = render((
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    ));
 
     expect(getByRole('link', { name: 'About' })).toBeInTheDocument();
     expect(getByRole('link', { name: 'Restaurants' })).toBeInTheDocument();
