08474e2f27b4667bc8c05b89f3b1f5cc60ac42d6
dngwoodo <dngwoodo@gmail.com>
2021-06-08 21:05:44 +0900

    feat: Load restaurant

--- src/pages/RestaurantPage/index.jsx
+++ src/pages/RestaurantPage/index.jsx
@@ -1,5 +1,19 @@
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useParams } from 'react-router';
+
+import { loadRestaurant } from '../../actions';
+
 import RestaurantContainer from '../../container/RestaurantContainer';
 
 export default function RestaurantPage() {
+  const { id: restaurantId } = useParams();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRestaurant({ restaurantId }));
+  });
+
   return <RestaurantContainer />;
 }
