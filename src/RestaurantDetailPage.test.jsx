85daad269fce1f01dd8abd1f15e4f363ab49cdc6
최한울 <hanul.choi95@nts-corp.com>
2021-06-09 23:47:30 +0900

    식당 상세정보에서 메뉴가 없을 경우에 대한 테스트를 작성한다

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -4,40 +4,82 @@ import { useDispatch, useSelector } from 'react-redux';
 import RestaurantDetailPage from './RestaurantDetailPage';
 
 describe('RestaurantDetailPage', () => {
-  it('renders', () => {
-    const dispatch = jest.fn();
+  const match = {
+    params: {
+      restaurantId: 1,
+    },
+  };
 
-    useDispatch.mockImplementation(() => dispatch);
-
-    useSelector.mockImplementation((selector) => selector({
-      regions: [],
-      categories: [],
-      restaurants: [],
-      restaurant: {
-        id: 1,
-        categoryId: 1,
-        name: '양천주가',
-        address: '서울 강남구',
-        menuItems: [
-          { id: 1, restaurantId: 1, name: '비빔밥' },
-        ],
-      },
-    }));
-
-    const match = {
-      params: {
-        restaurantId: 1,
-      },
-    };
-
-    const { queryByText } = render((
+  function renderRestaurant() {
+    return render((
       <RestaurantDetailPage match={match} />
     ));
+  }
 
-    expect(queryByText(/서울 강남구/)).not.toBeNull();
-    expect(queryByText('양천주가')).not.toBeNull();
-    expect(queryByText('비빔밥')).not.toBeNull();
+  describe('dispatch', () => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
 
-    expect(dispatch).toBeCalled();
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        restaurant: {
+          id: 0,
+          categoryId: 0,
+          name: '',
+          address: '',
+          menuItems: [],
+        },
+      }));
+    });
+
+    it('changes restaurant', () => {
+      renderRestaurant();
+      expect(dispatch).toBeCalled();
+    });
+  });
+
+  context('with menu', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        restaurant: {
+          id: 1,
+          categoryId: 1,
+          name: '양천주가',
+          address: '서울 강남구',
+          menuItems: [
+            { id: 1, restaurantId: 1, name: '비빔밥' },
+          ],
+        },
+      }));
+    });
+
+    it('renders restaurant', () => {
+      const { queryByText } = renderRestaurant();
+
+      expect(queryByText(/서울 강남구/)).not.toBeNull();
+      expect(queryByText('양천주가')).not.toBeNull();
+      expect(queryByText('비빔밥')).not.toBeNull();
+    });
+  });
+
+  context('without menu', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        restaurant: {
+          id: 1,
+          categoryId: 1,
+          name: '양천주가',
+          address: '서울 강남구',
+          menuItems: [],
+        },
+      }));
+    });
+    it('renders restaurant', () => {
+      const { queryByText } = renderRestaurant();
+
+      expect(queryByText(/서울 강남구/)).not.toBeNull();
+      expect(queryByText('양천주가')).not.toBeNull();
+      expect(queryByText('메뉴가 존재하지 않습니다.')).not.toBeNull();
+    });
   });
 });
