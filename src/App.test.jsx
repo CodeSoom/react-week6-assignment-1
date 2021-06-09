22cf4f9c4ef508db18066e92f37589c949b01231
dngwoodo <dngwoodo@gmail.com>
2021-06-09 17:55:45 +0900

    test: Refactor test case
    valid path를 굳이 context로 감싸줄 필요가 없어서 삭제

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -28,49 +28,47 @@ describe('App', () => {
     ));
   }
 
+  context('with path /', () => {
+    it('renders the home page', () => {
+      const { getByRole } = renderApp('/');
+
+      expect(getByRole('link', { name: 'About' })).toBeInTheDocument();
+      expect(getByRole('link', { name: 'Restaurants' })).toBeInTheDocument();
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders the about page', () => {
+      const { getByRole, getByText } = renderApp('/about');
+
+      expect(getByRole('heading', { name: 'About' })).toBeInTheDocument();
+      expect(getByText('About 페이지입니다.')).toBeInTheDocument();
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders the restaurants page', () => {
+      const { getByRole } = renderApp('/restaurants');
+
+      expect(dispatch).toBeCalled();
+
+      expect(getByRole('button', { name: '서울' })).toBeInTheDocument();
+    });
+  });
+
+  context('with path /restaurants/:id', () => {
+    it('renders the restaurants page', () => {
+      const { getByRole } = renderApp('/restaurants/1');
+
+      expect(getByRole('heading', { name: '메뉴' })).toBeInTheDocument();
+    });
+  });
+
   context('with invalid path', () => {
     it('renders the not found page', () => {
       const { getByRole } = renderApp('/xxx');
 
       expect(getByRole('heading', { name: '404 Not Found' })).toBeInTheDocument();
-    });
-  });
-
-  context('with valid path', () => {
-    context('with path /', () => {
-      it('renders the home page', () => {
-        const { getByRole } = renderApp('/');
-
-        expect(getByRole('link', { name: 'About' })).toBeInTheDocument();
-        expect(getByRole('link', { name: 'Restaurants' })).toBeInTheDocument();
-      });
-    });
-
-    context('with path /about', () => {
-      it('renders the about page', () => {
-        const { getByRole, getByText } = renderApp('/about');
-
-        expect(getByRole('heading', { name: 'About' })).toBeInTheDocument();
-        expect(getByText('About 페이지입니다.')).toBeInTheDocument();
-      });
-    });
-
-    context('with path /restaurants', () => {
-      it('renders the restaurants page', () => {
-        const { getByRole } = renderApp('/restaurants');
-
-        expect(dispatch).toBeCalled();
-
-        expect(getByRole('button', { name: '서울' })).toBeInTheDocument();
-      });
-    });
-
-    context('with path /restaurants/:id', () => {
-      it('renders the restaurants page', () => {
-        const { getByRole } = renderApp('/restaurants/1');
-
-        expect(getByRole('heading', { name: '메뉴' })).toBeInTheDocument();
-      });
     });
   });
 });
