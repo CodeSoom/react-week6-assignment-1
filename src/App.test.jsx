db5222122e85460eb376febea1a4838b2791763c
hxunz <guswl2843@gmail.com>
2021-06-09 20:35:21 +0900

    about 클릭 시 보여지는 텍스트 수정

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -45,7 +45,7 @@ describe('App', () => {
     it('renders the about page', () => {
       const { container } = renderApp({ path: '/about' });
 
-      expect(container).toHaveTextContent('20명에게 추천');
+      expect(container).toHaveTextContent('페이지');
     });
   });
 
