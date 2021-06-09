3947418a70a283d14af3033bd6126be20ddcc80b
hanvit7 <hanvit7@gmail.com>
2021-06-09 23:56:55 +0900

    구현 - Home에 링크를 <a>에서 <Link>로 변경

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
   return (
     <div>
       <h2>Home</h2>
       <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/restaurants">Restaurants</a></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/restaurants">Restaurants</Link></li>
+        <li><Link to="/xxx">멸망의 길</Link></li>
       </ul>
     </div>
   );
