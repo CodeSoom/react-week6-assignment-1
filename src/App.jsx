adfe60e8897c96d712113316994a64ca79f59073
hanvit7 <hanvit7@gmail.com>
2021-06-09 23:34:24 +0900

    구현 - App에 window객채를 이용한 라우터 구현
    - HomePage, NotFoundPage, AboutPage를 함수로 구현
    - window객채를 이용해 각각의 페이지로 라우팅 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,39 @@
 import RestaurantsPage from './RestaurantsPage';
 
-export default function App() {
+function HomePage() {
   return (
-    <RestaurantsPage />
+    <div>
+      <h2>Home</h2>
+      <ul>
+        <li><a href="/about">About</a></li>
+        <li><a href="/restaurants">Restaurants</a></li>
+      </ul>
+    </div>
+  );
+}
+
+function NotFoundPage() {
+  return (
+    <p>Not Found Page</p>
+  );
+}
+
+function AboutPage() {
+  return (
+    <p>About...</p>
+  );
+}
+
+export default function App() {
+  const { location: { pathname } } = window;
+
+  const MyComponent = {
+    '/': HomePage,
+    '/about': AboutPage,
+    '/restaurants': RestaurantsPage,
+  }[pathname] || NotFoundPage;
+
+  return (
+    <MyComponent />
   );
 }
