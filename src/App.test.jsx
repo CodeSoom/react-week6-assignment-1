ec51e48d4cf74f293ca46144568b9728e13d64ea
hanvit7 <hanvit7@gmail.com>
2021-06-10 23:12:37 +0900

    테스트 - App 테스트 추가
    - HomePage 랜터링 확인
    - AboutPage 랜터링 확인

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,43 @@
 import React from 'react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
 import App from './App';
 
-test('App', () => {
-  useSelector.mockImplementation((selector) => selector({
-    regions: [],
-    categories: [],
-    restaurants: [],
-  }));
+describe('App', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+      categories: [],
+      restaurants: [],
+    }));
+  });
 
-  render((<App />));
+  context('with path /', () => {
+    it('renders HomePage', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders th about page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/about']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('About...');
+    });
+  });
 });
