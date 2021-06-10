94dd7ce0813a51c6fc3d1a4a12843768b16d248f
dngwoodo <dngwoodo@gmail.com>
2021-06-10 14:59:59 +0900

    feat: Add Home title
    e2e 테스트를 위해 HomePage에 Home title추가

--- src/pages/Homepage/index.test.jsx
+++ src/pages/Homepage/index.test.jsx
@@ -4,6 +4,16 @@ import { MemoryRouter } from 'react-router';
 import HomePage from '.';
 
 describe('HomePage', () => {
+  it('renders title', () => {
+    const { getByRole } = render((
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    ));
+
+    expect(getByRole('heading', { name: 'Home' })).toBeInTheDocument();
+  });
+
   it('renders links', () => {
     const { getByRole } = render((
       <MemoryRouter>
