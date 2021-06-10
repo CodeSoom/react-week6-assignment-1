bd3a45a71fb711b4c80a9dba8cc50b0557e175c6
hanvit7 <hanvit7@gmail.com>
2021-06-10 23:26:04 +0900

    테스트 - App 테스트 추가
    - Restaurants Page 랜터링 확인
    - Not Found Page 랜터링 확인

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -4,14 +4,18 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
 describe('App', () => {
   beforeEach(() => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
     useSelector.mockImplementation((selector) => selector({
-      regions: [],
+      regions: [{ id: 1, name: '서울' }],
       categories: [],
       restaurants: [],
     }));
@@ -34,10 +38,26 @@ describe('App', () => {
   });
 
   context('with path /about', () => {
-    it('renders th about page', () => {
+    it('renders the about page', () => {
       const { container } = renderApp({ path: '/about' });
 
       expect(container).toHaveTextContent('About...');
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders the restaurants page', () => {
+      const { container } = renderApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent('서울');
+    });
+  });
+
+  context('with invalid path', () => {
+    it('renders the not found page', () => {
+      const { container } = renderApp({ path: '/xxx' });
+
+      expect(container).toHaveTextContent('Not Found');
     });
   });
 });
