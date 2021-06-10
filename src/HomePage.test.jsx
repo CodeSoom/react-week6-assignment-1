9d0e8b7495585dbcdd96a5a919add63afda8c1ab
hanvit7 <hanvit7@gmail.com>
2021-06-10 23:06:02 +0900

    테스트 - HomePage 테스트 오류 수정
    - MemoryRouter 추가

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
 test('HomePage', () => {
-  render((<HomePage />));
+  render((
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  ));
 });
