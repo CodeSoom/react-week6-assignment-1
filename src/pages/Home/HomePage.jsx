ec36e279a24f4fc72bcef5ede4e7cea5442e4ee1
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-06-11 16:23:40 +0900

    feat: HomePage 컴포넌트 구현

---
+++ src/pages/Home/HomePage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const HomePage = () => (
+  <div>
+    <h1>Home</h1>
+    <ul>
+      <li><a href="/">HOME</a></li>
+      <li><a href="/about">ABOUT</a></li>
+      <li><a href="/restaurants">RESTAURANTS</a></li>
+    </ul>
+  </div>
+);
+
+export default HomePage;
