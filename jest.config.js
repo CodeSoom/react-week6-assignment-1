e6765273c0f151a58f565a3b73499a888c3e776c
yujonglee <yujonglee@g.skku.edu>
2021-06-12 18:54:20 +0900

    커버레지조건을 원래대로 돌려라

--- jest.config.js
+++ jest.config.js
@@ -5,10 +5,11 @@ module.exports = {
   ],
   coverageThreshold: {
     global: {
-      branches: 0,
-      functions: 0,
-      lines: 0,
-      statements: 0,
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
     },
   },
+  coveragePathIgnorePatterns: ['<rootDir>/src/services/api.js'],
 };
