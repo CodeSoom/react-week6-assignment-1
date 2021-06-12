264a9e4254ff4661665fbacfe57e77c0bdc1f80e
devhyun637 <59258239+devhyun637@users.noreply.github.com>
2021-06-12 17:09:06 +0900

    test: Homepage 테스트코드에 memoryRouter 추가

--- src/pages/Home/HomePage.test.jsx
+++ src/pages/Home/HomePage.test.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
 test('HomePage', () => {
-  render((<HomePage />));
+  render((
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>));
 });
