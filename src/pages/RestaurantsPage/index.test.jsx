6c1a543dfa6513165f6ad0e050e62054ab75c899
dngwoodo <dngwoodo@gmail.com>
2021-06-13 20:44:47 +0900

    test: Refactor RestaurantPage test for clarify

--- src/pages/RestaurantsPage/index.test.jsx
+++ src/pages/RestaurantsPage/index.test.jsx
@@ -1,34 +1,87 @@
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
+import given from 'given2';
 
 import RestaurantsPage from '.';
 
-test('RestaurantsPage', () => {
+describe('RestaurantsPage', () => {
   const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+    useSelector.mockImplementation((selector) => selector({
+      regions: given.regions,
+      categories: given.categories,
+      restaurants: given.restaurants,
+    }));
 
-  const { queryByText } = render((
-    <MemoryRouter>
-      <RestaurantsPage />
-    </MemoryRouter>
-  ));
+    given('regions', () => [{
+      id: 1,
+      name: '서울',
+    }]);
+    given('categories', () => [{
+      id: 1,
+      name: '한식',
+    }]);
+    given('restaurants', () => [{
+      id: 1,
+      name: '마법사주방',
+    }]);
+  });
 
-  expect(dispatch).toBeCalled();
+  function renderRestaurantsPage() {
+    return render((
+      <MemoryRouter>
+        <RestaurantsPage />
+      </MemoryRouter>
+    ));
+  }
 
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+  it('loads initial data', () => {
+    renderRestaurantsPage();
+
+    expect(dispatch).toBeCalled();
+  });
+
+  context('with regions, categories, restaurants', () => {
+    beforeEach(() => {
+      given('regions', () => [{
+        id: 1,
+        name: '서울',
+      }]);
+      given('categories', () => [{
+        id: 1,
+        name: '한식',
+      }]);
+      given('restaurants', () => [{
+        id: 1,
+        name: '마법사주방',
+      }]);
+    });
+
+    it('renders links, buttons', () => {
+      const { getByRole } = renderRestaurantsPage();
+
+      expect(getByRole('link', { name: '마법사주방' })).toBeInTheDocument();
+      expect(getByRole('button', { name: '서울' })).toBeInTheDocument();
+      expect(getByRole('button', { name: '한식' })).toBeInTheDocument();
+    });
+  });
+
+  context('without regions, categories, restaurants', () => {
+    beforeEach(() => {
+      given('regions', () => []);
+      given('categories', () => []);
+      given('restaurants', () => []);
+    });
+
+    it("doesn't render link, button", () => {
+      const { container } = renderRestaurantsPage();
+
+      expect(container.innerHTML).not.toContain('<a href="');
+      expect(container.innerHTML).not.toContain('<button type="button"');
+    });
+  });
 });
