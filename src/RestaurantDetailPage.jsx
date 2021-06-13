ce7caf5a983810aa99c12ff4813d3c3d08fcb423
hanvit7 <hanvit7@gmail.com>
2021-06-13 22:09:49 +0900

    구현 - RestaurantDetailPage에서 loadRestaurant 구현

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,7 +1,21 @@
+import { useEffect } from 'react';
+
 import { useParams } from 'react-router-dom';
+
+import { useDispatch } from 'react-redux';
+
+import {
+  loadRestaurant,
+} from './actions';
 
 export default function RestaurantDetailPage() {
   const { id } = useParams();
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRestaurant({ restaurantId: id }));
+  }, []);
 
   return (
     <p>
