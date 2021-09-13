bb22f40c4568000132b3721b807b769f71b6d1c6
passwd10 <parkinseo94@naver.com>
2021-09-14 00:57:57 +0900

    feat: 두번째 영상 코드

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,24 @@
+import { Switch, Route, Link } from 'react-router-dom';
+
 import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
 import AboutPage from './AboutPage';
 import NotFoundPage from './NotFoundPage';
 
 export default function App() {
-  const { location: { pathname } } = window;
-
-  const MyComponent = {
-    '/': HomePage,
-    '/about': AboutPage,
-    '/restaurants': RestaurantsPage,
-  }[pathname] || NotFoundPage;
-
   return (
-    <MyComponent />
+    <div>
+      <header>
+        <h1>
+          <Link to="/">헤더 영역</Link>
+        </h1>
+      </header>
+      <Switch>
+        <Route exact path="/about" component={AboutPage} />
+        <Route exact path="/restaurants" component={RestaurantsPage} />
+        <Route exact path="/" component={HomePage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </div>
   );
 }
--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,7 +1,13 @@
+import { MemoryRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
 test('HomePage', () => {
-  render(<HomePage />);
+  render((
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  ));
 });
