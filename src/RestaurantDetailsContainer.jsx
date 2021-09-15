4be2e32f0bd58624944dc9a8b86eb911490a1414
lazy-sky <1cielo@naver.com>
2021-09-16 01:24:56 +0900

    refact: Branch processing in ResDetailContainer

--- src/RestaurantDetailsContainer.jsx
+++ src/RestaurantDetailsContainer.jsx
@@ -1,5 +1,18 @@
+import { useSelector } from 'react-redux';
+
 export default function RestaurantDetailsContainer() {
+  // TODO: 해당 state 충족하도록 action, reducer 수정
+  const restaurantDetails = useSelector((state) => state.restaurantDetails);
+
+  if (!restaurantDetails) {
+    return (
+      <h2>Loading...</h2>
+    );
+  }
+
   return (
-    <h2>Loading...</h2>
+    <div>
+      레스토랑 세부사항들
+    </div>
   );
 }
