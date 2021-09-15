11f378df8875471cc5bf93eb718bae19d6a1172f
dhkim <dhkim@crscube.co.kr>
2021-09-15 18:04:56 +0900

    feat: 과제 초기 설정
    - Home/About/Restaurants page 구현

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,34 +1,43 @@
 import React from 'react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
-test('App', () => {
-  const dispatch = jest.fn();
+describe('App', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+    useDispatch.mockImplementation(() => dispatch);
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+      categories: [],
+      restaurants: [],
+    }));
+  });
 
-  const { queryByText } = render((
-    <App />
-  ));
+  context.each`
+  pagename | path |  expected
+    ${'home'} |${'/'} | ${'Home'}
+    ${'about'} | ${'/about'} |  ${'About...'}
+    ${'restaurants list'} |${'/restaurants'} |  ${'서울'}
+    ${'invalid'} | ${'/invalid'} | ${'Not Found'}
+  `('with path $path', ({ path, pagename, expected }) => {
+    it(`renders the ${pagename} page`, () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={[path]}>
+          <App />
+        </MemoryRouter>
+      ));
 
-  expect(dispatch).toBeCalled();
-
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+      expect(container).toHaveTextContent(expected);
+    });
+  });
 });
