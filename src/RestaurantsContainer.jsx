93fafa65cb08bb02522584ad1eb6459f510cd37c
Aiden <rheech22@gmail.com>
2021-09-15 19:45:13 +0900

    Feature: Attach Links on restaurant list

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,15 +1,22 @@
 import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
 
 import { get } from './utils';
 
 export default function RestaurantsContainer() {
   const restaurants = useSelector(get('restaurants'));
 
+  function handleClickRestaurant(id) {
+    // todo: fetch by id
+  }
+
   return (
     <ul>
-      {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>
-          {restaurant.name}
+      {restaurants.map(({ id, name }) => (
+        <li key={id} onClick={() => handleClickRestaurant(id)}>
+          <Link to="/restaurant-details">
+            {name}
+          </Link>
         </li>
       ))}
     </ul>
