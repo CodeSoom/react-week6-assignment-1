bf6dd024aa1c1b1547e6b703d483cc5f81a5b88c
passwd10 <parkinseo94@naver.com>
2021-09-16 00:04:14 +0900

    feat: RestaurantDetailPage를 추가구현한다

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,12 +1,25 @@
 export default function RestaurantDetailPage() {
   return (
-    <ul>
-      <li>
-        비빔밥
-      </li>
-      <li>
-        짬뽕
-      </li>
-    </ul>
+    <>
+      <h1>양천주가</h1>
+      <h2>메뉴</h2>
+      <ul>
+        <li>
+          비빔밥
+        </li>
+        <li>
+          짬뽕
+        </li>
+      </ul>
+      <h2>리뷰</h2>
+      <ul>
+        <li>
+          훌륭하다 훌륭하다 지구인놈들
+        </li>
+        <li>
+          비룡 어디에 갔냐? 비룡 내놔라.
+        </li>
+      </ul>
+    </>
   );
 }
