68406ae1f3a8d9215a476bb86f9e7e1f34d14e12
passwd10 <parkinseo94@naver.com>
2021-09-16 01:09:14 +0900

    test: RestaurantDetailPage 테스트를 추가한다

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -1,42 +1,49 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetailPage from './RestaurantDetailPage';
 
 describe('RestaurantDetailPage', () => {
-  useSelector.mockImplementation((selector) => selector({
-    name: '식당1',
-    address: '서울시 주소 12',
-    menuItems: [
-      {
-        id: 1,
-        restaurantId: 1,
-        name: '메뉴1',
-      },
-      {
-        id: 2,
-        restaurantId: 1,
-        name: '메뉴2',
-      },
-    ],
-    reviews: [
-      {
-        id: 1,
-        restaurantId: 1,
-        name: '테스터',
-        score: 5,
-        description: '댓글1',
-      },
-      {
-        id: 2,
-        restaurantId: 1,
-        name: '테스터',
-        score: 1,
-        description: '댓글2',
-      },
-    ],
-  }));
+  const dispatch = jest.fn();
+
+  beforeEach(() => {
+    dispatch.mockClear();
+
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      name: '식당1',
+      address: '서울시 주소 12',
+      menuItems: [
+        {
+          id: 1,
+          restaurantId: 1,
+          name: '메뉴1',
+        },
+        {
+          id: 2,
+          restaurantId: 1,
+          name: '메뉴2',
+        },
+      ],
+      reviews: [
+        {
+          id: 1,
+          restaurantId: 1,
+          name: '테스터',
+          score: 5,
+          description: '댓글1',
+        },
+        {
+          id: 2,
+          restaurantId: 1,
+          name: '테스터',
+          score: 1,
+          description: '댓글2',
+        },
+      ],
+    }));
+  });
 
   it('renders restaurant detail', () => {
     const { container } = render(<RestaurantDetailPage />);
@@ -49,5 +56,11 @@ describe('RestaurantDetailPage', () => {
     expect(container).toHaveTextContent('리뷰');
     expect(container).toHaveTextContent('댓글1');
     expect(container).toHaveTextContent('댓글2');
+  });
+
+  it('calls dispatch', () => {
+    render(<RestaurantDetailPage />);
+
+    expect(dispatch).toBeCalled();
   });
 });
