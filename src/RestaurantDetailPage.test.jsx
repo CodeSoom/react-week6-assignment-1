564f09605fbaad819a02cc9966d0374c48e776cf
passwd10 <parkinseo94@naver.com>
2021-09-16 00:38:46 +0900

    test: RestaurantDetailPage 테스트를 추가한다

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -1,7 +1,53 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import RestaurantDetailPage from './RestaurantDetailPage';
 
-test('RestaurantDetailPage', () => {
-  render(<RestaurantDetailPage />);
+describe('RestaurantDetailPage', () => {
+  useSelector.mockImplementation((selector) => selector({
+    name: '식당1',
+    address: '서울시 주소 12',
+    menuItems: [
+      {
+        id: 1,
+        restaurantId: 1,
+        name: '메뉴1',
+      },
+      {
+        id: 2,
+        restaurantId: 1,
+        name: '메뉴2',
+      },
+    ],
+    reviews: [
+      {
+        id: 1,
+        restaurantId: 1,
+        name: '테스터',
+        score: 5,
+        description: '댓글1',
+      },
+      {
+        id: 2,
+        restaurantId: 1,
+        name: '테스터',
+        score: 1,
+        description: '댓글2',
+      },
+    ],
+  }));
+
+  it('renders restaurant detail', () => {
+    const { container } = render(<RestaurantDetailPage />);
+
+    expect(container).toHaveTextContent('식당1');
+    expect(container).toHaveTextContent('서울시 주소 12');
+    expect(container).toHaveTextContent('메뉴');
+    expect(container).toHaveTextContent('메뉴1');
+    expect(container).toHaveTextContent('메뉴2');
+    expect(container).toHaveTextContent('리뷰');
+    expect(container).toHaveTextContent('댓글1');
+    expect(container).toHaveTextContent('댓글2');
+  });
 });
