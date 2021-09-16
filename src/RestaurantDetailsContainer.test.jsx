6fde45b5743c07e9b5482569071e312b16b8793d
Aiden <rheech22@gmail.com>
2021-09-16 14:02:40 +0900

    Refactor: test code with given2

--- src/RestaurantDetailsContainer.test.jsx
+++ src/RestaurantDetailsContainer.test.jsx
@@ -1,14 +1,22 @@
 import { render } from '@testing-library/react';
+
 import { useSelector } from 'react-redux';
+
+import given from 'given2';
 
 import RestaurantDetailsContainer from './RestaurantDetailsContainer';
 
-describe('RestaurantDetailsContainer', () => {
-  context('식당의 정보가 없다면', () => {
-    useSelector.mockImplementation((selector) => selector({
-      restaurantDetails: null,
-    }));
+import DETAILS from '../fixtures/restaurantDetails';
 
+describe('RestaurantDetailsContainer', () => {
+  beforeEach(() => {
+    given('restaurantDetails', () => null);
+    useSelector.mockImplementation((selector) => selector({
+      restaurantDetails: given.restaurantDetails,
+    }));
+  });
+
+  context('식당의 정보가 없다면', () => {
     it('Loading..을 보여준다', () => {
       const { container } = render((
         <RestaurantDetailsContainer />
@@ -20,25 +28,7 @@ describe('RestaurantDetailsContainer', () => {
 
   context('식당의 정보가 있다면', () => {
     it('정보를 보여준다', () => {
-      useSelector.mockImplementation((selector) => selector({
-        restaurantDetails: {
-          id: 1,
-          name: '양천주가',
-          address: '서울 강남구 123456',
-          menuItems: [
-            {
-              id: 1,
-              restaurantId: 1,
-              name: '비빔밥',
-            },
-            {
-              id: 2,
-              restaurantId: 1,
-              name: '짬뽕',
-            },
-          ],
-        },
-      }));
+      given('restaurantDetails', () => DETAILS);
 
       const { container } = render((
         <RestaurantDetailsContainer />
