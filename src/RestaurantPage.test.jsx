a7ea0e669bd33d4019efa437a3bc3f87b59eb4a3
dhkim <dhkim@crscube.co.kr>
2021-09-16 09:51:07 +0900

    test: RestaurantPage test 수정
    - RestaurantContainer로 이동한 테스트 삭제

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -4,36 +4,8 @@ import RestaurantPage from './RestaurantPage';
 
 describe('RestaurantPage', () => {
   it('shows name, address, menus of Restaurant', () => {
-    const { name, address, menuItems } = {
-      id: 1,
-      categoryId: 1,
-      name: '양천주가',
-      address: '서울 강남구 123456',
-      menuItems: [
-        {
-          id: 1,
-          restaurantId: 1,
-          name: '비빔밥',
-        },
-        {
-          id: 2,
-          restaurantId: 1,
-          name: '짬뽕',
-        },
-        {
-          id: 16,
-          restaurantId: 1,
-          name: '탕수육',
-        },
-      ],
-    };
-
     const { getByText } = render((
-      <RestaurantPage
-        name={name}
-        address={address}
-        menuItems={menuItems}
-      />
+      <RestaurantPage />
     ));
 
     expect(getByText('양천주가')).not.toBeNull();
