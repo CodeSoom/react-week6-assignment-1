290b6d49aecacc64da3d33fdda56cd33a49a6db0
leeok <milkteatea0628@gmail.com>
2022-01-03 23:16:11 +0900

    두번째 영상 공부

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
+import { Link, Route, Routes } from 'react-router-dom';
+
 import RestaurantsPage from './RestaurantsPage';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import NotFoundPage from './NotFoundPage';
 
 export default function App() {
-  const { location: {pathname } } = window;
-  const MyComponent = {
-    '/': HomePage,
-    '/about': AboutPage,
-    '/restaurants': RestaurantsPage,
-  }[pathname] || NotFoundPage;
+  // const { location: {pathname } } = window;
+  // const MyComponent = {
+  //   '/': HomePage,
+  //   '/about': AboutPage,
+  //   '/restaurants': RestaurantsPage,
+  // }[pathname] || NotFoundPage;
+  // react-router-dom 버전업되면서 바뀐점.
+  // 1. Swtich => Routes
+  // 2. exact 는 더이상 사용하지 않는다. (반대로 중첩라우팅 매칭 방식 *를 사용)
+  // 3. component => element
+  // 4. element 작성방식은 jsx
 
   return (
-    <MyComponent />
+    <div>
+      <header>
+        <h1><Link to="/">헤더 영역</Link></h1>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </div>
   );
 }
