c0c389e093610122a54374943a9f49d6a4f0b294
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-03 18:53:21 +0900

    [feat] 웹팩 devServer 설정 - index.jsx로

--- webpack.config.js
+++ webpack.config.js
@@ -14,4 +14,11 @@ module.exports = {
   resolve: {
     extensions: ['.js', '.jsx'],
   },
+  devServer: {
+    // 포트 바꿔주기 가능
+    // 우리가 요청하는 것들을 다 여기로 잡아줄 수 있음
+    historyApiFallback: {
+      index: 'index.html',
+    },
+  },
 };
