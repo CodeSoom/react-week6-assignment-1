3d7a69aaf407810edb5f6dd044543b8f0e49075e
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-04 00:06:43 +0900

    [test] HomePage About 클릭시 이동 테스트 코드 추가

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,17 +1,28 @@
 import { MemoryRouter } from 'react-router-dom';
 
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
+  const renderComponent = (path) => render(
+    <MemoryRouter initialEntries={[path]}>
+      <HomePage />
+    </MemoryRouter>,
+  );
+
   it('renders home page', () => {
-    const { container } = render(
-      <MemoryRouter>
-        <HomePage />
-      </MemoryRouter>,
-    );
+    const { container } = renderComponent('/');
 
     expect(container).toHaveTextContent('Home');
+  });
+
+  it('clicks "About", move to /About', () => {
+    const { container, getByText } = renderComponent('/');
+
+    fireEvent.click(getByText('About'));
+
+    expect(container).not.toHaveTextContent('Home');
+    expect(container).toHaveTextContent('About');
   });
 });
