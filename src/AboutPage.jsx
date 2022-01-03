486e4fd30f5a76d4900c766750557f738e92f507
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-03 20:55:04 +0900

    [refactor] HomePage, AboutPage, RestaurantsPage 컴포넌트 분리

---
+++ src/AboutPage.jsx
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>About 페이지입니다.</p>
+    </>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,12 @@
 import RestaurantsPage from './RestaurantsPage';
+import HomePage from './HomePage';
+import AboutPage from './AboutPage';
+import NotFoundPage from './NotFoundPage';
 
 // 0. 지역, 분류 목록을 얻기
 // 1. 지역 선택 - Regions <- API (0)
 // 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
 // 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
-
-function HomePage() {
-  return (
-    <>
-      <h1>Home</h1>
-      <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/restaurants">Restaurants</a></li>
-      </ul>
-    </>
-  );
-}
-
-function AboutPage() {
-  return (
-    <>
-      <h1>About</h1>
-      <p>About 페이지입니다.</p>
-    </>
-  );
-}
-
-function NotFoundPage() {
-  return (
-    <>
-      <p>Not Found Page :P</p>
-    </>
-  );
-}
 
 export default function App() {
   const { location: { pathname } } = window;
