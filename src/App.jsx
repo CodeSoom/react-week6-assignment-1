0093e2e248d077adb3abd391ef85c6fdb47b4c10
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-03 19:07:50 +0900

    [refactor] App.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,4 @@
-import { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
-
 import RestaurantsPage from './RestaurantsPage';
-
-import {
-  loadInitialData,
-} from './actions';
 
 // 0. 지역, 분류 목록을 얻기
 // 1. 지역 선택 - Regions <- API (0)
@@ -14,15 +6,7 @@ import {
 // 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
-
   return (
-    <div>
-      <RestaurantsPage />
-    </div>
+    <RestaurantsPage />
   );
 }
