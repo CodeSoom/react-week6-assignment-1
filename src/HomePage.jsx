c12fec2f57ead8e6f52c1d0ad676e817c5a963cd
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-04 00:06:55 +0900

    [refactor] 이동 url 수정

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -8,7 +8,7 @@ export default function HomePage() {
       <Header />
       <h2>Home</h2>
       <ul>
-        <li><Link to="/About">About</Link></li>
+        <li><Link to="/about">About</Link></li>
         <li><Link to="/restaurants">Restaurants</Link></li>
       </ul>
     </>
