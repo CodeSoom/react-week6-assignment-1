6fb099cf62c87c03258c1a2e0fbbaa8bd4d0f87b
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-03 18:54:04 +0900

    [chore] devServer 주석 제거

--- webpack.config.js
+++ webpack.config.js
@@ -15,8 +15,6 @@ module.exports = {
     extensions: ['.js', '.jsx'],
   },
   devServer: {
-    // 포트 바꿔주기 가능
-    // 우리가 요청하는 것들을 다 여기로 잡아줄 수 있음
     historyApiFallback: {
       index: 'index.html',
     },
