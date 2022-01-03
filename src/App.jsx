f2643f560aad2f834ae22414795e2f6b61494c0f
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-04 03:07:32 +0900

    [refactor] App 라우팅으로 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
+import {
+  BrowserRouter,
+  Switch,
+  Route,
+} from 'react-router-dom';
+
 import RestaurantsPage from './RestaurantsPage';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
-import NotFoundPage from './NotFoundPage';
+// import NotFoundPage from './NotFoundPage';
 
 // 0. 지역, 분류 목록을 얻기
 // 1. 지역 선택 - Regions <- API (0)
@@ -9,15 +15,19 @@ import NotFoundPage from './NotFoundPage';
 // 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 export default function App() {
-  const { location: { pathname } } = window;
-
-  const MyComponent = {
-    '/': HomePage,
-    '/about': AboutPage,
-    '/restaurants': RestaurantsPage,
-  }[pathname] || NotFoundPage;
+  // const MyComponent = {
+  //   '/': HomePage,
+  //   '/about': AboutPage,
+  //   '/restaurants': RestaurantsPage,
+  // }[pathname] || NotFoundPage;
 
   return (
-    <MyComponent />
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
