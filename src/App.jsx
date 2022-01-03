f7f6856a7a63a449357023207c0d90cf65528315
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-04 08:27:16 +0900

    [feature] 헤더 추가, 제목 크기 변경

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import {
   Routes,
   Route,
+  Link,
 } from 'react-router-dom';
 
 import RestaurantsPage from './RestaurantsPage';
@@ -15,11 +16,18 @@ import NotFoundPage from './NotFoundPage';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/restaurants" element={<RestaurantsPage />} />
-      <Route path="/*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <header>
+        <h1>
+          <Link to="/">헤더</Link>
+        </h1>
+      </header>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
