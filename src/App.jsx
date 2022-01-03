b0df7bbcd7d1ae8c13990d4c734662f5e67682bd
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-03 20:45:24 +0900

    [feature] HomePage, AboutPage, NotFoundPage 컴포넌트 구현

--- src/App.jsx
+++ src/App.jsx
@@ -5,18 +5,45 @@ import RestaurantsPage from './RestaurantsPage';
 // 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
 // 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
+function HomePage() {
+  return (
+    <>
+      <h1>Home</h1>
+      <ul>
+        <li><a href="/about">About</a></li>
+        <li><a href="/restaurants">Restaurants</a></li>
+      </ul>
+    </>
+  );
+}
+
+function AboutPage() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>About 페이지입니다.</p>
+    </>
+  );
+}
+
+function NotFoundPage() {
+  return (
+    <>
+      <p>Not Found Page :P</p>
+    </>
+  );
+}
+
 export default function App() {
   const { location: { pathname } } = window;
 
-  if (pathname === '/') {
-    return (
-      <p>
-        Home
-      </p>
-    );
-  }
+  const MyComponent = {
+    '/': HomePage,
+    '/about': AboutPage,
+    '/restaurants': RestaurantsPage,
+  }[pathname] || NotFoundPage;
 
   return (
-    <RestaurantsPage />
+    <MyComponent />
   );
 }
