200119d6c9df7e1b0ecf09d3ec45e367d47830d0
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-04 21:44:32 +0900

    [feature] RestaurantsInfoPage 틀 잡기

---
+++ src/RestaurantsInfoPage.jsx
@@ -0,0 +1,12 @@
+export default function RestaurantsInfoPage() {
+  return (
+    <>
+      <h2>식당이름</h2>
+      <p>주소: </p>
+      <h3>메뉴</h3>
+      <ul>
+        <li>메뉴 리스트가 들어올 자리</li>
+      </ul>
+    </>
+  );
+}
