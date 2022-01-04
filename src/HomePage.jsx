3222644330d015296e8cd445f7aa844729283292
jeongseop.lee <sjl0614@naver.com>
2022-01-04 20:38:44 +0900

    HomPage path 대문자 소문자로 수정

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -5,8 +5,8 @@ export default function Homepage() {
     <div>
       <h2>Home</h2>
       <ul>
-        <li><Link to="/About">About</Link></li>
-        <li><Link to="/Restaurants">Restaurants</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/restaurants">Restaurants</Link></li>
       </ul>
     </div>
   );
