cb62beb075320f30e4baecf71fdd97c2a3a935b9
padawanR0k <padawanR0k@gmail.com>
2022-01-05 01:19:26 +0900

    404페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,15 @@
+import { Routes, Route } from 'react-router-dom';
+
+import NotFound from './page/NotFound';
 import Restaurants from './page/Restaurants';
 
 export default function App() {
   return (
     <div>
-      <Restaurants />
+      <Routes>
+        <Route path="/restaurant" element={Restaurants} />
+        <Route element={NotFound} />
+      </Routes>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,17 +1,16 @@
 import React from 'react';
-
 import { render } from '@testing-library/react';
-
 import { MemoryRouter } from 'react-router-dom';
+
 import App from './App';
 
 describe('App', () => {
   context('존재하지 않는 URL로 접근시 404페이지를 노출한다', () => {
-    const { container } = render((
+    const { getByText } = render((
       <MemoryRouter initialEntries={['/404']}>
         <App />
       </MemoryRouter>
     ));
-    expect(container).toHaveTextContent('404 Not Found');
+    expect(getByText(/404 Not Found/)).not.toBeNull();
   });
 });
