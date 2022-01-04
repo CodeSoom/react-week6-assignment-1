cf90d397e11181ca97651dc5b1d8c052a69b030b
Ravi <ravi@inflearn.com>
2022-01-05 00:12:53 +0900

    RestaurantDetailPage dispatch test

--- src/pages/RestaurantDetail/index.test.jsx
+++ src/pages/RestaurantDetail/index.test.jsx
@@ -1,9 +1,12 @@
 import { render } from '@testing-library/react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantDetailPage from '.';
+import { ACTION_TYPES } from '../../store/actions';
 
 describe('RestaurantDetailPage', () => {
+  const dispatch = jest.fn();
+
   useSelector.mockImplementation((selector) => selector({
     restaurant: {
       id: '1',
@@ -13,8 +16,12 @@ describe('RestaurantDetailPage', () => {
     },
   }));
 
-  it('renders restaurant\'s name, address and menu', () => {
+  useDispatch.mockImplementation(() => dispatch);
+
+  it('loads restaurant\'s name, address and menu', () => {
     const { getByText } = render(<RestaurantDetailPage />);
+
+    expect(dispatch).toBeCalled();
 
     expect(getByText('김밥천국')).toBeInTheDocument();
     expect(getByText('서울특별시 강남구 역삼동')).toBeInTheDocument();
