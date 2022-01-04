e41806d5428b6e18011c11e8f82743b5658e4a92
jeongseop.lee <sjl0614@naver.com>
2022-01-04 20:34:22 +0900

    App /restaurants router 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,8 +9,8 @@ import App from './App';
 describe('App', () => {
   beforeEach(() => {
     useSelector.mockImplementation((selector) => selector({
-      regions: [],
-      categories: [],
+      regions: [{ id: 1, name: '서울' }],
+      categories: [{ id: 1, name: '한식' }],
       restaurants: [],
     }));
   });
@@ -18,12 +18,25 @@ describe('App', () => {
   context('with path /', () => {
     it('renders the home page', () => {
       const { container } = render((
-        <MemoryRouter>
+        <MemoryRouter initialEntries={['/']}>
           <App />
         </MemoryRouter>
       ));
 
       expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders the restaurants page', () => {
+      const { container } = render((
+        <MemoryRouter initialEntries={['/restaurants']}>
+          <App />
+        </MemoryRouter>
+      ));
+
+      expect(container).toHaveTextContent('서울');
+      expect(container).toHaveTextContent('한식');
     });
   });
 });
