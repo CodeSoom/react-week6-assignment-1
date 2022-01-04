f76f5bb5921c22fa354842e152e20d72d2d88e92
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-04 22:11:05 +0900

    [refactor] actions loadRestaurt 인자 전달 수정

--- src/actions.js
+++ src/actions.js
@@ -78,7 +78,7 @@ export function loadRestaurants() {
 
 export function loadRestaurant({ restaurantId }) {
   return async (dispatch) => {
-    const restaurant = await fetchRestaurant(restaurantId);
+    const restaurant = await fetchRestaurant({ restaurantId });
 
     dispatch(setRestaurant(restaurant));
   };
