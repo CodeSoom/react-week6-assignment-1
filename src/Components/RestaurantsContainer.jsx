d8c9590a5a59075f714f055d8af3387826649c3f
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-04 21:45:01 +0900

    [구현] RestaurantContainer - 조건에 맞는 레스토랑 이름에 상세페이지 링크 연결

---
+++ src/Components/RestaurantsContainer.jsx
@@ -0,0 +1,20 @@
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+import { get } from '../utils';
+
+export default function RestaurantsContainer() {
+  const restaurants = useSelector(get('restaurants'));
+
+  return (
+    <ul>
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>
+          <Link to={`${restaurant.id}`}>
+            {restaurant.name}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
+}
