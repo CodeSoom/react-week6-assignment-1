cac222d10815d20eb6e0a9f10f095f6d09466e09
jeongseop.lee <sjl0614@naver.com>
2022-01-04 20:32:37 +0900

    HomePage 테스트 통과

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,7 +1,13 @@
+import { Link } from 'react-router-dom';
+
 export default function Homepage() {
   return (
     <div>
-      Home
+      <h2>Home</h2>
+      <ul>
+        <li><Link to="/About">About</Link></li>
+        <li><Link to="/Restaurants">Restaurants</Link></li>
+      </ul>
     </div>
   );
 }
