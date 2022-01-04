1fcba6efbcd7076e8cd9c99f61d995af51598365
jeongseop.lee <sjl0614@naver.com>
2022-01-04 19:53:26 +0900

    App Homepage router 테스트 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,20 +2,29 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
 
 import App from './App';
 
-test('App', () => {
-  const dispatch = jest.fn();
+describe('App', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+      categories: [],
+      restaurants: [],
+    }));
+  });
 
-  useDispatch.mockImplementation(() => dispatch);
-
-  useSelector.mockImplementation((selector) => selector({
-    regions: [],
-    categories: [],
-    restaurants: [],
-  }));
-
-  render((<App />));
+  context('with path /', () => {
+    it('renders the home page', () => {
+      const { container } = render((
+        <MemoryRouter>
+          <App />
+        </MemoryRouter>
+      ));
+      expect(container).toHaveTextContent('Home');
+    });
+  });
 });
