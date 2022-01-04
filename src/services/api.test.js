68521eade9166554703bec0a6d5fff22bae524db
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-04 21:49:27 +0900

    [테스트] api - fetchRestaurantInfo에서 fetch가 잘 작동하는지 점검

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,8 +1,33 @@
-import { fetchRegions, fetchCategories, fetchRestaurants } from './api';
+import {
+  fetchRegions, fetchCategories, fetchRestaurants, fetchRestaurantInfo,
+} from './api';
 
 import REGIONS from '../../fixtures/regions';
 import CATEGORIES from '../../fixtures/categories';
 import RESTAURANTS from '../../fixtures/restaurants';
+
+const RESTAURANTINFO = {
+  id: 1,
+  categoryId: 1,
+  name: '양천주가',
+  address: '서울 강남구 123456',
+  menuItems: [
+    {
+      id: 1,
+      restaurantId: 1,
+      name: '비빔밥',
+    },
+  ],
+  reviews: [
+    {
+      id: 1,
+      restaurantId: 1,
+      name: '테스터',
+      score: 5,
+      description: '훌륭하다 훌륭하다 지구인놈들',
+    },
+  ],
+};
 
 describe('api', () => {
   const mockFetch = (data) => {
@@ -47,6 +72,18 @@ describe('api', () => {
       });
 
       expect(restaurants).toEqual(RESTAURANTS);
+    });
+  });
+
+  describe('fetchRestaurantInfo', () => {
+    beforeEach(() => {
+      mockFetch(RESTAURANTINFO);
+    });
+    it('returns restaurantInfo', async () => {
+      const restaurantInfo = await fetchRestaurantInfo({
+        restaurantId: 1,
+      });
+      expect(restaurantInfo).toEqual(RESTAURANTINFO);
     });
   });
 });
