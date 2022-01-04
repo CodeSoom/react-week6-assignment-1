d5703ab9996306c0dcb9b0f19f3b78839994ede2
jeongseop.lee <sjl0614@naver.com>
2022-01-04 19:45:56 +0900

    RestaurantsPage생성

---
+++ src/RestaurantsPage.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+import { render } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import RestaurantsPage from './RestaurantsPage';
+
+test('RestaurantsPage', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+    categories: [
+      { id: 1, name: '한식' },
+    ],
+    restaurants: [
+      { id: 1, name: '마법사주방' },
+    ],
+  }));
+
+  const { container } = render((
+    <RestaurantsPage />
+  ));
+
+  expect(dispatch).toBeCalled();
+
+  expect(container).toHaveTextContent('서울');
+  expect(container).toHaveTextContent('한식');
+});
