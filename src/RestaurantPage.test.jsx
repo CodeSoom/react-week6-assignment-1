140a1723188fddf93bd39a5c664f5a6605f341d4
KIMSEUNGGYU <dev.seunggyu@gmail.com>
2022-01-04 22:10:24 +0900

    [test] Restaurant Page 수정

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -7,24 +7,22 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantPage from './RestaurantPage';
 
-import RESTAURANT from '../fixtures/restaurant';
-
 test('RestaurantPage', () => {
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
-    resturant: RESTAURANT,
+    resturant: null,
   }));
 
-  const { } = render((
-    <MemoryRouter>
+  render((
+    <MemoryRouter initialEntries={['/restaurants/1']}>
       <RestaurantPage />
     </MemoryRouter>
   ));
 
   expect(dispatch).toBeCalled();
 
-  // TODO: restaurant id 값 제대로 요청하는지 테스트
+  // TODO: restaurant id 값 제대로 요청하는지 테스트 필요한가?
 });
