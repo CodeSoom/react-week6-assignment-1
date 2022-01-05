588fa3d0a7ab42ff7fdb62bc31fcb8e3471d4b00
padawanR0k <padawanR0k@gmail.com>
2022-01-06 00:17:57 +0900

    About 페이지 테스트케이스 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -15,4 +15,15 @@ describe('App', () => {
       expect(getByText(/404 Not Found/)).not.toBeNull();
     });
   });
+
+  context('/about로 접근시', () => {
+    it('About페이지를 노출한다', () => {
+      const { getByText } = render((
+        <MemoryRouter initialEntries={['/about']}>
+          <App />
+        </MemoryRouter>
+      ));
+      expect(getByText(/About 페이지입니다/)).not.toBeNull();
+    });
+  });
 });
