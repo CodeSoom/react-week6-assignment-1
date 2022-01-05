f3f056d6b46f8c18d2f4e0d8a296267066a69086
padawanR0k <padawanR0k@gmail.com>
2022-01-06 00:18:27 +0900

    About 페이지 구현

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import NotFound from './page/NotFound';
 import Restaurants from './page/Restaurants';
+import About from './page/About';
 
 export default function App() {
   return (
     <div>
       <Routes>
         <Route path="/restaurant" element={<Restaurants />} />
+        <Route path="/about" element={<About />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
