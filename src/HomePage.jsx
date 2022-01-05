1c016ec3e079748ffce974c1dd83ea19faebfccf
CheolWoong Choi <bluesky4381@naver.com>
2022-01-06 00:23:16 +0900

    페이지 헤더 추가

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -3,11 +3,11 @@ import { Link } from 'react-router-dom';
 export default function HomePage() {
   return (
     <div>
-      <h1>Home</h1>
-      <Link to="about">
+      <h2>Home</h2>
+      <Link to="/about">
         <li>About</li>
       </Link>
-      <Link to="restaurants">
+      <Link to="/restaurants">
         <li>Restaurants</li>
       </Link>
     </div>
--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,9 @@
 import ReactDOM from 'react-dom';
 
 import { Provider } from 'react-redux';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {
+  BrowserRouter, Routes, Route, Link,
+} from 'react-router-dom';
 
 import HomePage from './HomePage';
 
@@ -15,6 +17,9 @@ ReactDOM.render(
   (
     <Provider store={store}>
       <BrowserRouter>
+        <Link to="/">
+          <h1>헤더</h1>
+        </Link>
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="about" element={<AboutPage />} />
