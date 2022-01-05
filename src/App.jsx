7819d8a59e9b40cec19f2485230d5017819a886f
Ravi <ravi@inflearn.com>
2022-01-05 23:48:57 +0900

    HomePage route

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from 'react-router-dom';
+
+import HomePage from './pages/HomePage';
+
 export default function App() {
   return (
     <div>
-      <main />
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,9 +1,26 @@
 import { MemoryRouter } from 'react-router-dom';
 import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
 describe('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+    categories: [
+      { id: 1, name: '한식' },
+    ],
+    restaurants: [
+      { id: 1, name: '마법사주방' },
+    ],
+  }));
+
   const renderComponent = ({ path }) => render(
     <MemoryRouter initialEntries={[path]}>
       <App />
