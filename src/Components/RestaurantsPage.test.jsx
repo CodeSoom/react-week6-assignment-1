bb892a6f6c44d9392751b3ef98d0efc46f93d549
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-05 19:12:44 +0900

    [테스트] RestaurantsPage - 전체 render 확인 Region과 Category를 선택했을 경우와 안선택했을 경우를 나누었다.

--- src/Components/RestaurantsPage.test.jsx
+++ src/Components/RestaurantsPage.test.jsx
@@ -2,9 +2,10 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import { MemoryRouter } from 'react-router-dom';
+
 import RestaurantsPage from './RestaurantsPage';
 
 import REGIONS from '../../fixtures/regions';
@@ -12,28 +13,64 @@ import CATEGORIES from '../../fixtures/categories';
 
 jest.mock('react-redux');
 describe('RestaurantsPage', () => {
-  const dispatch = jest.fn();
-
   const SEOUL = REGIONS[0];
   const KOREAN_FOOD = CATEGORIES[0];
 
-  useDispatch.mockImplementation(() => dispatch);
+  const renderRestaurantPage = () => render((
+    <MemoryRouter>
+      <RestaurantsPage />
+    </MemoryRouter>
+  ));
 
-  it('renders RegionsContainer/CategoriesContainer/RestaurantsContainer', () => {
-    useSelector.mockImplementation((selector) => selector({
-      regions: REGIONS,
-      selectedRegion: SEOUL,
-      categories: CATEGORIES,
-      selectedCategory: KOREAN_FOOD,
-      restaurants: [
-        { id: 1, name: '마법사주방' },
-      ],
-    }));
+  context('selected a Region and a Category', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        regions: REGIONS,
+        selectedRegion: SEOUL,
+        categories: CATEGORIES,
+        selectedCategory: KOREAN_FOOD,
+        restaurants: [
+          { id: 1, name: '마법사주방' },
+        ],
+      }));
+    });
 
-    render((
-      <MemoryRouter>
-        <RestaurantsPage />
-      </MemoryRouter>
-    ));
+    it('renders RestaurantsPage', () => {
+      const { getByText } = renderRestaurantPage();
+
+      expect(getByText(`${SEOUL.name}(V)`)).not.toBeNull();
+      expect(getByText(`${KOREAN_FOOD.name}(V)`)).not.toBeNull();
+      REGIONS.slice(1).forEach((REGION) => {
+        expect(getByText(REGION.name)).not.toBeNull();
+      });
+      CATEGORIES.slice(1).forEach((CATEGORY) => {
+        expect(getByText(CATEGORY.name)).not.toBeNull();
+      });
+      expect(getByText('마법사주방')).not.toBeNull();
+    });
+  });
+
+  context('selected nothing', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        regions: REGIONS,
+        categories: CATEGORIES,
+        restaurants: [
+          { id: 1, name: '마법사주방' },
+        ],
+      }));
+    });
+
+    it('renders RestaurantsPage', () => {
+      const { getByText } = renderRestaurantPage();
+
+      REGIONS.forEach((REGION) => {
+        expect(getByText(REGION.name)).not.toBeNull();
+      });
+      CATEGORIES.forEach((CATEGORY) => {
+        expect(getByText(CATEGORY.name)).not.toBeNull();
+      });
+      expect(getByText('마법사주방')).not.toBeNull();
+    });
   });
 });
