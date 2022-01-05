72cd3e4b1884cb6939254afe39698bd4ef26bd74
CheolWoong Choi <bluesky4381@naver.com>
2022-01-06 00:47:40 +0900

    경로 import 오류 수정

--- src/pages/RestaurantsPage.jsx
+++ src/pages/RestaurantsPage.jsx
@@ -4,7 +4,7 @@ import { useDispatch } from 'react-redux';
 
 import RegionsContainer from '../containers/RegionsContainer';
 import CategoriesContainer from '../containers/CategoriesContainer';
-import RestaurantsContainer from '../RestaurantsContainer';
+import RestaurantsContainer from '../containers/RestaurantsContainer';
 
 import {
   loadInitialData,
