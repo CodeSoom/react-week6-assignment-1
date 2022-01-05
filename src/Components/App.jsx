46e10ce9b181b0c9a7908b397f404c5807209472
jeongseop.lee <sjl0614@naver.com>
2022-01-05 20:42:09 +0900

    AboutPage 생성, 테스트 통과

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -4,6 +4,7 @@ import {
 } from 'react-router-dom';
 
 import HomePage from './Pages/HomePage';
+import AboutPage from './Pages/AboutPage';
 import RestaurantsPage from './Pages/RestaurantsPage';
 
 export default function App() {
@@ -12,6 +13,7 @@ export default function App() {
       <h1>헤더</h1>
       <Routes>
         <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
         <Route path="/restaurants" element={<RestaurantsPage />} />
       </Routes>
     </>
