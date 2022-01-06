ed691afba14f83300a957db837697cf660b1db35
CheolWoong Choi <bluesky4381@naver.com>
2022-01-06 23:04:28 +0900

    레스토랑 상세페이지 간단 구현

---
+++ src/pages/RestaurantDetailPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function RestaurantDetailPage() {
+  return (
+    <div>
+      DetailPage
+    </div>
+  );
+}
---
+++ src/pages/RestaurantDetailPage.test.jsx
@@ -0,0 +1,5 @@
+describe('RestaurantDetailPage', () => {
+  it('', () => {
+
+  });
+});
