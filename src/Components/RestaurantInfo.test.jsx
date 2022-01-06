6b986965ebaf1b7ba93dda8636e3ac8d77e2f486
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-06 11:56:38 +0900

    [테스트] RestaurantInfo 데이터 관리하는 Container와 분리

--- src/Components/RestaurantInfo.test.jsx
+++ src/Components/RestaurantInfo.test.jsx
@@ -1,30 +1,17 @@
 import { render } from '@testing-library/react';
-import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantInfo from './RestaurantInfo';
 
-jest.mock('react-redux');
-
 test('RestaurantInfo', () => {
-  const dispatch = jest.fn();
-  useDispatch.mockImplementation(() => dispatch);
-  useSelector.mockImplementation((selector) => selector({
-    restaurantInfo: {
+  const { getByText } = render(<RestaurantInfo
+    name="양천주가"
+    address="서울 강남구 123456"
+    menuItems={[{
       id: 1,
-      categoryId: 1,
-      name: '양천주가',
-      address: '서울 강남구 123456',
-      menuItems: [
-        {
-          id: 1,
-          restaurantId: 1,
-          name: '비빔밥',
-        },
-      ],
-    },
-  }));
-
-  const { getByText } = render(<RestaurantInfo />);
+      restaurantId: 1,
+      name: '비빔밥',
+    }]}
+  />);
 
   expect(getByText('양천주가')).not.toBeNull();
   expect(getByText('비빔밥')).not.toBeNull();
