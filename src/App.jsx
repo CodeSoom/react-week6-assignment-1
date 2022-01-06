e13631ef4d10779e5e47e062861b187d3c2bf34a
Superduper-India <sunyoung1542@gmail.com>
2022-01-06 16:48:23 +0900

    Update: App/ DetailPage로 연결되는 /detail 경로 구현

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
 import NotFoundPage from './NotFoundPage';
 import AboutPage from './AboutPage';
+import DetailPage from './DetailPage';
 
 export default function App() {
   return (
@@ -19,6 +20,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/detail" element={<DetailPage />} />
         <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </div>
