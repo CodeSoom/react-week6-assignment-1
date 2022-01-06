848c603a70e6f18ef367d87dd04542e267a6ae63
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-06 11:57:30 +0900

    [구현] RestaurantInfo 데이터 관리하는 Container와 분리

--- src/Components/RestaurantInfo.jsx
+++ src/Components/RestaurantInfo.jsx
@@ -1,27 +1,7 @@
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
-import { loadRestaurantInfo } from '../actions';
-
-export default function RestaurantInfo() {
-  const { restaurantId } = useParams();
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadRestaurantInfo(restaurantId));
-  }, [restaurantId]);
-
-  const {
-    restaurantName, address, menuItems,
-  } = useSelector((state) => ({
-    restaurantName: state.restaurantInfo.name,
-    address: state.restaurantInfo.address,
-    menuItems: state.restaurantInfo.menuItems,
-  }));
-
+export default function RestaurantInfo({ restaurantName, address, menuItems }) {
   return (
     <>
-      <h1>{restaurantName}</h1>
+      <h2>{restaurantName}</h2>
       <div>
         주소:
         {address}
