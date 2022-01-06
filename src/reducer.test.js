cbc35e85f11c38fd64eead1b5780891683213108
CheolWoong Choi <bluesky4381@naver.com>
2022-01-07 00:01:49 +0900

    setRestaurant 액션 테스트 코드 작성 & 구현

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -4,6 +4,7 @@ import {
   setRegions,
   setCategories,
   setRestaurants,
+  setRestaurant,
   selectRegion,
   selectCategory,
 } from './actions';
@@ -14,6 +15,7 @@ describe('reducer', () => {
       regions: [],
       categories: [],
       restaurants: [],
+      restaurant: {},
       selectedRegion: null,
       selectedCategory: null,
     };
@@ -70,6 +72,44 @@ describe('reducer', () => {
       const state = reducer(initialState, setRestaurants(restaurants));
 
       expect(state.restaurants).toHaveLength(1);
+    });
+  });
+
+  describe('setRestaurant', () => {
+    it('changes restaurant', () => {
+      const initialState = {
+        restaurant: {},
+      };
+
+      const restaurant = {
+        id: 1,
+        categoryId: 1,
+        name: '양천주가',
+        address: '서울 강남구 123456',
+        menuItems: [
+          {
+            id: 1,
+            restaurantId: 1,
+            name: '비빔밥',
+          },
+        ],
+        reviews: [
+          {
+            id: 1,
+            restaurantId: 1,
+            name: '테스터',
+            score: 5,
+            description: '훌륭하다 훌륭하다 지구인놈들',
+          },
+        ],
+        information: '양천주가 in 서울 강남구 123456',
+      };
+
+      const state = reducer(initialState, setRestaurant(restaurant));
+
+      expect(state.restaurant.id).toBe(1);
+      expect(state.restaurant.address).toBe('서울 강남구 123456');
+      expect(state.restaurant.name).toBe('양천주가');
     });
   });
 
