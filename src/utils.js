865544bf9c36d7ad6f21869f94450909fde13707
CheolWoong Choi <bluesky4381@naver.com>
2022-01-07 00:18:32 +0900

    빈 객체인지 확인하는 유틸함수 추가

--- src/utils.js
+++ src/utils.js
@@ -5,3 +5,7 @@ export function get(key) {
 export function equal(key, value) {
   return (obj) => obj[key] === value;
 }
+
+export function isEmptyObj(obj) {
+  return Object.values(obj).length === 0;
+}
