c35d5aa501dd18301716a3613d3a75b8c362de29
jeongseop.lee <sjl0614@naver.com>
2022-01-06 21:21:23 +0900

    RestaurantDetailPage loading테스트 작성

--- src/Components/Pages/RestaurantDetailPage.test.jsx
+++ src/Components/Pages/RestaurantDetailPage.test.jsx
@@ -8,16 +8,33 @@ import RestaurantDetailPage from './RestaurantDetailPage';
 
 import DETAIL from '../../../fixtures/restaurantDetail';
 
-test('DetailPage', () => {
-  useSelector.mockImplementation((selector) => selector({
-    detail: DETAIL,
-  }));
-  const { container } = render((
+describe('DetailPage', () => {
+  beforeEach(() => {
+    useSelector.mockImplementation((selector) => selector({
+      detail: DETAIL,
+    }));
+  });
+
+  const detailRender = () => render((
     <MemoryRouter>
       <RestaurantDetailPage />
     </MemoryRouter>
   ));
 
-  expect(container).toHaveTextContent(DETAIL.name);
-  expect(container).toHaveTextContent(DETAIL.address);
+  context('with restaurant detail', () => {
+    it('render detail page', () => {
+      const { container } = detailRender();
+
+      expect(container).toHaveTextContent(DETAIL.name);
+      expect(container).toHaveTextContent(DETAIL.address);
+    });
+  });
+
+  context('without restaurant detail', () => {
+    it('render loading', () => {
+      const { container } = detailRender();
+
+      expect(container).toHaveTextContent('Loading...');
+    });
+  });
 });
