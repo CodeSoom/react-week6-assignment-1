ef25e2696db3faf8ba88428ec317d2e17d32323d
leeok <milkteatea0628@gmail.com>
2022-01-07 00:17:42 +0900

    레스토랑 페이지에서 컴포넌트 추출

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -7,6 +7,8 @@ import {
   loadRestaurant,
 } from './actions';
 
+import RestaurantContainer from './RestaurantContainer'
+
 import { get } from './utils';
 
 export default function RestaurantPage() {
@@ -14,23 +16,14 @@ export default function RestaurantPage() {
   const restaurant = useSelector(get('restaurant'));
   
   const { restaurantId } = useParams();
-  
+
   useEffect(() => {  
     dispatch(loadRestaurant(restaurantId));
   }, []);
-  console.log(restaurant)
-  // api에서 레스토랑 정보를 받아와야 한다.
-  // 그러기 위해 레스토랑 아이디가 필요하다.
-  // 처음 restaurant가 null 값이 뜸. 비동기 문제인듯.
+
   return (
-    <div>
-      <h2>가게이름</h2>
-      <p>주소: </p>
-      <h3>메뉴</h3>
-      <ul>
-        <li>메뉴 1번</li>
-        <li>메뉴 2번</li>
-      </ul>
-    </div>
+    <>
+      <RestaurantContainer data={restaurant} />
+    </>
   );
 }
