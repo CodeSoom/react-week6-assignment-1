dcfed6667a0beffd08be9e68c4faa1bf3e4d85d9
CheolWoong Choi <bluesky4381@naver.com>
2022-01-07 00:23:49 +0900

    레스토랑 상세페이지 테스트 코드 작성 & 구현

--- src/pages/RestaurantPage.jsx
+++ src/pages/RestaurantPage.jsx
@@ -1,23 +1,46 @@
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import { useParams } from 'react-router-dom';
+
 import { loadRestaurant } from '../actions';
+import { isEmptyObj } from '../utils';
 
 export default function RestaurantPage() {
   const dispatch = useDispatch();
   const params = useParams();
 
+  const { restaurant } = useSelector((state) => state);
+
   useEffect(() => {
     dispatch(loadRestaurant(params.id));
   }, []);
 
+  if (isEmptyObj(restaurant)) {
+    return (
+      <div>
+        Loading...
+      </div>
+    );
+  }
+
+  const { name, address, menuItems } = restaurant;
+
   return (
     <div>
-      <h2>양천주가</h2>
-      <p>주소: 서울 강남구</p>
-      <h3>메뉴</h3>
-      <li>1</li>
-      <li>1</li>
+      <h2>{name}</h2>
+      <p>
+        주소:
+        {' '}
+        {address}
+      </p>
+      <div>
+        <h3>메뉴</h3>
+        <ul>
+          {menuItems.map((menu) => (
+            <li key={menu.id}>{menu.name}</li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
--- src/pages/RestaurantPage.test.jsx
+++ src/pages/RestaurantPage.test.jsx
@@ -2,6 +2,7 @@ import { render } from '@testing-library/react';
 import { useSelector, useDispatch } from 'react-redux';
 
 import RestaurantPage from './RestaurantPage';
+import RESTAURANT from '../../fixtures/restaurant';
 
 describe('RestaurantPage', () => {
   const dispatch = jest.fn();
@@ -9,14 +10,32 @@ describe('RestaurantPage', () => {
 
   beforeEach(() => {
     useDispatch.mockImplementation(() => dispatch);
-    useSelector.mockImplementation((selector) => {
-
-    });
+    useSelector.mockImplementation((selector) => selector({
+      restaurant: {},
+    }));
   });
 
   it('레스토랑 상세 정보 thunk를 디스패치한다.', () => {
     renderRestaurantPage();
 
     expect(dispatch).toBeCalled();
+  });
+
+  it('로딩 텍스트가 처음에 화면에 나타난다.', () => {
+    const { getByText } = renderRestaurantPage();
+
+    expect(getByText('Loading...')).not.toBeNull();
+  });
+
+  it('레스토랑 정보가 있으면, 화면에 나타난다.', () => {
+    useSelector.mockImplementation((selector) => selector({
+      restaurant: RESTAURANT,
+    }));
+
+    const { getByText } = renderRestaurantPage();
+
+    expect(getByText(RESTAURANT.name)).not.toBeNull();
+    expect(getByText(RESTAURANT.menuItems[0].name)).not.toBeNull();
+    expect(getByText(RESTAURANT.menuItems[1].name)).not.toBeNull();
   });
 });
