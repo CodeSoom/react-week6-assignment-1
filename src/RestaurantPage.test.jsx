cb4e787ed978dcc8299c80e13d3af7055e09275d
leeok <milkteatea0628@gmail.com>
2022-01-07 00:17:09 +0900

    레스토랑페이지 컴포넌트 테스트 useSelect 추가

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import { render } from '@testing-library/react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantPage from './RestaurantPage';
 
@@ -11,12 +11,15 @@ describe('RestaurantPage', () => {
   beforeEach(() => {
     dispatch.mockClear();
     useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      restaurant: { id: 1, name: '양천주가', menuItems: [ { id: 11, name: '메뉴 1번', }, ], },
+    }));
   });
 
   it('set restaurant information', () => {
     const { container } = render(<RestaurantPage />);
 
-    expect(container).toHaveTextContent('레스토랑 이름');
+    expect(container).toHaveTextContent('양천주가');
     expect(container).toHaveTextContent('메뉴 1번');
   });
 });
