7f70dfa47b866d652b0af277d41e43f8aaa9636a
Ravi <ravi@inflearn.com>
2022-01-06 23:29:16 +0900

    HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,7 +1,21 @@
+import { Link } from 'react-router-dom';
+
+const pages = [
+  { name: 'About', path: '/about' },
+  { name: '레스토랑 리스트', path: '/restaurants' },
+];
+
 export default function HomePage() {
   return (
     <section>
       <h1>홈페이지</h1>
+      <ul>
+        {pages.map(({ name, path }) => (
+          <li key={path}>
+            <Link to={path}>{name}</Link>
+          </li>
+        ))}
+      </ul>
     </section>
   );
 }
--- src/pages/HomePage/index.test.jsx
+++ src/pages/HomePage/index.test.jsx
@@ -1,10 +1,15 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import HomePage from '.';
 
 describe('HomePage', () => {
   it('renders title and links', () => {
-    const { getByText } = render(<HomePage />);
+    const { getByText } = render((
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    ));
     expect(getByText('홈페이지')).toBeInTheDocument();
     expect(getByText('About')).toBeInTheDocument();
     expect(getByText('레스토랑 리스트')).toBeInTheDocument();
