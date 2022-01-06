662fe4f73fa4313ce56e1b0cab99b7ea3aaaa161
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-06 12:31:22 +0900

    [chore] NotFoundPage.test.jsx에서 , 제거

--- src/Pages/NotFoundPage.test.jsx
+++ src/Pages/NotFoundPage.test.jsx
@@ -3,9 +3,9 @@ import { render } from '@testing-library/react';
 import NotFoundPage from './NotFoundPage';
 
 test('renders NotFoundPage', () => {
-  const { queryByText } = render(
-    <NotFoundPage />,
-  );
+  const { queryByText } = render((
+    <NotFoundPage />
+  ));
 
   expect(queryByText('404 Not Found')).not.toBeNull();
 });
