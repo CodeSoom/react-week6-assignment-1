ca733c9f5d10b54fa0064ef097151d6ef21b0e32
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-06 20:00:41 +0900

    [refactor] RestaurantsPage 프레그먼트로 수정

--- src/RestaurantsPage.jsx
+++ src/RestaurantsPage.jsx
@@ -18,10 +18,10 @@ export default function RestaurantsPage() {
   });
 
   return (
-    <div>
+    <>
       <RegionsContainer />
       <CategoriesContainer />
       <RestaurantsContainer />
-    </div>
+    </>
   );
 }
