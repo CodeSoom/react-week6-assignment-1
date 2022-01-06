22fb04383e7587014f96673ec8e49293fadf1d1e
Superduper-India <sunyoung1542@gmail.com>
2022-01-06 23:24:16 +0900

    Refactoring: DetailPage/ useParams로 '/detail/:id' 의 id값 받아옴

--- src/DetailPage.jsx
+++ src/DetailPage.jsx
@@ -1,5 +1,22 @@
+import { useParams } from 'react-router-dom';
+
+import { useSelector } from 'react-redux';
+
+import { get } from './utils';
+
 export default function DetailPage() {
+  const { id } = useParams();
+  console.log(id);
+
+  const restaurants = useSelector(get('restaurants'));
+
   return (
-    <p>마녀식당</p>
+    <ul>
+      {restaurants.map((restaurant) => (
+        <li key={restaurant.id}>
+          {restaurant.name}
+        </li>
+      ))}
+    </ul>
   );
 }
--- src/DetailPage.test.jsx
+++ src/DetailPage.test.jsx
@@ -2,6 +2,8 @@ import { render } from '@testing-library/react';
 
 import { useSelector } from 'react-redux';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import DetailPage from './DetailPage';
 
 describe('DetailPage', () => {
@@ -11,9 +13,19 @@ describe('DetailPage', () => {
     ],
   }));
 
-  it('renders contents of restaurants', () => {
-    const { container } = render((<DetailPage />));
+  const renderDetailPage = ({ path }) => render((
+    <MemoryRouter initialEntries={[path]}>
+      <DetailPage />
+    </MemoryRouter>
+  ));
 
-    expect(container).toHaveTextContent('마녀식당');
+  context('with id of restaurant', () => {
+    it('renders name of restaurant', () => {
+      const { container } = renderDetailPage(
+        { path: '/detail/1' },
+      );
+
+      expect(container).toHaveTextContent('마녀식당');
+    });
   });
 });
