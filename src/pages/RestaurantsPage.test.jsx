8e319704aec77b233a18e7bc943fe26b1b4de10e
CheolWoong Choi <bluesky4381@naver.com>
2022-01-06 23:04:14 +0900

    useHref 오류로 인한 레스토랑 페이지 테스트 코드 수정 & 구현

--- src/pages/RestaurantsPage.test.jsx
+++ src/pages/RestaurantsPage.test.jsx
@@ -1,34 +1,44 @@
-import React from 'react';
-
 import { render } from '@testing-library/react';
 
+import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 
 import RestaurantsPage from './RestaurantsPage';
 
-test('RestaurantsPage', () => {
+describe('RestaurantsPage', () => {
   const dispatch = jest.fn();
-
-  useDispatch.mockImplementation(() => dispatch);
-
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
-
-  const { queryByText } = render((
-    <RestaurantsPage />
+  const renderRestaurantsPage = () => render((
+    <MemoryRouter initialEntries={['/restaurants']}>
+      <RestaurantsPage />
+    </MemoryRouter>
   ));
 
-  expect(dispatch).toBeCalled();
+  beforeEach(() => {
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      regions: [
+        { id: 1, name: '서울' },
+      ],
+      categories: [
+        { id: 1, name: '한식' },
+      ],
+      restaurants: [
+        { id: 1, name: '마법사주방' },
+      ],
+    }));
+  });
 
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+  it('액션이 디스패치된다.', () => {
+    renderRestaurantsPage();
+
+    expect(dispatch).toBeCalled();
+  });
+
+  it('식당 정보를 화면에 보여준다.', () => {
+    const { queryByText } = renderRestaurantsPage();
+
+    expect(queryByText('서울')).not.toBeNull();
+    expect(queryByText('한식')).not.toBeNull();
+  });
 });
