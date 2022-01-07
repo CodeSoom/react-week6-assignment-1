ebfe09dd9063870c9a7cd0d02ff8a80bea36f616
padawanR0k <padawanR0k@gmail.com>
2022-01-08 00:15:07 +0900

    /restaurant URL 테스트 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
+import { useSelector, useDispatch } from 'react-redux';
 
 import App from './App';
 
 describe('App', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
   context('존재하지 않는 URL로 접근시', () => {
     it('404페이지를 노출한다', () => {
       const { getByText } = render((
@@ -24,6 +28,22 @@ describe('App', () => {
         </MemoryRouter>
       ));
       expect(getByText(/About 페이지입니다/)).not.toBeNull();
+    });
+  });
+
+  context('/restaurants로 접근시', () => {
+    useSelector.mockImplementation((selector) => selector({
+      regions: [],
+      categories: [],
+      restaurants: [],
+    }));
+    it('Restaurants페이지를 노출한다', () => {
+      const { getByText } = render((
+        <MemoryRouter initialEntries={['/restaurants']}>
+          <App />
+        </MemoryRouter>
+      ));
+      expect(getByText(/Restaurants/)).not.toBeNull();
     });
   });
 });
