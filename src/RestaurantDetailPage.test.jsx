3e975c3f439428626ee3dee47bc665597e241127
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2022-01-07 21:36:11 +0900

    RestaurantDetailPage 테스트 추가

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -1,8 +1,20 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import RestaurantDetailPage from './RestaurantDetailPage';
 
 test('RestaurantDetailPage', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurant: {
+      name: '냠냠떡볶이',
+      menuItems: [
+        { id: 1, name: '떡볶이' },
+        { id: 2, name: '순대 ' },
+      ],
+    },
+  }));
+
   const { getByText } = render(<RestaurantDetailPage />);
 
   expect(getByText(/냠냠떡볶이/)).not.toBeNull();
