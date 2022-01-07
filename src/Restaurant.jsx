608e6534b68f9d964ad6f6c3a86e70a617dd6c84
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2022-01-07 21:54:32 +0900

    Restaurant 테스트 통과하도록 구현

--- src/Restaurant.jsx
+++ src/Restaurant.jsx
@@ -1,11 +1,21 @@
+import { useSelector } from 'react-redux';
+
 export default function Restaurant() {
+  const restaurant = useSelector((state) => state.restaurant);
+
+  const { name, address, menuItems } = restaurant;
+
   return (
     <div>
-      <h2>냠냠떡볶이</h2>
-      <p>주소</p>
+      <h2>{name}</h2>
+      <p>{`주소: ${address}`}</p>
       <h3>메뉴</h3>
       <ul>
-        <li>떡볶이</li>
+        {menuItems.map((item) => (
+          <li key={item.id}>
+            {item.name}
+          </li>
+        ))}
       </ul>
     </div>
   );
--- src/Restaurant.test.jsx
+++ src/Restaurant.test.jsx
@@ -8,6 +8,7 @@ test('Restaurant', () => {
   useSelector.mockImplementation((selector) => selector({
     restaurant: {
       name: '냠냠떡볶이',
+      address: '경기도 화성시 동탄',
       menuItems: [
         { id: 1, name: '떡볶이' },
         { id: 2, name: '순대 ' },
