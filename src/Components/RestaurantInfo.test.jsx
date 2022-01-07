fc19305f1f5694eccba2bec44cf53d216f7dd9c1
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-07 20:09:50 +0900

    [chore] ,제거

--- src/Components/RestaurantInfo.test.jsx
+++ src/Components/RestaurantInfo.test.jsx
@@ -2,16 +2,17 @@ import { render } from '@testing-library/react';
 
 import RestaurantInfo from './RestaurantInfo';
 
-test('RestaurantInfo', () => {
-  const { getByText } = render(<RestaurantInfo
-    restaurantName="양천주가"
-    address="서울 강남구 123456"
-    menuItems={[{
-      id: 1,
-      restaurantId: 1,
-      name: '비빔밥',
-    }]}
-  />);
+test('renders RestaurantInfo', () => {
+  const { getByText } = render((
+    <RestaurantInfo
+      restaurantName="양천주가"
+      address="서울 강남구 123456"
+      menuItems={[{
+        id: 1,
+        restaurantId: 1,
+        name: '비빔밥',
+      }]}
+    />));
 
   expect(getByText('양천주가')).not.toBeNull();
   expect(getByText('비빔밥')).not.toBeNull();
--- src/Pages/AboutPage.test.jsx
+++ src/Pages/AboutPage.test.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react';
 import AboutPage from './AboutPage';
 
 test('renders About', () => {
-  const { queryByText } = render(<AboutPage />);
+  const { queryByText } = render((<AboutPage />));
 
   expect(queryByText('About')).not.toBeNull();
   expect(queryByText('About 페이지 입니다.')).not.toBeNull();
