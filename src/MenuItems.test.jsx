d788cd3116848cde9d0f49ea2b4a5483c981e208
Lerry <lerry@Lerryui-MacBookPro.local>
2022-01-08 13:20:56 +0900

    [feature] MenuItems 테스트 추가

---
+++ src/MenuItems.test.jsx
@@ -0,0 +1,31 @@
+import { render } from '@testing-library/react';
+
+import MenuItems from './MenuItems';
+
+describe('MenuItems', () => {
+  context('메뉴 아이템이 있을 때', () => {
+    it('메뉴를 렌더링한다.', () => {
+      const menuItems = [
+        { id: 1, name: '공기밥' },
+      ];
+
+      const { container } = render((
+        <MenuItems menuItems={menuItems} />
+      ));
+
+      expect(container).toHaveTextContainer('공기밥');
+    });
+  });
+
+  context('메뉴 아이템이 없을 때', () => {
+    it('"메뉴가 없어요!"를 렌더링 한다.', () => {
+      const menuItems = [];
+
+      const { container } = render((
+        <MenuItems menuItems={menuItems} />
+      ));
+
+      expect(container).toHaveTextContainer('메뉴가 없어요!');
+    });
+  });
+});
