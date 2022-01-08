84c680d459eb04173b888d67098e6d267949f8db
GUAJEGUICHAN <qkr135qkr@gmail.com>
2022-01-08 22:58:10 +0900

    [리팩토링] Regions 테스트코드 given2사용

--- src/Components/Regions.test.jsx
+++ src/Components/Regions.test.jsx
@@ -1,5 +1,7 @@
 import { render, fireEvent } from '@testing-library/react';
 
+import given from 'given2';
+
 import Regions from './Regions';
 
 import REGIONS from '../../fixtures/regions';
@@ -9,27 +11,29 @@ describe('Regions', () => {
 
   const SEOUL = REGIONS[0];
 
+  const renderRegions = () => (render((
+    <Regions
+      regions={REGIONS}
+      selectedRegion={given.selectedRegion}
+      onClick={handleClick}
+    />
+  )));
+
   context('with selectedRegion', () => {
+    given('selectedRegion', () => SEOUL);
+
     it('renders regions with selected region', () => {
-      const { container } = render((
-        <Regions
-          regions={REGIONS}
-          selectedRegion={SEOUL}
-        />
-      ));
+      const { container } = renderRegions();
 
       expect(container).toHaveTextContent(`${SEOUL.name}(V)`);
     });
   });
 
   context('without selectedRegion', () => {
+    given('selectedRegion', () => null);
+
     it('renders regions', () => {
-      const { container, getByText } = render((
-        <Regions
-          regions={REGIONS}
-          onClick={handleClick}
-        />
-      ));
+      const { container, getByText } = renderRegions();
 
       expect(container).toHaveTextContent(SEOUL.name);
 
