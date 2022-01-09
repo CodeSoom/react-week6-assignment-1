a075756c6f484761be21d8321189a63e2949d3ff
eunseo-kim <67737432+eunseo-kim@users.noreply.github.com>
2022-01-10 00:16:09 +0900

    RestaurantDetailPage 구현

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,7 +1,33 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useParams } from 'react-router-dom';
+
+import {
+  setRestaurantDetail,
+  loadRestauarantDetail,
+} from './actions';
+
+import { get } from './utils';
 
 export default function RestaurantDetailPage() {
-  const restaurant = useSelector((state) => state.restaurant);
+  const dispatch = useDispatch();
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    dispatch(setRestaurantDetail(null));
+    dispatch(loadRestauarantDetail({ restaurantId: id }));
+  }, []);
+
+  const restaurant = useSelector(get('restaurant'));
+
+  if (!restaurant) {
+    return (
+      <p>Loading...</p>
+    );
+  }
 
   const { name, address, menuItems } = restaurant;
 
--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -1,15 +1,20 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import RestaurantDetailPage from './RestaurantDetailPage';
 
 import restaurant from '../fixtures/restaurant';
 
 test('RestaurantDetailPage', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
   useSelector.mockImplementation((selector) => selector({ restaurant }));
 
-  const { getByText } = render(<RestaurantDetailPage />);
+  const { getByText } = render(
+    <RestaurantDetailPage />,
+  );
 
   expect(getByText(/양천주가/)).not.toBeNull();
   expect(getByText(/비빔밥/)).not.toBeNull();
