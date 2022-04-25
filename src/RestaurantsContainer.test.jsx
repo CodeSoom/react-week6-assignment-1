7caf7a426373ae8f04cdefb0f4d66e4c11a65589
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-25 23:14:30 +0900

    Add test about link to restaurant detail
    Add test about link to restaurant detail.

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -4,16 +4,27 @@ import { useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
-test('RestaurantsContainer', () => {
+describe('RestaurantsContainer', () => {
+  function renderRestaurantsContainer() {
+    return render((
+      <RestaurantsContainer />
+    ));
+  }
   useSelector.mockImplementation((selector) => selector({
     restaurants: [
       { id: 1, name: '마법사주방' },
     ],
   }));
 
-  const { container } = render((
-    <RestaurantsContainer />
-  ));
+  it('renders restaurants', () => {
+    const { container } = renderRestaurantsContainer();
 
-  expect(container).toHaveTextContent('마법사주방');
+    expect(container).toHaveTextContent('마법사주방');
+  });
+
+  it('has links to restaurant detail page', () => {
+    const { getByText } = renderRestaurantsContainer();
+
+    expect(getByText('양천주가')).toHaveAttribute('href', '/restaurants/1');
+  });
 });
