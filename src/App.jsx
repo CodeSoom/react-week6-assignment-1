c2990ba170f7c2af9150cec902423a8091447341
kjoobin <imkj1148@gmail.com>
2022-04-26 07:25:06 +0900

    헤더 구현

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,13 @@ import AboutPage from './AboutPage';
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/restaurants" element={<RestaurantsPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/" element={<HomePage />} />
-    </Routes>
+    <div>
+      <h1>헤더</h1>
+      <Routes>
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/" element={<HomePage />} />
+      </Routes>
+    </div>
   );
 }
