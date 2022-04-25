60906642a72942202ab2bf4fb801beea49a5449a
kjoobin <imkj1148@gmail.com>
2022-04-26 07:06:50 +0900

    route about 테스트

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { fireEvent, render } from '@testing-library/react';
 
 import HomePage from './HomePage';
 
@@ -10,11 +10,23 @@ describe('HomePage', () => {
 
     expect(container).toHaveTextContent('Home');
   });
+
   it('renders about link', () => {
     const { container } = render((
       <HomePage />
     ));
 
     expect(container).toHaveTextContent('About');
+  });
+
+  it('listens for click event on route /about', () => {
+    const { container, getByText } = render((
+      <HomePage />
+    ));
+
+    fireEvent.click(getByText('About'));
+
+    expect(container).toHaveTextContent('About');
+    expect(container).toHaveTextContent('About 페이지 입니다.');
   });
 });
