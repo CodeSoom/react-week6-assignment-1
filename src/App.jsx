25d5e88a0f1452c89a684adecce93d9a55934694
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-25 19:20:02 +0900

    Add homepage
    Add homepage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,13 @@
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
+  const { pathname } = window.location;
+
+  if (pathname === '/') {
+    return (
+      <p>Home</p>
+    );
+  }
   return (
     <RestaurantsPage />
   );
