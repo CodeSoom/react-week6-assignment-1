a21afa936a8cb629286c83c8b4507beeaea79e8e
kjoobin <imkj1148@gmail.com>
2022-04-26 08:14:43 +0900

    render restaurant name 테스트

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -1,13 +1,21 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import RestaurantPage from './RestaurantPage';
 
+import RESTAURANT from '../fixtures/restaurant';
+
 describe('RetaurantPage', () => {
-  it('renders restaurantPage', () => {
+  useSelector.mockImplementation((selector) => selector({
+    restaurant: RESTAURANT,
+  }));
+
+  it('renders restaurant name', () => {
     const { container } = render((
       <RestaurantPage />
     ));
 
-    expect(container).toHaveTextContent('RestaurantPage');
+    expect(container).toHaveTextContent('성전떡볶이');
   });
 });
