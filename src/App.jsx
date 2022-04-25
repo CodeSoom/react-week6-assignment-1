ea6acade8afba590a1afa1e206915f4cd7f067fc
free-ko <duddnr654@naver.com>
2022-04-25 21:42:09 +0900

    Feat: Restaurants router 설정 및 component 추가

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import {
 
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
+import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
@@ -18,6 +19,7 @@ export default function App() {
       <Routes>
         <Route exact path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
       </Routes>
     </div>
   );
