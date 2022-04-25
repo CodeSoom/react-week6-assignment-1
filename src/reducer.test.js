643d0b547219fc4d0d881a2f28f033b258623c77
kjoobin <imkj1148@gmail.com>
2022-04-25 18:23:48 +0900

    select resturant 테스트 initialState 수정

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -145,9 +145,9 @@ describe('reducer', () => {
 
   describe('selectRestaurant', () => {
     const initialState = {
-      selectedRestaurant: {
+      restaurants: [{
         id: 1, name: '김밥제국', category: '분식', address: '서울시 강남구 역삼동',
-      },
+      }],
     };
 
     it('changes select restaurant', () => {
