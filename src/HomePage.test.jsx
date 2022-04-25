5b0e39a04d4ad56d9e003a10b26213541de7b88b
kjoobin <imkj1148@gmail.com>
2022-04-26 07:03:18 +0900

    render title 테스트

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -3,6 +3,13 @@ import { render } from '@testing-library/react';
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
+  it('renders title', () => {
+    const { container } = render((
+      <HomePage />
+    ));
+
+    expect(container).toHaveTextContent('Home');
+  });
   it('renders about link', () => {
     const { container } = render((
       <HomePage />
