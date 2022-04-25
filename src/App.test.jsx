9a76d05f85d1268d0788ce1c8630c71d635d5d97
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-26 02:12:20 +0900

    Add App Test
    Add App Test.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,11 +2,15 @@ import { MemoryRouter } from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
 describe('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   beforeEach(() => {
     useSelector.mockImplementation((selector) => selector({
       regions: [],
@@ -28,6 +32,39 @@ describe('App', () => {
       const { container } = renderApp({ path: '/' });
 
       expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders AboutPage', () => {
+      const { container } = renderApp({ path: '/about' });
+
+      expect(container).toHaveTextContent('About');
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders RestaurantsPage', () => {
+      const { container } = renderApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent('Restaurants');
+    });
+  });
+
+  context('with path /restaurants/1', () => {
+    it('renders RestaurantDetailPage', () => {
+      const { container } = renderApp({ path: '/restaurants/1' });
+
+      expect(container).toHaveTextContent('주소');
+      expect(container).toHaveTextContent('메뉴');
+    });
+  });
+
+  context('with path /otherPage', () => {
+    it('renders error message', () => {
+      const { container } = renderApp({ path: '/otherPage' });
+
+      expect(container).toHaveTextContent('404 Not Found');
     });
   });
 });
