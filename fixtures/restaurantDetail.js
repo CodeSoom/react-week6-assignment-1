422353935bf1ca1cbd1e9842e73cc5e3bea94a0c
Jiwoo Choi <heyjiwoo@annotation.ai.com>
2022-04-26 00:08:57 +0900

    Add api test
    Add api test.

---
+++ fixtures/restaurantDetail.js
@@ -0,0 +1,15 @@
+const restaurantDetail = {
+  id: 1,
+  categoryId: 1,
+  name: '양천주가',
+  address: '서울 강남구',
+  menuItems: [
+    {
+      id: 1,
+      retaurantId: 1,
+      name: '탕수육',
+    },
+  ],
+};
+
+export default restaurantDetail;
