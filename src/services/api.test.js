d8f40940dc391a9acebd1bbe568984de220236b9
kjoobin <imkj1148@gmail.com>
2022-04-25 17:58:45 +0900

    fetch restaurant 구현

--- src/services/api.test.js
+++ src/services/api.test.js
@@ -1,10 +1,11 @@
-import { fetchRegions, fetchCategories, fetchRestaurants } from './api';
+import {
+  fetchRegions, fetchCategories, fetchRestaurants, fetchRestaurant,
+} from './api';
 
 import REGIONS from '../../fixtures/regions';
 import CATEGORIES from '../../fixtures/categories';
 import RESTAURANTS from '../../fixtures/restaurants';
 import RESTAURANT from '../../fixtures/restaurant';
-import { fetchRestaurant } from './__mocks__/api';
 
 describe('api', () => {
   const mockFetch = (data) => {
