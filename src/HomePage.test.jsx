b080a77139b1901bbd71fa6fda09125fb5ed521c
kjoobin <imkj1148@gmail.com>
2022-04-26 07:19:50 +0900

    Restaurants link 테스트

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,32 +1,31 @@
-import { fireEvent, render } from '@testing-library/react';
+import { render } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
 
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
-  it('renders title', () => {
-    const { container } = render((
+  const renderHomePage = () => render((
+    <MemoryRouter>
       <HomePage />
-    ));
+    </MemoryRouter>
+  ));
+
+  it('renders title', () => {
+    const { container } = renderHomePage();
 
     expect(container).toHaveTextContent('Home');
   });
 
   it('renders about link', () => {
-    const { container } = render((
-      <HomePage />
-    ));
+    const { container } = renderHomePage();
 
     expect(container).toHaveTextContent('About');
   });
 
-  it('listens for click event on route /about', () => {
-    const { container, getByText } = render((
-      <HomePage />
-    ));
+  it('renders restaurants link', () => {
+    const { container } = renderHomePage();
 
-    fireEvent.click(getByText('About'));
-
-    expect(container).toHaveTextContent('About');
-    expect(container).toHaveTextContent('About 페이지 입니다.');
+    expect(container).toHaveTextContent('Restaurants');
   });
 });
