2c440873b0554e41bd1540e5e656feeec90371c4
kjoobin <imkj1148@gmail.com>
2022-04-26 08:23:58 +0900

    메뉴 테스트

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -26,4 +26,16 @@ describe('RetaurantPage', () => {
 
     expect(container).toHaveTextContent('주소: 서울 강남구 강남대로94길 21');
   });
+
+  it('renders menu', () => {
+    const { container } = render((
+      <RestaurantPage />
+    ));
+
+    expect(container).toHaveTextContent('메뉴');
+
+    ['떡볶이', '튀김', '오뎅', '컵밥'].forEach((menu) => (
+      expect(container).toHaveTextContent(menu)
+    ));
+  });
 });
