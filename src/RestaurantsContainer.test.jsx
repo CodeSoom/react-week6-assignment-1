a63c5489b7eaf6f6cfa164ff7b451f720a3b3e06
free-ko <duddnr654@naver.com>
2022-04-26 20:29:03 +0900

    Test: Restaurant Page rendering 테스트 코드 추가

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,19 +1,50 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
-test('RestaurantsContainer', () => {
+beforeEach(() => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
   useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      { id: 1, name: '마법사주방' },
+    regions: [
+      { id: 1, name: '서울' },
     ],
+    categories: [],
+    restaurants: [],
+    restaurant: {
+      id: 200,
+      name: '마법사주방',
+      address: '서울 강남구',
+      menu: [
+        { id: 300, name: '탕수육' },
+        { id: 301, name: '팔보채' },
+      ],
+    },
   }));
+});
 
-  const { container } = render((
-    <RestaurantsContainer />
+function renderApp({ path }) {
+  return render((
+    <MemoryRouter initialEntries={[path]}>
+      <RestaurantsContainer />
+    </MemoryRouter>
   ));
+}
 
-  expect(container).toHaveTextContent('마법사주방');
+describe('restaurantsContainer', () => {
+  context('with path/restaurants/id', () => {
+    it('renders the restaurant page', () => {
+      const { container } = renderApp({ path: '/restaurants/200' });
+
+      expect(container).toHaveTextContent('서울 강남구');
+      expect(container).toHaveTextContent('탕수육');
+      expect(container).toHaveTextContent('팔보채');
+    });
+  });
 });
