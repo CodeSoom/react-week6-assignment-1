23628f93d30c75d4b970dec1abc6e489cc8f94bc
free-ko <duddnr654@naver.com>
2022-04-26 20:13:31 +0900

    Feat: Restaurant Page에 임시로 마크업 추가

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,5 +1,13 @@
 export default function RestaurantPage() {
   return (
-    <h2>Restaurant Page</h2>
+    <div>
+      <h2>Restaurant Page</h2>
+      <p>주소: 서울 강남구</p>
+      <h3>메뉴</h3>
+      <ul>
+        <li>탕수육</li>
+        <li>팔보채</li>
+      </ul>
+    </div>
   );
 }
