d2b8a1c3548c4ff4b9b7adf4e07c4434d6a1685b
free-ko <duddnr654@naver.com>
2022-04-26 19:42:42 +0900

    Test: Restuarants rendering 테스트 코드 추가

--- src/RestaurantsPage.test.jsx
+++ src/RestaurantsPage.test.jsx
@@ -1,15 +1,35 @@
-import { MemoryRouter } from 'react-router-dom';
-
 import { render } from '@testing-library/react';
+
+import { useSelector, useDispatch } from 'react-redux';
 
 import RestaurantsPage from './RestaurantsPage';
 
 describe('RestaurantsPage', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+    categories: [
+      { id: 1, name: '한식' },
+    ],
+    restaurants: [
+      { id: 1, name: '마법사주방' },
+    ],
+  }));
+
   it('renders the home page', () => {
-    render((
-      <MemoryRouter>
-        <RestaurantsPage />
-      </MemoryRouter>
+    const { queryByText } = render((
+      <RestaurantsPage />
     ));
+
+    expect(dispatch).toBeCalled();
+
+    expect((queryByText('서울'))).not.toBeNull();
+    expect((queryByText('서울'))).not.toBeNull();
+    expect((queryByText('서울'))).not.toBeNull();
   });
 });
