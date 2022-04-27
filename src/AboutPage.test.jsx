f1efef4fc08a6da5eb3b0cef45323ac96feaa3d6
kjoobin <imkj1148@gmail.com>
2022-04-28 08:15:28 +0900

    AboutPage text 2개를 한개로 병합

--- src/AboutPage.test.jsx
+++ src/AboutPage.test.jsx
@@ -3,19 +3,12 @@ import { render } from '@testing-library/react';
 import AboutPage from './AboutPage';
 
 describe('AboutPage', () => {
-  it('renders title', () => {
+  it('renders About', () => {
     const { container } = render((
       <AboutPage />
     ));
 
     expect(container).toHaveTextContent('About');
-  });
-
-  it('renders paragraph', () => {
-    const { container } = render((
-      <AboutPage />
-    ));
-
     expect(container).toHaveTextContent('About 페이지 입니다.');
   });
 });
