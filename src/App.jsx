bdec8cd1c05bd2881b89777b8b1b4cdd09957fec
hdpyo <hudongpyo.dev@gmail.com>
2022-04-28 02:49:21 +0900

    feat: 간단한 router 구현해보기
    - <a> 태그를 이용해서 화면을 이동하는 router 를 작성한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,37 @@
 import RestaurantsPage from './RestaurantsPage';
 
-export default function App() {
+function HomePage() {
   return (
-    <RestaurantsPage />
+    <div>
+      <p>Home</p>
+      <li><a href="/about">About</a></li>
+      <li><a href="/restaurants">Restaurants</a></li>
+    </div>
+  );
+}
+
+function AboutPage() {
+  return (
+    <p>About...</p>
+  );
+}
+
+function NotFoundPage() {
+  return (
+    <p>404 Not Found</p>
+  );
+}
+
+export default function App() {
+  const { pathname } = window.location;
+
+  const MyComponent = {
+    '/': HomePage,
+    '/about': AboutPage,
+    '/restaurants': RestaurantsPage,
+  }[pathname] || NotFoundPage;
+
+  return (
+    <MyComponent />
   );
 }
