9d6d26346b5ae522072bb47d1f20cd762190607f
hdpyo <hudongpyo.dev@gmail.com>
2022-04-28 02:31:10 +0900

    refactor: <App />
    - 사용하지 않는 코드들을 제거한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,6 @@
-import { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
-
 import RestaurantsPage from './RestaurantsPage';
 
-import {
-  loadInitialData,
-} from './actions';
-
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
-
   return (
     <RestaurantsPage />
   );
