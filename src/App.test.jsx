8df8d5e73854c88428782b2c576a4d8accb8b8ad
kjoobin <imkj1148@gmail.com>
2022-04-28 08:14:17 +0900

    라우트 별로 context 로 분리

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -32,7 +32,7 @@ describe('App', () => {
   }));
 
   const renderApp = () => render((
-    <MemoryRouter>
+    <MemoryRouter initialEntries={['/']}>
       <App />
     </MemoryRouter>
   ));
@@ -43,48 +43,56 @@ describe('App', () => {
     expect(container).toHaveTextContent('헤더');
   });
 
-  it('renders HomePage', () => {
-    const { container } = renderApp();
+  context('when route "/"', () => {
+    it('renders HomePage', () => {
+      const { container } = renderApp();
 
-    expect(container).toHaveTextContent('Home');
-    expect(container).toHaveTextContent('About');
-    expect(container).toHaveTextContent('Restaurants');
+      expect(container).toHaveTextContent('Home');
+      expect(container).toHaveTextContent('About');
+      expect(container).toHaveTextContent('Restaurants');
+    });
   });
 
-  it('listens for click event on route /about', () => {
-    const { container, getByText } = renderApp();
+  context('when route /about', () => {
+    it('renders RoutePage', () => {
+      const { container, getByText } = renderApp();
 
-    fireEvent.click(getByText('About'));
+      fireEvent.click(getByText('About'));
 
-    expect(container).toHaveTextContent('About');
-    expect(container).toHaveTextContent('About 페이지 입니다.');
+      expect(container).toHaveTextContent('About');
+      expect(container).toHaveTextContent('About 페이지 입니다.');
+    });
+
+    it('listens for click event on route /', () => {
+      const { container, getByText } = renderApp();
+
+      fireEvent.click(getByText('About'));
+      fireEvent.click(getByText('헤더'));
+
+      expect(container).toHaveTextContent('Home');
+    });
   });
 
-  it('listens for click event on route /', () => {
-    const { container, getByText } = renderApp();
+  context('when route "/restaurants"', () => {
+    it('renders RestaurantsPage', () => {
+      const { container, getByText } = renderApp();
 
-    fireEvent.click(getByText('About'));
-    fireEvent.click(getByText('헤더'));
+      fireEvent.click(getByText('Restaurants'));
 
-    expect(container).toHaveTextContent('Home');
+      expect(container).toHaveTextContent('서울');
+      expect(container).toHaveTextContent('한식');
+      expect(container).toHaveTextContent('마법사주방');
+    });
   });
 
-  it('listens for click event on route /restaurants', () => {
-    const { container, getByText } = renderApp();
+  context('when route "/restaurants/[id]"', () => {
+    it('renders RestaurantPage', () => {
+      const { container, getByText } = renderApp();
 
-    fireEvent.click(getByText('Restaurants'));
+      fireEvent.click(getByText('Restaurants'));
+      fireEvent.click(getByText('마법사주방'));
 
-    expect(container).toHaveTextContent('서울');
-    expect(container).toHaveTextContent('한식');
-    expect(container).toHaveTextContent('마법사주방');
-  });
-
-  it('listens for click event on route /restaurants/[id]', () => {
-    const { container, getByText } = renderApp();
-
-    fireEvent.click(getByText('Restaurants'));
-    fireEvent.click(getByText('마법사주방'));
-
-    expect(container).toHaveTextContent('마법사주방');
+      expect(container).toHaveTextContent('마법사주방');
+    });
   });
 });
