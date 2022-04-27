162fd16231ebea422f49bf1349672ce653def1de
hdpyo <hudongpyo.dev@gmail.com>
2022-04-28 02:51:05 +0900

    refactor: <App/>에서 HomePage 를 분리한다.
    - <HomePage /> 를 별도의 파일로 분리한다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,6 @@
 import RestaurantsPage from './RestaurantsPage';
 
-function HomePage() {
-  return (
-    <div>
-      <p>Home</p>
-      <li><a href="/about">About</a></li>
-      <li><a href="/restaurants">Restaurants</a></li>
-    </div>
-  );
-}
+import HomePage from './HomePage';
 
 function AboutPage() {
   return (
---
+++ src/HomePage.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div>
+      <p>Home</p>
+      <li><a href="/about">About</a></li>
+      <li><a href="/restaurants">Restaurants</a></li>
+    </div>
+  );
+}
