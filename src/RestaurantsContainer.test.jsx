ea77ceacb2fd1b6acc930e9454a8fbf390071230
free-ko <duddnr654@naver.com>
2022-04-27 19:56:56 +0900

    Refactor: beforeEach와 renderRestaurantsContainer 함수를 describe내부로 이동

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,4 +1,3 @@
-// 내꺼
 import { render } from '@testing-library/react';
 
 import { MemoryRouter } from 'react-router-dom';
@@ -7,27 +6,27 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
-beforeEach(() => {
-  const dispatch = jest.fn();
-
-  useDispatch.mockImplementation(() => dispatch);
-
-  useSelector.mockImplementation((selector) => selector({
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
-});
-
-function renderRestaurantsContainer() {
-  return render((
-    <MemoryRouter>
-      <RestaurantsContainer />
-    </MemoryRouter>
-  ));
-}
-
 describe('restaurantsContainer', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurants: [
+        { id: 1, name: '마법사주방' },
+      ],
+    }));
+  });
+
+  function renderRestaurantsContainer() {
+    return render((
+      <MemoryRouter>
+        <RestaurantsContainer />
+      </MemoryRouter>
+    ));
+  }
+
   context('with path/restaurants', () => {
     it('renders restaurants', () => {
       const { container } = renderRestaurantsContainer();
