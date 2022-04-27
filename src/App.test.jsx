e577e011d22997aca55d46f11370a1c1fad7b618
hdpyo <hudongpyo.dev@gmail.com>
2022-04-28 02:34:17 +0900

    refactor: <App/> test
    - 불필요한 테스트 코드를 제거한다.

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -12,23 +12,12 @@ test('App', () => {
   useDispatch.mockImplementation(() => dispatch);
 
   useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
+    regions: [],
+    categories: [],
+    restaurants: [],
   }));
 
   const { queryByText } = render((
     <App />
   ));
-
-  expect(dispatch).toBeCalled();
-
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
 });
