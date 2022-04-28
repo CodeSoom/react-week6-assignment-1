ddc8b1759bbf8c2409557f46f4563052545add59
free-ko <duddnr654@naver.com>
2022-04-28 21:11:15 +0900

    Test: add useDispatch, useSelector, MemoryRouter 테스트 코드에 적용

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -1,11 +1,25 @@
 import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import { MemoryRouter } from 'react-router-dom';
 
 import RestaurantPage from './RestaurantPage';
 
+import RESTAURANT from '../fixtures/restaurant';
+
 describe('restaurant', () => {
+  const dispatch = jest.fn();
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    restaurant: RESTAURANT,
+  }));
+
   it('renders the restaurant address and menu', () => {
     const { container } = render((
-      <RestaurantPage />
+      <MemoryRouter>
+        <RestaurantPage />
+      </MemoryRouter>
     ));
 
     expect(container).toHaveTextContent('서울 강남구');
