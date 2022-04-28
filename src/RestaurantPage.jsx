7143a616cc8a25178720c4427025ebe69a01806e
free-ko <duddnr654@naver.com>
2022-04-28 22:12:45 +0900

    Feat: set restaurant data

--- src/RestaurantPage.jsx
+++ src/RestaurantPage.jsx
@@ -1,12 +1,47 @@
+import { useEffect } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useParams } from 'react-router-dom';
+
+import { loadRestaurant } from './actions';
+
 export default function RestaurantPage() {
+  const dispatch = useDispatch();
+
+  const { restaurantId } = useParams();
+
+  useEffect(() => {
+    dispatch(loadRestaurant(restaurantId));
+  }, [restaurantId]);
+
+  const { restaurant } = useSelector((selector) => ({
+    restaurant: selector.restaurant,
+  }));
+
+  if (!restaurant) {
+    return (
+      <p>레스토랑 정보가 없네요...</p>
+    );
+  }
+
+  const { name, address, menuItems } = restaurant;
+
   return (
     <div>
-      <h2>Restaurant Page</h2>
-      <p>주소: 서울 강남구</p>
+      <h2>{name}</h2>
+      <p>
+        주소:
+        {' '}
+        {address}
+      </p>
       <h3>메뉴</h3>
       <ul>
-        <li>탕수육</li>
-        <li>팔보채</li>
+        {menuItems.map(({ id, name: menuName }) => (
+          <li key={id}>
+            {menuName}
+          </li>
+        ))}
       </ul>
     </div>
   );
