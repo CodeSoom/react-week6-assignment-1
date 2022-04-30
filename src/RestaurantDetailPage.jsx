2076ae338da75ac4ebbb4c38045d22ef1e987556
hdpyo <hudongpyo.dev@gmail.com>
2022-04-30 22:33:31 +0900

    Add: RestaurantContainer, RestaurantDetail
    - RestaurantContainer : React Router 를 몰라도 된다.
    - RestaurantDetail : Redux 를 몰라도 된다. RestaurantContainer 로부터 전달받은 props 를 사용할 뿐이다.
    - 분리하기 전에 function 으로 구현한다.
      ㄴ 이후에 별도 컴포넌트로 분리할 것.
    - useSelector 를 이용해서 Redux store 에서 restaurant 상태를 가져온다.

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,26 +1,44 @@
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react';
-import { fetchRestaurant } from './services/api';
+import { useDispatch, useSelector } from 'react-redux';
 
-async function loadRestaurant({ restaurantId }) {
-  const restaurant = await fetchRestaurant({ restaurantId });
-  return restaurant;
+import {
+  loadRestaurant,
+} from './actions';
+
+import { get } from './utils';
+
+function RestaurantDetail({ restaurant }) {
+  return (
+    <div>
+      레스토랑
+      {' '}
+      {restaurant.id}
+    </div>
+  );
 }
 
+function RestaurantContainer({ restaurantId }) {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRestaurant({ restaurantId }));
+  }, []);
+
+  const restaurant = useSelector(get('restaurant'));
+
+  return (
+    <RestaurantDetail restaurantId={restaurantId} />
+  );
+}
 export default function RestaurantDetailPage({ params }) {
   // * useParams : react-router 에서 사용하는 파라미터들을 확인할 수 있다.
   // * 이 컴포넌트에 전달받은 params 가 있으면 그걸 사용하고, 없으면 useParams 를 사용한다.
   const { restaurantId } = params || useParams();
 
-  useEffect(() => {
-    loadRestaurant({ restaurantId });
-  }, []);
-
   return (
-    <div>
-      레스토랑
-      {' '}
-      {restaurantId}
-    </div>
+    <RestaurantContainer
+      restaurantId={restaurantId}
+    />
   );
 }
