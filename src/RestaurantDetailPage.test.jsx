7c8940a38ebef14540a5dddbfca6f524ae529322
hdpyo <hudongpyo.dev@gmail.com>
2022-04-30 17:15:45 +0900

    Failing test: RestaurantDetailPage renders name of restaurant
    - 강의 풀의 내용대로 따라서 구현해보자.

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -1,33 +1,16 @@
 import { render } from '@testing-library/react';
 
-import { useSelector } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 import RestaurantDetailPage from './RestaurantDetailPage';
 
 describe('RestaurantDetailPage', () => {
-  it('renders restaurant information', () => {
+  it('renders name of restaurant', () => {
     const { container } = render(
-      <RestaurantDetailPage />,
+      <MemoryRouter initialEntries={['/restaurants/1']}>
+        <RestaurantDetailPage />
+      </MemoryRouter>,
     );
 
-    // - https://eatgo-customer-api.ahastudio.com/restaurants/1
-    useSelector.mockImplementation((selector) => selector({
-      restaurantInformation: [
-        {
-          id: 1,
-          category: 1,
-          name: '양천주가',
-          address: '서울 강남구 123456',
-          menuItems: [
-            { id: 1, restaurantId: 1, name: '비빔밥' },
-            { id: 2, restaurantId: 1, name: '짬뽕' },
-            { id: 32, restaurantId: 1, name: '유산슬' },
-          ],
-        },
-      ],
-    }));
-
-    expect(container).toHaveTextContent('양천주가');
-    expect(container).toHaveTextContent('서울 강남구 123456');
-    expect(container).toHaveTextContent('비빔밥');
+    expect(container).toHaveTextContent('레스토랑 1');
   });
 });
