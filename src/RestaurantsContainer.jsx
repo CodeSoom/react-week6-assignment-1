924035c08dd43ecda1f187a2376c15bbfc443973
hdpyo <hudongpyo.dev@gmail.com>
2022-04-30 15:02:28 +0900

    Add: RestaurantContainer 가 전달 받을 onClickRestaurant 을 어떻게 사용할 것인지에 대한 내용 추가
    - <a> 로 작성하면 기본적으로 화면이 아예 이동해버린다.
     ㄴ event.preventDefault() 로 해당 기본 동작을 막는다.
     ㄴ 이제 레스토랑 이름을 클릭해도 화면 이동이 되지 않는다.

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -4,14 +4,23 @@ import { useSelector } from 'react-redux';
 
 import { get } from './utils';
 
-export default function RestaurantsContainer({ Link }) {
+export default function RestaurantsContainer({ onClickRestaurant }) {
   const restaurants = useSelector(get('restaurants'));
+
+  function handleClick(restaurant) {
+    return (event) => {
+      event.preventDefault();
+      onClickRestaurant(restaurant);
+    };
+  }
 
   return (
     <ul>
       {restaurants.map((restaurant) => (
         <li key={restaurant.id}>
-          <Link to="/restaurants/1">{restaurant.name}</Link>
+          <a href="/restaurants/1" onClick={() => handleClick(restaurant)}>
+            {restaurant.name}
+          </a>
         </li>
       ))}
     </ul>
