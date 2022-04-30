403d3ca6cbeced9bc5dc1afb8d034ecfac04a9c0
hdpyo <hudongpyo.dev@gmail.com>
2022-04-30 22:54:21 +0900

    Add: useSelector, useDispatch mockImplementation in RestaurantPage test

--- src/RestaurantPage.test.jsx
+++ src/RestaurantPage.test.jsx
@@ -1,9 +1,23 @@
 import { render } from '@testing-library/react';
 
 import { MemoryRouter } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+
 import RestaurantPage from './RestaurantPage';
 
 describe('RestaurantPage', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurant: {
+        restaurantId: 1,
+        name: '마법사주방',
+      },
+    }));
+  });
+
   it('renders name of restaurant', () => {
     const params = { restaurantId: '1' };
 
