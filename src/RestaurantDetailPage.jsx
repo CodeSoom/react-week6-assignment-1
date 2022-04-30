6c5c9e6d6a4640608d49997811b86e624153a968
hdpyo <hudongpyo.dev@gmail.com>
2022-04-30 22:13:08 +0900

    Add: loadRestaurant
    - RestaurantDetailPage 가 렌더링될 때 특정 레스토랑 정보를 불러오는 loadRestaurant 함수를 추가한다.
    - 이 다음에는 dispatch 를 통해서 액션을 구현해야 한다.

--- src/RestaurantDetailPage.jsx
+++ src/RestaurantDetailPage.jsx
@@ -1,9 +1,20 @@
 import { useParams } from 'react-router-dom';
+import { useEffect } from 'react';
+import { fetchRestaurant } from './services/api';
+
+async function loadRestaurant({ restaurantId }) {
+  const restaurant = await fetchRestaurant({ restaurantId });
+  return restaurant;
+}
 
 export default function RestaurantDetailPage({ params }) {
   // * useParams : react-router 에서 사용하는 파라미터들을 확인할 수 있다.
   // * 이 컴포넌트에 전달받은 params 가 있으면 그걸 사용하고, 없으면 useParams 를 사용한다.
   const { restaurantId } = params || useParams();
+
+  useEffect(() => {
+    loadRestaurant({ restaurantId });
+  }, []);
 
   return (
     <div>
