ee777d792a85bf4f6f706591bb1029b567c51c54
kjoobin <imkj1148@gmail.com>
2022-05-01 20:54:57 +0900

    RestaurantDetail 테스트, 구현

--- src/RestaurantDetail.test.jsx
+++ src/RestaurantDetail.test.jsx
@@ -1,48 +1,35 @@
 import { render } from '@testing-library/react';
 
 import { useDispatch, useSelector } from 'react-redux';
+
 import { MemoryRouter } from 'react-router-dom';
 
 import given from 'given2';
 
-import RestaurantContainer from './RestaurantContainer';
+import RestaurantDetail from './RestaurantDetail';
 
 import RESTAURANT from '../fixtures/restaurant';
 
-describe('RestaurantContainer', () => {
+describe('RestaurantDetail', () => {
   beforeEach(() => {
-    jest.clearAllMocks();
-
-    given('restaurantId', () => 1);
     given('restaurant', () => RESTAURANT);
     given('loading', () => false);
+    given('error', () => false);
   });
 
-  const renderRestaurantContainer = () => render((
-    <MemoryRouter>
-      <RestaurantContainer restaurantId={given.restaurantId} />
-    </MemoryRouter>
+  const renderRestaurantDetail = () => render((
+    <RestaurantDetail
+      restaurant={given.restaurant}
+      loading={given.loading}
+      error={given.error}
+    />
   ));
 
-  const dispatch = jest.fn();
-
-  useDispatch.mockImplementation(() => dispatch);
-
-  useSelector.mockImplementation((selector) => selector({
-    restaurant: {
-      data: given.restaurant,
-      loading: given.loading,
-      error: null,
-    },
-  }));
-
-  context('without restaurant id', () => {
-    given('restaurantId', () => undefined);
-    given('loading', () => false);
-    given('restaurant', () => undefined);
+  context('with error', () => {
+    given('error', () => true);
 
     it('renders "잘못된 경로 입니다."', () => {
-      const { container } = renderRestaurantContainer();
+      const { container } = renderRestaurantDetail();
 
       expect(container).toHaveTextContent('잘못된 경로 입니다.');
     });
@@ -53,7 +40,7 @@ describe('RestaurantContainer', () => {
     given('restaurant', () => ({}));
 
     it('renders "loading..."', () => {
-      const { container } = renderRestaurantContainer();
+      const { container } = renderRestaurantDetail();
 
       expect(container).toHaveTextContent('loading...');
     });
@@ -64,7 +51,7 @@ describe('RestaurantContainer', () => {
     given('restaurant', () => undefined);
 
     it('renders "loading..."', () => {
-      const { container } = renderRestaurantContainer();
+      const { container } = renderRestaurantDetail();
 
       expect(container).toHaveTextContent('loading...');
     });
@@ -75,19 +62,19 @@ describe('RestaurantContainer', () => {
     given('restaurant', () => RESTAURANT);
 
     it('renders restaurant name', () => {
-      const { container } = renderRestaurantContainer();
+      const { container } = renderRestaurantDetail();
 
       expect(container).toHaveTextContent('성전떡볶이');
     });
 
     it('renders restaurant address', () => {
-      const { container } = renderRestaurantContainer();
+      const { container } = renderRestaurantDetail();
 
       expect(container).toHaveTextContent('주소: 서울 강남구 강남대로94길 21');
     });
 
     it('renders menu', () => {
-      const { container } = renderRestaurantContainer();
+      const { container } = renderRestaurantDetail();
 
       expect(container).toHaveTextContent('메뉴');
 
