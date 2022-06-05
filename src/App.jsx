be309fc8e027f63fe9a93c973e9a0844ebf111c9
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-06-06 07:53:09 +0900

    Refactor: react-router 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,19 @@
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
 import RestaurantsPage from './RestaurantsPage';
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import NotFoundPage from './NotFoundPage';
 
 export default function App() {
-  const { location: { pathname } } = window;
-
-  console.log(pathname);
-  const MyComponent = {
-    '/': HomePage,
-    '/about': AboutPage,
-    '/restaurants': RestaurantsPage,
-  }[pathname] || NotFoundPage;
-
   return (
-    <MyComponent />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,10 +1,12 @@
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
   return (
     <div>
       <h1>Home</h1>
       <ul>
-        <li><a href="/about">About</a></li>
-        <li><a href="/restaurants">Restaurants</a></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/restaurants">Restaurants</Link></li>
       </ul>
     </div>
   );
