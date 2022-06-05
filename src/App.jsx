f85aecd78539d88417734b2ee73149da72c43f90
DESKTOP-4KHFB1C\rukeni <rukeni@naver.com>
2022-06-06 08:26:38 +0900

    Add: 링크타고 HomePage로 갈 수 있는 헤더 생성

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 
 import RestaurantsPage from './RestaurantsPage';
 import HomePage from './HomePage';
@@ -7,13 +7,18 @@ import NotFoundPage from './NotFoundPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <>
+      <header>
+        <Link to="/">
+          <h1>헤더 영역</h1>
+        </Link>
+      </header>
       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/restaurants" component={RestaurantsPage} />
         <Route component={NotFoundPage} />
       </Switch>
-    </BrowserRouter>
+    </>
   );
 }
