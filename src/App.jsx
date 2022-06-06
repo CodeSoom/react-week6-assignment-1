2e4dcb0ec380c9f6d63df3ecf5896bc928a4c88a
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-06 22:22:41 +0900

    라우팅 기능을 위한 App.jsx, index.jsx 수정
    라우팅 기능을 위해 기존 App.jsx에서 react-router-dom의 Switch, Route와 Link를 이용하여
    라우팅 관련 설정을 했습니다. App.jsx에서 BrowserRouter를 사용해도 되지만, indent를
    줄이고자 index에 BrowserRotuer를 추가하여 Switch 밖에 Router를 넣어줘야 하는 문제를
    해결했습니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,24 @@
-import { useEffect } from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
 
-import { useDispatch } from 'react-redux';
-
-import RegionsContainer from './containers/RegionsContainer';
-import CategoriesContainer from './containers/CategoriesContainer';
-import RestaurantsContainer from './containers/RestaurantsContainer';
-
-import { loadInitialData } from './actions';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
+import HomePage from './pages/HomePage';
+import AboutPage from './pages/AboutPage';
+import RestaurantsPage from './pages/RestaurantsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(loadInitialData());
-  });
-
   return (
     <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
+      <header>
+        <h1>
+          <Link to="/">헤더</Link>
+        </h1>
+      </header>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+        <Route component={NotFoundPage} />
+      </Switch>
     </div>
   );
 }
