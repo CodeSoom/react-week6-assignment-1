98e1092b937766af1b877a0354bd16eb5bed1b89
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-06 22:26:18 +0900

    Switch 밖에는 Rotuer를 넣어주어야 한다 HomePage.test.jsx 테스트 코드를 실행하면 About과 Restaurants가 Link 태그로 되어있기 때문에 Link를 사용하려면 밖에 Router를 사용하라는 에러가 나게 되고 해당 에러를 해결하기 위해 renderHomePage 함수 HomePage 컴포넌트를 react-router-dom 테스트할 때 사용하는 MemoryRouter로 감싸 정상적으로 테스트를 진행할 수 있도록 수정하였습니다.

--- src/pages/HomePage.test.jsx
+++ src/pages/HomePage.test.jsx
@@ -1,9 +1,15 @@
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
-  const renderHomepage = () => render(<HomePage />);
+  const renderHomepage = () => render(
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>,
+  );
 
   it('"About"이라는 텍스트를 출력한다', () => {
     const { container } = renderHomepage();
