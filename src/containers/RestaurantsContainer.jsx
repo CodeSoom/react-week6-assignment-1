f8384c84d5aff8629224ed3ac5922b655f5b26bb
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-07 19:58:13 +0900

    RestaurantsContainer 컴포넌트 작성

--- src/containers/RestaurantsContainer.jsx
+++ src/containers/RestaurantsContainer.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from 'react-redux';
 
+import { Link } from 'react-router-dom';
+
 import { get } from '../utils';
 
 export default function RestaurantsContainer() {
@@ -9,7 +11,9 @@ export default function RestaurantsContainer() {
     <ul>
       {restaurants.map((restaurant) => (
         <li key={restaurant.id}>
-          {restaurant.name}
+          <Link to={`/restaurants/${restaurant.id}`}>
+            {restaurant.name}
+          </Link>
         </li>
       ))}
     </ul>
