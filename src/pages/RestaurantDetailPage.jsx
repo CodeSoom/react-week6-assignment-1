e486ef16d9b1026255b79765ea7f90649c9d12d3
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-07 12:50:45 +0900

    식당 정보를 출력하는 RestaurantDetailPage
    RestaurantDetailPage는 react-router-dom의 useParams를 이용하여 restaurantId를 받아와
    restaurantId에 해당하는 식당 정보를 출력합니다.

---
+++ src/pages/RestaurantDetailPage.jsx
@@ -0,0 +1,38 @@
+import { useEffect } from 'react';
+
+import { useParams } from 'react-router-dom';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import { loadRestaurantDetail } from '../actions';
+
+import { get } from '../utils';
+
+export default function RestaurantDetailPage() {
+  const { restaurantId } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadRestaurantDetail({ restaurantId }));
+  }, [restaurantId]);
+
+  const restaurantDetail = useSelector(get('restaurantDetail'));
+
+  const { name, address, menuItems } = restaurantDetail;
+
+  return (
+    <div>
+      <h2>{name}</h2>
+      <p>
+        주소:
+        {address}
+      </p>
+      <h3>메뉴</h3>
+      <ul>
+        {menuItems.map(({ id, name: menu }) => (
+          <li key={id}>{menu}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
