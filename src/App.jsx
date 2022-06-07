00943932c2769bbdeeb408ae68cf41f1eb9d4233
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-07 19:59:31 +0900

    restaurants/:id 추가

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,10 @@ import { Link, Route, Routes } from 'react-router-dom';
 
 import AboutPage from './pages/AboutPage';
 import HomePage from './pages/HomePage';
-import NotFoundPage from './pages/NotFoundPage';
 import RestaurantsPage from './components/RestaurantsPage';
+import NotFoundPage from './pages/NotFoundPage';
 
-/**
- * 2-1 RestaurantDetail -- 레스토랑 정보와 메뉴 목록 볼수있다. ("/restaurants/1" See 양천주가)
- * 2-2 상세 페이지 이동? (click) => 서울,한식 = 양천주가 | (click) => current url
- */
+import RestaurantsDetailsContainer from './containers/RestaurantsDetailsContainer';
 
 export default function App() {
   return (
@@ -18,6 +15,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/restaurants/:id" element={<RestaurantsDetailsContainer />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
