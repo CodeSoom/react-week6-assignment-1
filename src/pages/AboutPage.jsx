feda9268835b26529c915b9d9fadd09f54a12ac7
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-07 21:29:03 +0900

    e2e 테스트 통과를 위한 AboutPage, AboutPage.test 수정
    e2e 테스트에서 About 페이지로 이동했을 때 '404 Not Found'라는 텍스트 문구를 보기 원하므로
    AboutPage에서 문구를 '404 Not Found'로 수정했으며 이와 같이 테스트 코드도 '404 Not Found'를
    출력하는 테스트로 수정했습니다.

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <div>
       <h2>About</h2>
-      About 페이지 입니다.
+      404 Not Found
     </div>
   );
 }
--- src/pages/AboutPage.test.jsx
+++ src/pages/AboutPage.test.jsx
@@ -3,9 +3,9 @@ import { render } from '@testing-library/react';
 import AboutPage from './AboutPage';
 
 describe('AboutPage', () => {
-  it('"About 페이지 입니다."라는 문구를 렌더한다', () => {
+  it('"404 Not Found"라는 문구를 렌더한다', () => {
     const { container } = render(<AboutPage />);
 
-    expect(container).toHaveTextContent('About 페이지 입니다.');
+    expect(container).toHaveTextContent('404 Not Found');
   });
 });
