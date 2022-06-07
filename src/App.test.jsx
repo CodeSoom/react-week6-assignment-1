a56a14fb9f2787f65c3b8020226040e87a4146ed
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-07 17:58:14 +0900

    강의 수강 App.test 테스트코드 작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,19 +1,59 @@
-import React from 'react';
-
 import { render } from '@testing-library/react';
+import { useDispatch, useSelector } from 'react-redux';
 
-import { useSelector } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 
-import HomePage from './pages/HomePage';
+import App from './App';
 
-test('HomePage', () => {
-  useSelector.mockImplementation((selector) => selector({
-    regions: [],
-    categories: [],
-    restaurants: [],
-  }));
+describe('App', () => {
+  beforeEach(() => {
+    const dispatch = jest.fn();
 
-  render(
-    <HomePage />,
-  );
+    useDispatch.mockImplementation(() => dispatch);
+    useSelector.mockImplementation((selector) => selector({
+      regions: [{ id: 1, name: '서울' }],
+      categories: [],
+      restaurants: [],
+    }));
+  });
+
+  function renderApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
+
+  context('with path /', () => {
+    it('renders HomePage', () => {
+      const { container } = renderApp({ path: '/' });
+
+      expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  context('with path /about', () => {
+    it('renders HomePage', () => {
+      const { container } = renderApp({ path: '/about' });
+
+      expect(container).toHaveTextContent('20명에게 추천');
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('renders restaurants', () => {
+      const { container } = renderApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent('서울');
+    });
+  });
+
+  context('with invalid path', () => {
+    it('renders not found page', () => {
+      const { container } = renderApp({ path: '/xxx' });
+
+      expect(container).toHaveTextContent('404 Not Found');
+    });
+  });
 });
