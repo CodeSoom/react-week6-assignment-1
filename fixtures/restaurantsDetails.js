68e680ba0bccbde68311a453fb3e1f1f8d25e5b9
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-07 19:02:34 +0900

    레스토랑 데이터 생성하기

---
+++ fixtures/restaurantsDetails.js
@@ -0,0 +1,11 @@
+const restaurantsDetails = {
+  id: 1,
+  name: '양천주가',
+  address: '서울 강남구',
+  menuItems: [
+    { id: 101, name: '짜장면' },
+    { id: 102, name: '짬뽕' },
+  ],
+};
+
+export default restaurantsDetails;
