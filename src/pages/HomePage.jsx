4e520045fa9473914a04b99f0569e0c4a0e59d57
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-07 16:29:26 +0900

    About.jsx Link걸어주기

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,18 @@
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
   return (
     <div>
       <ul>
-        <li><a href="./about">about</a></li>
-        <li><a href="./restaurants">restaurants</a></li>
+        <li>
+          <Link to="./about">about</Link>
+        </li>
+        <li>
+          <Link to="./restaurants">restaurants</Link>
+        </li>
+        <li>
+          <Link to="./xxx">멸망의 길</Link>
+        </li>
       </ul>
     </div>
   );
