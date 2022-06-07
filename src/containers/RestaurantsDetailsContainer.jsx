8375e2cc16da54118ecca53fd96870abc6b6d741
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-07 19:59:19 +0900

    RestaurantsDetailsContainer 컴포넌트 작성

---
+++ src/containers/RestaurantsDetailsContainer.jsx
@@ -0,0 +1,33 @@
+import { useDispatch, useSelector } from 'react-redux';
+
+import { useParams } from 'react-router-dom';
+
+import { useEffect } from 'react';
+
+import { get } from '../utils';
+
+import { loadRestaurantsDetails } from '../actions';
+
+import NotFoundPage from '../pages/NotFoundPage';
+
+import RestaurantsDetails from '../components/RestaurantsDetails';
+
+export default function RestaurantsDetailsContainer() {
+  const dispatch = useDispatch();
+
+  const { id } = useParams();
+
+  if (!Number(id)) {
+    return (<NotFoundPage />);
+  }
+
+  useEffect(() => {
+    dispatch(loadRestaurantsDetails(id));
+  }, [id]);
+
+  const restaurantsDetails = useSelector(get('restaurantsDetails'));
+
+  return (
+    <RestaurantsDetails restaurantsDetails={restaurantsDetails} />
+  );
+}
