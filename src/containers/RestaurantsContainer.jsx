41dbf0ada0a103042e7d60e84594fe1958c6637d
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-07 11:48:07 +0900

    식당 정보를 확인하기 위해 텍스트를 Link로 변경
    식당 정보를 확인하기 위해 기존 식당 제목만 일반 텍스트로 나와있었던 것을 react-router-dom의
    Link로 변경하여 식당 제목을 클릭하면 식당 정보 페이지로 이동할 수 있도록 만들었습니다.

--- src/containers/RestaurantsContainer.jsx
+++ src/containers/RestaurantsContainer.jsx
@@ -1,5 +1,7 @@
 import { useSelector } from 'react-redux';
 
+import { Link } from 'react-router-dom';
+
 import { get } from '../utils';
 
 export default function RestaurantsContainer() {
@@ -7,8 +9,10 @@ export default function RestaurantsContainer() {
 
   return (
     <ul>
-      {restaurants.map((restaurant) => (
-        <li key={restaurant.id}>{restaurant.name}</li>
+      {restaurants.map(({ id, name }) => (
+        <li key={id}>
+          <Link to={`${id}`}>{name}</Link>
+        </li>
       ))}
     </ul>
   );
