b16455401de2f66bf2fc749b28e9fb265b3bdb32
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-07 12:16:47 +0900

    react-router-dom v6 업그레이드로 인한 마이그레이션
    react-router-dom이 v5에서 v6로 업그레이드 되었고 강의에선 v5로 진행했지만 v6가 최신 버전이니
    안정성을 위해 v6로 업데이트 했으며 이로 인한 변경 사항을 수정했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import RestaurantsPage from './pages/RestaurantsPage';
+import RestaurantDetailPage from './pages/RestaurantDetailPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
@@ -13,12 +14,16 @@ export default function App() {
           <Link to="/">헤더</Link>
         </h1>
       </header>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/restaurants" component={RestaurantsPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route
+          path="/restaurants/:restaurantId"
+          element={<RestaurantDetailPage />}
+        />
+        <Route path="/*" element={<NotFoundPage />} />
+      </Routes>
     </div>
   );
 }
