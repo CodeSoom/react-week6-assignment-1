d0ad9052ed088645d758afc5381af683cff73a59
Mika <solinepik@gmail.com>
2022-06-07 23:35:50 +0900

    location.pathname에 따라 보여줄 컴포넌트 분기

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import HomePage from './HomePage';
 import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
+  const { pathname } = window.location;
+
+  const Component = ({
+    '/': HomePage,
+    '/restaurants': RestaurantsPage,
+  }[pathname]) || HomePage;
+
   return (
-    <RestaurantsPage />
+    <Component />
   );
 }
