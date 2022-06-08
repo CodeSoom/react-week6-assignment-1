f81c8cc31c2410c7f081c295f28e215d9f41f09c
Ga-Young Song <dolphin2440@naver.com>
2022-06-09 02:53:47 +0900

    Upgrade react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import AboutPage from './AboutPage';
 import RestaurantInfoPage from './RestaurantInfoPage';
@@ -10,13 +10,13 @@ export default function App() {
   return (
     <>
       <h1><Link to="/">헤더</Link></h1>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route path="/restaurants/:id" component={RestaurantInfoPage} />
-        <Route path="/restaurants" component={RestaurantsPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants/:id" element={<RestaurantInfoPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route element={<NotFoundPage />} />
+      </Routes>
     </>
   );
 }
