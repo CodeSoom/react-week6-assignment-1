934c073e454be81f7dddedee8ec6362caf7eeb3d
Crong (SungEun Kim) <70146617+sinde530@users.noreply.github.com>
2022-06-08 15:48:43 +0900

    test code writing modify:

--- src/components/RestaurantsDetails.test.jsx
+++ src/components/RestaurantsDetails.test.jsx
@@ -13,20 +13,20 @@ function renderRestaurantsDetails(details) {
 describe('RestaurantDetail', () => {
   context('식당이 있는경우', () => {
     it('식당의 정보를 표시한다', () => {
-      const { queryByText } = renderRestaurantsDetails(undefined);
+      const { queryByText } = renderRestaurantsDetails(restaurantsDetails);
 
-      expect(queryByText(/Now is loading/)).not.toBeNull();
+      expect(queryByText('양천주가')).not.toBeNull();
+      expect(queryByText('서울 강남구')).not.toBeNull();
+      expect(queryByText('짜장면')).not.toBeNull();
+      expect(queryByText('짬뽕')).not.toBeNull();
     });
   });
 
-  context('식당이 없는경우', () => {
+  context('식당이 없는경우 ', () => {
     it('정보가 없음을 확인한다', () => {
-      const { queryByText } = renderRestaurantsDetails(restaurantsDetails);
+      const { queryByText } = renderRestaurantsDetails(undefined);
 
-      expect(queryByText(/양천주가/)).not.toBeNull();
-      expect(queryByText(/서울 강남구/)).not.toBeNull();
-      expect(queryByText(/짜장면/)).not.toBeNull();
-      expect(queryByText(/짬뽕/)).not.toBeNull();
+      expect(queryByText('Now is loading')).not.toBeNull();
     });
   });
 });
