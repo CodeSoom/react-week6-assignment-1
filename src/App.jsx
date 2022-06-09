9e1ebba08b61b64aa8606b93d1c23b2b1b56eb8a
Mika <solinepik@gmail.com>
2022-06-10 03:26:59 +0900

    ReactRouter 적용

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
 import NotFoundPage from './pages/NotFoundPage';
 
 export default function App() {
-  const { location: { pathname } } = window;
-
-  const Component = ({
-    '/': HomePage,
-    '/about': AboutPage,
-    '/restaurants': RestaurantsPage,
-  }[pathname]) || NotFoundPage;
-
   return (
-    <Component />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/restaurants" element={<RestaurantsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
   );
 }
