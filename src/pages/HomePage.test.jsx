853af6ff4285f66f55308cb9bdbbb706f22c1afd
Mika <solinepik@gmail.com>
2022-06-10 04:16:01 +0900

    HomePage 페이지 제목, 목차 렌더 테스트 작성

--- src/pages/HomePage.test.jsx
+++ src/pages/HomePage.test.jsx
@@ -1,8 +1,17 @@
 import { render } from '@testing-library/react';
 import HomePage from './HomePage';
 
-test('HomePage', () => {
-  const { container } = render(<HomePage />);
+describe('HomePage', () => {
+  it('제목이 보인다.', () => {
+    const { getByRole } = render(<HomePage />);
 
-  expect(container).toBeInTheDocument();
+    expect(getByRole('heading')).toHaveTextContent('HomePage');
+  });
+
+  it('홈페이지 메뉴들이 보인다.', () => {
+    const { getByRole, getAllByRole } = render(<HomePage />);
+
+    expect(getByRole('list')).toBeInTheDocument();
+    expect(getAllByRole('link')[0]).toHaveTextContent('HOME');
+  });
 });
