67fd164ad7f07e2cfb8eabc5f8218899c6ea1071
rukeni <rukeni@naver.com>
2022-06-09 16:16:23 +0900

    Add: Link태그 추가

--- src/features/restaurant/Restaurants.jsx
+++ src/features/restaurant/Restaurants.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Restaurants({ restaurants }) {
   return (
     <ul>
       {restaurants.map(({ id, name }) => (
         <li key={id}>
-          {name}
+          <Link to={`/restaurants/${id}`}>
+            {name}
+          </Link>
         </li>
       ))}
     </ul>
