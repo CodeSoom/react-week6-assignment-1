c25407378c5b1715cd0cf18adcfdd3f0afd016c9
rukeni <rukeni@naver.com>
2022-06-09 15:20:45 +0900

    Test: 레스토랑id에 맞는 상세정보를 보여줘야한다.

--- src/features/restaurant-detail/RestaurantDetail.test.jsx
+++ src/features/restaurant-detail/RestaurantDetail.test.jsx
@@ -1,9 +1,11 @@
 import { render } from '@testing-library/react';
 
+import RestaurantDetail from './RestaurantDetail';
+
 describe('RestaurantDetail', () => {
   context('with details', () => {
     const { container } = render(<RestaurantDetail />);
 
-    expect(container).toHaveContent('탕수육');
+    expect(container).toHaveTextContent('탕수육');
   });
 });
