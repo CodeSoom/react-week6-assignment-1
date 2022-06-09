e52e4031870c128edace91eaf41c23c48b3ee975
rukeni <rukeni@naver.com>
2022-06-09 16:58:27 +0900

    Test: loadRestaurantDetail 테스트 코드 추가
    1. selectedRestaurantId가 주어지면 setRestaurantDetail이 실행된다.
    
    2. selectedRestaurantId가없으면 어떤 액션도 실행되지 않는다.

--- src/apps/store/actions.test.js
+++ src/apps/store/actions.test.js
@@ -8,12 +8,14 @@ import {
   setCategories,
   loadRestaurants,
   setRestaurants,
+  loadRestaurantDetail,
+  setRestaurantDetail,
 } from './actions';
 
 const middlewares = [thunk];
 const mockStore = configureStore(middlewares);
 
-jest.mock('../../services/api');
+jest.mock('../services/api');
 
 describe('actions', () => {
   let store;
@@ -76,6 +78,40 @@ describe('actions', () => {
 
       it('does\'nt run any actions', async () => {
         await store.dispatch(loadRestaurants());
+
+        const actions = store.getActions();
+
+        expect(actions).toHaveLength(0);
+      });
+    });
+  });
+
+  describe('loadRestaurantDetail', () => {
+    context('with selectedRestaurantId', () => {
+      beforeEach(() => {
+        store = mockStore({
+          selectedRestaurantId: 1,
+        });
+      });
+
+      it('runs setRestaurantDetail', async () => {
+        await store.dispatch(loadRestaurantDetail());
+
+        const actions = store.getActions();
+
+        expect(actions[0]).toEqual(setRestaurantDetail({}));
+      });
+    });
+
+    context('without selectedRestaurantId', () => {
+      beforeEach(() => {
+        store = mockStore({
+          selectedRestaurantId: null,
+        });
+      });
+
+      it("doesn't run any actions", async () => {
+        await store.dispatch(loadRestaurantDetail());
 
         const actions = store.getActions();
 
