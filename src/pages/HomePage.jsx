a94e02718c35a0c1a3c24da240f6cc6d1cc016c3
Mika <solinepik@gmail.com>
2022-06-10 04:26:13 +0900

    HomePage 컴포넌트에 페이지 목차 구현
    - Link가 사용되어 테스트 에러 발생
      - 테스트코드에 테스트용 Router로 빠른 MemoryRouter 보강

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,15 @@
+import { Link } from 'react-router-dom';
+
 export default function HomePage() {
-  return 'HomePage';
+  return (
+    <div>
+      <h1>HomePage</h1>
+      <ul>
+        <li><Link to="/">HOME</Link></li>
+        <li><Link to="/about">ABOUT</Link></li>
+        <li><Link to="/restaurants">RESTAURANTS</Link></li>
+        <li><Link to="/sss">어디로</Link></li>
+      </ul>
+    </div>
+  );
 }
--- src/pages/HomePage.test.jsx
+++ src/pages/HomePage.test.jsx
@@ -1,15 +1,22 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 import HomePage from './HomePage';
+
+const renderPage = () => render(
+  <MemoryRouter>
+    <HomePage />
+  </MemoryRouter>,
+);
 
 describe('HomePage', () => {
   it('제목이 보인다.', () => {
-    const { getByRole } = render(<HomePage />);
+    const { getByRole } = renderPage();
 
     expect(getByRole('heading')).toHaveTextContent('HomePage');
   });
 
   it('홈페이지 메뉴들이 보인다.', () => {
-    const { getByRole, getAllByRole } = render(<HomePage />);
+    const { getByRole, getAllByRole } = renderPage();
 
     expect(getByRole('list')).toBeInTheDocument();
     expect(getAllByRole('link')[0]).toHaveTextContent('HOME');
