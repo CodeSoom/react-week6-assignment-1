f00b3d1d24e5373dca6597846e7ddb523b9af69b
ByeongSooLee-KR <djaxornwkd12@gmail.com>
2022-06-09 23:06:24 +0900

    Add: restaurantInfo fixture

---
+++ fixtures/restaurantInfo.js
@@ -0,0 +1,20 @@
+const restaurantInfo = {
+  id: 1,
+  category: 1,
+  name: '양천주가',
+  address: '서울 강남구 123456',
+  menuItems: [
+    {
+      id: 1,
+      restaurantId: 1,
+      name: '비빔밥',
+    },
+    {
+      id: 2,
+      restaurantId: 1,
+      name: '짬뽕',
+    },
+  ],
+};
+
+export default restaurantInfo;
