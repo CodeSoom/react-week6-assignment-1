0574b476c3367b4838af02e6d33e5334ac65cb8f
rukeni <rukeni@naver.com>
2022-06-09 17:59:40 +0900

    Refactor: 테스트코드를 통과하도록 컴포넌트 기능 추가

--- src/pages/Restaurants/Detail.jsx
+++ src/pages/Restaurants/Detail.jsx
@@ -1,32 +1,32 @@
 import { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
-
-import RegionsContainer from '../../features/region/RegionsContainer';
-import CategoriesContainer from '../../features/category/CategoriesContainer';
-import RestaurantsContainer from '../../features/restaurant/RestaurantsContainer';
+import { useDispatch, useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import {
-  loadInitialData,
+  loadRestaurantDetail,
 } from '../../apps/store/actions';
 
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
+import { get } from '../../apps/utils';
+
+import RestaurantDetail from '../../features/restaurant-detail/RestaurantDetail';
 
 export default function Restaurants() {
+  const { id } = useParams();
   const dispatch = useDispatch();
 
+  const restaurantDetail = useSelector(get('restaurantDetail'));
+
   useEffect(() => {
-    dispatch(loadInitialData());
-  });
+    dispatch(loadRestaurantDetail(id));
+  }, [id]);
 
   return (
-    <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
-    </div>
+    <>
+      <RestaurantDetail
+        name={restaurantDetail.name}
+        address={restaurantDetail.address}
+        menuItems={restaurantDetail.menuItems}
+      />
+    </>
   );
 }
