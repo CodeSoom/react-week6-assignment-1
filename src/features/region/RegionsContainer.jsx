26a0f6215cbef05abd08cfc77199fcb30c4a641d
rukeni <rukeni@naver.com>
2022-06-10 16:33:35 +0900

    Refactor: RegionsContainer에 loading, error 반영

--- src/features/region/RegionsContainer.jsx
+++ src/features/region/RegionsContainer.jsx
@@ -12,12 +12,27 @@ import Regions from './Regions';
 export default function RegionsContainer() {
   const dispatch = useDispatch();
 
-  const regions = useSelector(get('regions'));
+  const {
+    isLoading, isError, errorMessage, data: regions,
+  } = useSelector(get('regions'));
+
   const selectedRegion = useSelector(get('selectedRegion'));
 
   function handleClick(regionId) {
     dispatch(selectRegion(regionId));
     dispatch(loadRestaurants());
+  }
+
+  if (isLoading) return <div>로딩중...</div>;
+
+  if (isError) {
+    return (
+      <div>
+        에러:
+        {' '}
+        {errorMessage}
+      </div>
+    );
   }
 
   return (
