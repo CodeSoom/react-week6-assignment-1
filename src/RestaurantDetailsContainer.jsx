032227d921014f2c1d8b5b4383ba83329370304a
hellomargo <developer.margo@gmail.com>
2022-06-11 00:11:39 +0900

    식당 상세페이지 정보 가져오기 작업

--- src/RestaurantDetailsContainer.jsx
+++ src/RestaurantDetailsContainer.jsx
@@ -1,20 +1,58 @@
+import { useEffect } from 'react';
+
 import { useParams, useNavigate } from 'react-router-dom';
+
+import { useSelector, useDispatch } from 'react-redux';
+
+import { get } from './utils';
+
+import { loadRestaurantsDetails } from './actions';
 
 export default function RestaurantPage() {
   const { id } = useParams();
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
-  console.log('detail페이지 ', id);
+  useEffect(() => {
+    dispatch(loadRestaurantsDetails(id));
+  }, [id]);
+
+  const restaurantsDetails = useSelector(get('restaurantDetails'));
+
+  console.log('restaurantsDetails', restaurantsDetails);
+
+  const { name, address, menuItems, reviews } = restaurantsDetails;
 
   return (
     <div>
-      <h2>식당이름</h2>
-      <h3>식당메뉴</h3>
-      <ul>
-        <li>11</li>
-      </ul>
+      <h2>{name}</h2>
 
-      <button type="button" onClick={() => { navigate('/restaurant'); }}>목록으로</button>
+      <dl>
+        <dt>주소</dt>
+        <dd>{address}</dd>
+      </dl>
+      <dl>
+        <dt>메뉴</dt>
+        {menuItems && menuItems.map((menu) => (
+          <dd key={menu.id}>{menu.name}</dd>
+        ))}
+      </dl>
+      <dl>
+        <dt>식당 후기</dt>
+        {
+          reviews && reviews.map((review) => (
+            <dd key={review.id}>{review.description}</dd>
+          ))
+        }
+      </dl>
+      <button
+        type="button"
+        onClick={() => {
+          navigate('/restaurant');
+        }}
+      >
+        목록으로
+      </button>
     </div>
   );
 }
