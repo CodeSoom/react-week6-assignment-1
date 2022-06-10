2085a15ecda8840a950fe098c081079f64c9f0ed
ByeongSooLee-KR <djaxornwkd12@gmail.com>
2022-06-10 21:28:18 +0900

    Update: add optional chaining in case of non-existing restaurant

--- src/container/RestaurantInfoContainer.jsx
+++ src/container/RestaurantInfoContainer.jsx
@@ -7,15 +7,15 @@ export default function RestaurantInfoContainer() {
 
   return (
     <>
-      <h1>{restaurant.name}</h1>
+      <h1>{restaurant?.name}</h1>
       <h2>
         <span>주소:</span>
-        <span>{restaurant.address}</span>
+        <span>{restaurant?.address}</span>
       </h2>
       <h2>메뉴</h2>
       <ul>
-        {restaurant.menuItems?.map((menuItem) => (
-          <li key={menuItem.id}>menuItem.name</li>
+        {restaurant?.menuItems?.map((menuItem) => (
+          <li key={menuItem.id}>{menuItem.name}</li>
         ))}
       </ul>
     </>
