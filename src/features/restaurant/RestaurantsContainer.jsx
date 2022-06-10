dc25f67c374bf8307d7dfb897c4349e6d719d5c1
rukeni <rukeni@naver.com>
2022-06-10 15:08:51 +0900

    Refactor: isLoading, isError, errorMessage RestaurantsContainer컴포넌트에 반영

--- src/features/restaurant/RestaurantsContainer.jsx
+++ src/features/restaurant/RestaurantsContainer.jsx
@@ -5,7 +5,21 @@ import { get } from '../../apps/utils';
 import Restaurants from './Restaurants';
 
 export default function RestaurantsContainer() {
-  const restaurants = useSelector(get('restaurants'));
+  const {
+    isLoading, isError, errorMessage, data: restaurants,
+  } = useSelector(get('restaurants'));
+
+  if (isLoading) return <div>로딩중...</div>;
+
+  if (isError) {
+    return (
+      <div>
+        에러:
+        {' '}
+        {errorMessage}
+      </div>
+    );
+  }
 
   return (
     <Restaurants restaurants={restaurants} />
