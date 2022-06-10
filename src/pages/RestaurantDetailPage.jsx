560ce6bfdf57e3b11ab5356bbb6cf8bf15f588cc
jaeyooniverse <jaeyoon4819@naver.com>
2022-06-10 16:19:36 +0900

    에러가 발생할 경우의 처리를 위한 RestaurantDetailPage.jsx 수정
    기존 코드에서 에러가 발생할 경우 흰 화면이 나와서 사용자가 에러를 인지하지 못하고 있습니다.
    따라서, try catch문으로 기존 작업을 진행하다 에러가 발생하면 '존재하지 않는 식당이거나 데이터를
    불러오는 과정에서 에러가 발생했습니다.'라는 문구를 출력합니다.

--- src/pages/RestaurantDetailPage.jsx
+++ src/pages/RestaurantDetailPage.jsx
@@ -12,31 +12,39 @@ export default function RestaurantDetailPage() {
   const { restaurantId } = useParams();
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(loadRestaurantDetail({ restaurantId }));
-  }, [restaurantId]);
+  try {
+    useEffect(() => {
+      dispatch(loadRestaurantDetail({ restaurantId }));
+    }, [restaurantId]);
 
-  const restaurantDetail = useSelector(get('restaurantDetail'));
+    const restaurantDetail = useSelector(get('restaurantDetail'));
 
-  if (!restaurantDetail) {
-    return <p>로딩 중...</p>;
-  }
+    if (!restaurantDetail) {
+      return <p>로딩 중...</p>;
+    }
 
-  const { name, address, menuItems } = restaurantDetail;
+    const { name, address, menuItems } = restaurantDetail;
 
-  return (
-    <>
-      <h2>{name}</h2>
+    return (
+      <>
+        <h2>{name}</h2>
+        <p>
+          주소:
+          {address}
+        </p>
+        <h3>메뉴</h3>
+        <ul>
+          {menuItems.map(({ id, name: menu }) => (
+            <li key={id}>{menu}</li>
+          ))}
+        </ul>
+      </>
+    );
+  } catch (err) {
+    return (
       <p>
-        주소:
-        {address}
+        존재하지 않는 식당이거나 데이터를 불러오는 과정에서 에러가 발생했습니다.
       </p>
-      <h3>메뉴</h3>
-      <ul>
-        {menuItems.map(({ id, name: menu }) => (
-          <li key={id}>{menu}</li>
-        ))}
-      </ul>
-    </>
-  );
+    );
+  }
 }
