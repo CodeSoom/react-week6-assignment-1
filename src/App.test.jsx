2994a8a34a438f912dccd453eb7322085c71dce2
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-11 20:51:59 +0900

    test: (red) App.jsx 헤더와 페이지만 보이는 테스트코드 작성
    기존 코드를 지우고 헤더와 페이지가 보이는 걸 기대하는 테스트 코드작성

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -2,33 +2,23 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
-import { useDispatch, useSelector } from 'react-redux';
+import { MemoryRouter } from 'react-router-dom';
 
 import App from './App';
 
-test('App', () => {
-  const dispatch = jest.fn();
+function renderApp({ path }) {
+  return render(
+    <MemoryRouter initialEntries={[path]}>
+      <App />
+    </MemoryRouter>,
+  );
+}
 
-  useDispatch.mockImplementation(() => dispatch);
+describe('<AboutPage />', () => {
+  it('헤더와 페이지 이름이 보임', () => {
+    const { container } = renderApp({ path: '/' });
 
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
-
-  const { queryByText } = render((
-    <App />
-  ));
-
-  expect(dispatch).toBeCalled();
-
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+    expect(container).toHaveTextContent('헤더');
+    expect(container).toHaveTextContent('Home');
+  });
 });
