0d8bbf8aeb34c280f90672e6b60701f5f93e3de6
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-11 21:20:12 +0900

    fix: react-router-dom v6 업그레이드 오류

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import { Switch, Route, Link } from 'react-router-dom';
+import { Routes, Route, Link } from 'react-router-dom';
 
 import RestaurantsPage from './page/RestaurantsPage';
 import HomePage from './page/HomePage';
 import AboutPage from './page/AboutPage';
 import NotFoundPage from './page/NotFoundPage';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 export default function App() {
   return (
@@ -16,12 +11,12 @@ export default function App() {
       <header>
         <Link to="/"><h1>헤더</h1></Link>
       </header>
-      <Switch>
-        <Route exact path="/" component={HomePage} />
-        <Route path="/about" component={AboutPage} />
-        <Route exact path="/restaurants" component={RestaurantsPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
+      <Routes>
+        <Route exact path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route exact path="/restaurants" element={<RestaurantsPage />} />
+        <Route element={NotFoundPage} />
+      </Routes>
     </>
   );
 }
