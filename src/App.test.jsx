8a728fcc1e967e5595237f62722604edbf3b13ac
Woo Dy <kjop7419@naver.com>
2022-07-12 20:31:25 +0900

    refactor(App.test) beforeEach 제거

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,21 +9,19 @@ import App from './App';
 describe('App', () => {
   const dispatch = jest.fn();
 
-  beforeEach(() => {
-    useSelector.mockImplementation((selector) => selector({
-      regions: [
-        { id: 1, name: '서울' },
-      ],
-      categories: [
-        { id: 1, name: '한식' },
-      ],
-      restaurants: [
-        { id: 1, name: '마법사주방' },
-      ],
-    }));
+  useSelector.mockImplementation((selector) => selector({
+    regions: [
+      { id: 1, name: '서울' },
+    ],
+    categories: [
+      { id: 1, name: '한식' },
+    ],
+    restaurants: [
+      { id: 1, name: '마법사주방' },
+    ],
+  }));
 
-    useDispatch.mockImplementation(() => dispatch);
-  });
+  useDispatch.mockImplementation(() => dispatch);
 
   function renderApp({ path }) {
     return render(
