19d7cd3d2399d474023bc9453e1a97915c164c15
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-12 23:10:27 +0900

    test: (refactor) App.test.jsx - path별로 테스트 코드를 작성하라
    각 path마다 등록된 Page화면을 테스트하도록 추가

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -14,11 +14,45 @@ function renderApp({ path }) {
   );
 }
 
-describe('<AboutPage />', () => {
-  it('헤더와 페이지 이름이 보임', () => {
-    const { container } = renderApp({ path: '/' });
+context('등록된 path명일 때', () => {
+  describe('path="/"', () => {
+    it('HomePage가 보임', () => {
+      const { container } = renderApp({ path: '/' });
 
-    expect(container).toHaveTextContent('헤더');
-    expect(container).toHaveTextContent('Home');
+      expect(container).toHaveTextContent('Home');
+    });
+  });
+
+  describe('path="/about"', () => {
+    it('AboutPage가 보임', () => {
+      const { container } = renderApp({ path: '/about' });
+
+      expect(container).toHaveTextContent('About');
+    });
+  });
+
+  describe('path="/restaurants"', () => {
+    it('RestaurantsPage 보임', () => {
+      const { container } = renderApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent('Restaurants');
+    });
+  });
+
+  describe('path="/restaurants/1"', () => {
+    it('RestaurantDetailPage가 보임', () => {
+      const { container } = renderApp({ path: '/restaurants/1 ' });
+
+      expect(container).toHaveTextContent('주소');
+      expect(container).toHaveTextContent('메뉴');
+    });
+  });
+});
+
+context('등록 안 된 path명일 때', () => {
+  it('NotFoundPage가 보임', () => {
+    const { container } = renderApp({ path: '/codezoom' });
+
+    expect(container).toHaveTextContent('404 Not Found');
   });
 });
