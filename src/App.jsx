3ee54f87bcf0b33fdf4bf21fd90dbb692e733286
kyh196201 <kyh196201@gmail.com>
2022-07-12 20:27:44 +0900

    App 테스트 통과
    - Home Page 레이아웃을 작성했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,15 @@ export default function App() {
   return (
     <div>
       <Header />
+
+      <div>
+        <h2>Home</h2>
+
+        <ul>
+          <li>About</li>
+          <li>Restaurants</li>
+        </ul>
+      </div>
     </div>
   );
 }
