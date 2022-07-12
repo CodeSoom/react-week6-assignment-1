5650d7833bf8f4185588a194a3fdf9962f2d9dd1
yerin512 <73337811+yerin512@users.noreply.github.com>
2022-07-12 20:47:56 +0900

    feat: (green) 테스트 통과를 위한restaurantDetailPage 작성 및 App route 추가
    App.jsx에 restaurantId를 파라미터로 받는 restuarantDetailPage를 route로 추가
    메뉴가 보이는 간단한 RestaurantDetailPage 추가

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import RestaurantsPage from './page/RestaurantsPage';
 import HomePage from './page/HomePage';
 import AboutPage from './page/AboutPage';
 import NotFoundPage from './page/NotFoundPage';
+import RestaurantDetailPage from './page/RestaurantDetailPage';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route index element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/restaurants/:restaurantId" element={<RestaurantDetailPage />} />
         <Route path="/*" element={<NotFoundPage />} />
       </Routes>
     </>
