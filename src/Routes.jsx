76c1d3ef77926910255741dd77f936b8a165ac80
minsuk <alstjr0183@naver.com>
2022-07-12 20:52:26 +0900

    feat: Routes - RestaurantsPage 추가 restaurants url에선 RestaurantsPage가 나오게끔 라우트 연결을 해주었습니다

--- src/Routes.jsx
+++ src/Routes.jsx
@@ -3,12 +3,14 @@ import { Route, Switch } from 'react-router-dom';
 
 import AboutPage from './AboutPage';
 import Homepage from './Homepage';
+import RestaurantsPage from './RestaurantsPage';
 
 export default function Routes() {
   return (
     <Switch>
       <Route exact path="/" component={Homepage} />
       <Route path="/about" component={AboutPage} />
+      <Route path="/restaurants" component={RestaurantsPage} />
     </Switch>
   );
 }
