519be507eed2c726b32aa40e42510f090adf940a
kyh196201 <kyh196201@gmail.com>
2022-07-12 20:33:01 +0900

    HomePage 테스트 통과
    - HomePage 레이아웃을 작성했습니다.

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+export default function HomePage() {
+  return (
+    <div>
+      <h2>Home</h2>
+
+      <ul>
+        <li>About</li>
+        <li>Restaurants</li>
+      </ul>
+    </div>
+  );
+}
