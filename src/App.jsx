fceb84357c66682d1137d4b1b9ac26536ea39e6c
unknown <alstjr0183@naver.com>
2022-07-12 09:47:40 +0900

    fix: Routes - App에 적용 App에 Routes를 적용해주었습니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import { useEffect } from 'react';
-
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
-
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
 
 import {
   loadInitialData,
 } from './actions';
+
+import Routes from './Routes';
 
 // 0. 지역, 분류 목록을 얻기
 // 1. 지역 선택 - Regions <- API (0)
@@ -28,10 +25,7 @@ export default function App() {
       <h1>
         <Link to="/">헤더</Link>
       </h1>
-
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
+      <Routes />
     </div>
   );
 }
