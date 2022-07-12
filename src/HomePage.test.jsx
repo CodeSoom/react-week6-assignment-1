5f433ad751af768d393f310b1ccfc26c6057af26
Woo Dy <kjop7419@naver.com>
2022-07-12 20:38:48 +0900

    refactor(HomePage.test) MemoryRouter로 감싸 App 테스트 통과과

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -1,10 +1,18 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import HomePage from './HomePage';
 
 describe('HomePage', () => {
+  function renderHomePage() {
+    return render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>,
+    );
+  }
   it('renders HomePage', () => {
-    const { container } = render(<HomePage />);
+    const { container } = renderHomePage();
 
     expect(container).toHaveTextContent('Home');
   });
