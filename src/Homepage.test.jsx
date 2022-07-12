3ea22a5e8a2ca10cb47ad40f1eba054cdf7ac307
unknown <alstjr0183@naver.com>
2022-07-12 09:11:57 +0900

    feat: Homepage.test 작성 Homepage의 렌더링확인  테스트 코드를 작성하였습니다

---
+++ src/Homepage.test.jsx
@@ -0,0 +1,22 @@
+import { render } from '@testing-library/react';
+
+import { MemoryRouter } from 'react-router-dom';
+
+import Homepage from './Homepage';
+
+describe('Homepage', () => {
+  function renderHomepage() {
+    return render((
+      <MemoryRouter>
+        <Homepage />
+      </MemoryRouter>
+    ));
+  }
+  it('Homepage가 렌더링된다', () => {
+    const { container } = renderHomepage();
+
+    expect(container).toHaveTextContent('Home');
+    expect(container).toHaveTextContent('About');
+    expect(container).toHaveTextContent('Restaurants');
+  });
+});
