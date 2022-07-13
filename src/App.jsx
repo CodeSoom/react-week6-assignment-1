e845f870e502e34f011a9a77ed9f16a44362ac01
minsuk <alstjr0183@naver.com>
2022-07-13 19:52:05 +0900

    fix: app - 분리된 routes 다시 app쪽으로 합치기 라우팅하는쪽은 분리했었는데 다시 app쪽으로 합쳤습니다

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
+
+import AboutPage from './AboutPage';
+import Homepage from './Homepage';
+import RestaurantsPage from './RestaurantsPage';
 
 import {
   loadInitialData,
 } from './actions';
-
-import Routes from './Routes';
-
-// 0. 지역, 분류 목록을 얻기
-// 1. 지역 선택 - Regions <- API (0)
-// 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
-// 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
 
 export default function App() {
   const dispatch = useDispatch();
@@ -28,7 +25,11 @@ export default function App() {
         </h1>
       </header>
 
-      <Routes />
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/about" component={AboutPage} />
+        <Route path="/restaurants" component={RestaurantsPage} />
+      </Switch>
     </div>
   );
 }
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -9,13 +9,36 @@ describe('App', () => {
 
   useDispatch.mockImplementation(() => dispatch);
 
-  const { container } = render((
-    <MemoryRouter>
-      <App />
-    </MemoryRouter>
-  ));
+  function renderApp({ path }) {
+    return render((
+      <MemoryRouter initialEntries={[path]}>
+        <App />
+      </MemoryRouter>
+    ));
+  }
 
   it('app이 렌더링된다', () => {
+    const { container } = renderApp({ path: '/' });
+
     expect(container).toHaveTextContent('헤더');
+  });
+
+  context('path가 / 일때', () => {
+    it('Homepage가 렌더링된다', () => {
+      const { container } = renderApp({ path: '/' });
+
+      expect(container).toHaveTextContent('Home');
+      expect(container).toHaveTextContent('About');
+      expect(container).toHaveTextContent('Restaurants');
+    });
+  });
+
+  context('path가 /about 일때', () => {
+    it('About이 렌더링된다', () => {
+      const { container } = renderApp({ path: '/about' });
+
+      expect(container).toHaveTextContent('About');
+      expect(container).toHaveTextContent('About 페이지입니다.');
+    });
   });
 });
