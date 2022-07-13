235899c4ef87a7a2f8acc1f8b92c748ef1fb766e
seungwoo Kim <kyh196201@gmail.com>
2022-07-14 00:54:36 +0900

    RestaurantsPage 테스트 수정

--- src/pages/RestaurantsPage.test.jsx
+++ src/pages/RestaurantsPage.test.jsx
@@ -1,17 +1,69 @@
 import { render } from '@testing-library/react';
 
+import { useSelector } from 'react-redux';
+
 import REGIONS from '../../fixtures/regions';
+import CATEGORIES from '../../fixtures/categories';
+import RESTAURANTS from '../../fixtures/restaurants';
 
 import RestaurantsPage from './RestaurantsPage';
+
+jest.mock('react-redux');
 
 describe('<RestaurantsPage />', () => {
   const renderRestaurantsPage = () => render(<RestaurantsPage />);
 
-  it('renders regions', () => {
-    const { container } = renderRestaurantsPage();
+  context('with regions', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        regions: REGIONS,
+        categories: [],
+        restaurants: [],
+      }));
+    });
 
-    REGIONS.forEach((region) => {
-      expect(container).toHaveTextContent(region.name);
+    it('renders regions', () => {
+      const { container } = renderRestaurantsPage();
+
+      REGIONS.forEach((region) => {
+        expect(container).toHaveTextContent(region.name);
+      });
+    });
+  });
+
+  context('with categories', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        regions: [],
+        categories: CATEGORIES,
+        restaurants: [],
+      }));
+    });
+
+    it('renders categories', () => {
+      const { container } = renderRestaurantsPage();
+
+      CATEGORIES.forEach((category) => {
+        expect(container).toHaveTextContent(category.name);
+      });
+    });
+  });
+
+  context('with restaurants', () => {
+    beforeEach(() => {
+      useSelector.mockImplementation((selector) => selector({
+        regions: [],
+        categories: [],
+        restaurants: RESTAURANTS,
+      }));
+    });
+
+    it('renders restaurants', () => {
+      const { container } = renderRestaurantsPage();
+
+      RESTAURANTS.forEach((restaurant) => {
+        expect(container).toHaveTextContent(restaurant.name);
+      });
     });
   });
 });
