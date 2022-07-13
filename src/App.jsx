e25dd4631043feff38a62cefaef56b5357e38f4a
kyh196201 <kyh196201@gmail.com>
2022-07-13 20:54:36 +0900

    App 테스트 통과
    - App 테스트 내부에 RestaurantsPage 컴포넌트를 구현했습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,22 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
 
+import REGIONS from '../fixtures/regions';
+
+function RestaurantsPage() {
+  return (
+    <ul>
+      {
+        REGIONS.map((region) => (
+          <li key={region.id}>
+            {region.name}
+          </li>
+        ))
+      }
+    </ul>
+  );
+}
+
 export default function App() {
   return (
     <div>
@@ -15,6 +31,7 @@ export default function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
+          <Route path="/restaurants" element={<RestaurantsPage />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
