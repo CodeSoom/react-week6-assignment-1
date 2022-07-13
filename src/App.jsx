db0b6a1a3ed8f6888f13ef558838252dbe8c986c
kyh196201 <kyh196201@gmail.com>
2022-07-13 19:34:57 +0900

    App 테스트 통과
    - App 내부에 AboutPage 컴포넌트를 구현했습니다.
    - Page 컴포넌트마다 불필요한 div 엘리먼트를 줄이기 위해서 Routes를 div 태그로 감쌌습니다.

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,29 @@ import Header from './components/Header';
 import HomePage from './pages/HomePage';
 import NotFoundPage from './pages/NotFoundPage';
 
+function AboutPage() {
+  return (
+    <>
+      <h2>
+        About
+      </h2>
+      <p>About 페이지 입니다</p>
+    </>
+  );
+}
+
 export default function App() {
   return (
     <div>
       <Header />
 
-      <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <div>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
