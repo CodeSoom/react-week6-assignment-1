4853ea702426a87867789875ecc4d1667796549f
minsuk <alstjr0183@naver.com>
2022-07-14 20:02:51 +0900

    feat: App.test - 테스트케이스 추가 restauransPage의 테스트 케이스와 페이지가없을때 테스트 케이스를 추가했습니다

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,13 +1,27 @@
 import { render } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
+
+import REGIONS from '../fixtures/regions';
+import CATEGORIES from '../fixtures/categories';
+import RESTAURANTS from '../fixtures/restaurants';
 
 describe('App', () => {
   const dispatch = jest.fn();
 
   useDispatch.mockImplementation(() => dispatch);
+
+  const SEOUL = REGIONS[0];
+  const KOREAN_FOOD = CATEGORIES[0];
+  const KIMBAPHEAVEN = RESTAURANTS;
+
+  useSelector.mockImplementation((selector) => selector({
+    categories: CATEGORIES,
+    regions: REGIONS,
+    restaurants: RESTAURANTS,
+  }));
 
   function renderApp({ path }) {
     return render((
@@ -17,7 +31,7 @@ describe('App', () => {
     ));
   }
 
-  it('app이 렌더링된다', () => {
+  it('헤더가 렌더링된다', () => {
     const { container } = renderApp({ path: '/' });
 
     expect(container).toHaveTextContent('헤더');
@@ -34,11 +48,39 @@ describe('App', () => {
   });
 
   context('path가 /about 일때', () => {
-    it('About이 렌더링된다', () => {
+    it('AboutPage가 렌더링된다', () => {
       const { container } = renderApp({ path: '/about' });
 
       expect(container).toHaveTextContent('About');
       expect(container).toHaveTextContent('About 페이지입니다.');
+    });
+  });
+
+  context('path가 /restaurants 일때', () => {
+    it('지역버튼이 렌더링된다', () => {
+      const { container } = renderApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent(SEOUL.name);
+    });
+
+    it('음식종류 버튼이 렌더링된다', () => {
+      const { container } = renderApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent(KOREAN_FOOD.name);
+    });
+
+    it('레스토랑들이 렌더링된다', () => {
+      const { container } = renderApp({ path: '/restaurants' });
+
+      expect(container).toHaveTextContent(KIMBAPHEAVEN[0].name);
+    });
+  });
+
+  context('없는 페이지일때', () => {
+    it('페이지가없어요! 가 렌더링된다', () => {
+      const { container } = renderApp({ path: '/x' });
+
+      expect(container).toHaveTextContent('페이지가없어요!');
     });
   });
 });
