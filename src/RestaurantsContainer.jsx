3911f93ca7955665168f201cfe195802de3c8f4e
kyh196201 <kyh196201@gmail.com>
2022-07-15 20:22:54 +0900

    RestaurantsContainer 상세 페이지 링크 수정

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -11,7 +11,7 @@ export default function RestaurantsContainer() {
     <ul>
       {restaurants.map(({ id, name }) => (
         <li key={id}>
-          <Link to={`${id}`}>
+          <Link to={`/restaurants/${id}`}>
             {name}
           </Link>
         </li>
