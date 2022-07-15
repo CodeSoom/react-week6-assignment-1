5a5c11a758dba2190c0c5c6658401f2e0f226dea
Woo Dy <kjop7419@naver.com>
2022-07-15 19:42:48 +0900

    refactor(RestaurantsContainer) 테스트 코드 수정

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -5,18 +5,19 @@ import { useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
 
-test('RestaurantsContainer', () => {
+describe('RestaurantsContainer', () => {
   useSelector.mockImplementation((selector) => selector({
     restaurants: [
       { id: 1, name: '마법사주방' },
     ],
   }));
+  it('restaurants가 보여집니다.', () => {
+    const { container } = render((
+      <MemoryRouter>
+        <RestaurantsContainer />
+      </MemoryRouter>
+    ));
 
-  const { container } = render((
-    <MemoryRouter>
-      <RestaurantsContainer />
-    </MemoryRouter>
-  ));
-
-  expect(container).toHaveTextContent('마법사주방');
+    expect(container).toHaveTextContent('마법사주방');
+  });
 });
