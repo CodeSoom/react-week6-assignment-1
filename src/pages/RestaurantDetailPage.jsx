d4d446e9228dd055efae79d391ee2b98067e5ec0
kyh196201 <kyh196201@gmail.com>
2022-07-15 20:42:38 +0900

    RestaurantDetailPage 컴포넌트 리팩토링
    - UI에 관련된 부분을 RestaurantDetailContainer를 이용해서 대체했습니다.

--- src/pages/RestaurantDetailPage.jsx
+++ src/pages/RestaurantDetailPage.jsx
@@ -1,17 +1,7 @@
+import RestaurantDetailContainer from '../containers/RestaurantDetailContainer';
+
 export default function RestaurantDetailPage() {
   return (
-    <>
-      <h2>김밥제국</h2>
-      <span>서울시 강남구 역삼동</span>
-      <h3>메뉴</h3>
-      <ul>
-        <li>
-          김밥
-        </li>
-        <li>
-          떡볶이
-        </li>
-      </ul>
-    </>
+    <RestaurantDetailContainer />
   );
 }
