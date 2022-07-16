1a328315ea645f151b43d9226d2af20741673c3b
Woo Dy <kjop7419@naver.com>
2022-07-16 11:54:51 +0900

    refactor(Api) Api 복구로 인해 mock => 실 api로 데이터 받아오게끔 수정

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
 export async function fetchRegions() {
-  // const url = 'https://eatgo-customer-api.ahastudio.com/regions';
   const url = '/regions';
   const response = await fetch(url);
   const data = await response.json();
@@ -7,18 +6,15 @@ export async function fetchRegions() {
 }
 
 export async function fetchCategories() {
-  // const url = 'https://eatgo-customer-api.ahastudio.com/categories';
-  const url = '/categories';
+  const url = 'https://eatgo-customer-api.ahastudio.com/categories';
   const response = await fetch(url);
   const data = await response.json();
   return data;
 }
 
-// export async function fetchRestaurants({ regionName, categoryId }) {
-export async function fetchRestaurants() {
-  // const url = 'https://eatgo-customer-api.ahastudio.com/restaurants'
-  //   + `?region=${regionName}&category=${categoryId}`;
-  const url = '/restaurants';
+export async function fetchRestaurants({ regionName, categoryId }) {
+  const url = 'https://eatgo-customer-api.ahastudio.com/restaurants'
+    + `?region=${regionName}&category=${categoryId}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
