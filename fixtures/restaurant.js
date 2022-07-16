fdd961d64bfe95f6d498cafaa40d446e0b4bb2eb
seungwoo Kim <kyh196201@gmail.com>
2022-07-16 16:50:08 +0900

    restaurant fixture 데이터 수정
    - 실제 api에서 내려주는 데이터와 동일한 구조로 변경했습니다.

--- fixtures/restaurant.js
+++ fixtures/restaurant.js
@@ -3,7 +3,22 @@ const restaurant = {
   name: '김밥제국',
   category: '분식',
   address: '서울시 강남구 역삼동',
-  menu: ['김밥', '떡볶이'],
+  menuItems: [
+    {
+      id: 19,
+      restaurantId: 8,
+      name: '김밥',
+    },
+  ],
+  reviews: [
+    {
+      id: 131,
+      restaurantId: 8,
+      name: '테스터',
+      score: 5,
+      description: '매일 사먹는 제품입니다.',
+    },
+  ],
 };
 
 export default restaurant;
