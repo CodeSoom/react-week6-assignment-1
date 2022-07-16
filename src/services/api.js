18e73f6b4892c29d7c3b30fe205880ccd130a921
seungwoo Kim <kyh196201@gmail.com>
2022-07-16 16:44:19 +0900

    msw 실행 코드 제거
    - api가 정상적으로 동작해서 msw를 실행하는 코드를 제거했습니다.

--- src/services/api.js
+++ src/services/api.js
@@ -1,21 +1,21 @@
-// const API_ENDPOINT = 'https://eatgo-customer-api.ahastudio.com';
+const API_ENDPOINT = 'https://eatgo-customer-api.ahastudio.com';
 
 export async function fetchRegions() {
-  const url = '/regions';
+  const url = `${API_ENDPOINT}/regions`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
 }
 
 export async function fetchCategories() {
-  const url = '/categories';
+  const url = `${API_ENDPOINT}/categories`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
 }
 
 export async function fetchRestaurants({ regionName, categoryId }) {
-  const url = '/restaurants'
+  const url = `${API_ENDPOINT}/restaurants`
     + `?region=${regionName}&category=${categoryId}`;
   const response = await fetch(url);
   const data = await response.json();
@@ -23,7 +23,7 @@ export async function fetchRestaurants({ regionName, categoryId }) {
 }
 
 export async function fetchRestaurant({ restaurantId }) {
-  const url = `/restaurants/${restaurantId}`;
+  const url = `${API_ENDPOINT}/restaurants/${restaurantId}`;
   const response = await fetch(url);
   const data = await response.json();
   return data;
