399a193d03525079bd189a97bf5f0f7ac740bb3f
hanna244 <shhn0509@gmail.com>
2022-08-09 03:29:25 +0900

    HomePage test

---
+++ src/HomePage.test.jsx
@@ -0,0 +1,16 @@
+import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
+
+import HomePage from './HomePage'
+
+describe('home page', () => {
+  it('home page', () => {
+    const { container } = render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>,
+    );
+
+    expect(container).toHaveTextContent('Home');
+  });
+});
