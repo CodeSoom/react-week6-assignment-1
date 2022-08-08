ac5ed331e9cf4cdeaaded76170c596edfd877534
Heejae Chang <heejae0704@gmail.com>
2022-08-08 15:12:46 +0900

    라우팅 기능 구현을 위한 테스트 코드 작성 (RED)

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import {
+  MemoryRouter,
+} from 'react-router-dom';
 
 import { render } from '@testing-library/react';
 
@@ -6,29 +9,79 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import App from './App';
 
-test('App', () => {
-  const dispatch = jest.fn();
-
-  useDispatch.mockImplementation(() => dispatch);
-
-  useSelector.mockImplementation((selector) => selector({
-    regions: [
-      { id: 1, name: '서울' },
-    ],
-    categories: [
-      { id: 1, name: '한식' },
-    ],
-    restaurants: [
-      { id: 1, name: '마법사주방' },
-    ],
-  }));
-
-  const { queryByText } = render((
+const customRender = (path) => render((
+  <MemoryRouter initialEntries={[path]}>
     <App />
-  ));
+  </MemoryRouter>
+));
 
-  expect(dispatch).toBeCalled();
+describe('App', () => {
+  context('with path /', () => {
+    beforeEach(() => {
+      jest.clearAllMocks();
 
-  expect(queryByText('서울')).not.toBeNull();
-  expect(queryByText('한식')).not.toBeNull();
+      const dispatch = jest.fn();
+      useDispatch.mockImplementation(() => dispatch);
+
+      useSelector.mockImplementation((selector) => selector({
+        regions: [
+          { id: 1, name: '서울' },
+        ],
+        categories: [
+          { id: 1, name: '한식' },
+        ],
+        restaurants: [
+          { id: 1, name: '한국식 초밥' },
+        ],
+      }));
+    });
+
+    it('shows HOME heading', () => {
+      const { queryByText } = customRender('/');
+
+      expect(queryByText('Home')).not.toBeNull();
+    });
+
+    it('does not show ABOUT heading', () => {
+      const { queryByText } = customRender('/');
+
+      expect(queryByText('About 페이지입니다')).toBeNull();
+    });
+  });
+
+  context('with path /about', () => {
+    it('shows ABOUT heading', () => {
+      const { queryByText } = customRender('/about');
+
+      expect(queryByText('About 페이지입니다')).not.toBeNull();
+    });
+
+    it('does not show Home heading', () => {
+      const { queryByText } = customRender('/about');
+
+      expect(queryByText('Home')).toBeNull();
+    });
+  });
+
+  context('with path /restaurants', () => {
+    it('shows regions', () => {
+      const { queryByText } = customRender('/restaurants');
+
+      expect(queryByText('서울')).not.toBeNull();
+    });
+
+    it('shows categories', () => {
+      const { queryByText } = customRender('/restaurants');
+
+      expect(queryByText('한식')).not.toBeNull();
+    });
+  });
+
+  context('with path /restaurant', () => {
+    it('shows a restaurant', () => {
+      const { queryByText } = customRender('/restaurant');
+
+      expect(queryByText('한국식 초밥')).not.toBeNull();
+    });
+  });
 });
