471bfa68b84ba90324cd433e8e1828fc675cbc2f
hanna244 <shhn0509@gmail.com>
2022-08-09 03:29:38 +0900

    HomePage implement

---
+++ src/HomePage.jsx
@@ -0,0 +1,15 @@
+import { Link } from 'react-router-dom';
+
+const HomePage = () => (
+  <>
+    <h2>Home</h2>
+    <ul>
+      <li>
+        <Link to="/about">About</Link>
+        <Link to="/restaurants">Restaurants</Link>
+      </li>
+    </ul>
+  </>
+);
+
+export default HomePage;
