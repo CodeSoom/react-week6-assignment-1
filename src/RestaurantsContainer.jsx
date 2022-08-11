3a1efb191213466e22aabd639b508e45688458b3
hanna244 <shhn0509@gmail.com>
2022-08-12 03:46:20 +0900

    RestaurantsContainer dispatch 구현

--- src/RestaurantsContainer.jsx
+++ src/RestaurantsContainer.jsx
@@ -1,17 +1,26 @@
 import { Link } from 'react-router-dom';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { get } from './utils';
 
+import { selectRestaurant } from './actions';
+
 export default function RestaurantsContainer() {
+  const dispatch = useDispatch();
   const restaurants = useSelector(get('restaurants'));
+
+  function handleClickRestaurant(e) {
+    dispatch(selectRestaurant(e.target.id));
+  }
 
   return (
     <ul>
       {restaurants.map((restaurant) => (
         <li key={restaurant.id}>
-          <Link to="/restaurant-detail">{restaurant.name}</Link>
+          <Link to="/restaurant-detail" id={restaurant.id} onClick={handleClickRestaurant}>
+            {restaurant.name}
+          </Link>
         </li>
       ))}
     </ul>
