02de33144b6a945af230cd55b874f879411090b1
ChoJinmok <jjmok1127@gmail.com>
2022-09-15 20:39:12 +0900

    Red: About 페이지 테스트 - 'About', 'About  페이지 입니다.' 포함돼야한다.

--- src/AboutPage.test.jsx
+++ src/AboutPage.test.jsx
@@ -3,7 +3,10 @@ import { render } from '@testing-library/react';
 import AboutPage from './AboutPage';
 
 test('AboutPage', () => {
-  render((
+  const { container } = render((
     <AboutPage />
   ));
+
+  expect(container).toHaveTextContent('About');
+  expect(container).toHaveTextContent('About 페이지 입니다.');
 });
