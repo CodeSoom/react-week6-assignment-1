e919941630f92caad705c97e7cd903004ba89e15
0-Chan <kyc4421194@gmail.com>
2022-09-15 19:42:01 +0900

    :red_circle: Add test for Res-InfoPage

---
+++ src/pages/RestaurantInfoPage.test.jsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+
+import { useDispatch, useSelector } from 'react-redux';
+
+import RestaurantInfoPage from './RestaurantInfoPage';
+
+import { restaurantInfo as RESTAURANT_INFO } from '../../fixtures/restaurants';
+
+test('RestaurantInfoPage', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    restaurantInfo: RESTAURANT_INFO,
+  }));
+
+  const { queryByText } = render((
+    <RestaurantInfoPage />
+  ));
+
+  expect(dispatch).toBeCalled();
+
+  expect(queryByText('양천주가')).not.toBeNull();
+  expect(queryByText('강남구')).not.toBeNull();
+});
