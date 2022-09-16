39038a4e426605221cdeba697e32f8104b88dbd8
0-Chan <kyc4421194@gmail.com>
2022-09-16 20:10:41 +0900

    :red_circle::green_circle: Add test for Loading... text
    Made 100% test coverage so far!

--- src/pages/RestaurantInfoPage.test.jsx
+++ src/pages/RestaurantInfoPage.test.jsx
@@ -9,20 +9,42 @@ import { restaurantInfo as RESTAURANT_INFO } from '../../__fixtures__/restaurant
 describe('RestaurantInfoPage', () => {
   const dispatch = jest.fn();
 
-  useDispatch.mockImplementation(() => dispatch);
+  context('with restaurantInfo', () => {
+    beforeEach(() => {
+      useDispatch.mockImplementation(() => dispatch);
+      useSelector.mockImplementation((selector) => selector({
+        restaurantInfo: RESTAURANT_INFO,
+      }));
+    });
 
-  useSelector.mockImplementation((selector) => selector({
-    restaurantInfo: RESTAURANT_INFO,
-  }));
+    it('renders RestaurantInfoPage', () => {
+      const { queryByText } = render((
+        <RestaurantInfoPage />
+      ));
 
-  it('renders RestaurantInfoPage', () => {
-    const { queryByText } = render((
-      <RestaurantInfoPage />
-    ));
+      expect(dispatch).toBeCalled();
 
-    expect(dispatch).toBeCalled();
+      expect(queryByText('양천주가')).not.toBeNull();
+      expect(queryByText('주소: 서울 강남구')).not.toBeNull();
+    });
+  });
 
-    expect(queryByText('양천주가')).not.toBeNull();
-    expect(queryByText('주소: 서울 강남구')).not.toBeNull();
+  context('without restaurantInfo', () => {
+    beforeEach(() => {
+      useDispatch.mockImplementation(() => dispatch);
+      useSelector.mockImplementation((selector) => selector({
+        restaurantInfo: undefined,
+      }));
+    });
+
+    it('shows Loading...', () => {
+      const { queryByText } = render((
+        <RestaurantInfoPage />
+      ));
+
+      expect(dispatch).toBeCalled();
+
+      expect(queryByText('Loading...')).not.toBeNull();
+    });
   });
 });
