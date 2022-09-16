d457a25080b49dd4ffb74d87be2d89666f6b6211
ChoJinmok <jjmok1127@gmail.com>
2022-09-16 16:49:18 +0900

    Red: Home 페이지 더 상세하게 테스트 수정 (이미 구현은 완료)

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -4,12 +4,20 @@ import { MemoryRouter } from 'react-router-dom';
 
 import HomePage from './HomePage';
 
-test('HomePage', () => {
-  const { getAllByRole } = render((
+test('HomePage', async () => {
+  const { container, getAllByRole } = render((
     <MemoryRouter>
       <HomePage />
     </MemoryRouter>
   ));
 
+  const targetPaths = ['about', 'restaurants'];
+
   const linkList = getAllByRole('link');
+
+  expect(container).toHaveTextContent('Home');
+
+  linkList.forEach((link, index) => {
+    expect(link.href).toContain(targetPaths[index]);
+  });
 });
