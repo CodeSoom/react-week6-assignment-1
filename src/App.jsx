a938084a72a952ecb58667376822b0a175c8c41c
ChoJinmok <jjmok1127@gmail.com>
2022-09-16 23:02:39 +0900

    Green: App에 restaurant detail 경로 구현

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import {
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
 import RestaurantsPage from './RestaurantsPage';
+import RestaurantDetailContainer from './RestaurantDetailContainer';
 import NotFoundPage from './NotFoundPage';
 
 export default function App() {
@@ -21,6 +22,7 @@ export default function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
         <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="/restaurants/:id" element={<RestaurantDetailContainer />} />
         <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </>
