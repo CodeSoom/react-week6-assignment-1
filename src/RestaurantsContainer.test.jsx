7127a7daf2f11cbdeed676f185ac270f6be8ddda
nada1587 <nada1587@naver.com>
2022-10-21 17:18:52 +0900

    - 레스토랑 상세페이지 테스트 추가

--- src/RestaurantsContainer.test.jsx
+++ src/RestaurantsContainer.test.jsx
@@ -1,19 +1,35 @@
 import { render } from '@testing-library/react';
+import { MemoryRouter } from 'react-router-dom';
 
 import { useSelector } from 'react-redux';
 
 import RestaurantsContainer from './RestaurantsContainer';
+import RestaurantsDetailPage from './RestaurantsDetailPage';
 
-test('RestaurantsContainer', () => {
+describe('RestaurantsContainer', () => {
   useSelector.mockImplementation((selector) => selector({
     restaurants: [
       { id: 1, name: '마법사주방' },
     ],
   }));
 
-  const { container } = render((
-    <RestaurantsContainer />
-  ));
+  it('renders restaurants component', () => {
+    const { container } = render((
+      <RestaurantsContainer />
+    ));
 
-  expect(container).toHaveTextContent('마법사주방');
+    expect(container).toHaveTextContent('마법사주방');
+  });
+
+  context('with path /restaurantsDetailPage', () => {
+    it('renders the restaurants detail page', () => {
+      const { container } = render(
+        <MemoryRouter initialEntries={['/restaurantsDetailPage']}>
+          <RestaurantsDetailPage />
+        </MemoryRouter>,
+      );
+
+      expect(container).toHaveTextContent('마법사주방');
+    });
+  });
 });
