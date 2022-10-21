59d902c10e75c367a9980f04e8638caf0e16e038
nada1587 <nada1587@naver.com>
2022-10-22 00:59:08 +0900

    - restaurantDetail api 테스트 추가

---
+++ fixtures/restaurantDetail.js
@@ -0,0 +1,22 @@
+const restaurantDetail = [
+  {
+    id: 1,
+    categoryId: 1,
+    name: '양천주가',
+    address: '서울 강남구',
+    menuItems: [
+      {
+        id: 1,
+        restaurantId: 1,
+        name: '짜장면',
+      },
+      {
+        id: 2,
+        restaurantId: 1,
+        name: '짬뽕',
+      },
+    ],
+  },
+];
+
+export default restaurantDetail;
