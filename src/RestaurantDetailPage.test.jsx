91edbcb504a0492438d70ad5d76ecebb553d1cb9
nada1587 <nada1587@naver.com>
2022-10-22 00:39:23 +0900

    상세페이지 렌더링 테스트 추가

--- src/RestaurantDetailPage.test.jsx
+++ src/RestaurantDetailPage.test.jsx
@@ -6,7 +6,26 @@ import RestaurantDetailPage from './RestaurantDetailPage';
 
 describe('RestaurantsContainer', () => {
   useSelector.mockImplementation((selector) => selector({
-    restaurants: [],
+    restaurant: [
+      {
+        id: 1,
+        categoryId: 1,
+        name: '양천주가',
+        address: '서울 강남구',
+        menuItems: [
+          {
+            id: 1,
+            restaurantId: 1,
+            name: '짜장면',
+          },
+          {
+            id: 2,
+            restaurantId: 1,
+            name: '짬뽕',
+          },
+        ],
+      },
+    ],
   }));
 
   it('renders restaurant detail page', () => {
@@ -14,6 +33,8 @@ describe('RestaurantsContainer', () => {
       <RestaurantDetailPage />
     ));
 
-    expect(container).toHaveTextContent('Detail');
+    expect(container).toHaveTextContent('양천주가');
+    expect(container).toHaveTextContent('서울 강남구');
+    expect(container).toHaveTextContent('짜장면');
   });
 });
