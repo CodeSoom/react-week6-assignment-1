d1da94a55272f6ba702149a891ae1d00802fe1b3
nada1587 <nada1587@naver.com>
2022-10-21 16:57:13 +0900

    - not found 테스트추가

--- src/NotFoundPage.test.jsx
+++ src/NotFoundPage.test.jsx
@@ -4,8 +4,12 @@ import { render } from '@testing-library/react';
 
 import NotFoundPage from './NotFoundPage';
 
-test('NotFoundPage', () => {
-  render(
-    <NotFoundPage />,
-  );
+describe('NotFoundPage', () => {
+  it('renders text : 404 not found', () => {
+    const { getByText } = render(
+      <NotFoundPage />,
+    );
+
+    expect(getByText('404 Not Found')).not.toBeNull();
+  });
 });
