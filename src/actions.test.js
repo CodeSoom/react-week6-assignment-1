3585c2fd8eea9b4924ebf804ade89e503e1bd155
nada1587 <nada1587@naver.com>
2022-10-22 01:06:24 +0900

    - action에 레스토랑 디테일 관련 테스트 추가

--- src/actions.test.js
+++ src/actions.test.js
@@ -8,6 +8,8 @@ import {
   setCategories,
   loadRestaurants,
   setRestaurants,
+  loadRestaurantDetail,
+  setRestaurantDetail,
 } from './actions';
 
 const middlewares = [thunk];
@@ -81,6 +83,20 @@ describe('actions', () => {
 
         expect(actions).toHaveLength(0);
       });
+    });
+  });
+
+  describe('loadRestaurantDetail', () => {
+    beforeEach(() => {
+      store = mockStore({});
+    });
+
+    it('runs setRestaurantDetail', async () => {
+      await store.dispatch(loadRestaurantDetail());
+
+      const actions = store.getActions();
+
+      expect(actions[0]).toEqual(setRestaurantDetail([]));
     });
   });
 });
