ab41fc3a179ebef430b57ba2e78906994230b57d
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-15 04:11:54 +0900

    HomePage 테스트 라우팅 설정
    MemoryRouter로 라우팅 설정하여 테스트 내에서도 link 사용할 수 있도록 하였습니다

--- src/HomePage.test.jsx
+++ src/HomePage.test.jsx
@@ -2,8 +2,14 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { MemoryRouter } from 'react-router-dom';
+
 import HomePage from './HomePage';
 
 test('HomePage', () => {
-  render((<HomePage />));
+  render((
+    <MemoryRouter>
+      <HomePage />
+    </MemoryRouter>
+  ));
 });
