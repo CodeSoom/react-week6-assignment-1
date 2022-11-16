1b439ee222077137a17ce752eaed73f47f8c24eb
sejineeee <sejinee0918@gmail.com>
2022-11-16 12:03:17 +0900

    restaurants Route 연결

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Link, Route, Routes } from 'react-router-dom';
 
 import HomePage from './HomePage';
 import AboutPage from './AboutPage';
+import RestaurantsPage from './RestaurantsPage';
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
       </Routes>
     </div>
   );
