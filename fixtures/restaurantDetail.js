f65bbbe15870206aed61c0fbd5d2cda69aa7f043
sejineeee <sejinee0918@gmail.com>
2022-11-16 16:39:00 +0900

    detail 테스트용 데이터생성

---
+++ fixtures/restaurantDetail.js
@@ -0,0 +1,16 @@
+const restaurantDetail = [
+  {
+    id: 1,
+    name: '양천주가',
+    address: '서울 강남구',
+    menuItems: [
+      {
+        id: 1,
+        restaurantId: 1,
+        name: '짜장면',
+      },
+    ],
+  },
+];
+
+export default restaurantDetail;
