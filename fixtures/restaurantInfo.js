0c0aeaeedc47e193f5952bfd1b2b2de89004b05a
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-18 20:55:19 +0900

    테스트용 데이터 restaurantInfo 생성
    fixtures에 테스트용 데이터 restaurantInfo를 추가하였습니다

---
+++ fixtures/restaurantInfo.js
@@ -0,0 +1,12 @@
+const restaurantInfo = {
+  id: 1,
+  category: 1,
+  name: '양천주가',
+  address: '서울시 강남구',
+  menuItems: [
+    { id: 1, restaurantId: 1, name: '비빔밥' },
+    { id: 2, restaurantId: 1, name: '탕수육' },
+  ],
+};
+
+export default restaurantInfo;
