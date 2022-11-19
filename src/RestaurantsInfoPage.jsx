2d41c98f2fb1125998afe5e1e9ed26ef718807a7
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-20 00:22:33 +0900

    RestaurantsInfoPage에서 관심사에 따른 컴포넌트 분리
    페이지에서는 params로 라우팅만 할 수 있도록 관심사를 분리하였습니다

--- src/RestaurantsInfoPage.jsx
+++ src/RestaurantsInfoPage.jsx
@@ -1,23 +1,11 @@
-import { useEffect } from 'react';
-
-import { useDispatch } from 'react-redux';
-
 import { useParams } from 'react-router-dom';
-
-import { loadRestaurantsInfo } from './actions';
 
 import RestaurantsInfoContainer from './RestaurantsInfoContainer';
 
 export default function RestaurantsInfoPage() {
-  const dispatch = useDispatch();
-
   const { id } = useParams();
 
-  useEffect(() => {
-    dispatch(loadRestaurantsInfo(id));
-  }, [id]);
-
   return (
-    <RestaurantsInfoContainer />
+    <RestaurantsInfoContainer id={id} />
   );
 }
