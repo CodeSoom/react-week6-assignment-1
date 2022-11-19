c6af1bde6f94bc131790bde56fe71616b6c118e2
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-20 00:25:01 +0900

    RestaurantsInfoContainer 테스트 관심사 분리에 따른 테스트 수정
    페이지에서 id를 받아와 그에 해당하는 데이터를 리덕스에서 빼오는 것으로 관심사를 분리하여 id에 맞는 데이터를 받을 수 있도록 테스트를 수정하였습니다

--- src/RestaurantsInfoContainer.test.jsx
+++ src/RestaurantsInfoContainer.test.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import { render } from '@testing-library/react';
 
@@ -9,17 +9,25 @@ import RestaurantsInfoContainer from './RestaurantsInfoContainer';
 import restaurantInfo from '../fixtures/restaurantInfo';
 
 describe('RestaurantsInfoContainer', () => {
-  const renderRestaurantInfoContainer = () => render((
-    <RestaurantsInfoContainer />
+  beforeEach(() => {
+    const dispatch = jest.fn();
+
+    useDispatch.mockImplementation(() => dispatch);
+
+    useSelector.mockImplementation((selector) => selector({
+      restaurantInfo,
+    }));
+  });
+
+  const renderRestaurantInfoContainer = (id) => render((
+    <RestaurantsInfoContainer
+      id={id}
+    />
   ));
 
   context('레스토랑 데이터가 있을 시', () => {
     it('레스토랑 정보가 랜더링된다', () => {
-      useSelector.mockImplementation((selector) => selector({
-        restaurantInfo,
-      }));
-
-      const { container } = renderRestaurantInfoContainer();
+      const { container } = renderRestaurantInfoContainer(restaurantInfo.id);
 
       expect(container).toHaveTextContent(/양천주가/);
       expect(container).toHaveTextContent('비빔밥');
