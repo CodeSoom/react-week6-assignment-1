bfb0acd16fab7adb327a4807e38ca44817b1276b
yxxnhx <tjdbsgk514@hanmail.net>
2022-11-20 16:33:34 +0900

    RestaurantsInfoPage 컴포넌트에서 id 삭제
    직접 사용하는 컨테이너에서 사용할 수 있도록 삭제하였습니다

--- src/RestaurantsInfoPage.jsx
+++ src/RestaurantsInfoPage.jsx
@@ -1,11 +1,7 @@
-import { useParams } from 'react-router-dom';
-
 import RestaurantsInfoContainer from './RestaurantsInfoContainer';
 
 export default function RestaurantsInfoPage() {
-  const { id } = useParams();
-
   return (
-    <RestaurantsInfoContainer id={id} />
+    <RestaurantsInfoContainer />
   );
 }
