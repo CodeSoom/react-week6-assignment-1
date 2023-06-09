40a6799e28e9e8dd8eb29b2721d68c08069185ab
ctaaag <qudxor102@gmail.com>
2023-06-10 00:39:12 +0900

    ✨ADD: router 틀 생성

---
+++ pages/RestaurantsPage.jsx
@@ -0,0 +1,13 @@
+import RegionsContainer from '../src/RegionsContainer';
+import CategoriesContainer from '../src/CategoriesContainer';
+import RestaurantsContainer from '../src/RestaurantsContainer';
+
+export default function RestaurantsPage() {
+  return (
+    <div>
+      <RegionsContainer />
+      <CategoriesContainer />
+      <RestaurantsContainer />
+    </div>
+  );
+}
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,18 @@
 import { useEffect } from 'react';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
+import { loadInitialData } from './actions';
 
-import RegionsContainer from './RegionsContainer';
-import CategoriesContainer from './CategoriesContainer';
-import RestaurantsContainer from './RestaurantsContainer';
-
-import {
-  loadInitialData,
-} from './actions';
+import HomePage from '../pages/HomePage';
+import AboutPage from '../pages/AboutPage';
+import RestaurantsPage from '../pages/RestaurantsPage';
+import NotFoundPage from '../pages/NotFoundPage';
 
 // 0. 지역, 분류 목록을 얻기
 // 1. 지역 선택 - Regions <- API (0)
 // 2. 분류 선택 - Categories - 한식, 중식, 일식, ... <- API (0)
 // 3. 식당 목록 - Restaurants <- API (with region, category) -> 1, 2 모두 완료된 경우
+// 4. 식당 선택 - 디테일 페이지로 이동
 
 export default function App() {
   const dispatch = useDispatch();
@@ -23,10 +22,17 @@ export default function App() {
   });
 
   return (
-    <div>
-      <RegionsContainer />
-      <CategoriesContainer />
-      <RestaurantsContainer />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+          errorElement={<NotFoundPage />}
+        />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/restaurants" element={<RestaurantsPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
