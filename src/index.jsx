50a26e258fa9e3505bc21b160d4ae98197a80bc9
ctaaag <qudxor102@gmail.com>
2023-06-10 15:17:34 +0900

    🐛FIX : 경로설정 버그 해결

--- src/index.jsx
+++ src/index.jsx
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 
-import store from './store';
+import store from './redux/store';
 
 ReactDOM.render(
   <BrowserRouter>
